1538fb2586b2710ebe61f8de21ecf3753a6c7a3e
Serg <ochkarik1983@mail.ru@>
2019-09-03 21:43:13 +0300

    H

--- tr/urls.py
+++ tr/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
-
+    url(r'^profile/', include('profiles.urls')),
     url(r'^users/', include('users.urls')),
 
 ]  +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
