b107d9ba4b6de7af0c3648941da10f34ff85b7ff
Serg <ochkarik1983@mail.ru@>
2019-09-03 22:48:21 +0300

    H

--- circles/models.py
+++ circles/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-#from connections.models import Connection
+from connections.models import Connection
 #from posts.models import Post
 from django.utils import timezone
 
@@ -16,7 +16,7 @@ class Circle(models.Model):
     #creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='circles', null=True, verbose_name="Создатель")
     name = models.CharField(max_length=100, blank=False, null=False,verbose_name="Название")
     #posts = models.ManyToManyField(Post, related_name='circles', db_index=True, verbose_name="Посты")
-    #connections = models.ManyToManyField(Connection, related_name='circles', db_index=True, through=ConnectionCircle, verbose_name="Соединение")
+    connections = models.ManyToManyField(Connection, related_name='circles', db_index=True, through=ConnectionCircle, verbose_name="Соединение")
     created = models.DateTimeField(default=timezone.now, editable=False, verbose_name="Создан")
 
     def __str__(self):
