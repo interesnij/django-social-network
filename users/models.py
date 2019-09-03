b6af7e32e97a6a49220d8f1519caf17daef6f904
Serg <ochkarik1983@mail.ru@>
2019-09-03 21:59:15 +0300

    H

--- users/models.py
+++ users/models.py
@@ -27,7 +27,7 @@ class User(AbstractUser):
         verbose_name_plural = 'пользователи'
 
     def __str__(self):
-        return self.uuid
+        return str(self.uuid)
 
 
 class UserBlock(models.Model):
