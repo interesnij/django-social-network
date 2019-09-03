f9cb7f4abcd3682bd5b1dc20f112e0a39c83c6a2
Serg <ochkarik1983@mail.ru@>
2019-09-03 21:49:01 +0300

    H

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
 
+from users.models import Users
+
+
+class UsersAdmin(admin.ModelAdmin):
+    search_fields = ('last_name',)
+    #exclude = ['communities']
+
+
+admin.site.register(Users, UsersAdmin)
