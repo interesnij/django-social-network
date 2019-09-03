55918b4a1c6acffbf19ea3d3e158f0b7c04a3171
Serg <ochkarik1983@mail.ru@>
2019-09-03 23:13:20 +0300

    H

--- invitations/admin.py
+++ invitations/admin.py
@@ -5,12 +5,9 @@ from invitations.models import UserInvite
 
 class UserInviteAdmin(admin.ModelAdmin):
     model = UserInvite
-    search_fields = ('username', 'email')
-    list_display = ('name', 'username', 'email',) #'created_user', 'badge'
-    list_display_links = ['email', 'username']
-
-    def has_add_permission(self, request, obj=None):
-        return False
+    search_fields = ('email')
+    list_display = ('name', 'email',) #'created_user', 'badge'
+    list_display_links = ['email']
 
 
 admin.site.register(UserInvite, UserInviteAdmin)
