ba87d2e4645a8846127d52957e93d5bcb3ac5cfb
Serg <ochkarik1983@mail.ru@>
2019-09-03 20:30:10 +0300

    H

---
+++ generic/mixins.py
@@ -0,0 +1,20 @@
+from django.views.generic.base import ContextMixin
+from django.conf import settings
+
+
+
+class CategoryListMixin(ContextMixin):
+
+	def get_context_data(self,**kwargs):
+		context=super(CategoryListMixin,self).get_context_data(**kwargs)
+		context["current_url"]=self.request.path
+		return context
+
+class PageNumberMixin(CategoryListMixin):
+	def get_context_data(self,**kwargs):
+		context=super(PageNumberMixin,self).get_context_data(**kwargs)
+		try:
+			context["pn"]=self.request.GET["page"]
+		except KeyError:
+			context["pn"]="1"
+		return context
