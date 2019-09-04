8adb0b027ee1338c758aa37582e92492faef6d0f
Serg <ochkarik1983@mail.ru@>
2019-09-04 19:38:40 +0300

    H

--- main/views.py
+++ main/views.py
@@ -7,12 +7,10 @@ class MainPageView(TemplateView,CategoryListMixin):
 	template_name="main/mainpage.html"
 	client = OneSignal("608dcb86-a851-4acf-bca5-a292361bc538", "ZjEwZWU1NDQtZmYyMy00ZTEyLTliYWUtYmVhMTE4Y2U5MWU2")
 	notification_to_all_users = SegmentNotification(
-    contents={
-        "en": "Hello from OneSignal-Notifications"
-    },
+    contents={"en": "Hello from OneSignal-Notifications"},
     included_segments=SegmentNotification.ALL
-)
-client.send(notification_to_all_users)
+	)
+	client.send(notification_to_all_users)
 
 	def get_context_data(self,**kwargs):
 		context=super(MainPageView,self).get_context_data(**kwargs)
