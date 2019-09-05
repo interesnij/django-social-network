a0a6eab1e884d91f2c792f0819f7f0184e66a4b1
Serg <ochkarik1983@mail.ru@>
2019-09-05 20:54:51 +0300

    H

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^posts/$', PostsView.as_view(), name='posts'),
-    url(r'^(?P<uuid>\d+)/add/$', PostUserCreate.as_view(), name="post_add_user"),
+    url(r'^(?P<pk>\d+)/add/$', PostUserCreate.as_view(), name="post_add_user"),
 ]
