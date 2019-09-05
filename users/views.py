1a84a49d0c96c450a6fe609699400ae734781af6
Serg <ochkarik1983@mail.ru@>
2019-09-05 17:42:07 +0300

    H

--- users/views.py
+++ users/views.py
@@ -14,18 +14,10 @@ class ProfileUserView(TemplateView, CategoryListMixin):
     template_name = 'user.html'
 
     def get(self,request,*args,**kwargs):
-        self.user = User.objects.get(pk=self.kwargs["pk"])
-        self.profile = UserProfile.objects.get_or_create(user=self.user)[0]
-		self.online=0
-        aaa = datetime.now()
-        onl = self.profile.last_activity + timedelta(minutes=1)
-        if aaa < onl:
-            self.online = 1
+
         return super(ProfileUserView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(ProfileUserView, self).get_context_data(**kwargs)
         context['user'] = self.user
-        context['profile'] = self.profile
-        context['online'] = self.online
         return context
