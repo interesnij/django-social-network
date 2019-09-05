84df03bbcb614d2aeb68d88cff655560c4b816a7
Serg <ochkarik1983@mail.ru@>
2019-09-05 17:34:46 +0300

    H

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from users.views import AllUsers
 
 urlpatterns = [
     url(r'^all-users/$', AllUsers.as_view(), name='all_users')
+    url(r'^(?P<pk>\d+)/$', ProfileUserView.as_view(), name='profile-user'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,30 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
+from generic.mixins import CategoryListMixin
+from profiles.models import UserProfile
+from datetime import datetime, timedelta
+from users.models import User
 
 
 class AllUsers(TemplateView,CategoryListMixin):
 	template_name="all_users.html"
+
+
+class ProfileUserView(TemplateView, CategoryListMixin):
+    template_name = 'user.html'
+
+    def get(self,request,*args,**kwargs):
+        self.user = User.objects.get(id=self.kwargs["id"])
+        self.profile = UserProfile.objects.get_or_create(user=self.user)[0]
+        aaa = datetime.now()
+        onl = self.profile.last_activity + timedelta(minutes=1)
+        if aaa < onl:
+            self.online = 1
+        return super(ProfileUserView,self).get(request,*args,**kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileUserView, self).get_context_data(**kwargs)
+        context['user'] = self.user
+        context['profile'] = self.profile
+        context['online'] = self.online
+        return context
