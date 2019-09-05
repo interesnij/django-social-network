cb0b44a39378596d6bbf8d5e034f3dbcbeb71c72
Serg <ochkarik1983@mail.ru@>
2019-09-05 12:15:00 +0300

    H

--- posts/models.py
+++ posts/models.py
@@ -7,15 +7,12 @@ from django.utils import timezone
 from django.db.models import Count
 from pilkit.processors import ResizeToFit
 from rest_framework.exceptions import ValidationError
-#from moderation.models import ModeratedObject
+from moderation.models import ModeratedObject
 
 from django.conf import settings
 from common.models import Emoji
-#from common.utils.helpers import delete_file_field, sha256sum, extract_usernames_from_string
-
+from common.utils.helpers import delete_file_field, sha256sum, extract_usernames_from_string
 from imagekit.models import ProcessedImageField
-#from notifications.helpers import send_post_comment_user_mention_push_notification, \
-    #send_post_user_mention_push_notification
 from posts.helpers import upload_to_post_image_directory, upload_to_post_video_directory
 
 
@@ -23,10 +20,10 @@ class Post(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True,verbose_name="uuid")
     text = models.TextField(max_length=1000, blank=False, null=True,verbose_name="Текст")
     created = models.DateTimeField(editable=False, db_index=True,verbose_name="Создан")
-    #creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts',verbose_name="Создатель")
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts',verbose_name="Создатель")
     comments_enabled = models.BooleanField(default=True, editable=False, null=False,verbose_name="Разрешить комментарии")
     public_reactions = models.BooleanField(default=True, editable=False, null=False,verbose_name="Публичная реакция")
-    #community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, related_name='posts',null=True,blank=False,verbose_name="Сообщество")
+    community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, related_name='posts',null=True,blank=False,verbose_name="Сообщество")
     is_edited = models.BooleanField(default=False,verbose_name="Изменено")
     is_closed = models.BooleanField(default=False,verbose_name="Закрыто")
     is_deleted = models.BooleanField(default=False,verbose_name="Удалено")
@@ -54,7 +51,7 @@ class PostVideo(models.Model):
 
 
 class PostComment(models.Model):
-    #moderated_object = GenericRelation(ModeratedObject, related_query_name='post_comments',verbose_name="Модерация")
+    moderated_object = GenericRelation(ModeratedObject, related_query_name='post_comments',verbose_name="Модерация")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments',verbose_name="Пост")
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, related_name='replies', null=True, blank=True,verbose_name="Родительский комментарий")
     created = models.DateTimeField(editable=False, db_index=True,verbose_name="Создан")
@@ -68,7 +65,7 @@ class PostComment(models.Model):
 class PostReaction(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reactions')
     created = models.DateTimeField(editable=False)
-    #reactor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_reactions')
+    reactor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_reactions')
     emoji = models.ForeignKey(Emoji, on_delete=models.CASCADE, related_name='post_reactions')
 
 
@@ -76,25 +73,25 @@ class PostReaction(models.Model):
 class PostCommentReaction(models.Model):
     post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name='reactions',verbose_name="Комментарий к посту")
     created = models.DateTimeField(editable=False,verbose_name="Создан")
-    #reactor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_reactions',verbose_name="Кто отреагировал")
+    reactor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_reactions',verbose_name="Кто отреагировал")
     emoji = models.ForeignKey(Emoji, on_delete=models.CASCADE, related_name='post_comment_reactions',verbose_name="Смайл")
 
 
 class PostMute(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='mutes',verbose_name="Пост")
-    #muter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_mutes',verbose_name="Кто заглушил")
+    muter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_mutes',verbose_name="Кто заглушил")
 
 
 class PostCommentMute(models.Model):
     post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name='mutes',verbose_name="Пост")
-    #muter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_mutes',verbose_name="Кто заглушил")
+    muter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_mutes',verbose_name="Кто заглушил")
 
 
 class PostUserMention(models.Model):
-    #user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_mentions',verbose_name="Упоминаемый")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_mentions',verbose_name="Упоминаемый")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='user_mentions',verbose_name="Пост")
 
 
 class PostCommentUserMention(models.Model):
-    #user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_mentions',verbose_name="Упомянутый в комментарии")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_comment_mentions',verbose_name="Упомянутый в комментарии")
     post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name='user_mentions',verbose_name="Пост")
