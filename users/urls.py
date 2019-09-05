9d2a6bd1be8c1986157a46c0e7e9a36bf8e25aaa
Serg <ochkarik1983@mail.ru@>
2019-09-05 17:35:11 +0300

    H

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from users.views import AllUsers
 
 urlpatterns = [
-    url(r'^all-users/$', AllUsers.as_view(), name='all_users')
+    url(r'^all-users/$', AllUsers.as_view(), name='all_users'),
     url(r'^(?P<pk>\d+)/$', ProfileUserView.as_view(), name='profile-user'),
 ]
