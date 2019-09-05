98fd2d2457743c812c864d6bb8b5172ca037c981
Serg <ochkarik1983@mail.ru@>
2019-09-05 17:58:37 +0300

    H

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from generic.mixins import CategoryListMixin
 from generic.mixins import CategoryListMixin
 from profiles.models import UserProfile
 from datetime import datetime, timedelta
-from django.contrib.auth.models import User
+from users.models import User
 
 
 class AllUsers(TemplateView,CategoryListMixin):
@@ -14,7 +14,7 @@ class ProfileUserView(TemplateView, CategoryListMixin):
 	template_name = 'user.html'
 
 	def get(self,request,*args,**kwargs):
-		user=User.models.get(pk=self.kwargs["pk"])
+		user=User.objects.get(pk=self.kwargs["pk"])
 		return super(ProfileUserView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self, **kwargs):
