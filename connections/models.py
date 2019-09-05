90f1f0267ebd2537efecc8dd1526c83863b9a82f
Serg <ochkarik1983@mail.ru@>
2019-09-05 12:02:38 +0300

    H

--- connections/models.py
+++ connections/models.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 
 class Connection(models.Model):
-    #user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='connections',verbose_name="Кто связывается")
-    #target_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='targeted_connections', null=False,verbose_name="С кем связывается")
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='connections',verbose_name="Кто связывается")
+    target_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='targeted_connections', null=False,verbose_name="С кем связывается")
     target_connection = models.OneToOneField('self', on_delete=models.CASCADE, null=True,verbose_name="Связь")
