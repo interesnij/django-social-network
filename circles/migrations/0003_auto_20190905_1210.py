87f1949a356a3a09763548564f63411746719f9d
Serg <ochkarik1983@mail.ru@>
2019-09-06 20:34:00 +0300

    S

--- circles/migrations/0003_auto_20190905_1210.py
+++ circles/migrations/0003_auto_20190905_1210.py
@@ -8,7 +8,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('circles', '0002_auto_20190903_2250'),
     ]
