97bb51c7ee3c7e75e00d6dca18e71c8652c945c2
Serg <ochkarik1983@mail.ru@>
2019-09-06 20:27:10 +0300

    S

--- posts/helpers.py
+++ posts/helpers.py
@@ -24,14 +24,7 @@ def _upload_to_post_directory_directory(post, filename):
                                          'new_filename': new_filename, }
 
 def generate_uuid4_filename(filename):
-"""
-Generates a uuid4 (random) filename, keeping file extension
 
-:param filename: Filename passed in. In the general case, this will
-be provided by django-ckeditor's uploader.
-:return: Randomized filename in urn format.
-:rtype: str
-"""
     discard, ext = os.path.splitext(filename)
     basename = uuid.uuid4().urn
     return ''.join(basename, ext)
