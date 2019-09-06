d1e8f631b5471db4e0f1d46039863297359ef559
Serg <ochkarik1983@mail.ru@>
2019-09-06 19:08:56 +0300

    S

--- posts/forms.py
+++ posts/forms.py
@@ -4,21 +4,19 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 class PostHardForm(forms.ModelForm):
-	content_hard = forms.CharField(widget=CKEditorUploadingWidget, label='')
 
 	class Meta:
 		model = Post
 		fields = ['content_hard']
 
 class PostMediumForm(forms.ModelForm):
-	
+
 
 	class Meta:
 		model = Post
 		fields = ['content_medium']
 
 class PostLiteForm(forms.ModelForm):
-	content_lite = forms.CharField(widget=CKEditorUploadingWidget, label='')
 
 	class Meta:
 		model = Post
