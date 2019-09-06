098fae067b5985aff2d5917c7ae6242eaba9b4a5
Serg <ochkarik1983@mail.ru@>
2019-09-06 22:43:21 +0300

    S

--- users/views.py
+++ users/views.py
@@ -20,12 +20,12 @@ class ProfileUserView(LoginRequiredMixin, ListView):
 	template_name = 'user.html'
 	form = PostMediumForm
 	model=Post
+	paginate_by=2
 
 	def get(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
 
 		self.posts=Post.objects.filter(creator=self.user)
-		paginator = Paginator(self.posts, 10)
 		return super(ProfileUserView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self, **kwargs):
