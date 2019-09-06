a6fb626d06fb382bb955cd445245ee29f75d6d2e
Serg <ochkarik1983@mail.ru@>
2019-09-06 20:31:21 +0300

    S

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ from posts.helpers import upload_to_post_image_directory, upload_to_post_video_d
 
 
 class Post(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, db_index=True,verbose_name="uuid")
+    #uuid = models.UUIDField(default=uuid.uuid4, db_index=True,verbose_name="uuid")
     content_hard = RichTextUploadingField(blank=True, null=True, config_name='default',
                                       external_plugin_resources=[(
                                           'youtube',
