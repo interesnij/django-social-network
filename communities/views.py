b4bd39136ee715d4505db2facff097c18dc11e12
Serg <ochkarik1983@mail.ru@>
2019-09-07 17:12:03 +0300

    S

--- communities/views.py
+++ communities/views.py
@@ -1,15 +1,15 @@
 from django.views.generic import ListView
 from users.models import User
-from connections.models import Communities
+from connections.models import Community
 
 
 class CommunitiesView(ListView):
 	template_name="connections.html"
-	model=Communities
+	model=Community
 
 	def get(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
-		self.groups=Communities.objects.filter(target_connection__user=self.user)
+		self.groups=Community.objects.filter(target_connection__user=self.user)
 		return super(CommunitiesView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
