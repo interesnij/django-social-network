3eea844466575d8f7ba42835628ca99276491d1c
Serg <ochkarik1983@mail.ru@>
2019-09-07 20:08:29 +0300

    S

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,6 +1,11 @@
-from notifications.views import NotificationsView
 from django.conf.urls import url
 
+from notifications import *
+
+
 urlpatterns = [
-    url(r'^notifications/$', NotificationsView.as_view(), name='notifications')
+    url(r'^$', NotificationUnreadListView.as_view(), name='unread'),
+    url(r'^mark-as-read/(?P<slug>[-\w]+)/$', mark_as_read, name='mark_as_read'),
+    url(r'^mark-all-as-read/$', mark_all_as_read, name='mark_all_read'),
+    url(r'^latest-notifications/$', get_latest_notifications, name='latest_notifications'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -1,5 +1,60 @@
-from django.views.generic.base import TemplateView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView
+from notifications.models import Notification
 
 
-class NotificationsView(TemplateView):
-    template_name="notification.html"
+class NotificationUnreadListView(LoginRequiredMixin, ListView):
+    """Basic ListView implementation to show the unread notifications for
+    the actual user"""
+    model = Notification
+    context_object_name = 'notification_list'
+    template_name = 'notification_list.html'
+
+    def get_queryset(self, **kwargs):
+        return self.request.user.notifications.unread()
+
+
+@login_required
+def mark_all_as_read(request):
+    """View to call the model method which marks as read all the notifications
+    directed to the actual user."""
+    request.user.notifications.mark_all_as_read()
+    _next = request.GET.get('next')
+    messages.add_message(
+        request, messages.SUCCESS,
+        'Все уведомления были отмечены как прочитанные')
+
+    if _next:
+        return redirect(_next)
+
+    return redirect('unread')
+
+
+@login_required
+def mark_as_read(request, slug=None):
+    """View to call the model method which mark as read the provided
+    notification."""
+    if slug:
+        notification = get_object_or_404(Notification, slug=slug)
+        notification.mark_as_read()
+
+    messages.add_message(
+        request, messages.SUCCESS,
+        'The notification {notification.slug} has been marked as read.')
+    _next = request.GET.get('next')
+
+    if _next:
+        return redirect(_next)
+
+    return redirect('unread')
+
+
+@login_required
+def get_latest_notifications(request):
+    notifications = request.user.notifications.get_most_recent()
+    return render(request,
+                  'most_recent.html',
+                  {'notifications': notifications})
