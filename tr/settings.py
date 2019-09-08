460a7ad0769258f042b8e3dc53ce6135ecf3d1ef
Serg <ochkarik1983@mail.ru@>
2019-09-08 13:17:53 +0300

    H

--- tr/settings.py
+++ tr/settings.py
@@ -211,7 +211,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channelredis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(‘127.0.0.1’, 6379)],
+            'hosts': [ "redis://127.0.0.1:6379", ]
         },
     }
 }
