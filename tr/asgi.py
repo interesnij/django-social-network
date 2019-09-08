344919cc329542fbf22f58777265dd09a1b7d295
Serg <ochkarik1983@mail.ru@>
2019-09-08 17:50:08 +0300

    H

--- tr/asgi.py
+++ tr/asgi.py
@@ -1,13 +1,9 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
 import os
 import django
 from channels.routing import get_default_application
 
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tr.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 application = get_default_application()
