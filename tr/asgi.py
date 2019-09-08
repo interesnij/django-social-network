d9aa6e830716bc64dc7f5bfe32384150d4e6e9e1
Serg <ochkarik1983@mail.ru@>
2019-09-08 18:28:37 +0300

    H

--- tr/asgi.py
+++ tr/asgi.py
@@ -1,6 +1,9 @@
 import os
-from channels.asgi import get_channel_layer
+import django
+from channels.routing import get_default_application
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tr.settings")
-
-channel_layer = get_channel_layer()
+django.setup()
+application = get_default_application()
