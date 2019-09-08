ebe7fe3e1f57a9d219e83e7dc0c5911ef8cc74e8
Serg <ochkarik1983@mail.ru@>
2019-09-08 09:22:24 +0300

    H

--- tr/asgi.py
+++ tr/asgi.py
@@ -8,6 +8,6 @@ import django
 from channels.routing import get_default_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tr.settings')
 django.setup()
 application = get_default_application()
