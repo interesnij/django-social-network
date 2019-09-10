252032685a0bb627bd1b685d19a406417ebfab28
Serg <ochkarik1983@mail.ru@>
2019-09-10 15:26:31 +0300

    H

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
+from django.urls import path
 from chat.views import *
 
 
 
 urlpatterns = [
-    url(r'^$', MessagesListView.as_view(), name='messages_list'),
+    path(r'', MessagesListView.as_view(), name='messages_list'),
     path('<str:room_name>/', views.room, name='room'),
 ]
