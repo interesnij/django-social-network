45e922d66e7a72e2d6cd2306069dfd2ac6308e62
Serg <ochkarik1983@mail.ru@>
2019-09-10 15:42:12 +0300

    H

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,29 +1,17 @@
+from channels.generic.websocket import WebsocketConsumer
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
+    def disconnect(self, close_code):
+        pass
 
-class MessagerConsumer(AsyncWebsocketConsumer):
-    """Consumer to manage WebSocket connections for the Messager app.
-    """
-    async def connect(self):
-        """Consumer Connect implementation, to validate user status and prevent
-        non authenticated user to take advante from the connection."""
-        if self.scope["user"].is_anonymous:
-            # Reject the connection
-            await self.close()
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-        else:
-            # Accept the connection
-            await self.channel_layer.group_add(f"{self.scope['user'].id}", self.channel_name)
-            await self.accept()
-
-    async def disconnect(self, close_code):
-        """Consumer implementation to leave behind the group at the moment the
-        closes the connection."""
-        await self.channel_layer.group_discard(f"{self.scope['user'].id}", self.channel_name)
-
-    async def receive(self, text_data):
-        """Receive method implementation to redirect any new message received
-        on the websocket to broadcast to all the clients."""
-        await self.send(text_data=json.dumps(text_data))
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
