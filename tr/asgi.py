8e6c2a6b4a714ab733d0e224168ede0ac0200ed8
Serg <ochkarik1983@mail.ru@>
2019-09-10 14:11:14 +0300

    H

--- tr/asgi.py
+++ tr/asgi.py
@@ -4,9 +4,7 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tr.settings")
-django.setup()
 application = get_default_application()
