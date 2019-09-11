b58a76f11d2f5ee39918c67c7e59806ff62f4428
Serg <ochkarik1983@mail.ru@>
2019-09-11 21:17:47 +0300

    H

--- gallery/views.py
+++ gallery/views.py
@@ -6,9 +6,9 @@ class GuestbookView(TemplateView):
 
 	def get(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
-		return super(FrendsListView,self).get(request,*args,**kwargs)
+		return super(GuestbookView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
-		context=super(FrendsListView,self).get_context_data(**kwargs)
+		context=super(GuestbookView,self).get_context_data(**kwargs)
 		context['user'] = self.user
 		return context
