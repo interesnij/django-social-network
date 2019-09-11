c8a87162f3498b3afe7b69974b1e9168f8ce57f1
Serg <ochkarik1983@mail.ru@>
2019-09-11 21:19:39 +0300

    H

--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,14 @@
 from django.views.generic.base import TemplateView
 
 
-class GuestbookView(TemplateView):
+class GalleryView(TemplateView):
 	template_name="gallery.html"
 
 	def get(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
-		return super(GuestbookView,self).get(request,*args,**kwargs)
+		return super(GalleryView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
-		context=super(GuestbookView,self).get_context_data(**kwargs)
+		context=super(GalleryView,self).get_context_data(**kwargs)
 		context['user'] = self.user
 		return context
