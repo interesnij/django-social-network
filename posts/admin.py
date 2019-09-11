ce4e8c55ce025fddbacafcb826edd492935a842f
Serg <ochkarik1983@mail.ru@>
2019-09-11 16:46:08 +0300

    H

--- posts/admin.py
+++ posts/admin.py
@@ -1,17 +1,1 @@
-from django.contrib import admin
-from circles.models import Circle
-from posts.models import Post, PostComment
 
-
-
-class PostAdmin(admin.ModelAdmin):
-
-
-    list_display = (
-        'creator',
-        'created',
-        #'community',
-    )
-
-
-admin.site.register(Post, PostAdmin)
