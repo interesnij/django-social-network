e1b3d336ed3c05971fc4e4b6969e9badb429ca10
Serg <ochkarik1983@mail.ru@>
2019-09-11 20:43:27 +0300

    H

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from communities.models import Community
-#from users.models import User
+from users.models import User
 
 
 class Category(models.Model):
@@ -14,8 +14,8 @@ class Category(models.Model):
     avatar = models.ImageField(blank=False, null=True,verbose_name="Аватар")
 
     order = models.IntegerField(unique=False, default=0,verbose_name="Порядковый номер")
-    #creator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='created_categories', null=True, verbose_name="Создатель")
-    #communities = models.ManyToManyField(Community, related_name='categories', blank=True,verbose_name="Сообщество")
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='created_categories', null=True, verbose_name="Создатель")
+    communities = models.ManyToManyField(Community, related_name='categories', blank=True,verbose_name="Сообщество")
 
     def __str__(self):
         return 'Категория: ' + self.name
