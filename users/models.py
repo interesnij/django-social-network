6d2f956981a053645e94a5f8958fc80640b3b178
Serg <ochkarik1983@mail.ru@>
2019-09-11 21:02:24 +0300

    H

--- users/models.py
+++ users/models.py
@@ -37,7 +37,7 @@ class User(AbstractUser):
         return  str(self.last_name) + " " + str(self.first_name)
 
     def __str__(self):
-        return get_full_name
+        return self.user.get_full_name()
 
 
 class UserBlock(models.Model):
