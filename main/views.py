764b5d07c3ab3820fb857f368861e90fe29f871b
Serg <ochkarik1983@mail.ru@>
2019-09-12 10:52:09 +0300

    J

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
+from users.models import User
 
 
 class MainPageView(TemplateView,CategoryListMixin):
-	template_name="main/mainpage.html"
+	if request.user:
+		template_name="main/mainpage.html"
+	else:
+		template_name="main/auth.html"
 
+	def get(self,request,*args,**kwargs):
+		return super(MainPageView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
 		context=super(MainPageView,self).get_context_data(**kwargs)
