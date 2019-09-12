2de7903c6db47fda746e6a9db91c4c7ed1d7c3a7
Serg <ochkarik1983@mail.ru@>
2019-09-12 15:48:31 +0300

    J

--- communities/urls.py
+++ communities/urls.py
@@ -2,6 +2,6 @@ from communities.views import CommunitiesView, CommunityDetailView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', CommunitiesView.as_view(), name='communities')
+    url(r'^(?P<pk>\d+)/$', CommunitiesView.as_view(), name='communities'),
     url(r'^(?P<pk>\d+)/detail/$', CommunityDetailView.as_view(), name='communiy_detail')
 ]
