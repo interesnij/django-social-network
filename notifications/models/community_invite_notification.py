834ae1cf6f3507622876d89227b1ef78dc4f0f7a
Serg <ochkarik1983@mail.ru@>
2019-09-13 23:58:30 +0300

    H

--- notifications/models/community_invite_notification.py
+++ notifications/models/community_invite_notification.py
@@ -2,10 +2,10 @@ from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-#from communities.models import CommunityInvite
+from communities.models import CommunityInvite
 from notifications.models.notification import Notification
 
 
 class CommunityInviteNotification(models.Model):
     notification = GenericRelation(Notification)
-    #community_invite = models.ForeignKey(CommunityInvite, on_delete=models.CASCADE,verbose_name="Приглашение в сообщество")
+    community_invite = models.ForeignKey(CommunityInvite, on_delete=models.CASCADE,verbose_name="Приглашение в сообщество")
--- notifications/models/post_comment_notification.py
+++ notifications/models/post_comment_notification.py
@@ -1,9 +1,9 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from notifications.models.notification import Notification
-#from posts.models import PostComment
+from posts.models import PostComment
 
 
 class PostCommentNotification(models.Model):
     notification = GenericRelation(Notification, related_name='post_comment_notifications')
-    #post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE,verbose_name="О комментарии к посту")
+    post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE,verbose_name="О комментарии к посту")
