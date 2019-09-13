8d82752fd308b6a2b3a6a74d62d4ae0973ec173a
Serg <ochkarik1983@mail.ru@>
2019-09-13 15:21:50 +0300

    f

--- tr/serializers.py
+++ tr/serializers.py
@@ -9,14 +9,14 @@ from rest_framework.response import Response
 class RegisterSerializer(serializers.Serializer):
     first_name = serializers.CharField(required=True, write_only=True)
     last_name = serializers.CharField(required=True, write_only=True)
-    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
 
     def get_cleaned_data(self):
         return {
+            'email': self.validated_data.get('email', ''),
             'first_name': self.validated_data.get('first_name', ''),
             'last_name': self.validated_data.get('last_name', ''),
             'password1': self.validated_data.get('password1', ''),
-            'email': self.validated_data.get('email', ''),
+            'password2': self.validated_data.get('password2', ''),
             }
 
 
