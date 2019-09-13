58111321b3504ee29ae1969acc1b9dbadbf61b4b
Serg <ochkarik1983@mail.ru@>
2019-09-14 00:01:53 +0300

    H

--- notifications/models/post_comment_user_mention_notification.py
+++ notifications/models/post_comment_user_mention_notification.py
@@ -1,9 +1,8 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from notifications.models.notification import Notification
-from posts.models import PostCommentUserMention
 
 
 class PostCommentUserMentionNotification(models.Model):
     notification = GenericRelation(Notification, related_name='post_comment_user_mention_notifications')
-    post_comment_user_mention = models.ForeignKey(PostCommentUserMention, on_delete=models.CASCADE,verbose_name="На упоминание в комментарии к посту")
+    post_comment_user_mention = models.ForeignKey('posts.PostCommentUserMention', on_delete=models.CASCADE,verbose_name="На упоминание в комментарии к посту")
