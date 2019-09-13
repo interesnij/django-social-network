a913d72e56d313f13a197856b229567e3c07fcf2
Serg <ochkarik1983@mail.ru@>
2019-09-13 23:52:55 +0300

    H

--- notifications/models/post_comment_reply_notification.py
+++ notifications/models/post_comment_reply_notification.py
@@ -1,11 +1,9 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from notifications.models.notification import Notification
-from posts.models import PostComment
+#from posts.models import PostComment
 
 
 class PostCommentReplyNotification(models.Model):
     notification = GenericRelation(Notification, related_name='post_comment_reply_notifications')
-    post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE,verbose_name="На ответ к комментарию к посту")
-
-    
+    #post_comment = models.ForeignKey(PostComment, on_delete=models.CASCADE,verbose_name="На ответ к комментарию к посту")
