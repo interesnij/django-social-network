95c75ea130cd3ed6adaa5610407f85a5ec0ce537
Serg <ochkarik1983@mail.ru@>
2019-09-13 11:12:29 +0300

    f

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
 
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    #exclude = ['communities']
+
+
+admin.site.register(Post, PostAdmin)
