59d37c3e8b25c699fe6dd942621fadbf0de4bc5c
Serg <ochkarik1983@mail.ru@>
2019-09-13 16:55:26 +0300

    f

--- tr/serializers.py
+++ tr/serializers.py
@@ -7,29 +7,9 @@ from rest_framework.response import Response
 
 
 
-class RegisterSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
+class RegisterSerializer(RegisterSerializer):
     first_name = serializers.CharField(required=True, write_only=True)
     last_name = serializers.CharField(required=True, write_only=True)
-    password1 = serializers.CharField(required=True, write_only=True)
-    password2 = serializers.CharField(required=True, write_only=True)
-
-    def validate_email(self, email):
-        email = get_adapter().clean_email(email)
-        if allauth_settings.UNIQUE_EMAIL:
-            if email and email_address_exists(email):
-                raise serializers.ValidationError(
-                    "A user is already registered with this e-mail address.")
-        return email
-
-    def validate_password1(self, password):
-        return get_adapter().clean_password(password)
-
-    def validate(self, data):
-        if data['password1'] != data['password2']:
-            raise serializers.ValidationError(
-                "The two password fields didn't match.")
-        return data
 
     def get_cleaned_data(self):
         return {
