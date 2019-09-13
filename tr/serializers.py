9b2f4081105f1edb1d57031d01066fb498c5cf50
Serg <ochkarik1983@mail.ru@>
2019-09-13 16:10:32 +0300

    f

--- tr/serializers.py
+++ tr/serializers.py
@@ -11,12 +11,20 @@ class RegisterSerializer(serializers.Serializer):
     last_name = serializers.CharField(required=True, write_only=True)
     email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
 
-
+    def get_cleaned_data(self):
+        return {
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'email': self.validated_data.get('email', ''),
+            }
 
 
     def save(self, request):
         adapter = get_adapter()
         user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
         adapter.save_user(request, user, self)
         setup_user_email(request, user, [])
         user.save()
