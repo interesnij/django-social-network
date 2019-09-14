502caa2a59166936ab25f3e99517102966721ae2
Serg <ochkarik1983@mail.ru@>
2019-09-14 21:00:25 +0300

    D

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ from users.forms import GeneralUserForm, AboutUserForm
 from django.views.generic import ListView
 from posts.helpers import ajax_required, AuthorRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.views.decorators.http import require_http_methods
 
 
 
