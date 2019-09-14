466880ddebbdfac0c0a1578f9b7eeed60c958d5f
Serg <ochkarik1983@mail.ru@>
2019-09-14 14:18:19 +0300

    D

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^list/(?P<pk>\d+)/$', PostUserView.as_view(), name="post_user_list"),
     url(r'^about/(?P<pk>[0-9]+)/$',
         UserAboutChange.as_view(), name='user_about_form'),
-    url(r'^(?P<pk>\d+)/$', UserDesign.as_view(), name='user_design'),
+    url(r'^design/(?P<pk>\d+)/$', UserDesign.as_view(), name='user_design'),
 ]
