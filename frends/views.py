de65d4a0182b61756369260380408f1fed48836b
Serg <ochkarik1983@mail.ru@>
2019-09-14 11:03:52 +0300

    D

--- frends/views.py
+++ frends/views.py
@@ -6,12 +6,12 @@ from connections.models import Connection
 class FrendsListView(ListView):
 	template_name="frends.html"
 	model=User
-	all_user=User.objects.all()
 
 	def get(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
 		self.frends=Connection.objects.filter(target_connection__user=self.user)
 		self.frends2=Connection.objects.filter(target_connection__target_user=self.user)
+		self.all_user=User.objects.all()
 		return super(FrendsListView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
@@ -19,5 +19,5 @@ class FrendsListView(ListView):
 		context["frends"]=self.frends
 		context['frends2'] = self.frends2
 		context['user'] = self.user
-		context['all_user'] = all_user
+		context['all_user'] = self.all_user
 		return context
