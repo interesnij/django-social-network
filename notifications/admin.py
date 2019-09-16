cdf864c679b1301e0e581fc72e2ab94e085e9946
Serg <ochkarik1983@mail.ru@>
2019-09-16 16:18:43 +0300

    H

--- notifications/admin.py
+++ notifications/admin.py
@@ -5,5 +5,5 @@ from notifications.models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('recipient', 'actor', 'verb', 'unread', )
+    list_display = ('recipient', 'verb', 'unread', )
     list_filter = ('recipient', 'unread', )
