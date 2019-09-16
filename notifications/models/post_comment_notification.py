efea3776f77354970d26acaf5b4d131bbb6e3886
Serg <ochkarik1983@mail.ru@>
2019-09-16 16:30:13 +0300

    H

--- notifications/models/post_comment_notification.py
+++ notifications/models/post_comment_notification.py
@@ -1,7 +1,6 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from notifications.models.notification import Notification
-from posts.models import PostComment
 
 
 class PostCommentNotification(models.Model):
