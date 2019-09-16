553e2674e8289149a3e0f85d4463971fcd7c6cf3
Serg <ochkarik1983@mail.ru@>
2019-09-16 12:25:38 +0300

    H

--- users/views.py
+++ users/views.py
@@ -99,7 +99,8 @@ class UserAboutChange(TemplateView):
 
 	def post(self,request,*args,**kwargs):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
-		self.form=AboutUserForm(request.POST,instance=self.user.profile)
+		self.profile = UserProfile.objects.get_or_create(user=self.request.user)
+		self.form=AboutUserForm(request.POST,instance=self.profile)
 		if self.form.is_valid():
 			self.form.save()
 			if request.is_ajax():
