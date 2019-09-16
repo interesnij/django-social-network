62bc7f488d408082f2ca4b8df9048770828bdba7
Serg <ochkarik1983@mail.ru@>
2019-09-16 16:25:16 +0300

    H

--- categories/models.py
+++ categories/models.py
@@ -13,8 +13,8 @@ class Category(models.Model):
     created = models.DateTimeField(default=timezone.now, editable=False, verbose_name="Когда создана")
     avatar = models.ImageField(blank=False, null=True,verbose_name="Аватар")
     order = models.IntegerField(unique=False, default=0,verbose_name="Порядковый номер")
-    #creator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='created_categories', null=True, verbose_name="Создатель")
-    #communities = models.ManyToManyField(Community, related_name='categories', blank=True,verbose_name="Сообщество")
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='created_categories', null=True, verbose_name="Создатель")
+    communities = models.ManyToManyField(Community, related_name='categories', blank=True,verbose_name="Сообщество")
 
     def __str__(self):
         return 'Категория: ' + self.name
