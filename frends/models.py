4016895b66267605bc67a17e5da8ea6a30dffadd
Serg <ochkarik1983@mail.ru@>
2019-09-16 20:29:49 +0300

    H

--- frends/models.py
+++ frends/models.py
@@ -6,19 +6,3 @@ class Connect(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='connections',verbose_name="Инициатор перевода из подписчика в друзья")
     target_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='targeted_connections', null=False,verbose_name="Кого переводит из подписчика в друзья")
     target_connection = models.OneToOneField('self', on_delete=models.CASCADE, null=True,blank=True, verbose_name="Связь")
-
-    @classmethod
-    def connection_with_id_exists_for_user_with_id(cls, connection_id, user_id):
-        count = Connect.objects.filter(id=connect_id,
-                                          user_id=user_id).count()
-        if count > 0:
-            return True
-
-        return False
-
-    @classmethod
-    def connection_exists(cls, user_a_id, user_b_id):
-        count = Connect.objects.filter(user_id=user_a_id,target_connection__user_id=user_b_id).count()
-        count2 = Connect.objects.filter(user_id=user_a_id,target_connection__target_user_id=user_b_id).count()
-        count_all = count + count2
-        return count_all > 0
