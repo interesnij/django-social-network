643bfd74c44c19a1b4d26ac82068513ad6e8dd35
Serg <ochkarik1983@mail.ru@>
2019-09-16 18:27:25 +0300

    H

--- users/views.py
+++ users/views.py
@@ -35,10 +35,11 @@ class ProfileUserView(TemplateView):
 		self.frends2=Connect.objects.filter(target_user=self.user)
 		self.communities=Community.objects.filter(starrers=self.user)
 		self.posts=Post.objects.filter(creator=self.user)
-		try:
-			self.connect = Connect.objects.get(user=self.request.user,target_user=self.user)
-		except:
-			self.connect = Connect.objects.get(target_user=self.request.user,user=self.user)
+		self.connect = None
+		if self.user in self.frends:
+			self.connect = True
+		if self.user in self.frends2:
+			self.connect = True
 
 		return super(ProfileUserView,self).get(request,*args,**kwargs)
 
