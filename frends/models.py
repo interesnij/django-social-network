89dc209248c18b9e13c38362ee64e846b4649b0e
Serg <ochkarik1983@mail.ru@>
2019-09-16 19:18:26 +0300

    H

--- frends/models.py
+++ frends/models.py
@@ -15,3 +15,9 @@ class Connect(models.Model):
             return True
 
         return False
+
+    @classmethod
+    def connection_exists(cls, user_a_id, user_b_id):
+        count = Connect.objects.select_related('target_connection__user_id').filter(user_id=user_a_id,
+                                                                                       target_connection__user_id=user_b_id).count()
+        return count > 0
