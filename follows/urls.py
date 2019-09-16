216bb0e3a238e1986a515809e1f9b2c5cec03b4f
Serg <ochkarik1983@mail.ru@>
2019-09-16 17:38:39 +0300

    H

--- follows/urls.py
+++ follows/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from follows.views import FollowsView, FollowCreate, FollowDelete
+from follows.views import FrendsListView, FollowCreate, FollowDelete
 
 
 urlpatterns = [
-    url(r'^follows/$', FollowsView.as_view(), name='follows'),
+    url(r'^follows/$', FrendsListView.as_view(), name='follows'),
     url(r'^add/(?P<pk>\d+)/$', FollowCreate.as_view(), name="create_follow"),
     url(r'^delete/(?P<pk>\d+)/$', FollowDelete.as_view(), name="delete_follow"),
 ]
