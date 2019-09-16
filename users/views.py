81e461b1f9a0ca733d815cc57cc896f9f8f01272
Serg <ochkarik1983@mail.ru@>
2019-09-16 18:16:33 +0300

    H

--- users/views.py
+++ users/views.py
@@ -36,7 +36,7 @@ class ProfileUserView(TemplateView):
 		self.communities=Community.objects.filter(starrers=self.user)
 		self.posts=Post.objects.filter(creator=self.user)
 		try:
-			self.connect = Connect.objects.get(target_user=self.target_user,user=self.user)
+			self.connect = Connect.objects.get(target_user=self.request.user,user=self.user)
 		except:
 			self.connect = None
 		return super(ProfileUserView,self).get(request,*args,**kwargs)
