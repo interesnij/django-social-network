64c4694c854629eabb8c4ca1e74c2d895029082d
Serg <ochkarik1983@mail.ru@>
2019-09-16 15:01:17 +0300

    H

--- frends/admin.py
+++ frends/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from frends.models import Connect
 
 
-class ConnectionAdmin(admin.ModelAdmin):
+class ConnectAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     #exclude = ['communities']
 
 
-admin.site.register(Connection, ConnectionAdmin)
+admin.site.register(Connect, ConnectAdmin)
