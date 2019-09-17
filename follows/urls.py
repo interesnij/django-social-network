28a663fa37ee62be6d23c5dfe228b3940fc3efcc
Serg <ochkarik1983@mail.ru@>
2019-09-17 10:02:53 +0300

    H

--- follows/urls.py
+++ follows/urls.py
@@ -3,7 +3,7 @@ from follows.views import FrendsListView, FollowCreate, FollowDelete
 
 
 urlpatterns = [
-    url(r'^follows/$', FrendsListView.as_view(), name='follows'),
+    url(r'^(?P<pk>\d+)/$', FrendsListView.as_view(), name='follows'),
     url(r'^add/(?P<pk>\d+)/$', FollowCreate.as_view(), name="create_follow"),
     url(r'^delete/(?P<pk>\d+)/$', FollowDelete.as_view(), name="delete_follow"),
 ]
