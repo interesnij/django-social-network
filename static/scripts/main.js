27d7efc5bf470d3f67405397f4deb31866b58cfd
Serg <ochkarik1983@mail.ru@>
2019-09-19 17:49:03 +0300

    F

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -183,6 +183,39 @@ $(document).ready(function () {
 
 });
 
+/* swiper control */
+var mySwiper = new Swiper('.swiper-story', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    spaceBetween: 15
+});
+var mySwiper2 = new Swiper('.swiper-post', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+$(document).ready(function(){
+    /* photos gallery */
+    $('.grid').masonry({
+         itemSelector: '.grid-item',
+         percentPosition: true
+    });
+
+
+});
+$(window).on("load resize", function () {
+     $('.grid').masonry({
+         itemSelector: '.grid-item',
+         percentPosition: true
+     });
+ });
+ 
+
 $(window).on('load', function () {
     /* hide loader  */
     $('.loader').hide();
