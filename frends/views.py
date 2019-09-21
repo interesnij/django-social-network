3f429ca3bf1bc5a053eca34800bc4081113eeac1
Serg <ochkarik1983@mail.ru@>
2019-09-21 14:02:05 +0300

    G

--- frends/views.py
+++ frends/views.py
@@ -34,11 +34,7 @@ class ConnectCreate(TemplateView):
 		except:
 			self.connect = None
 		if not self.connect and self.target_user != request.user:
-			target_connection = 0
-			connection = Connect.objects.create(target_user=self.target_user, user=request.user,target_connection =target_connection)
-			target_connection.target_connection = connection
-
-			connection.save()
+			Connect.create_connection(target_user=self.target_user,user=request.user)
 			fol=Follow.objects.get(user=self.target_user, followed_user=request.user)
 			fol.delete()
 		else:
