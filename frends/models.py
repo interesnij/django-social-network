9c2ff76186800792a36b2ece9beab9c8d19ad16f
Serg <ochkarik1983@mail.ru@>
2019-09-21 14:47:26 +0300

    G

--- frends/models.py
+++ frends/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 class Connect(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='connections',verbose_name="Инициатор перевода из подписчика в друзья")
     target_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='targeted_connections', null=False,verbose_name="Кого переводит из подписчика в друзья")
-    target_connection = models.OneToOneField('self', on_delete=models.CASCADE, null=True,blank=True, verbose_name="Связь")
 
     @classmethod
     def create_connection(cls, user_id, target_user_id):
