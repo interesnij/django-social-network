ffdadbcb01333a74e3239e6976be438de95cea67
Serg <ochkarik1983@mail.ru@>
2019-09-22 18:19:13 +0300

    G

--- posts/models.py
+++ posts/models.py
@@ -66,10 +66,10 @@ class Post(models.Model):
             async_to_sync(channel_layer.group_send)('notifications', payload)
 
     def notification_like(self, creator):
-        notification_handler(creator, self.user,Notification.LIKED, action_object=self,id_value=str(self.uuid),key='social_update')
+        notification_handler(user, self.creator,Notification.LIKED, action_object=self,id_value=str(self.uuid),key='social_update')
 
     def notification_dislike(self, creator):
-        notification_handler(creator, self.user,Notification.DISLIKED, action_object=self,id_value=str(self.uuid),key='social_update')
+        notification_handler(user, self.creator,Notification.DISLIKED, action_object=self,id_value=str(self.uuid),key='social_update')
 
     def reply_this(self, creator, text):
 
