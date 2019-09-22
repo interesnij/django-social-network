2124e8ad8c0605663904c140e0abedcc109490e5
Serg <ochkarik1983@mail.ru@>
2019-09-22 18:38:35 +0300

    G

--- notifications/models/notification.py
+++ notifications/models/notification.py
@@ -124,7 +124,7 @@ class Notification(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify('{self.recipient} {self.uuid_id} {self.verb}', to_lower=True, max_length=200)
+            self.slug = slugify('{self.recipient} {self.uuid_id} {self.verb}')
 
         super().save(*args, **kwargs)
 
