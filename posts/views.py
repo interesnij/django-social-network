8d90cd291f2a123b59581d26c46ad6f62a9dce4b
Serg <ochkarik1983@mail.ru@>
2019-09-22 18:22:23 +0300

    G

--- posts/views.py
+++ posts/views.py
@@ -110,7 +110,7 @@ class PostLikeView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.post_like = Post.objects.get(pk=self.kwargs["pk"])
-        self.post_like.notification_like(request.user)
+        self.post_like.notification_like(post_like.creator)
         return super(PostLikeView,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
