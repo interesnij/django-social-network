80b24b75cfdce7fe7ffbf9d8c4af6b45588e2294
Serg <ochkarik1983@mail.ru@>
2019-09-23 12:11:25 +0300

    H

--- follows/models.py
+++ follows/models.py
@@ -5,3 +5,6 @@ from users.models import User
 class Follow(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follows',verbose_name="Подписчик")
     followed_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers', null=False,verbose_name="На кого подписывается")
+
+    def notification_follow(self, user):
+        notification_handler(user, self.user,Notification.CONNECTION_REQUEST, action_object=self,id_value=str(self.uuid),key='social_update')
