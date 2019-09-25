5951652eeffd67671458908c905cf8c98e40efeb
Serg <ochkarik1983@mail.ru@>
2019-09-25 18:17:34 +0300

    h

--- notifications/views.py
+++ notifications/views.py
@@ -54,6 +54,7 @@ def mark_as_read(request, slug=None):
 @login_required
 def get_latest_notifications(request):
     notifications = request.user.notifications.get_most_recent()
+    notifications.mark_all_as_read()
     return render(request,
                   'most_recent.html',
                   {'notifications': notifications})
