09289890e6b639d5d4d7f2f54e670f344d3f54ef
Serg <ochkarik1983@mail.ru@>
2019-09-26 12:13:02 +0300

    D

--- static/scripts/posts.js
+++ static/scripts/posts.js
@@ -25,24 +25,5 @@ $(function () {
             },
         });
     });
-    
-    $(".comment").on("click", function () {
-        var post = $(this).closest(".card");
-        var posts = $(post).closest("div").attr("posts-id");
-        $("#postThreadModal").modal("show");
-        $.ajax({
-            url: '/posts/get-thread/',
-            data: {'posts': posts},
-            cache: false,
-            beforeSend: function () {
-                $("#threadContent").html("<li class='loadcomment'><img src='/static/images/loading.gif'></li>");
-            },
-            success: function (data) {
-                $("input[name=parent]").val(data.uuid)
-                $("#postsContent").html(data.posts);
-                $("#threadContent").html(data.thread);
-            }
-        });
-        return false;
-    });
+
     });
