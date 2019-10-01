dacd5fd64623776c273ce3f1d4f939a46d83a845
Serg <ochkarik1983@mail.ru@>
2019-10-01 19:46:33 +0300

    H

--- posts/urls.py
+++ posts/urls.py
@@ -16,7 +16,7 @@ from posts.views import update_interactions, get_comment
 
 
 urlpatterns = [
-    url(r'^posts/$', PostsView.as_view(), name='posts'),
+    url(r'^$', PostsView.as_view(), name='posts'),
     url(r'^add_hard/$', PostUserHardCreate.as_view(), name="post_add_hard_user"),
     url(r'^add_medium/$', PostUserMediumCreate.as_view(), name="post_add_medium_user"),
     url(r'^add_lite/$', PostUserLiteCreate.as_view(), name="post_add_lite_user"),
