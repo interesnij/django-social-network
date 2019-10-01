4bac62a7b0439aed5a381e85e7c1586fa190adaa
Serg <ochkarik1983@mail.ru@>
2019-10-01 16:13:55 +0300

    H

--- posts/admin.py
+++ posts/admin.py
@@ -5,11 +5,11 @@ from posts.models import Post, PostComment
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('name',)
-    list_display = ['creator', 'created', 'community', 'is_deleted',]
+    list_display = ['created', 'community', 'is_deleted',]
     list_filter = ['community', 'is_deleted', 'created',]
 
 class PostCommentAdmin(admin.ModelAdmin):
-    list_filter = ['parent_comment', 'commenter',]
+    list_filter = ['parent_comment',]
 
 
 admin.site.register(Post, PostAdmin)
