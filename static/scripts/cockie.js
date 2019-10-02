bf2307dd7c10f1b0bc8cfbc2f60d6a4370b3e8ac
Serg <ochkarik1983@mail.ru@>
2019-10-02 18:32:51 +0300

    H

--- static/scripts/cockie.js
+++ static/scripts/cockie.js
@@ -1,3 +1,1 @@
-
-
 function getCookie(e){var t=null;if(document.cookie&&""!=document.cookie)for(var o=document.cookie.split(";"),n=0;n<o.length;n++){var r=jQuery.trim(o[n]);if(r.substring(0,e.length+1)==e+"="){t=decodeURIComponent(r.substring(e.length+1));break}}return t}var csrftoken=getCookie("csrftoken");function csrfSafeMethod(e){return/^(GET|HEAD|OPTIONS|TRACE)$/.test(e)}function sameOrigin(e){var t="//"+document.location.host,o=document.location.protocol+t;return e==o||e.slice(0,o.length+1)==o+"/"||e==t||e.slice(0,t.length+1)==t+"/"||!/^(\/\/|http:|https:).*/.test(e)}$.ajaxSetup({beforeSend:function(e,t){!csrfSafeMethod(t.type)&&sameOrigin(t.url)&&e.setRequestHeader("X-CSRFToken",csrftoken)}});
