1cadb5712a36354fe1caebf97c1153aaae01c85e
Serg <ochkarik1983@mail.ru@>
2019-10-02 15:02:45 +0300

    G

--- posts/views.py
+++ posts/views.py
@@ -162,7 +162,7 @@ class CommentCreateView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         comment = self.request.POST.get('text')
-        post = Post.objects.get(uuid=self.kwargs["uuid"])
+        post = Post.objects.get(pk=self.kwargs["pk"])
 
         new_comment = post.comments.create(creator=request.user, text=comment)
         data = [
