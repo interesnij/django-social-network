1e2cc3777acef7cea319796cf4c5ef972cb5465e
Serg <ochkarik1983@mail.ru@>
2019-10-03 20:59:28 +0300

    H

--- posts/views.py
+++ posts/views.py
@@ -220,7 +220,7 @@ def reply_comment(request):
     comment = PostComment.objects.get(pk=com)
     text = text.strip()
     if comment:
-        new_comment = PostComment.objects.create(text=text, commenter=request.user,parent_comment=comment,object_id=com,content_type_id=com)
+        new_comment = PostComment.objects.create(text=text, commenter=request.user,parent_comment=comment,object_id=comment.object_id,content_type_id=comment.content_type_id)
         html = render_to_string('generic/post_reply_comment.html',{'reply': new_comment,'request': request})
         return JsonResponse(html, safe=False)
 
