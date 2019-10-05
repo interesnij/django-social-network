153e1faa29543a4a128db6cb40967c76e35a091a
Serg <ochkarik1983@mail.ru@>
2019-10-05 15:08:16 +0300

    H

--- users/views.py
+++ users/views.py
@@ -33,7 +33,12 @@ class ProfileUserView(TemplateView):
 		self.user=User.objects.get(pk=self.kwargs["pk"])
 		self.frends = Connect.objects.filter(Q(user=self.user)|Q(target_user=self.user))[0:5]
 		self.pop_frends = self.frends[0:5]
-		self.online_frends = self.frends.filter(self.user=self.user.get_online())
+		self.online_frends = ()
+		for i in self.online_frends:
+			if i.user.get_online():
+				self.online_frends + 1
+			if i.target_user.get_online():
+				self.online_frends + 1
 		self.target = Connect.objects.filter(user=self.user)
 		self.communities=Community.objects.filter(starrers=self.user)
 		self.posts=Post.objects.filter(creator=self.user,is_deleted=False)
