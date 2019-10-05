3b288f4ed40ad8497af9527a2585a48d077aaee4
Serg <ochkarik1983@mail.ru@>
2019-10-05 14:24:30 +0300

    H

--- users/middleware.py
+++ users/middleware.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
-from users.models import UserProfile
+from users.models import User
 from django.conf import settings
 import datetime
 
@@ -9,5 +9,5 @@ class UpdateLastActivityMiddleware(MiddlewareMixin):
         assert hasattr(request, 'user'), 'UpdateLastActivityMiddleware требует установки промежуточного программного обеспечения аутентификации.'
         now = datetime.datetime.now()
 
-        UserProfile.objects.filter(user__id=request.user.id) \
+        User.objects.filter(user__id=request.user.id) \
                     .update(last_activity=now)
