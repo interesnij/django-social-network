d244fb27d5b54a9be2df720778b1ddf1801b6814
Serg <ochkarik1983@mail.ru@>
2019-10-05 13:47:09 +0300

    H

--- users/views.py
+++ users/views.py
@@ -38,10 +38,8 @@ class ProfileUserView(TemplateView):
 		self.now = datetime.now()
 		self.online_frends=[]
 		for i in self.popular_frends:
+			self.online_frends + i
 			
-			onl2 = i.target_user.last_activity + timedelta(minutes=1)
-			if self.now < onl2:
-				online_frends = self.online_frends + i
 
 		self.follows_count=Follow.objects.filter(followed_user=self.user).count()
 		self.connect_count=Connect.objects.filter(user=self.user).count()
