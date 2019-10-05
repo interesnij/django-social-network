78f821f919d8219706cbd9ef35ca52c567d88cee
Serg <ochkarik1983@mail.ru@>
2019-10-05 16:42:03 +0300

    H

--- users/views.py
+++ users/views.py
@@ -39,8 +39,7 @@ class ProfileUserView(TemplateView):
 				self.online_frends += ('object',)
 			if object.target_user.get_online():
 				self.online_frends += ('object',)
-			if self.online_frends.count = 5:
-				break
+		self.online_frends[0:5]
 
 		self.target = Connect.objects.filter(user=self.user)
 		self.communities=Community.objects.filter(starrers=self.user)
