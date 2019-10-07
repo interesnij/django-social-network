d4ef59f4c18008e551f812ad71b61e044c492b64
Serg <ochkarik1983@mail.ru@>
2019-10-07 20:22:56 +0300

    H

--- static/scripts/main2.js
+++ static/scripts/main2.js
@@ -287,8 +287,9 @@ $(document).ready(function () {
     });
 
     if ($.type($.cookie("stylesheetname")) != 'undefined' && $.cookie("stylesheetname") != '') {
-        var linkurl = $('#theme')
-        $('head').append("<link id='theme' rel='stylesheet' href='" + $.cookie("stylesheetname") + "' type='text/css'>");
+        var linkurl = $('#theme');
+        var href_l = "/static/styles/color/" + $.cookie("stylesheetname") + "'";
+        $('head').append("<link id='theme' type='text/css' rel='stylesheet' href=' + href_l>");
         $('.theme-color input[type="radio"]').prop("checked", false);
         $("label[data-title='" + $.cookie("themecolor") + "']").prev().prop("checked", true);
         setTimeout(function () {
