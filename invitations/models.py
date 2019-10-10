288abc4e1aaa730ce32806d92f1f5a0267481b34
Serg <ochkarik1983@mail.ru@>
2019-10-10 17:00:25 +0300

    H

--- invitations/models.py
+++ invitations/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.utils import six
-from main.models import Badge
 
 
 class UserInvite(models.Model):
@@ -14,7 +13,6 @@ class UserInvite(models.Model):
     nickname = models.CharField(max_length=35, null=True, blank=True)
     email = models.EmailField(null=True, blank=True, verbose_name="Емаил")
     username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
-    badge = models.ForeignKey(Badge, blank=True, null=True, on_delete=models.SET_NULL)
     token = models.CharField(max_length=255, unique=True)
     is_invite_email_sent = models.BooleanField(default=False)
 
