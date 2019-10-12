403f5eed809553bfa13ec9c8f0be851cc6eebe88
Serg <ochkarik1983@mail.ru@>
2019-10-12 12:28:17 +0300

    H

--- users/templatetags/liked_user.py
+++ users/templatetags/liked_user.py
@@ -6,3 +6,18 @@ def user_in(objects, user):
     if user.is_authenticated:
         return objects.filter(user=user).exists()
     return False
+
+
+@register.filter
+def rupluralize(value, arg="дурак,дурака,дураков"):
+    args = arg.split(",")
+    number = abs(int(value))
+    a = number % 10
+    b = number % 100
+
+    if (a == 1) and (b != 11):
+        return args[0]
+    elif (a >= 2) and (a <= 4) and ((b < 10) or (b >= 20)):
+        return args[1]
+    else:
+        return args[2]
