2f8c299cd8028ff8f7b73c0c1f9aa3047e0d76da
Serg <ochkarik1983@mail.ru@>
2019-10-13 14:54:13 +0300

    H

--- tr/settings.py
+++ tr/settings.py
@@ -172,7 +172,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'http://194.58.96.91'
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 MEDIA_SERVER = os.path.join(PROJECT_PATH, 'media')
 
