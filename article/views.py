cbfd837a447ec57d31ed5ff97e58a8448e0fddea
Serg <ochkarik1983@mail.ru@>
2019-10-14 10:15:29 +0300

    J

--- article/views.py
+++ article/views.py
@@ -22,7 +22,7 @@ class ArticleView(TemplateView):
     template_name="articles.html"
 
 
-class ArticleDetailView(PageNumberView,DetailView,PageNumberMixin):
+class ArticleDetailView(DetailView):
 	model=Article
 	template_name="article.html"
 
