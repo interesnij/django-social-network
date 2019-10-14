bbb1d78aeab99fa8afba2b07f4df9ab340b9f78a
Serg <ochkarik1983@mail.ru@>
2019-10-14 15:45:15 +0300

    J

--- users/urls.py
+++ users/urls.py
@@ -14,7 +14,6 @@ from users.views import (
 urlpatterns = [
     url(r'^all-users/$', AllUsers.as_view(), name='all_users'),
     url(r'^@(?P<pk>\d+)/$', ProfileUserView.as_view(), name='user'),
-    url(r'^(?P<pk>\d+)/item/(?P<pk>\d+)/$', UserItemView.as_view(), name='user_item'),
     url(r'^general/(?P<pk>[0-9]+)/$',
         UserGeneralChange.as_view(), name='user_general_form'),
     url(r'^list/(?P<pk>\d+)/$', PostUserView.as_view(), name="post_user_list"),
