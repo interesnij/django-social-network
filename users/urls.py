f22d8e497ee2d2aad2744281349b0f913bdbe26b
Serg <ochkarik1983@mail.ru@>
2019-10-16 13:51:21 +0300

    G

--- users/urls.py
+++ users/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^avatar/(?P<pk>[0-9]+)/$',
         UserAvatarChange.as_view(), name='user_avatar_form'),
     url(r'^profile_button/(?P<pk>[0-9]+)/$', ProfileButtonReload.as_view(), name='profile_button_reload'),
-    url(r'^fixed/(?P<id>\d+)/$', fixed, name='fixed'),
-    url(r'^unfixed/(?P<id>\d+)/$', unfixed, name='unfixed'),
-    url(r'^delete/(?P<id>\d+)/$', item_delete, name='item_delete'),
+    url(r'^fixed/$', fixed, name='fixed'),
+    url(r'^unfixed/$', unfixed, name='unfixed'),
+    url(r'^delete/$', item_delete, name='item_delete'),
 ]
