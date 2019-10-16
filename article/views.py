6ff20bd34771b45f9140d4234b5fa387a98708ad
Serg <ochkarik1983@mail.ru@>
2019-10-16 10:57:05 +0300

    G

--- article/views.py
+++ article/views.py
@@ -28,10 +28,10 @@ class ArticleNewView(TemplateView):
         self.article = Article.objects.get(uuid=self.kwargs["uuid"])
         self.article.views += 1
         self.article.save()
-        return super(ArticleDetailView,self).get(request,*args,**kwargs)
+        return super(ArticleNewView,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
-        context=super(ArticleDetailView,self).get_context_data(**kwargs)
+        context=super(ArticleNewView,self).get_context_data(**kwargs)
         context["object"]=self.article
         return context
 
