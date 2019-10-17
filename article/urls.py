c4bba55ce40b2a10f15e2070f99da0aa8b3d5882
Serg <ochkarik1983@mail.ru@>
2019-10-17 12:48:12 +0300

    H

--- article/urls.py
+++ article/urls.py
@@ -3,12 +3,6 @@ from article.views import (
                             ArticleUserCreate,
                             ArticleNewView,
                             ArticleDetailView,
-                            ArticleCommentCreateView,
-                            ArticleDislikeView,
-                            ArticleLikeView,
-                            ArticleCommentLikeView,
-                            ArticleCommentDislikeView,
-                            article_update_interactions,
                         )
 from django.conf.urls import url
 
@@ -18,10 +12,4 @@ urlpatterns = [
     url(r'^add/$', ArticleUserCreate.as_view(), name="article_add_user"),
     url(r'^new/(?P<uuid>[0-9a-f-]+)/$', ArticleNewView.as_view(), name='article_new'),
     url(r'^detail/(?P<uuid>[0-9a-f-]+)/$', ArticleDetailView.as_view(), name='article_detail'),
-    url(r'^like_window/(?P<pk>\d+)/$', ArticleLikeView.as_view(), name='article_like_window'),
-    url(r'^comment_like_window/(?P<pk>\d+)/$', ArticleCommentLikeView.as_view(), name='article_comment_like_window'),
-    url(r'^dislike_window/(?P<pk>\d+)/$', ArticleDislikeView.as_view(), name='article_dislike_window'),
-    url(r'^comment_dislike_window/(?P<pk>\d+)/$', ArticleCommentDislikeView.as_view(), name='article_comment_dislike_window'),
-    url(r'^update-interactions/$', article_update_interactions, name='article_update_interactions'),
-
 ]
