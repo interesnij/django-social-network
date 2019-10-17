d7b783b4384319966e41ad0ab4905687f96a3e70
Serg <ochkarik1983@mail.ru@>
2019-10-17 12:47:20 +0300

    H

--- posts/forms.py
+++ posts/forms.py
@@ -1,12 +1,6 @@
-from posts.models import Post, PostComment2
+from posts.models import Post
 from django import forms
 
-
-class PostCommentForm(forms.ModelForm):
-
-	class Meta:
-		model = PostComment2
-		fields = ['text']
 
 class PostRepostForm(forms.Form):
     repost_comment = forms.CharField(widget=forms.Textarea)
