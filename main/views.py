037e01599b28c6291fe4dcfd93b7d3383093f12d
Serg <ochkarik1983@mail.ru@>
2019-10-17 13:40:14 +0300

    H

--- main/views.py
+++ main/views.py
@@ -126,12 +126,12 @@ def get_comment(request):
     item = Item.objects.get(uuid=item_id)
     form_comment = CommentForm()
     comments = Comment.objects.filter(item=item, parent_comment=None).select_related().order_by("created")
-    item_html = render_to_string("generic/article.html", {"object": item})
+
     comments_html = render_to_string(
         "generic/comments.html", {"comments": comments,"form_comment": form_comment,"parent": item})
     return JsonResponse({
         "uuid": item_id,
-        "item": item_html,
+
         "comments": comments_html,
     })
 
