1347d2821ced9fac45325aabb39054252f825c25
Serg <ochkarik1983@mail.ru@>
2019-10-18 14:47:59 +0300

    J

--- gallery/views.py
+++ gallery/views.py
@@ -21,18 +21,15 @@ class GalleryView(TemplateView):
 class AjaxPhotoUploadView(TemplateView):
 	template_name="add.html"
 
-    def post_ajax(self, request, *args, **kwargs):
-        try:
-            album = Album.objects.get(pk=kwargs.get('pk'))
-        except Album.DoesNotExist:
-            error_dict = {'message': 'Album not found.'}
-            return self.render_json_response(error_dict, status=404)
+	def post_ajax(self, request, *args, **kwargs):
+		try:
+			album = Album.objects.get(pk=kwargs.get('pk'))
+		except Album.DoesNotExist:
+			error_dict = {'message': 'Album not found.'}
+			return self.render_json_response(error_dict, status=404)
 
-        uploaded_file = request.FILES['file']
-        Photo.objects.create(album=album, file=uploaded_file)
+		uploaded_file = request.FILES['file']
+		Photo.objects.create(album=album, file=uploaded_file)
 
-        response_dict = {
-            'message': 'File uploaded successfully!',
-        }
-
-        return self.render_json_response(response_dict, status=200)
+		response_dict = {'message': 'File uploaded successfully!',}
+		return self.render_json_response(response_dict, status=200)
