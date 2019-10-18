ecb808be262eca1b7be74974d762206d1b20b0e0
Serg <ochkarik1983@mail.ru@>
2019-10-18 14:13:05 +0300

    J

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
-﻿from gallery.views import GalleryView
+﻿from gallery.views import GalleryView, AjaxPhotoUploadView
 from django.conf.urls import url
+
 
 urlpatterns=[
 	url(r'^(?P<pk>\d+)/$', GalleryView.as_view(), name="gallery"),
+	url(r'^(?P<pk>\d+)/ajax-upload/$', AjaxPhotoUploadView.as_view(), name="ajax_photo_upload_view"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from users.models import User
+from gallery.models import Album, Photo
 
 
 class GalleryView(TemplateView):
@@ -13,3 +14,22 @@ class GalleryView(TemplateView):
 		context=super(GalleryView,self).get_context_data(**kwargs)
 		context['user'] = self.user
 		return context
+
+
+class AjaxPhotoUploadView(TemplateView):
+
+    def post_ajax(self, request, *args, **kwargs):
+        try:
+            album = Album.objects.get(pk=kwargs.get('pk'))
+        except Album.DoesNotExist:
+            error_dict = {'message': 'Album not found.'}
+            return self.render_json_response(error_dict, status=404)
+
+        uploaded_file = request.FILES['file']
+        Photo.objects.create(album=album, file=uploaded_file)
+
+        response_dict = {
+            'message': 'File uploaded successfully!',
+        }
+
+        return self.render_json_response(response_dict, status=200)
