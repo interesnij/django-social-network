67b8557527b693a712784f0b939fe4292611ac4f
Serg <ochkarik1983@mail.ru@>
2019-10-18 19:35:51 +0300

    J

--- notifications/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from notifications.models import Notification
-
-
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('recipient', 'verb', 'unread', )
-    list_filter = ('recipient', 'unread', )
