071ade30142e3c1bca24394cf88dbfc744748c3e
Serg <ochkarik1983@mail.ru@>
2019-10-20 16:58:46 +0300

    G

--- article/models.py
+++ article/models.py
@@ -13,7 +13,7 @@ from main.models import Item
 class Article(Item):
     title = models.CharField(max_length=100, blank=False, null=False, verbose_name="Заголовок" )
     image = ProcessedImageField(verbose_name='Главное изображение', blank=False, format='JPEG',
-                                 options={'quality': 80}, processors=[ResizeToFill(1024, upscale=True)],
+                                 options={'quality': 80}, processors=[ResizeToFill(1024, 650)],
                                  upload_to='articles/%Y/%m/%d')
     content = RichTextUploadingField(config_name='default',
                                       external_plugin_resources=[(
