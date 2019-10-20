68ddbc54fae5cae26902ba855a89176c9c966db7
Serg <ochkarik1983@mail.ru@>
2019-10-20 19:27:37 +0300

    G

--- goods/urls.py
+++ goods/urls.py
@@ -2,7 +2,7 @@ from .views import GoodCategoriesView, GoodSubCategoriesView, GoodsListView
 from django.conf.urls import url
 
 urlpatterns=[
-	url(r'^$', GoodsListView.as_view(), name="goods"),
+	url(r'^(?P<pk>\d+)/$', GoodsListView.as_view(), name="goods"),
     url(r'sub/^$', GoodSubCategoriesView.as_view(), name="good_sub_categories"),
     url(r'cat/^$', GoodCategoriesView.as_view(), name="good_categories"),
 ]
