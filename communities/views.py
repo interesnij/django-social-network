a925196ce86dd5d8b6a4a69c8858c19508e67dfe
Serg <ochkarik1983@mail.ru@>
2019-10-25 22:47:23 +0300

    J

--- communities/views.py
+++ communities/views.py
@@ -8,16 +8,16 @@ from django.views.generic.detail import DetailView
 class CommunitiesView(ListView):
 	template_name="communities.html"
 	model=Community
+	paginate_by=10
 
-	def get(self,request,*args,**kwargs):
-		self.user=User.objects.get(pk=self.kwargs["pk"])
-		self.groups=Community.objects.filter(starrers=self.user)
-		return super(CommunitiesView,self).get(request,*args,**kwargs)
+	def get_queryset(self):
+        self.user=User.objects.get(pk=self.kwargs["pk"])
+        groups=Community.objects.filter(starrers=self.user)
+        return groups
 
 	def get_context_data(self,**kwargs):
 		context=super(CommunitiesView,self).get_context_data(**kwargs)
-		context["groups"]=self.groups
-		context['user'] = self.user
+
 		return context
 
 class CommunityDetailView(DetailView):
@@ -27,4 +27,9 @@ class CommunityDetailView(DetailView):
 
 class AllCommunities(ListView):
     template_name="all_communities.html"
-    model=User
+    model=Community
+	paginate_by=10
+
+	def get_queryset(self):
+        groups=Community.objects.all()
+        return groups
