bfa03b9c9c1590717661da60fbf6ba5420428ef4
Serg <ochkarik1983@mail.ru@>
2019-10-25 22:38:34 +0300

    J

--- communities/urls.py
+++ communities/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', CommunitiesView.as_view(), name='communities'),
-    url(r'^(?P<pk>\d+)/detail/$', CommunityDetailView.as_view(), name='community_detail')б
+    url(r'^(?P<pk>\d+)/detail/$', CommunityDetailView.as_view(), name='community_detail'),
     url(r'^all-communities/$', AllCommunities.as_view(), name='all_communities'),
 ]
