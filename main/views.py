a730f6f79254b19c8739a9e36500344479667f36
Serg <ochkarik1983@mail.ru@>
2019-10-26 16:41:19 +0300

    J

--- main/views.py
+++ main/views.py
@@ -35,10 +35,7 @@ class RepostUser(View):
 
 	def post(self, request, *args, **kwargs):
 		self.item = Item.objects.get(pk=self.kwargs["pk"])
-		if self.item.post:
-			new_repost = Post.objects.create(creator=request.user, parent=self.item, is_repost=True)
-			return HttpResponse("!")
-		else:
+		if self.item.content:
 			new_repost = Article.objects.create(creator=request.user, parent=self.item, is_repost=True)
 			return HttpResponse("!")
 		if not self.item.post and not self.item.article:
