5a50472abe5b9f07cabe32e3aaa837af8b72aa7f
Serg <ochkarik1983@mail.ru@>
2019-10-28 09:33:06 +0300

    H

--- main/models.py
+++ main/models.py
@@ -166,7 +166,7 @@ class ItemReaction(models.Model):
         unique_together = ('reactor', 'item', 'emoji')
 
     def __str__(self):
-        return self.item
+        return self.emoji
 
     @classmethod
     def count_reactions_for_item_with_id(cls, item_id, reactor_id=None):
