e1685464bc83722ab9bfd05d78024754662e477e
Serg <ochkarik1983@mail.ru@>
2019-10-29 14:22:24 +0300

    J

--- communities/views.py
+++ communities/views.py
@@ -40,6 +40,10 @@ class CommunityCreate(TemplateView):
 	template_name="community_add.html"
 	form=None
 
+	def get(self,request,*args,**kwargs):
+		self.form=GoodForm()
+		return super(CommunityCreate,self).get(request,*args,**kwargs)
+
 	def get_context_data(self,**kwargs):
 		context=super(CommunityCreate,self).get_context_data(**kwargs)
 		context["form"]=self.form
