0ee70b778a12b326f1ace65cddb5a7f65767ec54
Serg <ochkarik1983@mail.ru@>
2019-10-29 10:47:54 +0300

    J

--- communities/admin.py
+++ communities/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from communities.models import CommunityCategory, Community, CommunityMembership
+from communities.models import CommunityCategory, CommunitySubCategory, Community, CommunityMembership
 
 
 class CommunityCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+class CommunitySubCategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 class CommunityAdmin(admin.ModelAdmin):
@@ -13,6 +16,7 @@ class CommunityMembershipAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+admin.site.register(CommunitySubCategory, CommunitySubCategoryAdmin)
 admin.site.register(CommunityCategory, CommunityCategoryAdmin)
 admin.site.register(Community, CommunityAdmin)
 admin.site.register(CommunityMembership, CommunityMembershipAdmin)
