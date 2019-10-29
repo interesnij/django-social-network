1d9f7a5aaec0c0bfa479dddd75dba6ea9a8e0aba
Serg <ochkarik1983@mail.ru@>
2019-10-29 21:56:52 +0300

    J

--- users/views.py
+++ users/views.py
@@ -45,7 +45,7 @@ class ItemListView(ListView, CategoryListMixin):
 
     def get_queryset(self):
         self.user=User.objects.get(pk=self.kwargs["pk"])
-        items = Item.objects.filter(creator=self.user,is_deleted=False).exclude(community=True)
+        items = Item.objects.filter(creator=self.user,is_deleted=False).exclude(community__creator=self.user)
         return items
 
     def get_context_data(self, **kwargs):
