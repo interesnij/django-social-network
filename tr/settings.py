fc96940e2038c7b14cc56fcc1f683db1ec5c7eaf
Serg <ochkarik1983@mail.ru@>
2019-10-31 12:37:04 +0300

    J

--- tr/settings.py
+++ tr/settings.py
@@ -102,7 +102,7 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tr_db',
+        'NAME': 'tr_db6',
         'USER': 'tr',
         'PASSWORD': 'DuhVremeni3',
         'HOST': 'localhost',
