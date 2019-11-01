300c4668631a27c81e2a77b8c376322873e31f3a
Serg <ochkarik1983@mail.ru@>
2019-11-01 15:45:28 +0300

    H

--- users/models.py
+++ users/models.py
@@ -123,6 +123,9 @@ class User(AbstractUser):
     def follow_user(self, user):
         return self.follow_user_with_id(user.pk)
 
+    def is_blocked_with_user_with_id(self, user_id):
+        return UserBlock.users_are_blocked(user_a_id=self.pk, user_b_id=user_id)
+
     def follow_user_with_id(self, user_id):
         check_can_follow_user_with_id(user=self, user_id=user_id)
         if self.pk == user_id:
