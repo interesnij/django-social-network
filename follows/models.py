017c1fce52d6f777d0ff1e99c441f6ed0eefe172
Serg <ochkarik1983@mail.ru@>
2019-11-01 15:33:03 +0300

    H

--- follows/models.py
+++ follows/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from users.models import User
 from notifications.models import Notification, notification_handler
+from django.conf import settings
 
 
 class Follow(models.Model):
-    user = models.ForeignKey(User, db_index=False, on_delete=models.CASCADE, related_name='follows', verbose_name="Подписчик")
-    followed_user = models.ForeignKey(User, db_index=False, on_delete=models.CASCADE, related_name='followers', null=False, verbose_name="На кого подписывается")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, db_index=False, on_delete=models.CASCADE, related_name='follows', verbose_name="Подписчик")
+    followed_user = models.ForeignKey(settings.AUTH_USER_MODEL, db_index=False, on_delete=models.CASCADE, related_name='followers', null=False, verbose_name="На кого подписывается")
 
     def notification_follow(self, user):
         notification_handler(user, self.followed_user, Notification.CONNECTION_REQUEST, action_object=self, id_value=str(user.uuid), key='notification')
