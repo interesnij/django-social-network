1cdbe1b85bc83e1e5da8cd6c8c14f8f34b9387e6
Serg <ochkarik1983@mail.ru@>
2019-11-01 15:38:57 +0300

    H

--- follows/views.py
+++ follows/views.py
@@ -27,7 +27,7 @@ class FollowCreate(View):
 
 	def get(self,request,*args,**kwargs):
 		self.followed_user = User.objects.get(pk=self.kwargs["pk"])
-		follow_user(self.followed_user.pk)
+		request.user.follow_user(self.followed_user.pk)
 		new_follow.notification_follow(request.user)
 		return HttpResponse("!")
 
@@ -36,5 +36,5 @@ class FollowDelete(View):
 
 	def get(self,request,*args,**kwargs):
 		self.followed_user = User.objects.get(pk=self.kwargs["pk"])
-		unfollow_user(self.followed_user.pk)
+		request.user.unfollow_user(self.followed_user.pk)
 		return HttpResponse("!")
