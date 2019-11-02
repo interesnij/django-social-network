dd6ced7d7ce8729be53e3bd8e359f4b0519f1809
Serg <ochkarik1983@mail.ru@>
2019-11-02 11:32:52 +0300

    H

--- posts/models.py
+++ posts/models.py
@@ -45,7 +45,7 @@ class Post(Item):
         if not text and not image:
             raise ValidationError('Нужно ввести текст или прикрепить фото')
         else:
-            post = Post.objects.create(creator=creator, created=timezone.now())
+            post = Post.objects.create(creator=creator)
 
         if text:
             post.text = text
