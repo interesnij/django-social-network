43c38434006e05cfd532d9cfb2d6960927a87b5d
Serg <ochkarik1983@mail.ru@>
2019-11-02 13:33:30 +0300

    H

--- article/models.py
+++ article/models.py
@@ -28,7 +28,7 @@ class Article(Item):
     def create_article(cls, creator, community=None, g_image=None, content=None,
                     created=None, is_draft=False ):
 
-        article = Article.objects.create(creator=creator, text=text, g_image=g_image, community=community)
+        article = Article.objects.create(creator=creator, content=content, g_image=g_image, community=community)
 
         channel_layer = get_channel_layer()
         payload = {
