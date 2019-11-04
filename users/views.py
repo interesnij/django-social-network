31ee0dbadbb7052b558566bb2c7e42af5b3abb94
Serg <ochkarik1983@mail.ru@>
2019-11-04 13:02:41 +0300

    l

--- users/views.py
+++ users/views.py
@@ -35,12 +35,12 @@ class AvatarReload(TemplateView):
     template_name="profile/avatar_reload.html"
 
     def get(self,request,*args,**kwargs):
-        self.user=User.objects.get(pk=self.kwargs["pk"])
+        self.user2=User.objects.get(pk=self.kwargs["pk"])
         return super(AvatarReload,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(AvatarReload, self).get_context_data(**kwargs)
-        
+
         return context
 
 
