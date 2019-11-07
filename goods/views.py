f5c405b5c781c661d14cab4f6bd7dbf3672e3767
Serg <ochkarik1983@mail.ru@>
2019-11-07 17:44:04 +0300

    H

--- goods/views.py
+++ goods/views.py
@@ -59,7 +59,7 @@ class GoodUserCreate(TemplateView):
 				html = render_to_string('good.html',{'object': new_good,'request': request})
 			return HttpResponse(html)
 		else:
-			return HttpResponse("!")
+			return JsonResponse({'error': True, 'errors': self.form.errors})
 		return super(GoodUserCreate,self).get(request,*args,**kwargs)
 
 
