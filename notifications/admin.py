6cc5f16503d4421bff5c977c521dc15dea0a9486
Serg <ochkarik1983@mail.ru@>
2019-11-07 18:25:20 +0300

    H

--- notifications/admin.py
+++ notifications/admin.py
@@ -3,4 +3,5 @@ from notifications.models import Notification, CommunityNotification
 
 
 
-admin.site.register(Notification, CommunityNotification)
+admin.site.register(Notification)
+admin.site.register(CommunityNotification)
