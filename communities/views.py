83a5ce6e334b91f5f3f6b2574b28be1b7636b1d7
Serg <ochkarik1983@mail.ru@>
2019-11-08 11:20:54 +0300

    H

--- communities/views.py
+++ communities/views.py
@@ -246,7 +246,7 @@ class CommunityGeneralChange(TemplateView):
 	def post(self,request,*args,**kwargs):
 		self.community = Community.objects.get(pk=self.kwargs["pk"])
 		self.form=GeneralCommunityForm(request.POST, request.FILES, instance=self.community)
-		if self.form.is_valid() and if request.user.is_authenticated and request.user.is_administrator_of_community_with_name(self.community.name):
+		if self.form.is_valid() and request.user.is_authenticated and request.user.is_administrator_of_community_with_name(self.community.name):
 			self.form.save()
 			if request.is_ajax():
 				return HttpResponse ('!')
