d30e1b0c575c0299cf36e0f984b6677202638d46
Serg <ochkarik1983@mail.ru@>
2019-11-08 15:00:41 +0300

    H

--- communities/forms.py
+++ communities/forms.py
@@ -16,14 +16,6 @@ class GeneralCommunityForm(forms.ModelForm):
 	description = forms.CharField( label="",widget=forms.Textarea(
             attrs={'class': 'form-control', 'rows': '5'}
         ))
-    class Meta:
-        model = Community
-        fields = (
-            'category',
-            'name',
-            'description',
-            'rules',
-            'avatar',
-            'cover',
-            'status',
-        )
+	class Meta:
+		model = Community
+		fields = 'category', 'name', 'description', 'rules', 'avatar', 'cover', 'status'
