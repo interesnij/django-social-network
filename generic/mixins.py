e92096be64ea915711c64f47cca6042a1006352c
Serg <ochkarik1983@mail.ru@>
2019-11-08 11:31:13 +0300

    H

--- generic/mixins.py
+++ generic/mixins.py
@@ -8,7 +8,7 @@ class EmojiListMixin(ContextMixin):
 	emojies_1 = Emoji.objects.filter(group=1)
 	emojies_2 = Emoji.objects.filter(group=2)
 	def get_context_data(self,**kwargs):
-		context=super(CategoryListMixin,self).get_context_data(**kwargs)
+		context=super(EmojiListMixin,self).get_context_data(**kwargs)
 		context["current_url"]=self.request.path
 		context["emojies_1"]=self.emojies_1
 		context["emojies_2"]=self.emojies_2
