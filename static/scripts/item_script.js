70a289cae7d64468cfff730477dd4b07f8962d20
Serg <ochkarik1983@mail.ru@>
2019-11-09 15:17:07 +0300

    J

--- static/scripts/item_script.js
+++ static/scripts/item_script.js
@@ -47,8 +47,8 @@ $('#ajax').on('click', '.fullscreen', function () {
 	 var item = $(this);
 	 var item_pk = item.data("pk");
 	 var user_uuid = item.data("uuid");
-	 console.log(item_uuid);
-	 console.log(user_pk);
+	 console.log(item_pk);
+	 console.log(user_uuid);
 	 $('#item_loader').html('').load("/users/item/" + user_pk + "/" + item_uuid + "/")
 	 $('.item_fullscreen').show();
 });
