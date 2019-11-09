517a281345ba465d5c099750b304fa1662bd8607
Serg <ochkarik1983@mail.ru@>
2019-11-09 18:20:16 +0300

    J

--- users/models.py
+++ users/models.py
@@ -291,7 +291,7 @@ class User(AbstractUser):
 
         posts_select_related = ('creator', 'creator__profile', 'community')
 
-        items_only = ('id')
+        items_only = ('id', 'uuid')
 
         reported_posts_exclusion_query = ~Q(moderated_object__reports__reporter_id=self.pk)
 
