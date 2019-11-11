4ef04f53b6fed990842a91c2636300a52e9fb848
Serg <ochkarik1983@mail.ru@>
2019-11-11 10:59:39 +0300

    J

--- users/views.py
+++ users/views.py
@@ -92,7 +92,7 @@ class ProfileUserView(TemplateView):
     def get(self,request,*args,**kwargs):
         self.user=User.objects.get(pk=self.kwargs["pk"])
         if request.user.is_connected_with_user(self.user):
-            is_frend=True
+            self.is_frend=True
         self.communities=Community.objects.filter(memberships__user__id=self.user.pk)[0:5]
         return super(ProfileUserView,self).get(request,*args,**kwargs)
 
