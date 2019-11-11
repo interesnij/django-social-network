cf6f91ef1753835e449228539da1393482d36265
Serg <ochkarik1983@mail.ru@>
2019-11-11 16:32:51 +0300

    J

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,7 @@ from users.models import User
 from gallery.helpers import upload_to_photo_directory
 from pilkit.processors import ResizeToFill, ResizeToFit
 from imagekit.models import ProcessedImageField
-
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Album(models.Model):
