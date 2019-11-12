c706ebccedf2113ea7687e35b419966a580b1080
Serg <ochkarik1983@mail.ru@>
2019-11-12 10:24:50 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -48,7 +48,7 @@ class ItemListView(View, EmojiListMixin):
             check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
             if self.user.is_closed_profile:
                 check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
-        item_list = self.user.get_posts()
+        item_list = self.user.get_posts().order_by('-created')
         current_page = Paginator(item_list, 10)
         page = request.GET.get('page')
         context['user'] = self.user
