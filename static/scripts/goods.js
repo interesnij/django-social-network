1951957f0cb7055cf77f3e040f50d32b199a4fa5
Serg <ochkarik1983@mail.ru@>
2019-11-12 11:17:29 +0300

    J

---
+++ static/scripts/goods.js
@@ -0,0 +1,22 @@
+
+$('.infinite-container').on('click', '.good_detail', function() {
+    var good = $(this);
+    var good_id = good.data("id");
+    $('#good_loader').html('').load("/goods/detail/" + good_id)
+    $('.good_fullscreen').show();
+});
+
+var infinite = new Waypoint.Infinite({
+    element: $('.infinite-container')[0],
+    onBeforePageLoad: function() {
+        $('.load').show();
+    },
+    onAfterPageLoad: function($items) {
+        $('.load').hide();
+    }
+});
+
+$('.infinite-container').on('click', '.good_fullscreen_hide', function() {
+    $('.good_add_fullscreen').hide();
+    $('#good_add_loader').empty();
+});
