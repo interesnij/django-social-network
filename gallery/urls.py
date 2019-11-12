fd4ce02e74ffd9f8d1fe5ca78d80bdbfea3c7a85
Serg <ochkarik1983@mail.ru@>
2019-11-12 14:06:51 +0300

    J

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
 	url(r'^user_album/(?P<uuid>[0-9a-f-]+)/$', UserAlbomView.as_view(), name="user_album"),
 	url(r'^user_album/reload/(?P<uuid>[0-9a-f-]+)/$', UserAlbomReload.as_view(), name="user_album"),
 	url(r'^user_albums/(?P<uuid>[0-9a-f-]+)/$', UserAlbumsList.as_view(), name="user_albums"),
+	url(r'^user_photos/(?P<uuid>[0-9a-f-]+)/$', UserPhotosList.as_view(), name="user_photos"),
 	url(r'^user_add_photo/(?P<uuid>[0-9a-f-]+)/$', PhotoUserCreate.as_view(), name="photo_add_user"),
 	url(r'^user_add_album/(?P<uuid>[0-9a-f-]+)/$', AlbumUserCreate.as_view(), name="album_add_user"),
 	url(r'^gygyg/(?P<uuid>[0-9a-f-]+)/$', AlbomGygView.as_view(), name="album_ggg"),
