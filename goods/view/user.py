f55c63eb1481557b2dba74c94215e5494c766ecd
Serg <ochkarik1983@mail.ru@>
2019-11-13 18:03:38 +0300

    J

--- goods/view/user.py
+++ goods/view/user.py
@@ -36,9 +36,12 @@ class UserGoodsList(View):
                 check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
             goods_list = self.user.get_goods().order_by('-created')
             current_page = Paginator(goods_list, 6)
-        if request.user.is_anonymous and self.user.is_closed_profile():
+        elif self.user == request.user:
+            goods_list = self.user.get_goods().order_by('-created')
+            current_page = Paginator(goods_list, 6)
+        elif request.user.is_anonymous and self.user.is_closed_profile():
             raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
-        if request.user.is_anonymous and not self.user.is_closed_profile():
+        elif request.user.is_anonymous and not self.user.is_closed_profile():
             goods_list = self.user.get_goods().order_by('-created')
             current_page = Paginator(goods_list, 6)
 
