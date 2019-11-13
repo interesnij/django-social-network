5c1257d86faf9bcc09290d3d15ce0efcae380106
Serg <ochkarik1983@mail.ru@>
2019-11-13 08:54:40 +0300

    J

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from users.models import User, UserProfile, UserNotificationsSettings, UserPrivateSettings
+from users.models import *
 
 
 class UserProfileInline(admin.TabularInline):
@@ -23,3 +23,5 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(UserBlock)
