c11d486e7430966f2400fd38986f44fbc8d69843
Serg <ochkarik1983@mail.ru@>
2019-11-13 09:49:26 +0300

    J

--- users/models.py
+++ users/models.py
@@ -189,7 +189,10 @@ class User(AbstractUser):
         return self.banned_of_communities.filter(name=community_name).exists()
 
     def is_closed_profile(self):
-        return self.user_private.is_private
+        if UserPrivateSettings.objects.get(user=self).exists():
+            return self.user_private.is_private
+        else:
+            UserPrivateSettings.objects.create(user=self)
 
     def is_creator_of_community_with_name(self, community_name):
         return self.created_communities.filter(name=community_name).exists()
