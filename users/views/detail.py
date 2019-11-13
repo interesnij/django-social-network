10c8356833373ba6249bc6210da0a318f4941c99
Serg <ochkarik1983@mail.ru@>
2019-11-13 13:12:01 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -90,7 +90,7 @@ class ProfileUserView(TemplateView):
             self.is_frend = request.user.is_connected_with_user(self.user)
         except:
             self.is_frend = None
-        if self.user.is_authenticated:
+        if request.user.is_authenticated:
             self.is_blocked = request.user.has_blocked_user_with_id(self.user)
         self.communities=Community.objects.filter(memberships__user__id=self.user.pk)[0:5]
         return super(ProfileUserView,self).get(request,*args,**kwargs)
