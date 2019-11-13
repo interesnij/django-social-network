c1a1e3a289a8e35daef48e50f91e533b96a371f7
Serg <ochkarik1983@mail.ru@>
2019-11-13 10:54:06 +0300

    J

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from users.views.detail import ProfileUserView
 
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^detail/', include('users.url.detail')),
     url(r'^settings/', include('users.url.settings')),
     url(r'^load/', include('users.url.load')),
+    url(r'^(?P<pk>\d+)/$', ProfileUserView.as_view(), name='user'),
 ]
