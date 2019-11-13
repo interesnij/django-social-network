541a31440fdd30b9bc93d764db053afb0f80455d
Serg <ochkarik1983@mail.ru@>
2019-11-13 18:24:20 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -56,17 +56,17 @@ class ItemListView(View, EmojiListMixin):
             check_is_not_blocked_with_user_with_id(user=self.request.user, user_id=self.user.id)
             if self.user.is_closed_profile():
                 check_is_connected_with_user_with_id(user=self.request.user, user_id=self.user.id)
-            item_list = self.user.get_posts().order_by('-created')
-            self.current_page = Paginator(item_list, 10)
+            items_list = self.user.get_posts().order_by('-created')
+            self.current_page = Paginator(items_list, 10)
             page = request.GET.get('page')
         elif request.user.is_anonymous and self.user.is_closed_profile():
             raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
         elif request.user.is_anonymous and not self.user.is_closed_profile():
-            item_list = self.user.get_posts().order_by('-created')
-            current_page = Paginator(item_list, 12)
+            items_list = self.user.get_posts().order_by('-created')
+            current_page = Paginator(items_list, 12)
         elif self.user == request.user:
-            item_list = self.user.get_posts().order_by('-created')
-            current_page = Paginator(item_list, 12)
+            items_list = self.user.get_posts().order_by('-created')
+            current_page = Paginator(items_list, 12)
 
         context['user'] = self.user
         try:
