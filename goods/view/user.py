fedf8c185497503ad621b5ddb33c0bce64177bd4
Serg <ochkarik1983@mail.ru@>
2019-11-13 18:05:36 +0300

    J

--- goods/view/user.py
+++ goods/view/user.py
@@ -61,6 +61,7 @@ class UserGood(EmojiListMixin, TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user=User.objects.get(uuid=self.kwargs["uuid"])
+        self.good = Good.objects.get(pk=self.kwargs["pk"])
         if self.user != request.user and request.user.is_authenticated:
             check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
             if self.user.is_closed_profile:
