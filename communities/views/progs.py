e8fefce6b6650ac22842301080b5414cf7f58255
Serg <ochkarik1983@mail.ru@>
2019-11-15 13:44:44 +0300

    J

--- communities/views/progs.py
+++ communities/views/progs.py
@@ -31,7 +31,7 @@ class CommunityCreate(TemplateView):
 		self.form=CommunityForm(request.POST)
 		if self.form.is_valid():
 			new_community=self.form.save(commit=False)
-			Community.create_community(
+			community = Community.create_community(
 										name=new_community.name,
 										category=new_community.category,
 										type=new_community.type,
@@ -39,19 +39,19 @@ class CommunityCreate(TemplateView):
 										)
 			Album.objects.create(
 	                                creator=request.user,
-	                                community=new_community,
+	                                community=community,
 	                                title="Сохраненные фото",
 	                                is_generic=True,
 	                            )
 			Album.objects.create(
 	                                creator=request.user,
-	                                community=new_community,
+	                                community=community,
 	                                title="Фото со стены",
 	                                is_generic=True,
 	                            )
 			Album.objects.create(
 	                                creator=request.user,
-	                                community=new_community,
+	                                community=community,
 	                                title="Фото со страницы",
 	                                is_generic=True,
 	                            )
