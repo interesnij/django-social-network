934c11a74ae1900f00fd2b1ce9fabe7f92583f0b
Serg <ochkarik1983@mail.ru@>
2019-11-28 12:42:43 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -98,7 +98,7 @@ class ProfileUserView(TemplateView):
     def get(self,request,*args,**kwargs):
         self.user=User.objects.get(pk=self.kwargs["pk"])
         self.avatar_album = Album.objects.get(creator=self.user, title="Фото со страницы", is_generic=True)
-        self.avatar = Photo.objects.get(album_2=self.avatar_album)[:1]
+        self.avatar = Photo.objects.filter(album_2=self.avatar_album)[:1]
         try:
             self.is_frend = request.user.is_connected_with_user(self.user)
         except:
