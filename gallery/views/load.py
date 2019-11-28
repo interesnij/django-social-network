93193a02114984b4383b78eb47649e7ea80fcc40
Serg <ochkarik1983@mail.ru@>
2019-11-28 20:06:44 +0300

    J

--- gallery/views/load.py
+++ gallery/views/load.py
@@ -66,7 +66,7 @@ class UserDetailAvatar(EmojiListMixin, TemplateView):
             self.photos = self.user.get_avatar_photos()
         elif self.user == request.user and request.user.is_authenticated:
             self.photos = self.user.get_avatar_photos()
-            self.avatar_album = Album.objects.get(creator_id=self.id, title="Фото со страницы", is_generic=True)
+            self.avatar_album = Album.objects.get(creator_id=self.user.id, title="Фото со страницы", is_generic=True)
             self.photos_query = Q(creator_id=self.user.id, is_deleted=False, community=None, album_2=avatar_album)
             self.exclude_reported_and_approved_photos_query = ~Q(moderated_object__status=ModeratedObject.STATUS_APPROVED)
             self.photos_query.add(exclude_reported_and_approved_photos_query, Q.AND)
