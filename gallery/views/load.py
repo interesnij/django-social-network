391d3e8edc6c8f0edad9134ca509a547314b24a0
Serg <ochkarik1983@mail.ru@>
2019-11-28 20:11:33 +0300

    J

--- gallery/views/load.py
+++ gallery/views/load.py
@@ -1,18 +1,12 @@
 from django.views.generic.base import TemplateView
 from users.models import User
 from gallery.models import Album, Photo
-from gallery.helpers import AjaxResponseMixin, JSONResponseMixin
-from django.views.generic import ListView
-from gallery.forms import AlbumForm, AvatarUserForm
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.views import View
 from generic.mixins import EmojiListMixin
 from common.checkers import check_is_not_blocked_with_user_with_id, check_is_connected_with_user_with_id
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response
 from rest_framework.exceptions import PermissionDenied
-from django.db.models import Q
-from moderation.models import ModeratedObject
 
 
 class UserPhoto(EmojiListMixin, TemplateView):
@@ -67,12 +61,7 @@ class UserDetailAvatar(EmojiListMixin, TemplateView):
                 check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
             self.photos = self.user.get_avatar_photos()
         elif self.user == request.user and request.user.is_authenticated:
-            
-            self.avatar_album = Album.objects.get(creator_id=self.user.id, title="Фото со страницы", is_generic=True)
-            self.photos_query = Q(creator_id=self.user.id, is_deleted=False, community=None, album_2=self.avatar_album)
-            self.exclude_reported_and_approved_photos_query = ~Q(moderated_object__status=ModeratedObject.STATUS_APPROVED)
-            self.photos_query.add(self.exclude_reported_and_approved_photos_query, Q.AND)
-            self.photos = Photo.objects.filter(self.photos_query)
+            self.photos = self.user.get_avatar_photos()
         elif self.user.is_closed_profile() and request.user.is_anonymous:
             raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
         elif not self.user.is_closed_profile() and request.user.is_anonymous:
