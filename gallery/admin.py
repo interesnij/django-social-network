b1d58c71d8c708171c872e00bc450fc004ebfa9c
Serg <ochkarik1983@mail.ru@>
2019-11-28 14:12:42 +0300

    J

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from gallery.models import Album, Photo
 
 
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ('creator',)
+    list_display = ['creator','created', 'is_deleted']
+    list_filter = ['creator', ]
+
+class PhotoAdmin(admin.ModelAdmin):
+    search_fields = ('creator',)
+    list_display = ['creator','created', 'is_deleted']
+    list_filter = ['creator', ]
 
 admin.site.register(Album)
 admin.site.register(Photo)
