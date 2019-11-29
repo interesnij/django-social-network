bafaf42d1c00cbb626d1fabd6a94981b120bdd3d
Serg <ochkarik1983@mail.ru@>
2019-11-29 12:48:43 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
-from main.views import MainPageView, ComingView, NewsListView, VotesView
+from main.views import MainPageView, ComingView, NewsListView
 
 
 urlpatterns = [
 	url(r'^$', ComingView.as_view(), name="coming"),
 	url(r'^main/$', MainPageView.as_view(), name="main"),
 	url(r'^main/news/$', NewsListView.as_view(), name="news_list"),
-	
+
 	url(r'^user/', include('main.url.user')),
 	url(r'^votes/', include('main.url.votes')),
 ]
