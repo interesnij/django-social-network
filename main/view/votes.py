9c9a521aa8f2c58dc17dc4ff9e2be2a4fda96989
Serg <ochkarik1983@mail.ru@>
2019-11-30 17:24:03 +0300

    J

--- main/view/votes.py
+++ main/view/votes.py
@@ -101,7 +101,7 @@ class ItemUserDislikeCreate(View):
                 check_is_connected_with_user_with_id(user=request.user, user_id=user.id)
         try:
             likedislike = ItemVotes.objects.get(parent=item, user=request.user)
-            if likedislike.vote is ItemVotes.DISLIKE:
+            if likedislike.vote is not ItemVotes.DISLIKE:
                 likedislike.vote = ItemVotes.DISLIKE
                 likedislike.save(update_fields=['vote'])
                 result = True
