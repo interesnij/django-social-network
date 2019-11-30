698dfccd61d1b6ab20262c8998a74e14d15f8c37
Serg <ochkarik1983@mail.ru@>
2019-11-30 16:46:41 +0300

    J

--- main/view/votes.py
+++ main/view/votes.py
@@ -110,7 +110,7 @@ class ItemUserDislikeCreate(View):
                 likedislike.delete()
                 result = False
         except ItemVotes.DoesNotExist:
-            ItemVotes.objects.create(parent=item, user=request.user, vote=1)
+            ItemVotes.objects.create(parent=item, user=request.user, vote=0)
             result = True
         likes = ItemVotes.objects.filter(parent=item, vote__gt=0)
         dislikes = ItemVotes.objects.filter(parent=item, vote__lt=0)
