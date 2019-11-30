4c836dbf43015301b69b73297f9cd03a3d37bb46
Serg <ochkarik1983@mail.ru@>
2019-11-30 12:45:10 +0300

    J

--- main/migrations/0010_itemcomment_votes.py
+++ main/migrations/0010_itemcomment_votes.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('common', '0003_likedislike'),
         ('main', '0009_item_votes'),
     ]
 
