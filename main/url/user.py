29eb6ecc098651ab8f952f29b7e4e74bd99a944b
Serg <ochkarik1983@mail.ru@>
2019-12-01 15:41:21 +0300

    J

--- main/url/user.py
+++ main/url/user.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-	url(r'^comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommentUserCreate.as_view()),
-    url(r'^post-comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', login_required(CommentUserCreate.as_view())),
+	url(r'^comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserCommentList.as_view()),
+    url(r'^post-comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', login_required(ItemCommentUserCreate.as_view())),
     url(r'^reply-comment/$', login_required(item_reply_comment)),
 
 	url(r'^fixed/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', login_required(user_fixed)),
