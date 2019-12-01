a35d8db7d67e8e5bd064791426b1b61a4eb6bd70
Serg <ochkarik1983@mail.ru@>
2019-12-01 12:02:11 +0300

    J

--- main/url/community.py
+++ main/url/community.py
@@ -3,11 +3,11 @@ from main.view.user import *
 
 
 urlpatterns = [
-	url(r'^comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityCommentList.as_view()),
-    url(r'^post-comment/$', item_community_post_comment),
-    url(r'^reply-comment/$', item_community_reply_comment),
+	#url(r'^comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityCommentList.as_view()),
+    #url(r'^post-comment/$', item_community_post_comment),
+    #url(r'^reply-comment/$', item_community_reply_comment),
 
-	url(r'^fixed/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_fixed),
-    url(r'^unfixed/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_unfixed),
-    url(r'^delete/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_delete),
+	#url(r'^fixed/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_fixed),
+    #url(r'^unfixed/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_unfixed),
+    #url(r'^delete/(?P<pk>\d+)/(?P<user_uuid>[0-9a-f-]+)/$', item_community_delete),
 ]
