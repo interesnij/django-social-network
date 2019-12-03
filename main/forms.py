5de4aba6d82a62d92df743142904f4cb346e8b36
Serg <ochkarik1983@mail.ru@>
2019-12-03 19:06:01 +0300

    J

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from main.models import ItemCommentPhoto
+from main.models import ItemComment, ItemCommentPhoto
 
 
 class CommentForm(forms.ModelForm):
-	text = forms.CharField(required=False,max_length=500,label='Текст')
 
 	class Meta:
-		model = ItemCommentPhoto
+		model = ItemComment
 		fields = ['item_comment_photo', 'item_comment_photo2', 'text']
