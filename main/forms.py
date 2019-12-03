e922e253a995b216c2ecd3b2d88fad2cf37756f5
Serg <ochkarik1983@mail.ru@>
2019-12-03 19:36:05 +0300

    J

--- main/forms.py
+++ main/forms.py
@@ -3,8 +3,6 @@ from main.models import ItemComment, ItemCommentPhoto
 
 
 class CommentForm(forms.ModelForm):
-	item_comment_photo = forms.FileField()
-	item_comment_photo2 = forms.FileField()
 
 	class Meta:
 		model = ItemComment
--- main/view/user.py
+++ main/view/user.py
@@ -51,10 +51,7 @@ class ItemCommentUserCreate(View):
 			new_comment = comment.create_user_comment(
 														commenter=request.user,
 														item=self.item,
-														text=self.text,
-														item_comment_photo=comment.item_comment_photo,
-														item_comment_photo2=comment.item_comment_photo2
-													)
+														text=self.text)
 			new_comment.notification_user_comment(request.user)
 			html = render_to_string('item_user/parent_comment.html',{'comment': new_comment, 'request': request})
 			return JsonResponse(html, safe=False)
