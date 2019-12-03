cda700b25bb52acff71ab5ef3b44f2409438ed48
Serg <ochkarik1983@mail.ru@>
2019-12-03 18:35:57 +0300

    J

--- main/view/user.py
+++ main/view/user.py
@@ -50,9 +50,9 @@ class ItemCommentUserCreate(View):
 					if user.is_closed_profile:
 						check_is_connected_with_user_with_id(user=request.user, user_id = self.user.id)
 				new_comment = ItemComment.objects.create(item=self.item, text=self.text, commenter=request.user)
-				new_comment.notification_user_comment(request.user)
-				new_comment_images = ItemCommentPhoto.objects.create(item=self.item, item_comment_photo=comment.item_comment_photo, item_comment_photo2=comment.item_comment_photo2)
-				return HttpResponse("!")
+				
+				html = render_to_string('item_user/parent_comment.html',{'comment': new_comment, 'request': request})
+				return JsonResponse(html, safe=False)
 		else:
 			return HttpResponseBadRequest()
 
