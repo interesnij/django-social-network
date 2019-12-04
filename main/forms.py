26166e3a4fdb2f8cad8d9fd09e9699f7093ae03a
Serg <ochkarik1983@mail.ru@>
2019-12-04 12:23:03 +0300

    H

--- main/forms.py
+++ main/forms.py
@@ -4,6 +4,9 @@ from main.models import ItemComment, ItemCommentPhoto
 
 class CommentForm(forms.ModelForm):
 
+	item_comment_photo = forms.FileField()
+	item_comment_photo2 = forms.FileField()
+
 	class Meta:
 		model = ItemComment
 		fields = ['text']
