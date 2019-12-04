dd0b552715feaa03734dfdd228d6151458125c37
Serg <ochkarik1983@mail.ru@>
2019-12-04 18:07:50 +0300

    H

--- users/templatetags/liked_user.py
+++ users/templatetags/liked_user.py
@@ -2,9 +2,9 @@ from django import template
 register=template.Library()
 
 @register.filter
-def user_in(objects, request):
+def user_in(objects, user):
     if user.is_authenticated:
-        return objects.filter(user=request.user).exists()
+        return objects.filter(user=user).exists()
     return False
 
 
