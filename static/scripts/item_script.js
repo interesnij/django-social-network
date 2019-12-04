71eea698c1ac53875c88b0ee1062e89fe1b21037
Serg <ochkarik1983@mail.ru@>
2019-12-04 19:16:19 +0300

    H

--- static/scripts/item_script.js
+++ static/scripts/item_script.js
@@ -265,8 +265,8 @@ $("#ajax").on('click', '.u_dislike', function() {
             success: function(json) {
                 dislike.find("[data-count='dislike']").text(json.dislike_count);
                 like.find("[data-count='like']").text(json.like_count);
-                dislike.find(".like").hide();
-                dislike.find(".like_act").show();
+                like.find(".like").hide();
+                like.find(".like_act").show();
             }
         });
         return false;
