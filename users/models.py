b4921935142ebae63e61dd30b8d52ef7b0ce5ecd
Serg <ochkarik1983@mail.ru@>
2019-12-05 17:56:14 +0300

    J

--- users/models.py
+++ users/models.py
@@ -483,7 +483,7 @@ class UserPrivateSettings(models.Model):
 class UserProfile(models.Model):
     id = models.AutoField(primary_key=True, db_index=False)
     user = models.OneToOneField(User, db_index=False, null=True, related_name="profile", verbose_name="Пользователь", on_delete=models.CASCADE)
-    cover = models.ImageField(blank=True, null=True, upload_to=upload_to_user_cover_directory, verbose_name="Фон")
+    cover = models.ImageField(blank=True, null=True, upload_to="/users", verbose_name="Фон")
     bio = models.TextField(max_length=settings.PROFILE_BIO_MAX_LENGTH, blank=True, verbose_name="Биография")
     followers_count_visible = models.BooleanField(blank=False, null=False, default=False, verbose_name="Число подписчиков видно")
     sity = models.CharField(max_length=settings.PROFILE_LOCATION_MAX_LENGTH, blank=True, verbose_name="Местоположение")
