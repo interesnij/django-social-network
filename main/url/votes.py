610728fbacfca6178ce5a194f8db4ce1adc6b7b5
Serg <ochkarik1983@mail.ru@>
2019-12-07 20:26:55 +0300

    J

--- main/url/votes.py
+++ main/url/votes.py
@@ -19,7 +19,7 @@ urlpatterns = [
 	url(r'^u_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserLikeWindow.as_view()),
 	url(r'^u_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserDislikeWindow.as_view()),
     url(r'^u_comment_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserCommentLikeWindow.as_view()),
-	url(r'^u_comment_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserCommentDislikeWindow.as_view())
+	url(r'^u_comment_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserCommentDislikeWindow.as_view()),
 	url(r'^c_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityLikeWindow.as_view()),
 	url(r'^c_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityDislikeWindow.as_view()),
     url(r'^c_comment_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityCommentLikeWindow.as_view()),
