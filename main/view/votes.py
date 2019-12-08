2970e1a5f1aad72e831fa9bd7446e80cc9064cd7
Serg <ochkarik1983@mail.ru@>
2019-12-08 11:01:45 +0300

    J

--- main/view/votes.py
+++ main/view/votes.py
@@ -10,7 +10,7 @@ from common.checkers import check_is_not_blocked_with_user_with_id, check_is_con
 from common.checkers import check_can_get_posts_for_community_with_name
 from django.contrib.contenttypes.models import ContentType
 from rest_framework.exceptions import PermissionDenied
-from rest_framework import serializers
+from django.core import serializers
 
 
 class ItemUserLikeWindow(TemplateView):
