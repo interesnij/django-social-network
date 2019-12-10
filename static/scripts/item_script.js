c510511825a3e3c3eeac360930c11640539a000f
Serg <ochkarik1983@mail.ru@>
2019-12-10 10:58:54 +0300

    J

--- static/scripts/item_script.js
+++ static/scripts/item_script.js
@@ -40,13 +40,7 @@ $('#ajax').on('click', '.u_comment', function() {
     });
     return false;
 });
-$('#ajax').on('click', '.comments_open', function() {
-  var btn = $(this);
-  var item = btn.closest(".infinite-item");
-  var container = item.find(".load_comments");
-  container.clean();
-  btn.removeClass('comments_open')''
-});
+
 
 $('#ajax').on('click', '.c_comment', function() {
     var item = $(this).closest(".infinite-item").attr("item-id");
