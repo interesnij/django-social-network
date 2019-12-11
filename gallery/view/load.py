18be109de3edbcea971e609871bd12a99cf1f0c5
Serg <ochkarik1983@mail.ru@>
2019-12-11 14:24:18 +0300

    J

--- gallery/view/load.py
+++ gallery/view/load.py
@@ -59,13 +59,13 @@ class UserCommentPhoto(TemplateView):
             check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
             if self.user.is_closed_profile:
                 check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
-            self.photo = Photo.objects.get(photo_uuid=self.kwargs["photo_uuid"])
+            self.photo = Photo.objects.get(uuid=self.kwargs["photo_uuid"])
         elif self.user == request.user and request.user.is_authenticated:
-            self.photo = Photo.objects.get(photo_uuid=self.kwargs["photo_uuid"])
+            self.photo = Photo.objects.get(uuid=self.kwargs["photo_uuid"])
         elif self.user.is_closed_profile() and request.user.is_anonymous:
             raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
         elif not self.user.is_closed_profile() and request.user.is_anonymous:
-            self.photo = Photo.objects.get(photo_uuid=self.kwargs["photo_uuid"])
+            self.photo = Photo.objects.get(uuid=self.kwargs["photo_uuid"])
         return super(UserCommentPhoto,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
