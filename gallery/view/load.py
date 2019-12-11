ec4a50a5704ed799ba5276570d81bda154872b81
Serg <ochkarik1983@mail.ru@>
2019-12-11 14:30:19 +0300

    J

--- gallery/view/load.py
+++ gallery/view/load.py
@@ -53,7 +53,7 @@ class UserCommentPhoto(TemplateView):
     template_name="photo_user/comment_image.html"
 
     def get(self,request,*args,**kwargs):
-        self.user=User.objects.get(uuid=self.kwargs["uuid"])
+        self.user=User.objects.get(pk=self.kwargs["pk"])
         if self.user != request.user and request.user.is_authenticated:
             check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
             if self.user.is_closed_profile:
