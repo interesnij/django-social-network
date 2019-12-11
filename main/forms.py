38e123a72f476331e047ac964975b4ddb51bbf58
Serg <ochkarik1983@mail.ru@>
2019-12-11 12:19:41 +0300

    J

--- main/forms.py
+++ main/forms.py
@@ -3,8 +3,8 @@ from main.models import ItemComment
 
 
 class CommentForm(forms.ModelForm):
-	item_comment_photo = forms.ImageField()
-	item_comment_photo2 = forms.ImageField()
+	item_comment_photo = forms.ImageField(required=False)
+	item_comment_photo2 = forms.ImageField(required=False)
 	text=forms.CharField(required=False, widget=forms.TextInput(
             attrs={'class': 'form-control text-comment form-control-rounded'}
         ))
