533023e35898c4732620e7a25cbc144617c8783d
Serg <ochkarik1983@mail.ru@>
2019-12-11 12:00:40 +0300

    J

--- main/forms.py
+++ main/forms.py
@@ -3,14 +3,15 @@ from main.models import ItemComment
 
 
 class CommentForm(forms.ModelForm):
-	
+	item_comment_photo = forms.FileField()
+	item_comment_photo2 = forms.FileField()
 	text=forms.CharField(required=False, widget=forms.TextInput(
             attrs={'class': 'form-control text-comment form-control-rounded'}
         ))
 
 	class Meta:
 		model = ItemComment
-		fields = ['text', 'item_comment_photo', 'item_comment_photo2']
+		fields = ['text']
 
 
 class CommentReplyForm(forms.ModelForm):
