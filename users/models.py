1717a011cd6f20e9bd9811a1204270e1c5089684
Serg <ochkarik1983@mail.ru@>
2019-12-11 19:39:06 +0300

    J

--- users/models.py
+++ users/models.py
@@ -296,13 +296,13 @@ class User(AbstractUser):
         my_connections = self.get_all_connection()
         query = Q()
         for frend in my_connections:
-            user = User.objects.get(pk=frend.user.pk)
-            if user.pk != self.pk:
-                connection_query = Q(target_connection__user_id=user.id)
-                connection_query = ~Q(target_user_id=self.id,user_id=self.id)
+            user_id = frend.user.pk
+            if user_id != self.pk:
+                connection_query = Q(target_connection__user_id=user_id)
+                connection_query = ~Q(target_user_id=self.pk,user_id=self.pk)
                 exclude_reported_and_approved_posts_query = ~Q(target_connection__user__moderated_object__status=ModeratedObject.STATUS_APPROVED)
                 connection_query.add(exclude_reported_and_approved_posts_query, Q.AND)
-                connection_query.add(~Q(Q(target_connection__user__blocked_by_users__blocker_id=user.pk) | Q(target_connection__user__user_blocks__blocked_user_id=user.pk)), Q.AND)
+                connection_query.add(~Q(Q(target_connection__user__blocked_by_users__blocker_id=user_id) | Q(target_connection__user__user_blocks__blocked_user_id=user_id)), Q.AND)
                 query.add(connection_query, Q.AND)
         queryset = Connect.objects.filter(query)
         return queryset
