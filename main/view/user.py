a063ae5ea97d31ded7971c47ba3c9ebff9b13cb6
Serg <ochkarik1983@mail.ru@>
2019-12-11 12:42:37 +0300

    J

--- main/view/user.py
+++ main/view/user.py
@@ -63,10 +63,10 @@ class ItemCommentUserCreate(View):
 			new_comment = comment.create_user_comment(commenter=request.user, parent_comment=None, item=item, text=comment.text)
 			if item_comment_photo:
 				album=Album.objects.get(creator=request.user, title="Сохраненные фото", is_generic=True, community=None)
-				Photo.objecs.create(creator=request.user, file=item_comment_photo,community=None,is_public=True, album=album, item_comment=new_comment)
+				Photo.objects.create(creator=request.user, file=item_comment_photo,community=None,is_public=True, album=album, item_comment=new_comment)
 			if item_comment_photo2:
 				album=Album.objects.get(creator=request.user, title="Сохраненные фото", is_generic=True, community=None)
-				Photo.objecs.create(creator=request.user, file=item_comment_photo2,community=None,is_public=True, album=album, item_comment=new_comment)
+				Photo.objects.create(creator=request.user, file=item_comment_photo2,community=None,is_public=True, album=album, item_comment=new_comment)
 			new_comment.notification_user_comment(request.user)
 			html = render_to_string('item_user/parent_comment.html',{'comment': new_comment, 'request_user': request.user, "form_reply": CommentReplyForm(), 'request': request})
 			return JsonResponse(html, safe=False)
