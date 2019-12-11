1238274a03f44a3067111d8a2fba38ff2bdb7d75
Serg <ochkarik1983@mail.ru@>
2019-12-11 11:44:59 +0300

    J

--- main/view/user.py
+++ main/view/user.py
@@ -51,15 +51,17 @@ class ItemCommentUserCreate(View):
 		item = Item.objects.get(uuid=item_uuid)
 
 		if form_post.is_valid():
-			comment=form_post.save(commit=False)
-			if not comment.text and not comment.item_comment_photo and not comment.item_comment_photo2:
+
+			text = form_post.cleaned_data['text']
+			item_comment_photo = form_post.cleaned_data['item_comment_photo']
+			item_comment_photo2 = form_post.cleaned_data['item_comment_photo2']
+			if not text and not item_comment_photo and not item_comment_photo2:
 				raise ValidationError('Для добавления комментария необходимо написать что-то или прикрепить изображение')
 			if request.user.pk != user.pk:
 				check_is_not_blocked_with_user_with_id(user=request.user, user_id = user.pk)
 				if user.is_closed_profile:
 					check_is_connected_with_user_with_id(user=request.user, user_id = user.pk)
-
-			new_comment = comment.create_user_comment(commenter=request.user, parent_comment=None, item=item, text=comment.text)
+			new_comment = comment.create_user_comment(commenter=request.user, parent_comment=None, item=item, text=text)
 			new_comment.notification_user_comment(request.user)
 			html = render_to_string('item_user/parent_comment.html',{'comment': new_comment, 'request_user': request.user, "form_reply": CommentReplyForm(), 'request': request})
 			return JsonResponse(html, safe=False)
