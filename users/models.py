1e642555ae991e48fa480b3977c1ba3c00eb1de7
Serg <ochkarik1983@mail.ru@>
2019-12-12 15:03:41 +0300

    J

--- users/models.py
+++ users/models.py
@@ -404,7 +404,7 @@ class User(AbstractUser):
         user = User.objects.get(pk=user_id)
         user_connections = user.connections.values('user_id')
         user_frends_ids = [target_user['user_id'] for target_user in user_connections]
-        query_user = Q(target_connection__target_user_id__in=frends_ids)
+        query_user = Q(target_connection__user_id_id__in=frends_ids)
         if not connections and user_connections:
             return "not connections"
 
