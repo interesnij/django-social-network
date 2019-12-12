8e3cd21292d25b34cd191e0e32b0868f049cd913
Serg <ochkarik1983@mail.ru@>
2019-12-12 16:59:07 +0300

    J

--- users/models.py
+++ users/models.py
@@ -299,7 +299,7 @@ class User(AbstractUser):
         query = []
         for frend in my_frends:
             if frend in user_frends:
-                query + list(frend)
+                query = query + list(frend)
 
         return query
 
