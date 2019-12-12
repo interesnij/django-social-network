37d67b81050f9f4453834a6e1c6967a7f5762bc9
Serg <ochkarik1983@mail.ru@>
2019-12-12 13:56:21 +0300

    J

--- users/models.py
+++ users/models.py
@@ -380,15 +380,15 @@ class User(AbstractUser):
         return final_queryset
 
     def get_possible_friends(self):
-        frends = self.connections.values('target_user_id')
+        frends = self.connections.values('target_connection_id')
         if not frends:
             return "not frends"
-        frends_ids = [target_user['target_user_id'] for target_user in frends]
+        frends_ids = [target_user['target_connection_id'] for target_user in frends]
         query = Q()
         for frend in frends_ids:
             user = User.objects.get(pk=frend)
-            frends_frends = user.connections.values('user_id')
-            frend_frend_ids = [target_user['user_id'] for target_user in frends_frends]
+            frends_frends = user.connections.values('target_connection_id')
+            frend_frend_ids = [target_user['target_connection_id'] for target_user in frends_frends]
             _query = Q(target_connection__user_id__in=frend_frend_ids)
             blocked = ~Q(Q(target_connection__user__blocked_by_users__blocker_id=self.pk) | Q(target_connection__user__user_blocks__blocked_user_id=self.pk))
             connections = ~Q(Q(target_connection__user_id=self.pk) | Q(target_connection__target_user_id=self.pk))
