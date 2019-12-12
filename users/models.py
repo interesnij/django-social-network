0bacb81f45be17d329afc320a2edadc1d217cfd0
Serg <ochkarik1983@mail.ru@>
2019-12-12 09:58:21 +0300

    J

--- users/models.py
+++ users/models.py
@@ -304,11 +304,9 @@ class User(AbstractUser):
         return query
 
     def get_common_friend2(self):
-        connection_query = Q(target_connection__user_id=self.id)
-        exclude_reported_and_approved_posts_query = ~Q(target_connection__user__moderated_object__status=ModeratedObject.STATUS_APPROVED)
-        connection_query.add(exclude_reported_and_approved_posts_query, Q.AND)
+        connection_query = self.get_all_connection()
         for frend in connection_query:
-            connection_frend = Q(target_connection__user_id=frend.usser.id)
+            connection_frend = Q(target_connection__user_id=frend.user.id)
             connection_frend.add(~Q(Q(target_connection__user__blocked_by_users__blocker_id=frend.user.id) | Q(target_connection__user__user_blocks__blocked_user_id=frend.user.id)), Q.AND)
             connection_frend.add(~Q(Q(target_connection__user_id=self.id) | Q(target_connection__target_user_id=self.id)), Q.AND)
             connection_query.add(connection_frend, Q.AND)
