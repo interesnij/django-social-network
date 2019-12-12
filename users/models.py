f3fd5a18084cb8bda86e2d8e93006b33d4a37bb0
Serg <ochkarik1983@mail.ru@>
2019-12-12 14:29:00 +0300

    J

--- users/models.py
+++ users/models.py
@@ -400,11 +400,11 @@ class User(AbstractUser):
     def get_common_friends(self,user_id):
         connections = self.connections.values('target_user_id')
         frends_ids = [target_user['target_user_id'] for target_user in connections]
-        query = Q(target_connection__user_id__in=frends_ids)
+        query = Q(target_connection__target_user_id__in=frends_ids)
         user = User.objects.get(pk=user_id)
         user_connections = user.connections.values('target_user_id')
         user_frends_ids = [target_user['target_user_id'] for target_user in user_connections]
-        query_user = Q(target_connection__user_id__in=user_frends_ids)
+        query_user = Q(target_connection__target_user_id__in=user_frends_ids)
         if not connections and user_connections:
             return "not connections"
 
