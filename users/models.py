aa91149a1c05c8eec4c29640fec3023dd1770086
Serg <ochkarik1983@mail.ru@>
2019-12-12 10:37:52 +0300

    J

--- users/models.py
+++ users/models.py
@@ -415,7 +415,7 @@ class User(AbstractUser):
             frends_frends = user.connections.values('target_user_id')
             frend_frend_ids = [target_user['target_user_id'] for target_user in frends_frends]
             blocked = ~Q(Q(target_connection__user__blocked_by_users__blocker_id=frend_frend_ids) | Q(target_connection__user__user_blocks__blocked_user_id=frend_frend_ids))
-            connections = ~Q(Q(target_connection__user_id=frends_frends) | Q(target_connection__target_user_id=frends_frends))
+            connections = ~Q(Q(target_connection__user_id=frend_frend_ids) | Q(target_connection__target_user_id=frend_frend_ids))
             query_.add(~Q(blocked), Q.AND)
             query_.add(~Q(connections), Q.AND)
             query.add(query_, Q.AND)
