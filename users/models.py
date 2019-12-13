3e8afe2badb98df75278bcc7d7814ef45c34ac98
Serg <ochkarik1983@mail.ru@>
2019-12-13 18:03:38 +0300

    J

--- users/models.py
+++ users/models.py
@@ -234,7 +234,7 @@ class User(AbstractUser):
     def is_following_user_with_username(self, user_username):
         return self.follows.filter(followed_user__username=user_username).exists()
 
-    def is_album_exists(self):
+    def is_album_exists(self): 
         return self.created_user.filter(creator__id=self.pk, is_generic=False).exists()
 
     def is_suspended(self):
