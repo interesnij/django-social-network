0859ebb442248946ef0de58362dbbf8bfda137cf
Serg <ochkarik1983@mail.ru@>
2019-12-14 18:47:03 +0300

    D

--- users/url/detail.py
+++ users/url/detail.py
@@ -4,7 +4,7 @@ from users.views.detail import AllUsers, ItemListView, UserItemView, AllCommonUs
 
 urlpatterns = [
     url(r'^all-users/$', AllUsers.as_view(), name='all_users'),
-    url(r'^common-users/$', AllCommonUsers.as_view(), name='all_common_users'),
+    url(r'^common-users/(?P<pk>\d+)/$', AllCommonUsers.as_view(), name='all_common_users'),
     url(r'^item/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserItemView.as_view(), name='user_item'),
     url(r'^list/(?P<pk>\d+)/$', ItemListView.as_view(), name="user_item_list"),
 
