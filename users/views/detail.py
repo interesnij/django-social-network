4eb690662f2034c3c1de11ed539ea437e33fe83b
Serg <ochkarik1983@mail.ru@>
2019-12-14 14:27:04 +0300

    D

--- users/views/detail.py
+++ users/views/detail.py
@@ -94,7 +94,7 @@ class ProfileUserView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user=User.objects.get(pk=self.kwargs["pk"])
-        if request.user.is_authenticated():
+        if request.user.is_authenticated: 
             self.is_frend = self.user.is_connected_with_user(request.user)
             self.is_blocked = self.user.has_blocked_user_with_id(request.user)
             self.common_frends = self.user.get_common_friends_of_user(request.user)[0:5]
