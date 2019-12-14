1d6ff23dff08cc894e0ce593d451a1f6ca45562b
Serg <ochkarik1983@mail.ru@>
2019-12-14 10:20:51 +0300

    D

--- users/templatetags/liked_user.py
+++ users/templatetags/liked_user.py
@@ -1,12 +1,13 @@
 from django import template
+import pymorphy2
 register=template.Library()
+
 
 @register.filter
 def user_in(objects, user):
     if user.is_authenticated:
         return objects.filter(user=user).exists()
     return False
-
 
 @register.filter
 def rupluralize(value, arg="дурак,дурака,дураков"):
@@ -21,3 +22,11 @@ def rupluralize(value, arg="дурак,дурака,дураков"):
         return args[1]
     else:
         return args[2]
+
+
+@register.filter
+def gent(value):
+    morph = pymorphy2.MorphAnalyzer()
+    word = morph.parse(value)[0]
+    word.inflect({'gent'})
+    return word
