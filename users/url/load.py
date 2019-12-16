6a1068bb57d253733d1fa72aff9d9c71021a4128
Serg <ochkarik1983@mail.ru@>
2019-12-16 11:51:31 +0300

    J

--- users/url/load.py
+++ users/url/load.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from users.views.load import ProfileStatReload, ProfileButtonReload, ProfileReload, ImagesLoad
+from users.views.load import ProfileStatReload, ProfileButtonReload, ProfileReload, UserImagesLoad
 
 
 urlpatterns = [
