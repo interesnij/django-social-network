f9ab1ca2368ff85b9427892a639a73f525738fab
Serg <ochkarik1983@mail.ru@>
2019-12-16 16:47:23 +0300

    J

--- static/scripts/item_script.js
+++ static/scripts/item_script.js
@@ -217,6 +217,6 @@ $('#ajax').on('click', '.upload_photo', function() {
   $('#ajax').on('click', '.dropdown_link', function() {
     link = $(this);
     link.next().focus();link.next().select(); document.execCommand("copy");
-    console.log(link.next())
+    console.log(link,link.next().val())
     $.toast({heading: 'Информация',text: 'Ссылка скопирована',showHideTransition: 'fade',icon: 'success'})
   });
