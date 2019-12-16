3c59d83a404db45d55e89881decc09cc1ddd54ce
Serg <ochkarik1983@mail.ru@>
2019-12-16 17:20:08 +0300

    J

--- static/scripts/item_script.js
+++ static/scripts/item_script.js
@@ -219,4 +219,11 @@ $('#ajax').on('click', '.upload_photo', function() {
   var copyTextarea = document.querySelector('.js-copytextarea');
   copyTextarea.focus();
   copyTextarea.select();
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
 });
