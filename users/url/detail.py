e4cc4a4d83799b0cfbd0f67eb53239da1a982604
Serg <ochkarik1983@mail.ru@>
2019-12-17 11:19:56 +0300

    J

--- users/url/detail.py
+++ users/url/detail.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from users.views.detail import AllUsers, ItemListView, UserItemView, AllCommonUsers, AllUsersLists
+from users.views.detail import AllUsers, ItemListView, UserItemView, AllCommonUsers, AllUsersLists, AllUsers
 
 
 urlpatterns = [
