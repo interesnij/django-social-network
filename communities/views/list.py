032465a087e2409704359b9e1b91056d9d72241f
Serg <ochkarik1983@mail.ru@>
2019-12-17 16:20:01 +0300

    J

--- communities/views/list.py
+++ communities/views/list.py
@@ -40,5 +40,5 @@ class AllCommunities(ListView):
 	paginate_by=15
 
 	def get_queryset(self):
-		groups=Community.objects.only('id').order_by('memberships__user__id.count()')
+		groups=Community.objects.only('id').order_by('memberships__user__id.')
 		return groups
