74850b26811ccf876061745cc21c58c1331eb7e2
Serg <ochkarik1983@mail.ru@>
2019-12-17 14:37:53 +0300

    J

--- users/models.py
+++ users/models.py
@@ -439,8 +439,8 @@ class User(AbstractUser):
             frends_frends = user.connections.values('user_id')
             frend_frend_ids = [target_user['user_id'] for target_user in frends_frends]
             _query = Q(id__in=frend_frend_ids)
-            blocked = ~Q(Q(blocked_by_users__blocker_id=self.pk) | Q(user_blocks__blocked_user_id=self.pk))
-            connections = ~Q(Q(target_connection__user_id=self.pk) | Q(target_connection__target_user_id=self.pk))
+            blocked = ~Q(Q(connections_id=self.pk) | Q(user_blocks__blocked_user_id=self.pk))
+            connections = ~Q(Q(connections_id=self.pk) | Q(targeted_connections=self.pk))
             _query.add(blocked, Q.AND)
             _query.add(connections, Q.AND)
             query.add(_query, Q.AND)
