3bd976d31a76b957c6fc20dcea12f909742e2129
Serg <ochkarik1983@mail.ru@>
2019-12-19 15:58:18 +0300

    H

--- frends/urls.py
+++ frends/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$',FrendsListView.as_view(), name="frends"),
-    url(r'^all/(?P<pk>\d+)/$',AllFrendsListView.as_view(), name="all_frends"),
+    url(r'^all/(?P<pk>\d+)/$',FrendsListView.as_view(), name="frends"),
+    url(r'^all-list/(?P<pk>\d+)/$',AllFrendsListView.as_view(), name="all_frends"),
     url(r'^online/(?P<pk>\d+)/$',OnlineFrendsListView.as_view(), name="online_frends"),
     url(r'^common/(?P<pk>\d+)/$',CommonFrendsListView.as_view(), name="common_frends"),
     url(r'^add/(?P<pk>\d+)/$', ConnectCreate.as_view(), name="create_connect"),
