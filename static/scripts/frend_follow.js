4f179bfe4e99cd324bee563117cfd2e5acdfde36
Serg <ochkarik1983@mail.ru@>
2019-12-20 12:37:24 +0300

    J

--- static/scripts/frend_follow.js
+++ static/scripts/frend_follow.js
@@ -1,43 +1,40 @@
-$('.user_block').on('click', function() {
+
+$('#ajax').on('click', '.user_block', function() {
   pk = $(this).parent().data("pk");
   $.ajax({
       url: "/users/progs/block/" + pk + "/",
       success: function (data) {$('#button_load').html('').load("/users/load/profile_button/" + pk + "/");}
   });
 });
-
-$('.user_unblock').on('click', function() {
+$('#ajax').on('click', '.user_unblock', function() {
   pk = $(this).parent().data("pk");
   $.ajax({
       url: "/users/progs/unblock/" + pk + "/",
       success: function (data) {$('#button_load').html('').load("/users/load/profile_button/" + pk + "/");}
   });
 });
-$('.follow_create').on('click', function() {
+$('#ajax').on('click', '.follow_create', function() {
   pk = $(this).data("pk");
   $.ajax({
       url: "/follows/add/" + pk + "/",
       success: function (data) {$('#button_load').html('').load("/users/load/profile_button/" + pk + "/");}
   });
   });
-
-$('.follow_delete').on('click', function() {
+$('#ajax').on('click', '.follow_delete', function() {
   pk = $(this).parent().data("pk");
     $.ajax({
       url: "/follows/delete/" + pk + "/",
       success: function (data) {$('#button_load').html('').load("/users/load/profile_button/" + pk + "/");}
     });
 });
-
-$('.connect_create').on('click', function() {
+$('#ajax').on('click', '.connect_create', function() {
   pk = $(this).parent().data("pk");
   $.ajax({
       url: "/frends/add/" + pk + "/",
       success: function (data) {$('#button_load').html('').load("/users/load/profile_button/" + pk + "/");}
   });
 });
-
-$('.connect_delete').on('click', function() {
+$('#ajax').on('click', '.connect_delete', function() {
   pk = $(this).parent().data("pk");
   $.ajax({
       url: "/frends/delete/" + pk + "/",
