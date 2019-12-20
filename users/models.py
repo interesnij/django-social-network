73d84d727eac66c5dc10c57b05252f3cf8879eee
Serg <ochkarik1983@mail.ru@>
2019-12-20 15:03:59 +0300

    J

--- users/models.py
+++ users/models.py
@@ -432,7 +432,7 @@ class User(AbstractUser):
         target_frends = self.connections.values('target_user_id')
         target_frends_ids = [target_user['target_user_id'] for target_user in target_frends]
         in_frends = self.connections.values('user_id')
-        in_frends_ids = [target_user['target_user_id'] for target_user in in_frends]
+        in_frends_ids = [target_user['user_id'] for target_user in in_frends]
         frends = target_frends_ids + in_frends_ids
         if not frends:
             return "not frends"
