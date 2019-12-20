81c0b7fae0699073f227a1d0fda006f519cd49b9
Serg <ochkarik1983@mail.ru@>
2019-12-20 14:25:24 +0300

    J

--- users/models.py
+++ users/models.py
@@ -428,8 +428,8 @@ class User(AbstractUser):
         connection = Connect.objects.filter(query)
         return connection
 
-    def get_possible_friends(user):
-        frends = user.connections.values('target_user_id')
+    def get_possible_friends(self):
+        frends = self.connections.values('target_user_id')
         if not frends:
             return "not frends"
         frends_ids = [target_user['target_user_id'] for target_user in frends]
@@ -439,8 +439,8 @@ class User(AbstractUser):
             frends_frends = _user.connections.values('target_user_id')
             frend_frend_ids = [target_user['target_user_id'] for target_user in frends_frends]
             _query = Q(id__in=frend_frend_ids)
-            blocked = ~Q(Q(blocked_by_users__blocker_id=user.pk) | Q(user_blocks__blocked_user_id=user.pk))
-            connections = ~Q(Q(connections__user_id=user.pk) | Q(targeted_connections__target_user_id=user.pk))
+            blocked = ~Q(Q(blocked_by_users__blocker_id=self.pk) | Q(user_blocks__blocked_user_id=self.pk))
+            connections = ~Q(Q(connections__user_id=self.pk) | Q(targeted_connections__target_user_id=self.pk))
             _query.add(blocked, Q.AND)
             _query.add(connections, Q.AND)
             query.add(_query, Q.AND)
