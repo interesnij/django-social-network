282ee40e105523ba8ef9e872012ca0bd4d7b4bc2
Serg <ochkarik1983@mail.ru@>
2019-12-20 19:49:13 +0300

    J

--- users/models.py
+++ users/models.py
@@ -429,7 +429,7 @@ class User(AbstractUser):
         return connection
 
     def get_possible_friends(self):
-        frends = self.connections.values('user_id')
+        frends = self.targeted_connections.values('user_id')
         if not frends:
             return "not frends"
         frends_ids = [target_user['user_id'] for target_user in frends]
