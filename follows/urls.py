63a8e3096d054873adf04132e8c7285be972db21
Serg <ochkarik1983@mail.ru@>
2019-12-20 10:44:47 +0300

    J

--- follows/urls.py
+++ follows/urls.py
@@ -3,8 +3,8 @@ from follows.views import *
 
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', FollowsView.as_view(), name='follows'),
-    url(r'^list/(?P<pk>\d+)/$', FollowsListView.as_view(), name='follows_list'),
+    url(r'^(?P<uuid>[0-9a-f-]+)/$', FollowsView.as_view(), name='follows'),
+    url(r'^list/(?P<uuid>[0-9a-f-]+)/$', FollowsListView.as_view(), name='follows_list'),
     url(r'^add/(?P<pk>\d+)/$', FollowCreate.as_view(), name="create_follow"),
     url(r'^delete/(?P<pk>\d+)/$', FollowDelete.as_view(), name="delete_follow"),
     url(r'^add_member/(?P<pk>\d+)/$', CommunityFollowCreate.as_view(), name="create_community_follow"),
--- follows/views.py
+++ follows/views.py
@@ -47,7 +47,7 @@ class FollowsView(TemplateView):
 class FollowsListView(View):
     def get(self, request, *args, **kwargs):
         context = {}
-        self.user=User.objects.get(pk=self.kwargs["pk"])
+        self.user=User.objects.get(uuid=self.kwargs["uuid"])
         if self.user != request.user and request.user.is_authenticated:
             check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
             if self.user.is_closed_profile():
