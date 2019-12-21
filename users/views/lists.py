2aa56dc2467687b2207c0358a7dcdc8233860c5e
Serg <ochkarik1983@mail.ru@>
2019-12-21 15:09:57 +0300

    J

--- users/views/lists.py
+++ users/views/lists.py
@@ -11,36 +11,34 @@ from rest_framework.exceptions import PermissionDenied
 
 class UserCommunities(View):
 	def get(self, request, *args, **kwargs):
-        context = {}
-        template = None
-        self.user=User.objects.get(pk=self.kwargs["pk"])
-        if self.user != request.user and request.user.is_authenticated:
-            check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
-            if self.user.is_closed_profile():
-                check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
-            communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
-            template = 'user_communities/communities_list.html'
-            current_page = Paginator(communities_list, 10)
-        elif request.user.is_anonymous and self.user.is_closed_profile():
-            raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
-        elif request.user.is_anonymous and not self.user.is_closed_profile():
-            communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
-            template = 'user_communities/communities_list.html'
-            current_page = Paginator(communities_list, 10)
-        elif self.user == request.user:
-            communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
-            template = 'user_communities/communities_list.html'
-            current_page = Paginator(communities_list, 10)
-        context['user'] = self.user
-        page = request.GET.get('page')
-        try:
-            context['communities_list'] = current_page.page(page)
-        except PageNotAnInteger:
-            context['communities_list'] = current_page.page(1)
-        except EmptyPage:
-            context['communities_list'] = current_page.page(current_page.num_pages)
-
-        return render_to_response(template, context)
+		context = {}
+		template = None
+		self.user=User.objects.get(pk=self.kwargs["pk"])
+		if self.user != request.user and request.user.is_authenticated:
+			check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
+			if self.user.is_closed_profile():
+				check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
+			communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
+			template = 'user_communities/communities_list.html'
+			current_page = Paginator(communities_list, 10)
+		elif request.user.is_anonymous and self.user.is_closed_profile():
+			raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
+		elif request.user.is_anonymous and not self.user.is_closed_profile():
+			communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
+			template = 'user_communities/communities_list.html'
+			current_page = Paginator(communities_list, 10)
+		elif self.user == request.user:
+			communities_list = Community.objects.filter(memberships__user__id=self.user.pk).order_by('-created')
+			template = 'user_communities/communities_list.html'
+			current_page = Paginator(communities_list, 10)
+		page = request.GET.get('page')
+		try:
+			context['communities_list'] = current_page.page(page)
+		except PageNotAnInteger:
+			context['communities_list'] = current_page.page(1)
+		except EmptyPage:
+			context['communities_list'] = current_page.page(current_page.num_pages)
+		return render_to_response(template, context)
 
 
 class AllUsersList(ListView):
