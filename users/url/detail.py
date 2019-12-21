3226f4c94c09e5e918a0b7266e3f82d653da05ad
Serg <ochkarik1983@mail.ru@>
2019-12-21 18:27:23 +0300

    J

--- users/url/detail.py
+++ users/url/detail.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^all_users_list/$', AllUsersList.as_view(), name='all_users_list'),
     url(r'^item/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserItemView.as_view(), name='user_item'),
     url(r'^communities/(?P<uuid>[0-9a-f-]+)/$', UserCommunitiesList.as_view()),
-    url(r'^list/(?P<pk>\d+)/$', AllPossibleUsersList.as_view()),
+    url(r'^list/(?P<pk>\d+)/$', ItemListView.as_view()),
     url(r'^possible/(?P<uuid>[0-9a-f-]+)/$', AllPossibleUsersList.as_view()),
 
 ]
