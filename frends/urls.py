092ca40f0913d296a0c23352e1219c1b7552f502
Serg <ochkarik1983@mail.ru@>
2019-12-21 11:46:19 +0300

    J

--- frends/urls.py
+++ frends/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^all/(?P<pk>\d+)/$',FrendsListView.as_view(), name="frends"),
+    url(r'^(?P<pk>\d+)/$',FrendsListView.as_view(), name="frends"),
     url(r'^all-list/(?P<uuid>[0-9a-f-]+)/$',AllFrendsListView.as_view(), name="all_frends"),
     url(r'^online/(?P<uuid>[0-9a-f-]+)/$',OnlineFrendsListView.as_view(), name="online_frends"),
     url(r'^common/(?P<uuid>[0-9a-f-]+)/$',CommonFrendsListView.as_view(), name="common_frends"),
