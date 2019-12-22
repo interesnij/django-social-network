87c43029b20d78d9b1292b8abc7a019d9af5ad88
Serg <ochkarik1983@mail.ru@>
2019-12-22 14:27:32 +0300

    J

--- communities/views/details.py
+++ communities/views/details.py
@@ -93,19 +93,15 @@ class CommunityDetail(TemplateView):
                 self.template_name = "c_detail/admin_community.html"
             elif request.user.is_star_from_community_with_name(self.community.name):
                 self.template_name = "c_detail/star_community.html"
-            
+            else:
+                self.template_name = "c_detail/member_community.html"
+                self.common_friends = request.user.get_common_friends_of_community(self.community.name)[0:5]
         elif request.user.is_authenticated and request.user.is_follow_from_community_with_name(self.community.pk):
             self.common_friends = request.user.get_common_friends_of_community(self.community.name)[0:5]
             self.template_name = "c_detail/follow_community.html"
         elif request.user.is_authenticated and request.user.is_banned_from_community_with_name(self.community):
             self.template_name = "c_detail/block_community.html"
 
-        elif request.user.is_anonymous and self.community.is_public:
-            self.template_name = "c_detail/anon_public_community.html"
-        elif request.user.is_anonymous and self.community.is_closed:
-            self.template_name = "c_detail/anon_close_community.html"
-        elif request.user.is_anonymous and self.community.is_private:
-            self.template_name = "c_detail/private_community.html"
         elif request.user.is_authenticated and self.community.is_public:
             self.common_friends = request.user.get_common_friends_of_community(self.community)[0:5]
             self.template_name = "c_detail/public_community.html"
@@ -115,7 +111,12 @@ class CommunityDetail(TemplateView):
         elif request.user.is_authenticated and self.community.is_private:
             self.template_name = "c_detail/private_community.html"
 
-
+        elif request.user.is_anonymous and self.community.is_public:
+            self.template_name = "c_detail/anon_public_community.html"
+        elif self.community.is_closed and request.user.is_anonymous:
+            self.template_name = "c_detail/anon_close_community.html"
+        elif request.user.is_anonymous and self.community.is_private:
+            self.template_name = "c_detail/private_community.html"
         return super(CommunityDetail,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
