76363b0c1bb80e845e5504b2027b9727165654ef
Serg <ochkarik1983@mail.ru@>
2019-12-26 16:28:12 +0300

    H

--- static/scripts/auth/community.js
+++ static/scripts/auth/community.js
@@ -12,6 +12,13 @@ $('#ajax').on('click', '.item_fullscreen_hide', function() { $('.item_fullscreen
 $('#ajax').on('click', '.votes_fullscreen_hide', function() { $('.votes_fullscreen').hide(); $('#votes_loader').empty(); });
 $('#ajax').on('click', '.article_fullscreen_hide', function() {$('.article_fullscreen').hide(); $('#article_loader').empty();});
 
+/*!
+   auth script of community
+  */
+$('#ajax').on('click', '.member_create', function() {var member_create = $(this);var pk = member_create.data('id');$.ajax({url: "/communities/add_community_member/" + pk + "/",success: function () {$('#ajax').html('').load("/communities/reload/" + pk + "/");}});});
+$('#ajax').on('click', '.member_delete', function() {var member_delete = $(this);var pk = member_delete.data('id');$.ajax({url: "/communities/delete_community_member/" + pk + "/",success: function () {$('#ajax').html('').load("/communities/reload/" + pk + "/");}});});
+$('#ajax').on('click', '.member_follow_create', function() {var member_follow_create = $(this);var pk = member_follow_create.data('id');$.ajax({url: "/follows/add_member/" + pk + "/",success: function () {$('#ajax').html('').load("/communities/reload/" + pk + "/");}});});
+$('#ajax').on('click', '.member_follow_delete', function() {var member_follow_delete = $(this);var pk = member_follow_delete.data('id');$.ajax({url: "/follows/delete_member/" + pk + "/",success: function () {$('#ajax').html('').load("/communities/reload/" + pk + "/");}});});
 
 /*!
    get comment script of community
