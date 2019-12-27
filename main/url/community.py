510b64628ac1ac182c2497bc399ba596b2303c25
Serg <ochkarik1983@mail.ru@>
2019-12-27 18:19:03 +0300

    J

--- main/url/community.py
+++ main/url/community.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
 	url(r'^comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityCommentList.as_view()),
     url(r'^post-comment/$', login_required(ItemCommunityCommentCreate.as_view())),
-    url(r'^reply-comment/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', login_required(ItemCommunityReplyCreate.as_view())),
+    url(r'^reply-comment/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(ItemCommunityReplyCreate.as_view())),
 	url(r'^fixed/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_fixed)),
     url(r'^unfixed/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_unfixed)),
     url(r'^delete/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_item_delete)),
