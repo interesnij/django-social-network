5fd2fd554cef5962388c358442078004ee6fa3e3
Serg <ochkarik1983@mail.ru@>
2019-12-28 18:19:02 +0300

    J

---
+++ static/scripts/anon/gallery.js
@@ -0,0 +1,12 @@
+
+$('.photos-container').on('click', '.photo_detail', function() {
+    photo = $(this); photo_id = photo.data("id"); user_uuid = photo.data("uuid");
+    $('#photo_loader').html('').load("/gallery/load/photo/" + photo_id + "/" + user_uuid + "/")
+    $('.photo_fullscreen').show();
+});
+
+$('.album-container').on('click', '.album_photo_detail', function() {
+    photo = $(this); pk = photo.data("pk"); uuid = photo.data("uuid"); uuid2 = photo.data("uuid2");
+    $('#photo_loader').html('').load("/gallery/load/u_photo/" + pk + "/" + uuid + "/" + uuid2 + "/")
+    $('.photo_fullscreen').show();
+});
