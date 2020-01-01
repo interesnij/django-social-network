1901d287cff2b39992d3df8b3d0443e6f7341009
Serg <ochkarik1983@mail.ru@>
2020-01-01 15:25:01 +0300

    H

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', AllMusicView.as_view(), name='all_music'),
-    url(r'^all_list/$', AllMusicListView.as_view(), name='all_music_list'),
+    url(r'^all_music_list/$', AllMusicListView.as_view(), name='all_music_list'),
     url(r'^search_music/$', AllSearchMusicView.as_view(), name='search_all_music'),
 ]
