ae2373112d6e4f8310995ad7fd961b6c06627fa8
Serg <ochkarik1983@mail.ru@>
2020-01-01 18:26:36 +0300

    H

--- music/views.py
+++ music/views.py
@@ -18,14 +18,10 @@ class AllMusicListView(View):
         page_size = 10
         client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
         all_tracks = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
-        for track in all_tracks.collection:
-            print(c,track.title)
-            c=c+1
+
         while all_tracks.next_href != None:
             all_tracks = client.get(all_tracks.next_href, limit=page_size, order='created_at', linked_partitioning=1)
-            for track in all_tracks.collection:
-                print(c,track.title)
-                c=c+1
+
         context['request_user'] = request.user
         context['all_tracks'] = all_tracks
         return render_to_response('all_music_list.html', context)
