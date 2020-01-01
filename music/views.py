18e16617f5df1c24b4f4bca42703a0df7c6d0f44
Serg <ochkarik1983@mail.ru@>
2020-01-01 17:07:24 +0300

    H

--- music/views.py
+++ music/views.py
@@ -10,10 +10,10 @@ class AllMusicView(TemplateView):
 
 
 class AllMusicListView(View):
-    template_name="all_music_list.html"
+    template_name = "all_music_list.html"
     def get(self,request,*args,**kwargs):
         context = {}
-        page_size = 100
+        page_size = 50
         client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
         all_tracks = client.get('/tracks', order='created_at', limit=page_size)
         context['request_user'] = request.user
