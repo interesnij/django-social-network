8377894db5aec298b168dc1880662b41694081b3
Serg <ochkarik1983@mail.ru@>
2020-01-01 19:44:32 +0300

    H

--- music/views.py
+++ music/views.py
@@ -22,7 +22,7 @@ class AllMusicListView(View):
         N = 143729872
         S = []
         while N > 143729000:
-            S = list.append(N)
+            S = list.append(str(N))
             N = N - 1
 
         track_ids = list(range(1, N+1))
