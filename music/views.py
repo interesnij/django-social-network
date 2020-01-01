43b657efda5402d2d6b23dabe9d30267af0c0540
Serg <ochkarik1983@mail.ru@>
2020-01-01 19:29:00 +0300

    H

--- music/views.py
+++ music/views.py
@@ -20,7 +20,7 @@ class AllMusicListView(View):
         #all_tracks = client.get('/tracks', order='created_at', limit=page_size,)
         client_id='dce5652caa1b66331903493735ddd64d'
         N = 1000
-        S = ["13158665", "6621631", "6621632"]
+        S = ["13158665", "6621631", "6621632", "6621631", "6621631", "6621631", "6621631"]
         track_ids = list(range(1, N+1))
         tracks_url ='http://api.soundcloud.com/tracks'
         payload = {'client_id': client_id, 'ids': ','.join(S)}
