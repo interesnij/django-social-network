6873a70a08535cbe883b034c6195c852dbfe8a66
Serg <ochkarik1983@mail.ru@>
2020-01-01 18:12:21 +0300

    H

--- music/views.py
+++ music/views.py
@@ -17,7 +17,7 @@ class AllMusicListView(View):
         page_size = 10
         client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
         all_tracks = client.get('/tracks', order='created_at')
-        if all_tracks.count() > 10:
+        if all_tracks:
             all_tracks = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
         context['request_user'] = request.user
         context['all_tracks'] = all_tracks
