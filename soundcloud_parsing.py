027a294f93ff66b70df7f096fad3b10ef4706b7e
Serg <ochkarik1983@mail.ru@>
2020-01-02 11:46:45 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -19,18 +19,20 @@ client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
 page_size = 200
 all_tracks = client.get('/tracks', order='created_at', limit=page_size, linked_partitioning=1)
 for track in all_tracks.collection:
-    a = track.created_at
-    a = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+    created_at = track.created_at
+    created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
     SoundParsing.objects.create(
                                 artwork_url=track.artwork_url,
                                 bpm=track.bpm,
-                                created_at=a,
+                                created_at=created_at,
                                 duration=track.duration,
                                 genre=track.genre,
                                 permalink=track.permalink,
                                 permalink_url=track.permalink_url,
-                                #stream_url=track.stream_url,
-                                #streamable=track.streamable,
+                                stream_url=track.stream_url,
+                                streamable=track.streamable,
+                                release_month=release_month,
+                                release_year=release_year,
                                 tag_list=track.tag_list,
                                 title=track.title,
                                 uri=track.uri,
@@ -40,20 +42,20 @@ for track in all_tracks.collection:
 while all_tracks.next_href != None and all_tracks.count() < 301:
     all_tracks = client.get(all_tracks.next_href, order='created_at', limit=page_size, linked_partitioning=1)
     for track in all_tracks.collection:
+        created_at = track.created_at
+        created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
         SoundParsing.objects.create(
                                     artwork_url=track.artwork_url,
                                     bpm=track.bpm,
-                                    created_at=track.created_at,
+                                    created_at=created_at,
                                     duration=track.duration,
                                     genre=track.genre,
                                     permalink=track.permalink,
                                     permalink_url=track.permalink_url,
-                                    release=track.release,
-                                    release_day=track.release_day,
-                                    release_month=track.release_month,
-                                    release_year=track.release_year,
                                     stream_url=track.stream_url,
                                     streamable=track.streamable,
+                                    release_month=release_month,
+                                    release_year=release_year,
                                     tag_list=track.tag_list,
                                     title=track.title,
                                     uri=track.uri,
