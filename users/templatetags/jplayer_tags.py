968287c60876c446b2c7cad8cd1bce08d4832e16
Serg <ochkarik1983@mail.ru@>
2020-01-03 14:57:23 +0300

    J

--- users/templatetags/jplayer_tags.py
+++ users/templatetags/jplayer_tags.py
@@ -1,6 +1,7 @@
 from django import template
 register = template.Library()
 
+@register.filter
 def render_jplayer(context, player):
     return {'player': player}
 register.inclusion_tag('jplayer/player.html', takes_context=True)(render_jplayer)
