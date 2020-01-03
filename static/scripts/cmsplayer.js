87bfb6988d546760782363dca14d1d86d5bccc23
Serg <ochkarik1983@mail.ru@>
2020-01-03 18:43:54 +0300

    J

--- static/scripts/cmsplayer.js
+++ static/scripts/cmsplayer.js
@@ -85,7 +85,7 @@
             $("#playlist_item_"+playerid+"_"+index).addClass("playlist_current");
             playItem = index;
             var playlist = get_playlist();
-            if (playlist[playItem].uri)
+            if (playlist[playItem].mp3)
             {
                 element.jPlayer("setFile", playlist[playItem].mp3, playlist[playItem].ogg);
             }
