caba88970a5327748ebe53d3ccb7c9f4021b67e3
Serg <ochkarik1983@mail.ru@>
2020-01-03 13:17:50 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -15,8 +15,8 @@ from music.models import SoundParsing, SounGenres
 from datetime import datetime, date, time
 
 
-genres_list = SounGenres.objects.values(name)
-genres_list_names = [name[name] for name in genres_list]
+genres_list = SounGenres.objects.values('name')
+genres_list_names = [name['name'] for name in genres_list]
 client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
 page_size = 10
 all_tracks = client.get('/tracks', order="playback_count", limit=page_size, linked_partitioning=1)
@@ -25,6 +25,7 @@ count = 0
 for track in all_tracks.collection:
     created_at = track.created_at
     created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+    genre = "'" + track.genre + "'"
     try:
         SoundParsing.objects.get(id=track.id)
     except:
@@ -32,7 +33,7 @@ for track in all_tracks.collection:
             stream_url = track.stream_url
         except:
             stream_url = ''
-        if track.genre in genres_list_names:
+        if genre in genres_list_names:
             SoundParsing.objects.create(
                                 id=track.id,
                                 artwork_url=track.artwork_url,
