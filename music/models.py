42eaae089bbe11580a479a86682afff18e1fc4e6
Serg <ochkarik1983@mail.ru@>
2020-01-03 12:39:57 +0300

    J

--- music/models.py
+++ music/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
 
+class SounGenres(models.Model):
+    name = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
 class SoundParsing(models.Model):
     id = models.IntegerField(primary_key=True)
     artwork_url = models.URLField(max_length=255, blank=True, null=True)
     bpm = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(max_length=255, blank=True, null=True)
     duration = models.CharField(max_length=255, blank=True, null=True)
-    genre = models.CharField(max_length=255, blank=True, null=True)
+    genre = models.ForeignKey(SounGenres, on_delete=models.CASCADE, verbose_name="Жанр трека")
     permalink = models.CharField(max_length=255, blank=True, null=True)
     release_day = models.CharField(max_length=255, blank=True, null=True)
     release_month = models.CharField(max_length=255, blank=True, null=True)
