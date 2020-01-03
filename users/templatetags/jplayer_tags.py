62d963241d258a29fbe27924b63c5ddedd778407
Serg <ochkarik1983@mail.ru@>
2020-01-03 14:58:54 +0300

    J

--- users/templatetags/jplayer_tags.py
+++ users/templatetags/jplayer_tags.py
@@ -4,4 +4,4 @@ register = template.Library()
 @register.filter
 def render_jplayer(context, player):
     return {'player': player}
-register.inclusion_tag('jplayer/player.html', takes_context=True)(render_jplayer)
+register.inclusion_tag('base_block/player.html', takes_context=True)(render_jplayer)
