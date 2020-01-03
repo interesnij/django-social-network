0a627baebe599c1e32fd89d40d3b359834549351
Serg <ochkarik1983@mail.ru@>
2020-01-03 20:29:32 +0300

    J

--- music/models.py
+++ music/models.py
@@ -52,7 +52,7 @@ class SoundParsing(models.Model):
     created_at = models.DateTimeField(max_length=255, blank=True, null=True)
     duration = models.CharField(max_length=255, blank=True, null=True)
     genre = models.ForeignKey(SounGenres, on_delete=models.CASCADE, verbose_name="Жанр трека")
-    permalink = models.CharField(max_length=255, blank=True, null=True)
+    permalink = models.CharField(max_length=500, blank=True, null=True)
     stream_url = models.URLField(max_length=255, blank=True, null=True)
     streamable = models.BooleanField(default=True, null=True)
     title = models.CharField(max_length=255, blank=True, null=True)
