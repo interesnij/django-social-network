3baddc74356e8421f2d892059ccd1c2f4fffc4e9
Serg <ochkarik1983@mail.ru@>
2020-01-04 11:12:26 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -31,7 +31,7 @@ for track in all_tracks.collection:
     except:
         if track.genre and track.stream_url and track.genre in genres_list_names:
             genre =SounGenres.objects.get(name=track.genre.replace("'", '') )
-            stream_url = client.get(track.stream_url, allow_redirects=False)
+            stream_url = track.uri + 'stream?client_id=' + 'dce5652caa1b66331903493735ddd64d'
             new_track = SoundParsing.objects.create(
                                 id=track.id,
                                 artwork_url=track.artwork_url,
@@ -39,7 +39,7 @@ for track in all_tracks.collection:
                                 created_at=created_at,
                                 duration=track.duration,
                                 genre=genre,
-                                stream_url=stream_url.location,
+                                stream_url=stream_url,
                                 streamable=track.streamable,
                                 title=track.title,
                                 uri=track.uri,
@@ -57,7 +57,7 @@ while all_tracks.next_href != None and count < 10:
         except:
             if track.genre and track.stream_url and track.genre in genres_list_names:
                 genre = SounGenres.objects.get(name=track.genre.replace("'", '') )
-                stream_url = client.get(track.stream_url, allow_redirects=False)
+                stream_url = track.uri + 'stream?client_id=' + 'dce5652caa1b66331903493735ddd64d'
                 new_track = SoundParsing.objects.create(
                                     id=track.id,
                                     artwork_url=track.artwork_url,
@@ -65,7 +65,7 @@ while all_tracks.next_href != None and count < 10:
                                     created_at=created_at,
                                     duration=track.duration,
                                     genre=genre,
-                                    stream_url=stream_url.location,
+                                    stream_url=stream_url,
                                     streamable=track.streamable,
                                     title=track.title,
                                     uri=track.uri,
