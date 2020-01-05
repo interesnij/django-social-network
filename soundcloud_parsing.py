9622b60d73dc54d2336cfceb1725b8ab1b3b5abc
Serg <ochkarik1983@mail.ru@>
2020-01-05 10:41:32 +0300

    h

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -28,38 +28,34 @@ for tag in a_rus_list:
     tracks = client.get('/tracks', q=tag, limit=page_size, linked_partitioning=1)
     if tracks:
         for track in tracks.collection:
+            created_at = track.created_at
+            created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
             try:
                 SoundParsing.objects.get(id=track.id)
             except:
+                try:
+                    self_tag = SoundTags.objects.get(name=tag, simbol="А")
+                except:
+                    self_tag = SoundTags.objects.create(name=tag, symbol="А")
                 if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
                     genre =SounGenres.objects.get(name=track.genre.replace("'", '') )
-                    new_track = SoundParsing.objects.create(id=track.id, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year, tag=track.tag)
+                    new_track = SoundParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year, tag=track.tag)
                     all_track_playlist.track.add(new_track)
                 count = count + 1
-
-
-for track in all_tracks.collection:
-    created_at = track.created_at
-    created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-    try:
-        SoundParsing.objects.get(id=track.id)
-    except:
-        if track.genre and track.release_year and track.genre in genres_list_names:
-            genre =SounGenres.objects.get(name=track.genre.replace("'", '') )
-            new_track = SoundParsing.objects.create(id=track.id, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year, tag=track.tag)
-            all_track_playlist.track.add(new_track)
-        count = count + 1
-
-while all_tracks.next_href != None and count < 2000:
-    all_tracks = client.get(all_tracks.next_href, limit=page_size, linked_partitioning=1)
-    for track in all_tracks.collection:
-        created_at = track.created_at
-        created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-        try:
-            SoundParsing.objects.get(id=track.id)
-        except:
-            if track.genre and track.release_year and track.genre in genres_list_names:
-                genre = SounGenres.objects.get(name=track.genre.replace("'", '') )
-                new_track = SoundParsing.objects.create(id=track.id, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year, tag=track.tag)
-                all_track_playlist.track.add(new_track)
-        count = count + 1
+        while tracks.next_href != None and count < 1000:
+            tracks = client.get(tracks.next_href, limit=page_size, linked_partitioning=1)
+            for track in tracks.collection:
+                created_at = track.created_at
+                created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+                try:
+                    SoundParsing.objects.get(id=track.id)
+                except:
+                    try:
+                        self_tag = SoundTags.objects.get(name=tag, simbol="А")
+                    except:
+                        self_tag = SoundTags.objects.create(name=tag, symbol="А")
+                    if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
+                        genre =SounGenres.objects.get(name=track.genre.replace("'", '') )
+                        new_track = SoundParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year, tag=track.tag)
+                        all_track_playlist.track.add(new_track)
+                    count = count + 1
