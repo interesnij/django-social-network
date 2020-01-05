86575a0b2ed4727487a74914a5fd36fa26bc63e5
Serg <ochkarik1983@mail.ru@>
2020-01-05 10:16:21 +0300

    h

--- users/templatetags/jplayer_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from music.models import Playlist
-register = template.Library()
-
-
-@register.filter
-def render_jplayer(context, player):
-    all_track_playlist = Playlist.objects.get(id=1)
-    return {'player': all_track_playlist}
