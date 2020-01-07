f378dbd63057ffb8d09504f63fca9b7675798847
Serg <ochkarik1983@mail.ru@>
2020-01-07 13:01:23 +0300

    J

--- common/parsing_soundcloud/parsing_е_rus.py
+++ common/parsing_soundcloud/parsing_е_rus.py
@@ -235,7 +235,10 @@ for tag in е_rus_list_1:
                     SoundParsing.objects.get(id=track.id)
                 except:
                     if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
-                        self_tag = SoundTags.objects.get(name=tag, symbol=litera)
+                        try:
+                            self_tag = SoundTags.objects.get(name=tag, symbol=litera)
+                        except:
+                            self_tag = SoundTags.objects.create(name=tag, symbol=litera)
                         genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
                         new_track = SoundParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year)
                     count = count + 1
