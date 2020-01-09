802ac55656c1dd4d8b4b56f0306880a57186caa3
Serg <ochkarik1983@mail.ru@>
2020-01-09 15:50:21 +0300

    J

--- music/view/list.py
+++ music/view/list.py
@@ -4,37 +4,38 @@ from music.models import *
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-class AllMusicListView(View):
-
-    def get(self,request,*args,**kwargs):
-        context = {}
-        player = SoundList.objects.get(id=2)
-        all_tracks = player.get_json_playlist()
-        context['all_tracks'] = all_tracks
-        context['player'] = player
-        return render_to_response('music/all_music_list2.html', context)
-
-
-class AllTagListView(View):
-
-    def get(self,request,*args,**kwargs):
-        context = {}
-        tag=SoundTags.objects.get(pk=self.kwargs["pk"])
-        all_tracks = tag.get_json_playlist()
-        context['all_tracks'] = all_tracks
-        context['tag'] = tag
-        context['request_user'] = request.user
-        return render_to_response('music/tag_music_list.html', context)
-
-
 class TagsList(View):
+    """
+    Список тегов отдельной буквы поиска музыки
+    """
     def get(self, request, *args, **kwargs):
         context = {}
         symbol = SoundSymbol.objects.get(pk=self.kwargs["pk"])
-        tag_list = SoundTags.objects.filter(symbol=symbol)
-        current_page = Paginator(tag_list, 24)
+        tags_list = SoundTags.objects.filter(symbol=symbol)
+        current_page = Paginator(tags_list, 24)
         page = request.GET.get('page')
         context['symbol'] = symbol
+        context['request_user'] = request.user
+        try:
+            context['tags_list'] = current_page.page(page)
+        except PageNotAnInteger:
+            context['tags_list'] = current_page.page(1)
+        except EmptyPage:
+            context['tags_list'] = current_page.page(current_page.num_pages)
+        return render_to_response('music/tags_list.html', context)
+
+
+class AllTagListView(View):
+    """
+    Список треков отдельного тега
+    """
+    def get(self,request,*args,**kwargs):
+        context = {}
+        tag=SoundTags.objects.get(pk=self.kwargs["pk"])
+        tag_list = tag.get_json_playlist()
+        current_page = Paginator(tag_list, 24)
+        page = request.GET.get('page')
+        context['tag'] = tag
         context['request_user'] = request.user
 
         try:
@@ -43,4 +44,4 @@ class TagsList(View):
             context['tag_list'] = current_page.page(1)
         except EmptyPage:
             context['tag_list'] = current_page.page(current_page.num_pages)
-        return render_to_response('music/tags_list.html', context)
+        return render_to_response('music/tag_music_list.html', context)
