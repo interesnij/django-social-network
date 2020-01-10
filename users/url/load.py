4146c4e0c2f407d1817d9f414cfd87d6f3641aa9
Serg <ochkarik1983@mail.ru@>
2020-01-10 13:10:27 +0300

    H

--- users/url/load.py
+++ users/url/load.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^profile_stat/(?P<pk>\d+)/$', ProfileStatReload.as_view(), name='profile_stat_reload'),
     url(r'^(?P<pk>\d+)/profile_reload/$', ProfileReload.as_view(), name='profile_reload'),
     url(r'^img_load/(?P<uuid>[0-9a-f-]+)/$', UserImagesLoad.as_view()),
-    url(r'^playlist_load/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistLoad.as_view()),
+    url(r'^playlist/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistLoad.as_view()),
 ]
