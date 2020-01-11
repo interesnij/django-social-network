196fda3b044cb9c8cef0f9be68a4e49897ddc650
Serg <ochkarik1983@mail.ru@>
2020-01-11 10:43:31 +0300

    H

--- static/scripts/music.js
+++ static/scripts/music.js
@@ -1,9 +1,27 @@
 $('body').on('click', '.track_add', function() {
-  pk = $(this).parent().data("pk");
+  btn = $(this)
+  block = btn.parent();
+  pk = block.parent().data("pk");
   $.ajax({
       url: "/music/manage/add_track/" + pk + "/",
       success: function (data) {
-        $.toast({heading: 'Информация',text: 'Трек добавлен в Ваш основной плейлист!',showHideTransition: 'fade',icon: 'info'})
+        $.toast({heading: 'Информация',text: 'Трек добавлен в Ваш основной плейлист!',showHideTransition: 'fade',icon: 'info'});
+        btn.remove();
+        block.append("<span class='track_remove' title='Удалить'><svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' style='width:20px;' class='svg_default' viewBox='0 0 24 24'><path fill='none' d='M0 0h24v24H0z'/><path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg></span>")
+      }
+  });
+});
+
+$('body').on('click', '.track_remove', function() {
+  btn = $(this)
+  block = btn.parent();
+  pk = block.parent().data("pk");
+  $.ajax({
+      url: "/music/manage/delete_track/" + pk + "/",
+      success: function (data) {
+        $.toast({heading: 'Информация',text: 'Трек удален из Вашего основного плейлиста!',showHideTransition: 'fade',icon: 'info'});
+        btn.remove();
+        block.append("<span class='track_add' title='Добавить'><svg fill='currentColor' style='width:25px;' class='svg_default' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/><path d='M0 0h24v24H0z' fill='none'/></svg></span>")
       }
   });
 });
