6d76171105eaa6ac59457d119cd25115d881689b
Serg <ochkarik1983@mail.ru@>
2020-01-11 09:18:09 +0300

    H

--- users/templatetags/get_key.py
+++ users/templatetags/get_key.py
@@ -3,10 +3,9 @@ register=template.Library()
 
 
 @register.filter
-def is_track_exists(track_id): 
+def is_track_exists(track_id):
     try:
         true = request.user.is_track_exists(track_id)
         return True
     except:
         return False
-    return dictionary.get(key)
