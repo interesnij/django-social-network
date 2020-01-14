017dbb80fb6c0a2062da9e428f806e08e40ed9af
Serg <ochkarik1983@mail.ru@>
2020-01-14 10:27:56 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -174,6 +174,8 @@ for tag in б_rus_list_1:
             created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
             if track.description:
                 description = track.description[:500]
+            else:
+                description=None
             try:
                 SoundcloudParsing.objects.get(id=track.id)
             except:
@@ -192,6 +194,8 @@ for tag in б_rus_list_1:
                 created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
                 if track.description:
                     description = track.description[:500]
+                else:
+                    description=None
                 try:
                     SoundcloudParsing.objects.get(id=track.id)
                 except:
