045449986f61fa86e6c5d6810f240229e410925d
Serg <ochkarik1983@mail.ru@>
2020-01-14 09:50:55 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -911,7 +911,7 @@ for tag in a_rus_list_1:
                     except:
                         self_tag = SoundTags.objects.create(name=tag, symbol=litera)
                     genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
-                    new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, description=track.description, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year)
+                    new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, description=track.description, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, title=track.title, uri=track.uri, release_year=track.release_year)
                 count = count + 1
         while tracks.next_href != None and count < 2000:
             tracks = client.get(tracks.next_href, limit=page_size, linked_partitioning=1)
@@ -927,5 +927,5 @@ for tag in a_rus_list_1:
                         except:
                             self_tag = SoundTags.objects.create(name=tag, symbol=litera)
                         genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
-                        new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, description=track.description, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, stream_url=track.stream_url, title=track.title, uri=track.uri, release_year=track.release_year)
+                        new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, description=track.description, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, title=track.title, uri=track.uri, release_year=track.release_year)
                     count = count + 1
