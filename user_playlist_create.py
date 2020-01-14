37f16e91377367e0b04b8238a00f26a3a19eb1de
Serg <ochkarik1983@mail.ru@>
2020-01-14 17:50:55 +0300

    J

--- user_playlist_create.py
+++ user_playlist_create.py
@@ -13,4 +13,7 @@ from music.models import *
 from users.models import User
 
 user = User.objects.get(pk=1)
+list = SoundList.objects.filter(creator=user, name="my_first_generic_playlist_number_12345678900000000")
+list.delete()
+
 SoundList.objects.create(creator=user, name="my_first_generic_playlist_number_12345678900000000")
