3b93422470938c6375fd981f591151fd18334448
Serg <ochkarik1983@mail.ru@>
2020-01-16 14:03:08 +0300

    J

--- common/parsing_soundcloud/parsing_r_angl.py
+++ common/parsing_soundcloud/parsing_r_angl.py
@@ -19,65 +19,15 @@ page_size = 200
 genres_list = SoundGenres.objects.values('name')
 genres_list_names = [name['name'] for name in genres_list]
 
-q_rus_list_1 = [
-"Q Machette feat. Bobby V",
-"Q o d e s feat. Athena",
-"Q-Sign feat. K.O.",
-"Qadance",
-"QAMI",
-"Qaran feat. R3hab",
-"Qleo feat. Lazee",
-"Qmir",
-"Qobee & Raikhana Mukhlis",
-"Qobee feat. Jamaru",
-"Qoma",
-"Qontrast",
-"QQ feat. Major Lazer & Swick",
-"QQUN",
-"Quality Control feat. Lil Baby & DaBaby",
-"Quando Rondo feat. A Boogie Wit Da Hoodie & 2 Chainz",
-"Quartissimo",
-"Quavo & Saweetie",
-"Quavo feat. Madonna & Cardi B",
-"QUB",
-"Quba",
-"Qubicon",
-"Que",
-"Queen",
-"Queen Bicycle Race",
-"Queen Latifah",
-"Queen Of Hearts",
-"Queens",
-"Queensberry",
-"Queensley Felix",
-"Quentin Mosimann",
-"Quentyn",
-"Quest Pistols",
-"Quickstaff",
-"Quico M.",
-"Quiet Riot",
-"Quietdrive",
-"Quigley",
-"Quincy Jagher",
-"Quincy Jones",
-"Quinn Lewis",
-"Quinn XCII",
-"Quintino",
-"Quique Tejada feat. Monica Guech",
-"Quix",
-"Qulinez",
-"Quman feat. Emily Kane",
-"Qunst",
-"Qveen Herby",
-"Qwote",
-"QZB",
+r_rus_list_1 = [
+
 ]
 
-litera = SoundSymbol.objects.get(name="Q")
+litera = SoundSymbol.objects.get(name="R")
 
 count = 0
 
-for tag in q_rus_list_1:
+for tag in r_rus_list_1:
     tracks = client.get('/tracks', q=tag, limit=page_size, linked_partitioning=1)
     if tracks:
         for track in tracks.collection:
