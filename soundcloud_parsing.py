307bc6d74725dcb7f29726815091a9d75baf4e62
Serg <ochkarik1983@mail.ru@>
2020-01-16 18:44:49 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -2518,7 +2518,7 @@ litera = SoundSymbol.objects.get(name="S")
 
 count = 0
 
-for tag in s_rus_list_44:
+for tag in s_rus_list_45:
     tracks = client.get('/tracks', q=tag, limit=page_size, linked_partitioning=1)
     if tracks:
         for track in tracks.collection:
