8a8a0b9acafdb422e7be42dc09286de516af4349
Serg <ochkarik1983@mail.ru@>
2020-01-17 09:23:13 +0300

    J

--- common/parsing_soundcloud/parsing_2.py
+++ common/parsing_soundcloud/parsing_2.py
@@ -19,43 +19,15 @@ page_size = 200
 genres_list = SoundGenres.objects.values('name')
 genres_list_names = [name['name'] for name in genres_list]
 
-list_1_1 = [
-"1 RDJ",
-"1 World & J.R. feat. Cardi B & Romy Dya",
-"10 Years",
-"100 Watt Vipers",
-"100 Атмосфер",
-"1000volts & Dion Timmer & Redman & Jayceeoh feat. Kid Capri",
-"100UM feat. ChipaChip",
-"101 Далматинец",
-"104",
-"10AGE",
-"10cc",
-"10НЕБО",
-"12 Fukkk Offf",
-"12 Stones",
-"12 стульев",
-"128 Ударов В Минуту",
-"12AM",
-"12AM feat. Jez Dior",
-"12th Hour",
-"12th Planet & Mayhem feat. Pennybirdrabbit",
-"140 УВМ",
-"140 Ударов В Минуту",
-"150 Красного",
-"18PLS",
-"1950 feat. Ruby Blu",
-"19EIGHTY7",
-"1DAFUL",
-"1NQ",
-"1WaN",
+list_2_1 = [
+
 ]
 
-litera = SoundSymbol.objects.get(name="1")
+litera = SoundSymbol.objects.get(name="2")
 
 count = 0
 
-for tag in list_1_1 = [:
+for tag in list_2_1 = [:
     tracks = client.get('/tracks', q=tag, limit=page_size, linked_partitioning=1)
     if tracks:
         for track in tracks.collection:
