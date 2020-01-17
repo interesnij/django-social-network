c818eedc667325b1db87b1aaf03ff1a5af2999bd
Serg <ochkarik1983@mail.ru@>
2020-01-17 10:15:03 +0300

    J

--- soundcloud_parsing.py
+++ soundcloud_parsing.py
@@ -19,53 +19,37 @@ page_size = 200
 genres_list = SoundGenres.objects.values('name')
 genres_list_names = [name['name'] for name in genres_list]
 
-list_3_1 = [
-"3 Are Legend feat. W&W",
-"3 Doors Down",
-"3 Monkeyzz feat. Louise Mambell",
-"3 Sud Est",
-"3 Years Hollow",
-"3 Желания",
-"3 плюс 2",
-"3-Rd Force",
-"3-ий Январь",
-"30 Dекабря feat. Денис RiDer",
-"30 Seconds To Mars",
-"30.02",
-"303 Каратиста",
-"3030 & Bhaskar feat. Dubdogz",
-"31 июня",
-"311",
-"33Hz",
-"351 Lake Shore Drive",
-"36 Crazyfists",
-"3Caps",
-"3D Nation",
-"3D-Mix",
-"3GUN feat. IZZY",
-"3Heartbeats",
-"3JS",
-"3LAU",
-"3LOGY",
-"3OH!3",
-"3OP feat. Amara",
-"3R",
-"3rd Force",
-"3rd Prototype feat. Meg Dia",
-"3rd. Force",
-"3Sonn",
-"3South",
-"3Upfront",
-"3XL PRO",
-"3Xlpro",
-"3XLPRO feat. Харизмо",
+list_4_1 = [
+"4 Seas",
+"4 Strings",
+"4 The Cause",
+"40 Miles feat. Sveva Gray",
+"40s Mood",
+"430 Steps",
+"45 Калибр",
+"4atty aka Tilla",
+"4B feat. Chris Brown",
+"4BLOCK",
+"4Fun",
+"4G",
+"4i",
+"4Joann",
+"4K",
+"4Magic",
+"4Minute",
+"4Noize",
+"4POST (Дима Бикбаев)",
+"4Q's feat. Темча К.",
+"4SGM",
+"4естно & Айк Дым",
+"4МТА feat. Денис Океан & Дима Кузьмин",
 ]
 
-litera = SoundSymbol.objects.get(name="3")
+litera = SoundSymbol.objects.get(name="4")
 
 count = 0
 
-for tag in list_3_1:
+for tag in list_4_1:
     tracks = client.get('/tracks', q=tag, limit=page_size, linked_partitioning=1)
     if tracks:
         for track in tracks.collection:
