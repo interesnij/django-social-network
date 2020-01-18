be2a729822834bf7b89b098e90454cdc7ca9ed36
Serg <ochkarik1983@mail.ru@>
2020-01-18 18:50:05 +0300

    J

--- communities/forms.py
+++ communities/forms.py
@@ -10,9 +10,9 @@ class CommunityForm(forms.ModelForm):
 
 class GeneralCommunityForm(forms.ModelForm):
 	name = forms.CharField( label="",widget=forms.TextInput(attrs={'class': 'form-control'}))
-	description = forms.CharField( label="", required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'rows': '4'}))
-	rules = forms.CharField( label="", required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-	status = forms.CharField( label="",required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
+	rules = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
+	status = forms.CharField( label="",required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
 	class Meta:
 		model = Community
 		fields = ['name', 'description', 'rules', 'status',]
