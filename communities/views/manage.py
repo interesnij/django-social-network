3798476414e3a28fd7551c6771a13263ff5b09ab
Serg <ochkarik1983@mail.ru@>
2020-01-19 14:43:57 +0300

    J

--- communities/views/manage.py
+++ communities/views/manage.py
@@ -272,11 +272,13 @@ class CommunityMemberManageView(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.community = Community.objects.get(pk=self.kwargs["pk"])
+		self.administrators = Community.get_community_with_name_administrators(self.community)
 		return super(CommunityMemberManageView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
 		context=super(CommunityMemberManageView,self).get_context_data(**kwargs)
 		context["community"]=self.community
+		context["administrators"]=self.administrators
 		return context
 
 	def get_queryset(self):
