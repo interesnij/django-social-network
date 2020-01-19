5142d878956806dc2363ab723e7d521f7929bf78
Serg <ochkarik1983@mail.ru@>
2020-01-19 15:39:10 +0300

    J

--- communities/models.py
+++ communities/models.py
@@ -259,12 +259,6 @@ class Community(models.Model):
         return User.objects.filter(community_administrators_query)
 
     @classmethod
-    def get_community_with_name_creator(cls, community_id):
-        community = Community.objects.get(pk=community_id)
-        User = get_user_model()
-        return User.objects.filter(id=community.creator.pk)
-
-    @classmethod
     def get_community_with_name_editors(cls, community_name):
         community_moderators_query = Q(communities_memberships__community__name=community_name, communities_memberships__is_editor=True)
         User = get_user_model()
