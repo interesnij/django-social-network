2521e110a5349ef9c8e522c8de1356c5885030ea
Serg <ochkarik1983@mail.ru@>
2020-01-20 18:38:50 +0300

    J

--- static/scripts/my/staff_script.js
+++ static/scripts/my/staff_script.js
@@ -28,8 +28,18 @@ $('#ajax').on('click', '.manage_window_fullscreen_hide', function() {
    var member_create = $(this); var li = member_create.parents(".list-group-item"); var pk = li.data('pk'); var uuid = li.data('uuid'); $.ajax({ url: "/communities/progs/add_member/" + pk + "/" + uuid + "/", success: function () { member_create.parent().html("<span class='show_staff_window' style='cursor:pointer'>Изменить полномочия</span> |<span class='community_member_delete' style='cursor:pointer'>Удалить</span>"); li.removeClass("style_removed_object"); }});
    });
    $('#ajax').on('click', '.community_follow_delete', function() {
-    var community_follow_delete = $(this); var li = community_follow_delete.parents(".list-group-item"); var pk = li.data('pk'); var uuid = li.data('uuid'); $.ajax({ url: "/follows/progs/delete_member/" + pk + "/" + uuid + "/", success: function () { community_follow_delete.parent().html("<span class='community_member_create' style='cursor:pointer;color:rgba(0, 0, 0, 1);'>Восстановить</span>""); li.addClass("style_removed_object"); }});
+    var community_follow_delete = $(this);
+    var li = community_follow_delete.parents(".list-group-item");
+    var pk = li.data('pk');
+    var uuid = li.data('uuid');
+    $.ajax({
+      url: "/follows/progs/delete_member/" + pk + "/" + uuid + "/",
+      success: function () {
+        community_follow_delete.parent().html("<span class='community_member_create' style='cursor:pointer;color:rgba(0, 0, 0, 1);'>Восстановить</span>");
+        li.addClass("style_removed_object"); }
+      });
     });
+    
   $('#ajax').on('click', '.community_member_delete', function() {
    var member_delete = $(this); var li = member_delete.parents(".list-group-item"); var pk = li.data('pk'); var uuid = li.data('uuid'); $.ajax({ url: "/communities/progs/delete_member/" + pk + "/" + uuid + "/", success: function () { member_delete.parent().html("<span class='community_member_create' style='cursor:pointer;color:rgba(0, 0, 0, 1);'>Восстановить</span>"); li.addClass("style_removed_object"); }});
    });
