85c0c53137717dc95841ba77f8da72b1b49b4d4e
Serg <ochkarik1983@mail.ru@>
2020-01-21 19:54:37 +0300

    J

--- static/scripts/my/gallery.js
+++ static/scripts/my/gallery.js
@@ -1,5 +1,5 @@
 
-$('.photos-container').on('click', '.photo_detail', function() {
+$('#ajax').on('click', '.photo_detail', function() {
     photo = $(this); photo_id = photo.data("id"); user_uuid = photo.data("uuid");
     $('#photo_loader').html('').load("/gallery/load/photo/" + photo_id + "/" + user_uuid + "/")
     $('.photo_fullscreen').show();
@@ -11,7 +11,7 @@ $('.album-container').on('click', '.album_photo_detail', function() {
     $('.photo_fullscreen').show();
 });
 
-$("#photos_add").click(function() { $('#photos_add_window').show(); })
-$("#albums_add").click(function() { var user = $(this); var user_id = user.data("uuid"); $('#photo_add_loader').html('').load("/gallery/user/add_album/" + user_id + "/"); $('.add_fullscreen').show(); })
-$('.add_fullscreen_hide').on('click', function() { $('.add_fullscreen').hide(); $('#photo_loader').empty(); });
-$('.photo_fullscreen_hide').on('click', function() { $('.photo_fullscreen').hide(); $('#photo_loader').empty(); });
+$('#ajax').on('click', '.photos_add', function() { $('#photos_add_window').show(); })
+$('#ajax').on('click', '.albums_add', function() { var user = $(this); var user_id = user.data("uuid"); $('#photo_add_loader').html('').load("/gallery/user/add_album/" + user_id + "/"); $('.add_fullscreen').show(); })
+$('#ajax').on('click', '.add_fullscreen_hide', function() { $('.add_fullscreen').hide(); $('#photo_loader').empty(); });
+$('#ajax').on('click', '.photo_fullscreen_hide', function() { $('.photo_fullscreen').hide(); $('#photo_loader').empty(); });
