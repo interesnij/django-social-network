bae3b58d5281fbab9d0b9c1e73d4b78e50a521c9
Serg <ochkarik1983@mail.ru@>
2020-01-21 18:38:48 +0300

    J

--- static/scripts/my/gallery.js
+++ static/scripts/my/gallery.js
@@ -10,3 +10,8 @@ $('.album-container').on('click', '.album_photo_detail', function() {
     $('#photo_loader').html('').load("/gallery/load/u_photo/" + pk + "/" + uuid + "/" + uuid2 + "/")
     $('.photo_fullscreen').show();
 });
+
+$("#photos_add").click(function() { $('#photos_add_window').show(); })
+$("#albums_add").click(function() { var user = $(this); var user_id = user.data("uuid"); $('#photo_add_loader').html('').load("/gallery/user/add_album/" + user_id + "/"); $('.add_fullscreen').show(); })
+$('.add_fullscreen_hide').on('click', function() { $('.add_fullscreen').hide(); $('#photo_loader').empty(); });
+$('.photo_fullscreen_hide').on('click', function() { $('.photo_fullscreen').hide(); $('#photo_loader').empty(); });
