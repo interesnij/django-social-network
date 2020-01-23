e37168c2efc5a726af00b88d7af0fb1f76832f1b
Serg <ochkarik1983@mail.ru@>
2020-01-23 12:12:42 +0300

    J

--- gallery/models.py
+++ gallery/models.py
@@ -123,7 +123,9 @@ class Photo(models.Model):
         return self.get_comment_replies_for_comment_with_post(post_comment=post_comment)
 
     def is_avatar(self):
-        if self.album_2.exists():
+        if self.album_2:
+            last = self.album_2.last()
+            if self.pk == last.pk
             return True
         else:
             return False
