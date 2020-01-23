625eec5cf3d46de47c20bb1f72bd7e1af0101254
Serg <ochkarik1983@mail.ru@>
2020-01-23 11:45:03 +0300

    J

--- static/scripts/my/gallery.js
+++ static/scripts/my/gallery.js
@@ -21,25 +21,6 @@ $('#ajax').on('click', '.u_photo_edit', function() {
   $('#block_description_form').show();
 });
 
-
-
- $('#ajax').on('click', '#user_photo_remove', function() {
-     button = $(this);
-     display = button.parents(".data_display");
-     remove_block = button.parent();
-     pk = display.data('pk');
-     uuid = display.data('uuid');
-     $.ajax({
-         url: '/gallery/user_progs/delete/' + pk + "/" + uuid + "/",
-         success: function(data) {
-           remove_block.empty().append("<span style='cursor:pointer;color:rgba(0, 0, 0, 1);' id='user_photo_abort_remove'>Восстановить</span>");
-           display.addClass("style_removed_object");
-           $.toast({heading: 'Информация',text: 'Фотография успешно удалена',showHideTransition: 'fade',icon: 'link'});
-         }});
-     return false;
- });
-
-
  $('#ajax').on('click', '.u_photo_off_comment', function() {
      button = $(this);
      display = button.parents(".data_display");
@@ -122,6 +103,58 @@ $('#ajax').on('click', '.u_photo_edit', function() {
          success: function(data) {
            remove_block.empty().append('<span style="cursor:pointer" class="u_unset_avatar">Убрать аватар</span>');
            $.toast({heading: 'Информация',text: 'Аватар успешно обновился',showHideTransition: 'fade',icon: 'link'});
+         }});
+     return false;
+ });
+
+ $('#ajax').on('click', '#user_photo_abort_remove', function() {
+     button = $(this);
+     display = button.parents(".data_display");
+     remove_block = button.parent();
+     pk = display.data('pk');
+     uuid = display.data('uuid');
+     $.ajax({
+         url: '/gallery/user_progs/abort_delete/' + pk + "/" + uuid + "/",
+         success: function(data) {
+           remove_block.empty().append('<span  style="cursor:pointer" id="user_photo_remove">Удалить</span>');
+           remove_block.next().show();
+           display.removeClass("style_removed_object");
+           $.toast({heading: 'Информация',text: 'Фотография успешно восстановлена',showHideTransition: 'fade',icon: 'link'});
+         }});
+     return false;
+ });
+ $('#ajax').on('click', '#user_photo_remove', function() {
+     button = $(this);
+     display = button.parents(".data_display");
+     remove_block = button.parent();
+     pk = display.data('pk');
+     uuid = display.data('uuid');
+     $.ajax({
+         url: '/gallery/user_progs/delete/' + pk + "/" + uuid + "/",
+         success: function(data) {
+           remove_block.empty().append("<span style='cursor:pointer;color:rgba(0, 0, 0, 1);' id='user_photo_abort_remove'>Восстановить</span>");
+           remove_block.next().hide();
+           display.addClass("style_removed_object");
+           $.toast({heading: 'Информация',text: 'Фотография успешно удалена',showHideTransition: 'fade',icon: 'link'});
+         }});
+     return false;
+ });
+
+ $('#ajax').on('click', '#u_photo_description_btn', function() {
+     var button = $(this);
+     var form2 = button.parent().parent().parent();
+     display = button.parents(".data_display");
+     description_block = form2.parent().prev();
+     var pk = display.data('pk');
+     var uuid = display.data('uuid');
+     $.ajax({
+         url: '/gallery/user_progs/description/' + pk + "/" + uuid + "/",
+         data: form2.serialize(),
+         type: 'post',
+         success: function(data) {
+           description = $("#id_description").val();
+           description_block.empty().append(description + "<br><br><span style='cursor:pointer' class='u_photo_edit'>Редактировать</span>");
+           form2.parent().hide();
          }});
      return false;
  });
