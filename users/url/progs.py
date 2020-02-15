b273a01adb0768bcab77e85906e4e89d5b72abc0
Serg <ochkarik1983@mail.ru@>
2020-02-15 15:09:11 +0300

    J

--- users/url/progs.py
+++ users/url/progs.py
@@ -5,5 +5,5 @@ from users.views.progs import UserBanCreate, UserUnbanCreate, UserColorChange
 urlpatterns = [
     url(r'^block/(?P<pk>\d+)/$', UserBanCreate.as_view()),
     url(r'^unblock/(?P<pk>\d+)/$', UserUnbanCreate.as_view()),
-    url(r'^color/(?P<color>\[a-w]+)/$', UserColorChange.as_view()),
+    url(r'^color/<color>/$', UserColorChange.as_view()),
 ]
