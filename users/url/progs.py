32c616beea3182e2e9a246b500c15362bd585a95
Serg <ochkarik1983@mail.ru@>
2020-02-15 14:28:46 +0300

    J

--- users/url/progs.py
+++ users/url/progs.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from users.views.progs import UserBanCreate, UserUnbanCreate
+from users.views.progs import UserBanCreate, UserUnbanCreate, UserColorChange
 
 
 urlpatterns = [
     url(r'^block/(?P<pk>\d+)/$', UserBanCreate.as_view()),
     url(r'^unblock/(?P<pk>\d+)/$', UserUnbanCreate.as_view()),
+    url(r'^color/(?P<color>\[a-w]+)/$', UserColorChange.as_view()),
 ]
--- users/views/progs.py
+++ users/views/progs.py
@@ -1,7 +1,7 @@
 from django.template.loader import render_to_string
 from django.views import View
 from common.checkers import check_is_not_blocked_with_user_with_id, check_is_connected_with_user_with_id
-from users.models import User
+from users.models import User, UserColorSettings
 from django.http import HttpResponse
 
 
@@ -17,3 +17,15 @@ class UserUnbanCreate(View):
         self.user = User.objects.get(pk=self.kwargs["pk"])
         request.user.unblock_user_with_pk(self.user.pk)
         return HttpResponse('Пользователь разблокирован')
+
+
+class UserColorChange(View): 
+    def get(self,request,*args,**kwargs):
+        model = UserColorSettings.objects.get(user=request.user, color=self.kwargs["color"])
+        color = self.kwargs["color"]
+        if model.color == color:
+            return HttpResponse('Этот цвет уже выбран')
+        else:
+            model.color = color
+            model.save(update_fields=['color'])
+            return HttpResponse('Цвет выбран')
