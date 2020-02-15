2d56c7556e15b6c2452c13cbb5a6c9782eefab9c
Serg <ochkarik1983@mail.ru@>
2020-02-15 11:52:25 +0300

    J

--- users/admin.py
+++ users/admin.py
@@ -11,12 +11,16 @@ class UserNotificationsSettingsInline(admin.TabularInline):
 class UserPrivateSettingsInline(admin.TabularInline):
     model = UserPrivateSettings
 
+class UserColorSettingsInline(admin.TabularInline):
+    model = UserColorSettings
+
 
 class UserAdmin(admin.ModelAdmin):
     inlines = [
         UserProfileInline,
         UserNotificationsSettingsInline,
         UserPrivateSettingsInline,
+        UserColorSettingsInline,
     ]
     search_fields = ('last_name','first_name')
 
