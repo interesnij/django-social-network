d54745f1c5619431ac200c4eee4f890c26ba0f8e
Serg <ochkarik1983@mail.ru@>
2020-02-17 10:37:32 +0300

    J

--- static/scripts/post.js
+++ static/scripts/post.js
@@ -6,7 +6,7 @@
       like = $(this); item = like.parents('.infinite-item');pk = item.attr("user-id");uuid = item.attr("item-id");dislike = like.next().next();
       $.ajax({url: "/votes/user_like/" + uuid + "/" + pk + "/",type: 'POST',data: {'obj': pk},
           success: function(json) {
-              like.find("[data-count='like']").text(json.like_count); like.toggleClass('btn_success'); like.next().html('').load("/window/u_like_window/" + uuid + "/" + pk + "/");
+              like.find("[data-count='like']").text(json.like_count); like.toggleClass('btn_success btn_default'); like.next().html('').load("/window/u_like_window/" + uuid + "/" + pk + "/");
               dislike.find("[data-count='dislike']").text(json.dislike_count); dislike.removeClass('btn_danger'); dislike.next().html('').load("/window/u_dislike_window/" + uuid + "/" + pk + "/")
           }
       });return false;
@@ -17,7 +17,7 @@
               url: "/votes/user_dislike/" + uuid + "/" + pk + "/", type: 'POST', data: {'obj': pk},
               success: function(json) {
                 like.find("[data-count='like']").text(json.like_count); like.removeClass('btn_success'); like.removeClass('btn_success'); like.next().html('').load("/window/u_like_window/" + uuid + "/" + pk + "/");
-                dislike.find("[data-count='dislike']").text(json.dislike_count); dislike.toggleClass('btn_danger'); dislike.next().html('').load("/window/u_dislike_window/" + uuid + "/" + pk + "/")
+                dislike.find("[data-count='dislike']").text(json.dislike_count); dislike.toggleClass('btn_danger btn_default'); dislike.next().html('').load("/window/u_dislike_window/" + uuid + "/" + pk + "/")
               }
           });return false;
   });
