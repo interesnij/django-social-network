201005c9cff299e7234cc697378e7690793d9339
Serg <ochkarik1983@mail.ru@>
2020-02-18 21:24:55 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,18 +1,8 @@
 "use strict";
 $(document).ready(function () {
 
-    $('#left-menu').on('click', function () {
-        $('body').toggleClass('sidebar-left-close');
-    });
-    $('.sidebar-left + div.backdrop').on('click', function () {
-        $('body').addClass('sidebar-left-close');
-    });
 
-    if ($(window).width() <= 1100) {
-        $('body').addClass('sidebar-left-close');
-    }
 });
 
 
   $.ajaxPrefilter(function( options, original_Options, jqXHR ) {options.async = true;});
-  
