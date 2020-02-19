d49db589ee2a34b7e12d46bd03920f73aa06d3ee
Serg <ochkarik1983@mail.ru@>
2020-02-19 18:54:53 +0300

    J

--- communities/views/progs.py
+++ communities/views/progs.py
@@ -34,7 +34,7 @@ class CommunityCreate(TemplateView):
 			new_community=self.form.save(commit=False)
 			community = Community.create_community(name=new_community.name,category=new_community.category,type=new_community.type,creator=request.user)
 			return HttpResponse("!")
-		return super(CommunityCreate,self).get(request,*args,**kwargs)
+		return super(CommunityCreate,self).post(request,*args,**kwargs)
 
 
 class CommunitiesCatsView(TemplateView):
