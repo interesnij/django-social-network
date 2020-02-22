db6477e0cb51090800530a6e35294b5c88e7fdd6
Serg <ochkarik1983@mail.ru@>
2020-02-22 16:55:31 +0300

    J

--- main/views.py
+++ main/views.py
@@ -23,6 +23,12 @@ class NewsListView(ListView):
 	template_name="news_list.html"
 	model=Item
 	paginate_by=16
+	self.page = request.GET.get('page')
+
+	def get_context_data(self,**kwargs):
+		context=super(NewsListView,self).get_context_data(**kwargs)
+		context["page"]=self.page
+		return context
 
 	def get_queryset(self):
 		self.user=self.request.user
