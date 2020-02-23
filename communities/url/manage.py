ec8ebf71a2f26f3fd3d05b318bbb324207c58636
Serg <ochkarik1983@mail.ru@>
2020-02-23 16:27:48 +0300

    J

--- communities/url/manage.py
+++ communities/url/manage.py
@@ -11,13 +11,13 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/settings_notify/$', login_required(CommunityNotifyView.as_view()), name='community_notify'),
     url(r'^(?P<pk>[0-9]+)/settings_private/$', login_required(CommunityPrivateView.as_view()), name='community_private'),
 
-    url(r'^(?P<pk>[0-9]+)/admins/$', login_required(CommunityAdminView.as_view()), name='community_admins'),
-    url(r'^(?P<pk>[0-9]+)/moders/$', login_required(CommunityModersView.as_view()), name='community_moders'),
-    url(r'^(?P<pk>[0-9]+)/black_list/$', login_required(CommunityBlackListView.as_view()), name='community_black_list'),
-    url(r'^(?P<pk>[0-9]+)/follows/$', login_required(CommunityFollowsView.as_view()), name='community_follows'),
-    url(r'^(?P<pk>[0-9]+)/members/$', login_required(CommunityMemberManageView.as_view()), name='community_member_manage'),
-    url(r'^(?P<pk>[0-9]+)/editors/$', login_required(CommunityEditorsView.as_view()), name='community_editors'),
-    url(r'^(?P<pk>[0-9]+)/rekl/$', login_required(CommunityAdvertisersView.as_view()), name='community_advertisers'),
+    url(r'^admins/(?P<pk>[0-9]+)/$', login_required(CommunityAdminView.as_view()), name='community_admins'),
+    url(r'^moders/(?P<pk>[0-9]+)/$', login_required(CommunityModersView.as_view()), name='community_moders'),
+    url(r'^black_list/(?P<pk>[0-9]+)/$', login_required(CommunityBlackListView.as_view()), name='community_black_list'),
+    url(r'^follows/(?P<pk>[0-9]+)/$', login_required(CommunityFollowsView.as_view()), name='community_follows'),
+    url(r'^members/(?P<pk>[0-9]+)/$', login_required(CommunityMemberManageView.as_view()), name='community_member_manage'),
+    url(r'^editors/(?P<pk>[0-9]+)/$', login_required(CommunityEditorsView.as_view()), name='community_editors'),
+    url(r'^rekl/(?P<pk>[0-9]+)/$', login_required(CommunityAdvertisersView.as_view()), name='community_advertisers'),
 
     url(r'^staff_window/(?P<pk>[0-9]+)/(?P<uuid>[0-9a-f-]+)/$', login_required(CommunityStaffWindow.as_view())),
 ]
