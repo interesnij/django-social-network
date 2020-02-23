c445dbbce3a74d621c29b865b07af7461ab7032e
Serg <ochkarik1983@mail.ru@>
2020-02-23 15:06:56 +0300

    J

--- users/views/load.py
+++ users/views/load.py
@@ -20,26 +20,26 @@ class UserPlaylistLoad(TemplateView):
 
 
 class UserImagesLoad(View):
-	def get(self,request,**kwargs):
-		context = {}
-		self.user = User.objects.get(uuid=self.kwargs["uuid"])
-		if self.user != request.user and request.user.is_authenticated:
-			check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
-			if self.user.is_closed_profile():
-				check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
-			photo_list = self.user.get_photos().order_by('-created')
-		elif self.user == request.user and self.user.is_authenticated:
-			photo_list = self.user.get_photos().order_by('-created')
+    def get(self,request,**kwargs):
+        context = {}
+        self.user = User.objects.get(uuid=self.kwargs["uuid"])
+        if self.user != request.user and request.user.is_authenticated:
+            check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
+            if self.user.is_closed_profile():
+                check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
+            photo_list = self.user.get_photos().order_by('-created')
+        elif self.user == request.user and self.user.is_authenticated:
+            photo_list = self.user.get_photos().order_by('-created')
         current_page = Paginator(photo_list, 15)
-		page = request.GET.get('page')
-		context['user'] = self.user
-		try:
-			context['photo_list'] = current_page.page(page)
-		except PageNotAnInteger:
-			context['photo_list'] = current_page.page(1)
-		except EmptyPage:
-			context['photo_list'] = current_page.page(current_page.num_pages)
-		return render_to_response('photos_load.html', context)
+        page = request.GET.get('page')
+        context['user'] = self.user
+        try:
+            context['photo_list'] = current_page.page(page)
+        except PageNotAnInteger:
+            context['photo_list'] = current_page.page(1)
+        except EmptyPage:
+            context['photo_list'] = current_page.page(current_page.num_pages)
+        return render_to_response('photos_load.html', context)
 
 
 class ProfileReload(TemplateView):
