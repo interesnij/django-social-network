eda0a28791dff3a7c2e594b319533d47b3060cde
Serg <ochkarik1983@mail.ru@>
2020-02-24 13:14:28 +0300

    J

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ class MainPageView(TemplateView):
 class NewsListView(ListView):
 	template_name="news_list.html"
 	model=Item
-	paginate_by=30
+	paginate_by=8
 
 	def get_queryset(self):
 		self.user=self.request.user
