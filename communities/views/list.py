ba2c40c555748ce139ad44152599143ccbce14d2
Serg <ochkarik1983@mail.ru@>
2020-02-25 16:01:36 +0300

    J

--- communities/views/list.py
+++ communities/views/list.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from communities.models import Community, CommunityMembership
+from communities.models import Community, CommunityMembership, CommunityCategory
 from users.models import User
 
 
