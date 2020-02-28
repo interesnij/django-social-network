e0e511885fd470b5296a168058c8f222f24c9a1a
Serg <ochkarik1983@mail.ru@>
2020-02-28 07:43:51 +0300

    D

--- users/models.py
+++ users/models.py
@@ -649,11 +649,11 @@ class User(AbstractUser):
 
     def get_client_ip(request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
 
 
 class UserProfile(models.Model):
