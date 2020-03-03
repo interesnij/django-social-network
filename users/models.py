c2e92520c6c8500935b3c66c1ebac3e4c290424c
Serg <ochkarik1983@mail.ru@>
2020-03-03 20:27:00 +0300

    J

--- users/models.py
+++ users/models.py
@@ -410,6 +410,17 @@ class User(AbstractUser):
         photos_query.add(exclude_reported_and_approved_photos_query, Q.AND)
         photos = Photo.objects.filter(photos_query)
         return photos
+
+    def get_profile_photos(self):
+        from gallery.models import Photo
+        from moderation.models import ModeratedObject
+
+        photos_query = Q(creator_id=self.id, is_deleted=False, is_public=True, community=None)
+        exclude_reported_and_approved_photos_query = ~Q(moderated_object__status=ModeratedObject.STATUS_APPROVED)
+        photos_query.add(exclude_reported_and_approved_photos_query, Q.AND)
+        photos = Photo.objects.filter(photos_query)
+        return photos[0:5]
+        
     def get_my_photos(self):
         from gallery.models import Photo
         from moderation.models import ModeratedObject
