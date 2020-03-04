18bb378f4cba30f6b2d9f0ffe65b9e199270970b
Serg <ochkarik1983@mail.ru@>
2020-03-04 18:29:53 +0300

    J

--- users/models.py
+++ users/models.py
@@ -368,7 +368,7 @@ class User(AbstractUser):
     def get_pop_communities(self):
         from communities.models import Community
 
-        query = Community.objects.filter(communities_memberships__pk=self.pk)
+        query = Community.objects.filter(memberships__pk=self.pk)
         return query[0:6]
 
     def get_online_connection_count(self):
