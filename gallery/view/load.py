41a4f35ef2e39f019cee01202ac1c5e63e5d987d
Serg <ochkarik1983@mail.ru@>
2020-03-05 17:12:30 +0300

    J

--- gallery/view/load.py
+++ gallery/view/load.py
@@ -60,9 +60,9 @@ class UserAlbumPhoto(TemplateView):
     def get_context_data(self,**kwargs):
         context=super(UserAlbumPhoto,self).get_context_data(**kwargs)
         context["object"]=self.photo
-        context["photos"]=self.photos
+        context["album"]=self.album
         context["user"]=self.user
-        context["next"]=self.next 
+        context["next"]=self.next
         context["prev"]=self.prev
         context["avatar"]=self.avatar
         context["form_image"]=self.form_image
