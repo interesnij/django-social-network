3e4ff7d010f032dfff16c6d7f3c9dd8f5c5afc8a
Serg <ochkarik1983@mail.ru@>
2020-03-05 13:59:50 +0300

    J

--- goods/view/user.py
+++ goods/view/user.py
@@ -3,14 +3,9 @@ from goods.models import Good, GoodSubCategory, GoodCategory
 from users.models import User
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from goods.forms import GoodForm
-from django.template.loader import render_to_string
-from django.views.generic.detail import DetailView
 from common.checkers import check_is_not_blocked_with_user_with_id, check_is_connected_with_user_with_id
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views import View
 from django.shortcuts import render_to_response
 from rest_framework.exceptions import PermissionDenied
-from common.utils import is_mobile
 
 
 class UserGoods(TemplateView):
@@ -26,33 +21,24 @@ class UserGoods(TemplateView):
         context["user"]=self.user
         return context
 
+class UserGoodsList(ListView):
+	template_name = None
+    model = Good
+	paginate_by = 30
 
-class UserGoodsList(View):
-    def get(self,request,**kwargs):
-        context = {}
-        self.user=User.objects.get(pk=self.kwargs["pk"])
-        if self.user != request.user and request.user.is_authenticated:
-            check_is_not_blocked_with_user_with_id(user=request.user, user_id=self.user.id)
-            if self.user.is_closed_profile():
-                check_is_connected_with_user_with_id(user=request.user, user_id=self.user.id)
-            goods_list = self.user.get_goods().order_by('-created')
-        elif self.user == request.user:
-            goods_list = self.user.get_my_goods().order_by('-created')
-        elif request.user.is_anonymous and self.user.is_closed_profile():
-            raise PermissionDenied('Это закрытый профиль. Только его друзья могут видеть его информацию.')
-        elif request.user.is_anonymous and not self.user.is_closed_profile():
-            goods_list = self.user.get_goods().order_by('-created')
-        current_page = Paginator(goods_list, 10)
+	def get(self,request,*args,**kwargs):
+		self.user = User.objects.get(uuid=self.kwargs["uuid"])
+		self.template_name = self.user.get_permission_list_user(folder="good_user/", template="list.html", request=request)
+		return super(UserGoodsList,self).get(request,*args,**kwargs)
 
-        page = request.GET.get('page')
-        context['user'] = self.user
-        try:
-            context['goods_list'] = current_page.page(page)
-        except PageNotAnInteger:
-            context['goods_list'] = current_page.page(1)
-        except EmptyPage:
-            context['goods_list'] = current_page.page(current_page.num_pages)
-        return render_to_response('good_user/goods_list.html', context)
+	def get_context_data(self,**kwargs):
+		context = super(UserGoodsList,self).get_context_data(**kwargs)
+		context['user'] = self.user
+		return context
+
+	def get_queryset(self):
+		goods_list = self.user.get_goods().order_by('-created')
+		return goods_list
 
 
 class UserGood(TemplateView):
