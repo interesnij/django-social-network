2f0c13fff5b655b7e48be3e9f025f25fce5431d7
Serg <ochkarik1983@mail.ru@>
2020-03-05 14:30:29 +0300

    J

--- goods/url/user.py
+++ goods/url/user.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 
 urlpatterns=[
-	url(r'^goods/(?P<pk>\d+)/$', UserGoodsList.as_view(), name="user_goods_list"),
+	url(r'^goods/(?P<uuid>[0-9a-f-]+)/$', UserGoodsList.as_view(), name="user_goods_list"),
 	url(r'^(?P<pk>\d+)/$', UserGoods.as_view(), name="user_goods"),
 	url(r'^good/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserGood.as_view(), name='user_good'),
 	url(r'^add/(?P<pk>\d+)/$', GoodUserCreate.as_view(), name="good_add_user"),
