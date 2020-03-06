58fd0514d854cd99252a24481eb9728fd1f91e68
Serg <ochkarik1983@mail.ru@>
2020-03-06 11:51:04 +0300

    F

--- users/models.py
+++ users/models.py
@@ -236,7 +236,7 @@ class User(AbstractUser):
         return self.created_communities.filter(name=community_name).exists()
 
     def is_staffed_user(self):
-        return self.communities_memberships.filter(Q(memberships__user=self, memberships__is_administrator=True) | Q(memberships__user=self, memberships__is_moderator=True) | Q(memberships__user=self, memberships__is_editor=True)).exists()
+        return self.communities_memberships.filter(Q(is_administrator=True) | Q(is_moderator=True) | Q(is_editor=True)).exists()
 
     def is_staff_of_community_with_name(self, community_name):
         return self.is_administrator_of_community_with_name(community_name=community_name) or self.is_moderator_of_community_with_name(community_name=community_name) or self.is_editor_of_community_with_name(community_name=community_name) or self.is_advertiser_of_community_with_name(community_name=community_name)
