1c90901c84928b21beaa8c1ba7955d72f70f1de9
Serg <ochkarik1983@mail.ru@>
2020-03-08 15:32:09 +0300

    J

--- stst/admin.py
+++ stst/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from stst.models import UserNumbers, CommuityNumbers, GoodNumbers, VideoNumbers, MusicNumbers, ItemNumbers
 
-# Register your models here.
+
+admin.site.register(UserNumbers)
+admin.site.register(CommuityNumbers)
+admin.site.register(GoodNumbers)
+admin.site.register(VideoNumbers)
+admin.site.register(MusicNumbers)
+admin.site.register(ItemNumbers)
