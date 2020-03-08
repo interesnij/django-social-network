0b937f67fca42fe79d0869bb287c8020ef4ed511
Serg <ochkarik1983@mail.ru@>
2020-03-08 17:09:25 +0300

    J

--- stst/views.py
+++ stst/views.py
@@ -15,21 +15,72 @@ class StatView(TemplateView):
         except:
             self.olds_ip = IPUser.objects.create(user=self.user)
 
-        if olds_ip.ip_3 and self.olds_ip.ip_1 != self.ip:
+        if not olds_ip.ip_1:
             self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
             self.data = self.response.json()
-            loc = OneUserLocation.objects.get(user=self.user)
-            loc.sity_ru = self.data.sity.name_ru
-            loc.sity_en = self.data.sity.name_en
-            loc.region_ru = self.data.region.name_ru
-            loc.region_en = self.data.region.name_en
-            loc.country_ru = self.data.sity.country_ru
-            loc.country_en = self.data.sity.country_en
+            try:
+                self.loc = OneUserLocation.objects.get(user=self.user)
+            except:
+                self.loc = OneUserLocation.objects.create(user=self.user)
+            self.loc.sity_ru = self.data.sity.name_ru
+            self.loc.sity_en = self.data.sity.name_en
+            self.loc.region_ru = self.data.region.name_ru
+            self.loc.region_en = self.data.region.name_en
+            self.loc.country_ru = self.data.sity.country_ru
+            self.loc.country_en = self.data.sity.country_en
+            self.loc.save()
+        elif not olds_ip.ip_2 and self.olds_ip.ip_3 != self.ip and self.olds_ip.ip_2 != self.ip and self.olds_ip.ip_1 != self.ip:
+            self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
+            self.data = self.response.json()
+            try:
+                self.loc = TwoUserLocation.objects.get(user=self.user)
+            except:
+                self.loc = TwoUserLocation.objects.create(user=self.user)
+            self.loc.sity_ru = self.data.sity.name_ru
+            self.loc.sity_en = self.data.sity.name_en
+            self.loc.region_ru = self.data.region.name_ru
+            self.loc.region_en = self.data.region.name_en
+            self.loc.country_ru = self.data.sity.country_ru
+            self.loc.country_en = self.data.sity.country_en
+            self.loc.save()
+        elif not olds_ip.ip_3 and self.olds_ip.ip_3 != self.ip and self.olds_ip.ip_2 != self.ip and self.olds_ip.ip_1 != self.ip:
+            self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
+            self.data = self.response.json()
+            try:
+                self.loc = ThreeUserLocation.objects.get(user=self.user)
+            except:
+                self.loc = ThreeUserLocation.objects.create(user=self.user)
+            self.loc.sity_ru = self.data.sity.name_ru
+            self.loc.sity_en = self.data.sity.name_en
+            self.loc.region_ru = self.data.region.name_ru
+            self.loc.region_en = self.data.region.name_en
+            self.loc.country_ru = self.data.sity.country_ru
+            self.loc.country_en = self.data.sity.country_en
+            self.loc.save()
+        elif olds_ip.ip_3 and self.olds_ip.ip_3 != self.ip and self.olds_ip.ip_2 != self.ip and self.olds_ip.ip_1 != self.ip::
+            self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
+            self.data = self.response.json()
+            try:
+                self.loc = ThreeUserLocation.objects.get(user=self.user)
+            except:
+                self.loc = ThreeUserLocation.objects.create(user=self.user)
+            self.loc.ip_1 = self.ip
+            self.loc.ip_2 = None
+            self.loc.ip_3 = None
+            self.loc.sity_ru = self.data.sity.name_ru
+            self.loc.sity_en = self.data.sity.name_en
+            self.loc.region_ru = self.data.region.name_ru
+            self.loc.region_en = self.data.region.name_en
+            self.loc.country_ru = self.data.sity.country_ru
+            self.loc.country_en = self.data.sity.country_en
+            self.loc.save()
+        else:
+            pass
 
         return super(StatView,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
         context=super(StatView,self).get_context_data(**kwargs)
         context["ip"]=self.ip
-        context["data"]=self.data
+        context["data"]=self.loc
         return context
