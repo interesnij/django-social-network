8b604306fab3ceffa9e46754c70a78510aa1b99e
Serg <ochkarik1983@mail.ru@>
2020-03-08 17:28:01 +0300

    J

--- stst/views.py
+++ stst/views.py
@@ -18,7 +18,7 @@ class StatView(TemplateView):
         if not self.olds_ip.ip_1:
             self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
             self.js = self.response.json()
-            self.data = json.loads(self.js)
+            self.data = json.dumps(response["city"])
             try:
                 self.loc = OneUserLocation.objects.get(user=self.user)
             except:
