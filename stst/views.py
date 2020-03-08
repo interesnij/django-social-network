87bb144416940775b52091f0e3b83079ad656fbb
Serg <ochkarik1983@mail.ru@>
2020-03-08 18:14:25 +0300

    J

--- stst/views.py
+++ stst/views.py
@@ -24,12 +24,14 @@ class StatView(TemplateView):
             except:
                 self.loc = OneUserLocation.objects.create(user=self.user)
             self.sity = self.data['city']
+            self.region = self.data['region']
+            self.country = self.data['country']
             self.loc.city_ru = self.sity['city_ru']
-            self.loc.city_en = self.data.get('sity_en', None)
-            self.loc.region_ru = self.data.get('region_ru', None)
-            self.loc.region_en = self.data.get('region_en', None)
-            self.loc.country_ru = self.data.get('country_ru', None)
-            self.loc.country_en = self.data.get('country_en', None)
+            self.loc.city_en = self.sity.['city_en']
+            self.loc.region_ru = self.region.['region_ru']
+            self.loc.region_en = self.region.['region_en']
+            self.loc.country_ru = self.country.['country_ru']
+            self.loc.country_en = self.country.['country_en']
             self.loc.save()
         elif not self.olds_ip.ip_2 and self.olds_ip.ip_3 != self.ip and self.olds_ip.ip_2 != self.ip and self.olds_ip.ip_1 != self.ip:
             self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
@@ -38,12 +40,12 @@ class StatView(TemplateView):
                 self.loc = TwoUserLocation.objects.get(user=self.user)
             except:
                 self.loc = TwoUserLocation.objects.create(user=self.user)
-            self.loc.sity_ru = self.data.sity.name_ru
-            self.loc.sity_en = self.data.sity.name_en
+            self.loc.city_ru = self.data.city.name_ru
+            self.loc.city_en = self.data.city.name_en
             self.loc.region_ru = self.data.region.name_ru
             self.loc.region_en = self.data.region.name_en
-            self.loc.country_ru = self.data.sity.country_ru
-            self.loc.country_en = self.data.sity.country_en
+            self.loc.country_ru = self.data.city.country_ru
+            self.loc.country_en = self.data.city.country_en
             self.loc.save()
         elif not self.olds_ip.ip_3 and self.olds_ip.ip_3 != self.ip and self.olds_ip.ip_2 != self.ip and self.olds_ip.ip_1 != self.ip:
             self.response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + self.ip)
@@ -52,8 +54,8 @@ class StatView(TemplateView):
                 self.loc = ThreeUserLocation.objects.get(user=self.user)
             except:
                 self.loc = ThreeUserLocation.objects.create(user=self.user)
-            self.loc.sity_ru = self.data.sity.name_ru
-            self.loc.sity_en = self.data.sity.name_en
+            self.loc.city_ru = self.data.city.name_ru
+            self.loc.city_en = self.data.sity.name_en
             self.loc.region_ru = self.data.region.name_ru
             self.loc.region_en = self.data.region.name_en
             self.loc.country_ru = self.data.sity.country_ru
