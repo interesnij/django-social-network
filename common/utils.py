b86d783f0c3744e87852c846fdf67bfaed2bac56
Serg <ochkarik1983@mail.ru@>
2020-03-08 20:42:44 +0300

    J

--- common/utils.py
+++ common/utils.py
@@ -24,3 +24,90 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+def get_location(request):
+    import json, requests
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    try:
+        olds_ip = request.user.user_ip
+    except:
+        olds_ip = IPUser.objects.create(user=request.user)
+
+    if not olds_ip.ip_1:
+        data = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + ip)
+        try:
+            loc = request.user.user_location
+        except:
+            loc = OneUserLocation.objects.create(user=request.user)
+        sity = data['city']
+        region = data['region']
+        country = data['country']
+        loc.city_ru = sity['name_ru']
+        loc.city_en = sity['name_en']
+        loc.region_ru = region['name_ru']
+        loc.region_en = region['name_en']
+        loc.country_ru = country['name_ru']
+        loc.country_en = country['name_en']
+        olds_ip.ip_1 = ip
+        olds_ip.save()
+        loc.save()
+    elif not olds_ip.ip_2 and olds_ip.ip_3 != ip and olds_ip.ip_2 != ip and olds_ip.ip_1 != ip:
+        response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + ip)
+        data = response.json()
+        try:
+            loc = request.user.user_location_2
+        except:
+            loc = TwoUserLocation.objects.create(user=self.user)
+        sity = data['city']
+        region = data['region']
+        country = data['country']
+        loc.city_ru = sity['name_ru']
+        loc.city_en = sity['name_en']
+        loc.region_ru = region['name_ru']
+        loc.region_en = region['name_en']
+        loc.country_ru = country['name_ru']
+        loc.country_en = country['name_en']
+        olds_ip.ip_2 = ip
+        olds_ip.save()
+        loc.save()
+    elif not olds_ip.ip_3 and olds_ip.ip_3 != ip and olds_ip.ip_2 != ip and olds_ip.ip_1 != ip:
+        response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + ip)
+        data = response.json()
+        try:
+            loc = request.user.user_location_3
+        except:
+            loc = ThreeUserLocation.objects.create(user=self.user)
+        sity = data['city']
+        region = data['region']
+        country = data['country']
+        loc.city_ru = sity['name_ru']
+        loc.city_en = sity['name_en']
+        loc.region_ru = region['name_ru']
+        loc.region_en = region['name_en']
+        loc.country_ru = country['name_ru']
+        loc.country_en = country['name_en']
+        olds_ip.ip_3 = ip
+        olds_ip.save()
+        loc.save()
+    elif olds_ip.ip_3 and olds_ip.ip_3 != ip and olds_ip.ip_2 != ip and olds_ip.ip_1 != ip:
+        response = requests.get(url= "http://api.sypexgeo.net/8Dbm8/json/" + ip)
+        data = response.json()
+        sity = data['city']
+        region = data['region']
+        country = data['country']
+        loc.city_ru = sity['name_ru']
+        loc.city_en = sity['name_en']
+        loc.region_ru = region['name_ru']
+        loc.region_en = region['name_en']
+        loc.country_ru = country['name_ru']
+        loc.country_en = country['name_en']
+        olds_ip.ip_1 = ip
+        olds_ip.save()
+        loc.save()
+    else:
+        pass
