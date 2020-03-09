002d7d0704439ed21c2509b5e18f1e2f57d1c90f
Serg <ochkarik1983@mail.ru@>
2020-03-09 12:31:39 +0300

    J

--- users/templatetags/visitors.py
+++ users/templatetags/visitors.py
@@ -9,10 +9,11 @@ def count_visitor_for_user(user, user_id):
 @register.filter
 def count_visitor_pluralize(value, arg="дурак,дурака"):
     args = arg.split(",")
-    a = int(value) % 10
-    b = int(value) % 100 
-
+    a = value % 10
+    b = value % 100
     if (a == 1) and (b != 11):
         return args[0]
     elif (a >= 2) and (a <= 4) and ((b < 10) or (b >= 20)):
         return args[1]
+    else:
+        pass 
