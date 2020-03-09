c86c1a669737992e0ae169baeb338bd187b503a1
Serg <ochkarik1983@mail.ru@>
2020-03-09 14:12:27 +0300

    J

--- users/models.py
+++ users/models.py
@@ -891,9 +891,9 @@ class User(AbstractUser):
     def get_visited_for_user(self):
         from stst.models import UserNumbers
 
-        v_s = UserNumbers.objects.filter(target=self.pk).values('visitor')
-        query = Q(id__in=v_s)
-        visitors = User.objects.filter(query).order_by("pk")
+        v_s = UserNumbers.objects.filter(target=self.pk).values('visitor').order_by("pk")
+        #query = Q(id__in=v_s)
+        visitors = User.objects.filter(id__in=v_s)
         return visitors
 
     def get_unical_visited_for_user(self):
