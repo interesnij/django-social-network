740543f40522caca494bdc9a9a89ab98f1347eca
Serg <ochkarik1983@mail.ru@>
2020-03-09 09:44:00 +0300

    J

--- users/models.py
+++ users/models.py
@@ -886,8 +886,8 @@ class User(AbstractUser):
         from stst.models import UserNumbers
         v_s = UserNumbers.objects.filter(target=self.pk).values('visitor')
         query = Q(id__in=v_s)
-        visitors = User.objects.filter(query)
-        return visitors.reverse()
+        visitors = User.objects.filter(query).order_by(v_s)
+        return visitors
 
 
     def get_count_visitor_for_user(self, user_id):
