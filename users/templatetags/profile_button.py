9e26d3e048bc1f113be70d0319be649503988491
Serg <ochkarik1983@mail.ru@>
2020-03-10 13:42:16 +0300

    F

---
+++ users/templatetags/profile_button.py
@@ -0,0 +1,27 @@
+from django import template
+register=template.Library()
+
+
+@register.filter
+def is_blocked_user(user, request_user):
+    try:
+        request_user.is_authenticated and request_user.has_blocked_user_with_id(user)
+        return True
+    except:
+        return False
+
+@register.filter
+def is_request_follow(user, request_user):
+    try:
+        Follow.objects.get(followed_user=user,user=request_user)
+        return True
+    except:
+        return False
+
+@register.filter
+def is_user_follow(user, request_user):
+    try:
+        Follow.objects.get(followed_user=request_user,user=user)
+        return True
+    except:
+        return False
