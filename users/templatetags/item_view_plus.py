018b8af6e8a9df84b61823c1af952a994c9d7fd8
Serg <ochkarik1983@mail.ru@>
2020-03-11 10:10:07 +0300

    H

--- users/templatetags/item_view_plus.py
+++ users/templatetags/item_view_plus.py
@@ -4,6 +4,8 @@ register=template.Library()
 
 
 @register.filter
-def item_view_plus(item_id, user_id):
-    view = user.item_views_plus(item_id, user_id)
+def item_view_plus(user_id, item_id):
+    from common.utils import item_views_plus
+
+    view = user.item_views_plus(user_id, item_id)
     return HttpResponse('Программа сработала!')
