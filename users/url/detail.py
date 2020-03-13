56394f8d8e641df0c810d88e48e6a1031c735e8e
Serg <ochkarik1983@mail.ru@>
2020-03-13 12:48:12 +0300

    J

--- users/url/detail.py
+++ users/url/detail.py
@@ -4,7 +4,7 @@ from users.views.lists import *
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^item/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', UserItemView.as_view(), name='user_item'),
+    url(r'^item/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserItemView.as_view(), name='user_item'),
     url(r'^communities/(?P<uuid>[0-9a-f-]+)/$', UserCommunitiesList.as_view()),
     url(r'^visit_communities/(?P<pk>\d+)/$', UserVisitCommunities.as_view(), name='visit_communities'),
     url(r'^manage_communities/(?P<uuid>[0-9a-f-]+)/$', UserManageCommunitiesList.as_view()),
