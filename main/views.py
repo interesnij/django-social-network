8715fda11a11280431e10605b1bed24d9d3990a6
Serg <ochkarik1983@mail.ru@>
2020-03-14 09:37:13 +0300

    J

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
+from main.models import Item
 
 
-class MainPageView(TemplateView):
+class MainPageView(ListView):
 	template_name=None
+
 	def get(self,request,*args,**kwargs):
 		from common.utils import is_mobile
+
+		model=Item
+		paginate_by=30
 
 		if request.user.is_authenticated:
 			if is_mobile(request):
@@ -17,26 +22,11 @@ class MainPageView(TemplateView):
 				self.template_name="main/mob_auth.html"
 			else:
 				self.template_name="main/auth.html"
-		return super(MainPageView,self).get(request,*args,**kwargs)
+		return super(ListView,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
-		context=super(MainPageView,self).get_context_data(**kwargs)
+		context=super(ListView,self).get_context_data(**kwargs)
 		return context
-
-
-class NewsListView(ListView):
-	from main.models import Item
-
-	template_name="news_list.html"
-	model=Item
-	paginate_by=30
-
-	def get_queryset(self):
-		if self.request.user.is_authenticated:
-			items = self.request.user.get_timeline_posts().order_by('-created')
-		else:
-			items=None
-		return items
 
 
 class ComingView(TemplateView):
