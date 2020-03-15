44cb728a949412ac0a93b1574ff602ce39f02e08
Serg <ochkarik1983@mail.ru@>
2020-03-15 12:36:47 +0300

    H

--- static/scripts/community_get.js
+++ static/scripts/community_get.js
@@ -1,5 +1,4 @@
 
-$('body').on('click', '.c_all_likes', function() {var btn = $(this); item = $(this).parents('.infinite-item');pk = item.attr("community-id");uuid = item.attr("item-id");$('#votes_loader').html('').load("/window/all_community_like/" + uuid + "/" + pk + "/"); $('.votes_fullscreen').show();console.log("likes community open")});
 $('body').on('click', '.c_all_dislikes', function() {var btn = $(this); item = $(this).parents('.infinite-item');pk = item.attr("community-id");uuid = item.attr("item-id");$('#votes_loader').html('').load("/window/all_community_dislike/" + uuid + "/" + pk + "/"); $('.votes_fullscreen').show();console.log("dislikes community open")});
 $('body').on('click', '.c_all_reposts', function() {var btn = $(this); item = $(this).parents('.infinite-item');pk = item.attr("community-id");uuid = item.attr("item-id");$('#votes_loader').html('').load("/window/all_community_reposts/" + uuid + "/" + pk + "/"); $('.votes_fullscreen').show();console.log("reposts community open")});
 $('#ajax').on('click', '#community_add', function() {$('#community_loader').html('').load("/communities/progs/add/");$('.community_fullscreen').show();console.log("add community open")})
@@ -9,10 +8,19 @@ $('body').on('click', '.c_comment.comments_close', function() {var btn = $(this)
 
 $('body').on('click', '.c_comment.comments_open', function() {var btn = $(this); var item = btn.closest(".infinite-item"); var container = item.find(".load_comments");container.empty(); btn.removeClass('comments_open').addClass("comments_close");console.log("hide comments community")});
 
+
 on('#ajax', 'click', '.community_fullscreen_hide', function() {document.querySelector(".community_fullscreen").style.display = "none";document.getElementById("community_loader").innerHTML=""});
 on('#ajax', 'click', '.community_manage_fullscreen_hide', function() {document.querySelector(".manage_window_fullscreen").style.display = "none";document.getElementById("load_staff_window").innerHTML=""});
 
-
+on('#ajax', 'click', '.c_all_likes', function() {
+  parent = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  pk = parent.parentElement.getAttribute("community-id");
+  uuid = parent.getAttribute("item-id");
+  loader = document.getElementById("votes_loader");
+  url = "/window/all_community_like/" + uuid + "/" + pk + "/";
+  list_load(loader, url);
+  loader.parentElement.style.display = "block";
+});
 on('#ajax', 'click', '.show_staff_window', function() {
   parent = this.parentElement.parentElement.parentElement.parentElement.parentElement;
   pk = parent.getAttribute("data-pk");
@@ -22,7 +30,6 @@ on('#ajax', 'click', '.show_staff_window', function() {
   list_load(loader, url);
   loader.parentElement.style.display = "block";
 });
-
 on('#ajax', 'click', '.c_fullscreen', function() {
   parent = this.parentElement; parent2 = parent.parentElement;
   pk = parent2.getAttribute("community-id");
@@ -32,7 +39,6 @@ on('#ajax', 'click', '.c_fullscreen', function() {
   list_load(loader, url);
   loader.parentElement.style.display = "block";
 });
-
 on('#ajax', 'click', '.c_article_detail', function() {
   parent = this.parentElement; parent2 = parent.parentElement;
   pk = parent2.getAttribute("community-id");
