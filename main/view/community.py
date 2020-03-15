640f3221ed0dde327c9ab45ad9c3247df3039bb3
Serg <ochkarik1983@mail.ru@>
2020-03-15 17:19:04 +0300

    H

--- main/view/community.py
+++ main/view/community.py
@@ -8,7 +8,7 @@ from django.views import View
 from common.checkers import check_can_get_posts_for_community_with_name
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from gallery.models import Album, Photo
-from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
 
 
 class ItemCommunityCommentList(ListView):
