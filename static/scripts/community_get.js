ebdaf14a327c4ccbe48782bef7c2d126f3b69594
Serg <ochkarik1983@mail.ru@>
2020-03-15 12:39:37 +0300

    H

--- static/scripts/community_get.js
+++ static/scripts/community_get.js
@@ -1,5 +1,5 @@
 
-$('body').on('click', '.c_all_dislikes', function() {var btn = $(this); item = $(this).parents('.infinite-item');pk = item.attr("community-id");uuid = item.attr("item-id");$('#votes_loader').html('').load("/window/all_community_dislike/" + uuid + "/" + pk + "/"); $('.votes_fullscreen').show();console.log("dislikes community open")});
+
 $('body').on('click', '.c_all_reposts', function() {var btn = $(this); item = $(this).parents('.infinite-item');pk = item.attr("community-id");uuid = item.attr("item-id");$('#votes_loader').html('').load("/window/all_community_reposts/" + uuid + "/" + pk + "/"); $('.votes_fullscreen').show();console.log("reposts community open")});
 $('#ajax').on('click', '#community_add', function() {$('#community_loader').html('').load("/communities/progs/add/");$('.community_fullscreen').show();console.log("add community open")})
 $('#ajax').on('click', '#community_article_add', function() {btn = $(this);pk = btn.data('pk');$('#article_loader').html('').load("/article/add_community/" + pk + "/");$('.article_fullscreen').show();console.log("add community article open")})
@@ -12,6 +12,24 @@ $('body').on('click', '.c_comment.comments_open', function() {var btn = $(this);
 on('#ajax', 'click', '.community_fullscreen_hide', function() {document.querySelector(".community_fullscreen").style.display = "none";document.getElementById("community_loader").innerHTML=""});
 on('#ajax', 'click', '.community_manage_fullscreen_hide', function() {document.querySelector(".manage_window_fullscreen").style.display = "none";document.getElementById("load_staff_window").innerHTML=""});
 
+on('#ajax', 'click', '.c_all_reposts', function() {
+  parent = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  pk = parent.parentElement.getAttribute("community-id");
+  uuid = parent.getAttribute("item-id");
+  loader = document.getElementById("votes_loader");
+  url = "/window/all_community_reposts/" + uuid + "/" + pk + "/";
+  list_load(loader, url);
+  loader.parentElement.style.display = "block";
+});
+on('#ajax', 'click', '.c_all_dislikes', function() {
+  parent = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  pk = parent.parentElement.getAttribute("community-id");
+  uuid = parent.getAttribute("item-id");
+  loader = document.getElementById("votes_loader");
+  url = "/window/all_community_dislike/" + uuid + "/" + pk + "/";
+  list_load(loader, url);
+  loader.parentElement.style.display = "block";
+});
 on('#ajax', 'click', '.c_all_likes', function() {
   parent = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   pk = parent.parentElement.getAttribute("community-id");
