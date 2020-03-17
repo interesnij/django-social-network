4f51a4ba67e6bf76f30c64da0cb8dfb3426b6acd
Serg <ochkarik1983@mail.ru@>
2020-03-17 20:15:33 +0300

    J

--- communities/views/manage.py
+++ communities/views/manage.py
@@ -360,8 +360,8 @@ class CommunityStateCobertura(TemplateView):
 	def get(self,request,*args,**kwargs):
 		self.community = Community.objects.get(pk=self.kwargs["pk"])
 		self.template_name = self.community.get_manage_template(folder="manage/", template="stat_cobertura.html", request=request)
-		
-		self.today = datetime.datetime.now()
+
+		self.today = datetime.now()
 		self.query = self.community.get_unical_visiter_query(year=None, month=None, week=self.today.week, day=None)
 		self.unical_users_count = len(self.query)
 		self.phone_count = self.query.filter(platform=1)
