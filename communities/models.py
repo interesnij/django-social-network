cc48384d3e5076925deb5db1d398196a78c289d7
Serg <ochkarik1983@mail.ru@>
2020-03-17 15:49:41 +0300

    J

--- communities/models.py
+++ communities/models.py
@@ -278,9 +278,9 @@ class Community(models.Model):
         MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
         if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
             template_name = "mob_" + template_name
-            CommunityNumbers.objects.create(visitor=request.user.pk, target=self.pk, platform=1)
+            CommunityNumbers.objects.create(user=request.user.pk, community=self.pk, platform=1)
         else:
-            CommunityNumbers.objects.create(visitor=request.user.pk, target=self.pk, platform=0)
+            CommunityNumbers.objects.create(user=request.user.pk, community=self.pk, platform=0)
         return template_name
 
     def get_manage_template(self, folder, template, request):
