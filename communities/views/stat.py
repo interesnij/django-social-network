c81deb567f41132324876771d8620268d89fe349
Serg <ochkarik1983@mail.ru@>
2020-03-18 17:33:17 +0300

    J

--- communities/views/stat.py
+++ communities/views/stat.py
@@ -9,23 +9,19 @@ class CommunityCoberturaMonth(TemplateView):
 	def get(self,request,*args,**kwargs):
 		self.community = Community.objects.get(pk=self.kwargs["pk"])
 		self.template_name = self.community.get_manage_template(folder="community_stat/", template="cobertura_month.html", request=request)
-		self._months = CommunityNumbers.objects.values('created__month').distinct()
+		self._months = CommunityNumbers.objects.values('created__month').distinct()[0:10]
 		self.months = [i['created__month'] for i in self._months]
-		self.month_query = CommunityNumbers.objects.filter(community=self.community.pk, created__month=self.months[0]).distinct("user").values('platform')
-		self.phone_count = self.month_query.filter(platform=1)
-		self.comp_count = self.month_query.filter(platform=0)
-		self.phone = len(self.phone_count)/len(self.month_query)*100
-		self.comp = len(self.comp_count)/len(self.month_query)*100
+		self.views = []
+		for i in self.months:
+			view = CommunityNumbers.objects.filter(created__month=i, community=self.community.pk).distinct("user").count()
+			self.views += [view,]
 		return super(CommunityCoberturaMonth,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
 		context = super(CommunityCoberturaMonth,self).get_context_data(**kwargs)
 		context["community"] = self.community
-		context["phone"] = round(self.phone)
-		context["comp"] = round(self.comp)
-		context["months"] = self.months[:5 ]
-		context["month"] = self.months[0]
-		context["month_views"] = len(self.month_query)
+		context["months"] = self.months
+		context["views"] = self.views
 		return context
 
 class CommunityCoberturaDay(TemplateView):
