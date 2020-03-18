ca2a95df52cb87901e3fef40bc0541a1c70960e0
Serg <ochkarik1983@mail.ru@>
2020-03-18 18:07:12 +0300

    J

--- users/templatetags/get_month_name.py
+++ users/templatetags/get_month_name.py
@@ -6,25 +6,25 @@ register=template.Library()
 def get_month_name(value):
     if value == 1:
         return 'январь'
-    if value == 2:
+    elif value == 2:
         return 'февраль'
-    if value == 3:
+    elif value == 3:
         return 'март'
-    if value == 4:
+    elif value == 4:
         return 'апрель'
-    if value == 5:
+    elif value == 5:
         return 'май'
-    if value == 6:
+    elif value == 6:
         return 'июнь'
-    if value == 7:
+    elif value == 7:
         return 'июль'
-    if value == 8:
+    elif value == 8:
         return 'август'
-    if value == 9:
+    elif value == 9:
         return 'сентябрь'
-    if value == 10:
+    elif value == 10:
         return 'октябрь'
-    if value == 11:
+    elif value == 11:
         return 'ноябрь'
-    if value == 12:
+    elif value == 12:
         return 'декабрь'
