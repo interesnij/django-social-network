18c5f75e10ebc5ef79d0a4892850c39011e5f172
Serg <ochkarik1983@mail.ru@>
2020-03-18 08:07:46 +0300

    J

--- users/templatetags/get_unical_visit_of_month_for_community.py
+++ users/templatetags/get_unical_visit_of_month_for_community.py
@@ -6,4 +6,4 @@ from stst.models import CommunityNumbers
 @register.filter
 def get_views(month, community_id):
     views = CommunityNumbers.objects.filter(community=community_id, created__month=month).distinct().values('platform')
-    return views
+    return len(views)
