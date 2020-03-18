e586333f7daf6dea933063a0c040e2edf280f023
Serg <ochkarik1983@mail.ru@>
2020-03-18 10:41:45 +0300

    J

--- communities/views/stat.py
+++ communities/views/stat.py
@@ -9,7 +9,7 @@ class CommunityCoberturaMonth(TemplateView):
 	def get(self,request,*args,**kwargs):
 		self.community = Community.objects.get(pk=self.kwargs["pk"])
 		self.template_name = self.community.get_manage_template(folder="community_stat/", template="cobertura_month.html", request=request)
-		self.months = [i.month for i in CommunityNumbers.objects.values_list('created__month', flat=True).distinct()]
+		self.months = [i['created__month'] for i in CommunityNumbers.objects.values_list('created__month', flat=True).distinct()]
 		self.month_query = CommunityNumbers.objects.filter(community=self.community.pk, created__month=self.months[0]).distinct().values('platform')
 		self.phone_count = self.month_query.filter(platform=1)
 		self.comp_count = self.month_query.filter(platform=0)
