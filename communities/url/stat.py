9e755760560f7488230f84594e8211504a7da482
Serg <ochkarik1983@mail.ru@>
2020-03-18 12:25:30 +0300

    J

--- communities/url/stat.py
+++ communities/url/stat.py
@@ -4,4 +4,5 @@ from communities.views.stat import CommunityCoberturaMonth
 
 urlpatterns = [
     url(r'^cuberture_month/(?P<pk>[0-9]+)/$', CommunityCoberturaMonth.as_view(), name='community_cuberture_month'),
+    url(r'^cuberture_day/(?P<pk>[0-9]+)/$', CommunityCoberturaaDay.as_view(), name='community_cuberture_day'),
 ]
