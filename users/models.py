0041127d6f997297f06768a24f7a3bb5a4b007f6
Serg <ochkarik1983@mail.ru@>
2020-03-19 21:36:03 +0300

    H

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="uuid")
     last_activity= models.DateTimeField(default=timezone.now, blank=True, verbose_name='Активность')
     phone = models.CharField(blank=True, max_length=40)
-    #USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = 'phone'
 
     class Meta:
         verbose_name = 'пользователь'
