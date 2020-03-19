be02e5f6d37484b6fb6117ba2e8a3fd720b01f2c
Serg <ochkarik1983@mail.ru@>
2020-03-19 22:37:50 +0300

    H

--- users/models.py
+++ users/models.py
@@ -16,8 +16,7 @@ class User(AbstractUser):
     is_deleted = models.BooleanField(verbose_name="Удален", default=False, )
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="uuid")
     last_activity= models.DateTimeField(default=timezone.now, blank=True, verbose_name='Активность')
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=False, unique=True)
+    phone = models.CharField(max_length=17, blank=False, unique=True)
     USERNAME_FIELD = 'phone'
 
     class Meta:
