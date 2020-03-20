f7ae06d0b2e12c0f3382adee2500c3d570fb6aff
Serg <ochkarik1983@mail.ru@>
2020-03-20 16:22:15 +0300

    H

--- main/views.py
+++ main/views.py
@@ -52,11 +52,9 @@ class MainPhoneSend(TemplateView):
 		import json, requests
 		from django.http import HttpResponse, HttpResponseBadRequest
 		from common.model.other import PhoneCodes
-
-		self.service_id = 12203
-		self.secret_key = "GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck"
+		
 		self.phone = self.kwargs["phone"]
-		self.response = requests.get(url="https://api.ucaller.ru/v1.0/initCall?service_id='.self.service_id.'&key='.self.secret_key.'&phone='.self.client_phone")
+		self.response = requests.get(url="https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=" + self.phone)
 		self.data = self.response.json()
 		PhoneCodes.objects.create(phone=self.phone, code=self.data['code'])
 		return HttpResponse(self.phone)
