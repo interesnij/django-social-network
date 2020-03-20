096fe2d77bc16038efbcb3786e073360b8febda9
Serg <ochkarik1983@mail.ru@>
2020-03-20 09:34:14 +0300

    H

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ from rest_framework.exceptions import PermissionDenied
 class User(AbstractUser):
     moderated_object = GenericRelation('moderation.ModeratedObject', related_query_name='users')
     is_email_verified = models.BooleanField(default=False)
-    are_guidelines_accepted = models.BooleanField(default=False)
+    is_phone_accepted = models.BooleanField(default=False)
     is_deleted = models.BooleanField(verbose_name="Удален", default=False, )
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="uuid")
     last_activity= models.DateTimeField(default=timezone.now, blank=True, verbose_name='Активность')
