a7a57ef720e4059c3564fbe4e41a0a2fc9010d8c
Serg <ochkarik1983@mail.ru@>
2020-03-20 07:31:21 +0300

    H

--- tr/serializers.py
+++ tr/serializers.py
@@ -8,8 +8,9 @@ from gallery.models import Album
 
 
 class RegisterSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
+    #email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
     first_name = serializers.CharField(required=True, write_only=True)
+    phone = serializers.CharField(required=True, write_only=True)
     last_name = serializers.CharField(required=True, write_only=True)
     password1 = serializers.CharField(required=True, write_only=True)
     password2 = serializers.CharField(required=True, write_only=True)
@@ -36,7 +37,7 @@ class RegisterSerializer(serializers.Serializer):
             'first_name': self.validated_data.get('first_name', ''),
             'last_name': self.validated_data.get('last_name', ''),
             'password1': self.validated_data.get('password1', ''),
-            'email': self.validated_data.get('email', ''),
+            'phone': self.validated_data.get('phone', ''),
         }
 
     def save(self, request):
