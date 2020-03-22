9383531e764abf8a9681cc7cc0f41ddcc03eeb09
Serg <ochkarik1983@mail.ru@>
2020-03-22 16:12:43 +0300

    H

--- communities/views/stat.py
+++ communities/views/stat.py
@@ -169,13 +169,13 @@ class CommunityTrafficWeek(TemplateView):
 		self.un_views = []
 		self.range = []
 		for i in self.weeks:
-			days = [i.day, i.day + 1, i.day + 2, i.day + 3, i.day + 4, i.day + 5, i.day + 6]
 			view = CommunityNumbers.objects.filter(created__day__in=days, community=self.community.pk).count()
 			i6 = i + datetime.timedelta(days=7)
 			self.range += [str(i.strftime('%d.%m.%Y')) + " - " + str(i6.strftime('%d.%m.%Y'))]
 			self.views += [view ,]
 		for i in self.weeks:
-			view = CommunityNumbers.objects.filter(created__week=i.week, community=self.community.pk).distinct("user").count()
+			days = [i.day, i.day + 1, i.day + 2, i.day + 3, i.day + 4, i.day + 5, i.day + 6]
+			view = CommunityNumbers.objects.filter(created__week=days, community=self.community.pk).distinct("user").count()
 			self.un_views += [view,]
 		return super(CommunityTrafficWeek,self).get(request,*args,**kwargs)
 
