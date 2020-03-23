11922ef0653674de2fd3ec108034811abc7f4e97
Serg <ochkarik1983@mail.ru@>
2020-03-23 09:26:26 +0300

    F

--- static/scripts/chart_v1.js
+++ static/scripts/chart_v1.js
@@ -121,7 +121,7 @@ var valuess = []
 var dates = ctx.getAttribute('data-datew');
 //dates = dates.apply(this, args);
 var data_1 = ctx.getAttribute('data-data_1');
-for (i in dates){valuess + i} 
+for (i in dates){valuess = valuess + i};
 var data_2 = ctx.getAttribute('data-data_2');
 var label_1 = ctx.getAttribute('data-label_1');
 var label_2 = ctx.getAttribute('data-label_2');
