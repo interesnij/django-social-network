59e49e169090565ee801c579e9e956378fba9ed8
Serg <ochkarik1983@mail.ru@>
2020-03-23 11:10:10 +0300

    F

--- static/scripts/chart_v1.js
+++ static/scripts/chart_v1.js
@@ -117,15 +117,11 @@ Samples.utils.srand(Date.now());
 function load_chart() {
 //  try{
 var ctx = document.getElementById('canvas');
-var dates = ctx.getAttribute('data-datew').split(",");
+var dates = ctx.getAttribute('data-dates').split(",");
 var data_1 = ctx.getAttribute('data-data_1').split(",");
 var data_2 = ctx.getAttribute('data-data_2').split(",");
 var label_1 = ctx.getAttribute('data-label_1');
 var label_2 = ctx.getAttribute('data-label_2');
-console.log(dates);
-console.log(data_1);
-console.log(data_2);
-console.log(label_1);
 
 var config = {
 type: 'line',
