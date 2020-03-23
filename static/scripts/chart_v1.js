f5f1dacb84f3a07eda6687036534d0ce2c00e893
Serg <ochkarik1983@mail.ru@>
2020-03-23 08:47:21 +0300

    F

--- static/scripts/chart_v1.js
+++ static/scripts/chart_v1.js
@@ -130,7 +130,7 @@ console.log(label_1);
 var config = {
 type: 'line',
 data: {
-  labels: dates.apply(this, args);,
+  labels: dates.apply(this, args),
   datasets: [{
     label: label_1,
     backgroundColor: 'rgb(255, 99, 132)',
