433f6946e12b8bd48d557ab994254fe47a9b9ff7
Serg <ochkarik1983@mail.ru@>
2020-03-26 22:28:40 +0300

    J

---
+++ static/scripts/music.js
@@ -0,0 +1,29 @@
+var cssSelector = {jPlayer: "#jquery_jplayer_1",cssSelectorAncestor: ".main-header"};
+//var options = { swfPath: "/static/jquery.jplayer.swf", supplied: "oga, mp3", wmode: "window", smoothPlayBar: false, keyEnabled: true};
+var playlist = document.querySelector("#user_playlist").getAttribute('data-list');
+_playlist = JSON.parse(playlist);
+var myPlaylist = new jPlayerPlaylist(cssSelector, _playlist);
+document.querySelector(".music_header_btn").style.display = "block";
+
+on('#ajax', 'click', '.tag_track', function(e) {
+var track_id = this.getAttribute('data-counter');
+var playlist_block = document.querySelector(".music-dropdown");
+var tag_pk = document.querySelector(".tag_playlist").getAttribute('data-pk');
+if (!playlist_block.classList.contains('tag_' + tag_pk) && tag_pk){
+  var playlist_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  playlist_link.open( 'GET', '/music/manage/temp_tag/' + tag_pk, true );
+  playlist_link.onreadystatechange = function () {
+    if ( playlist_link.readyState == 4 && playlist_link.status == 200 ) {
+      playlist_block.className = "";
+      playlist_block.classList.add("dropdown-menu", "music-dropdown", "tag_" + tag_pk);
+      new_playlist = document.querySelector("#export_playlist").getAttribute('data-list');
+      pllll = JSON.parse(new_playlist);
+      myPlaylist.setPlaylist(pllll);
+      setTimeout(function(){ myPlaylist.play(track_id);},2000);
+      }
+    };
+    playlist_link.send( null );
+    }else{
+      myPlaylist.play(track_id);
+    };
+  });
