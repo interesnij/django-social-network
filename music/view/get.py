72d9ea63c7d4fb6d8cd0d98b73a31f8c9587d4ba
Serg <ochkarik1983@mail.ru@>
2020-03-30 21:59:03 +0300

    H

--- music/view/get.py
+++ music/view/get.py
@@ -15,5 +15,5 @@ class TagMusicGet(TemplateView):
     def get_context_data(self,**kwargs):
         context = super(TagMusicGet,self).get_context_data(**kwargs)
         context["tag"] = self.tag
-        context["list"] = self.parse_list
+        context["list"] = self.list
         return context
