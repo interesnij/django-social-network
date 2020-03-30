7ecbb46ecdbc2c5f4ff2121590da8fe07b760a69
Serg <ochkarik1983@mail.ru@>
2020-03-30 18:01:21 +0300

    H

--- music/view/get.py
+++ music/view/get.py
@@ -7,7 +7,7 @@ class TagMusicGet(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.tag = SoundTags.objects.get(pk=self.kwargs["pk"])
-        self.list = SoundcloudParsing.filter(tag=self.tag)
+        self.list = SoundcloudParsing.objects.filter(tag=self.tag)
         return super(TagMusicGet,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
