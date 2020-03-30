b58820b413fdd3e04305d6d7dc85e805104871bd
Serg <ochkarik1983@mail.ru@>
2020-03-30 13:21:33 +0300

    J

--- static/scripts/music_player.js
+++ static/scripts/music_player.js
@@ -4433,7 +4433,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
             })
         };
         this.downloadMp3 = function(e, t) {
-          console.log(e.id)
+          console.log(id)
         };
         this.getValidEmail = function() {
             var e = prompt("Please enter your email address where the mp3 download link will be sent:");
