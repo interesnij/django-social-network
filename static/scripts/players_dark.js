2334b5db0c5ecd4e7d3bceaead876859689b1fcf
Serg <ochkarik1983@mail.ru@>
2020-03-30 19:23:55 +0300

    H

--- static/scripts/players_dark.js
+++ static/scripts/players_dark.js
@@ -362,9 +362,8 @@ if (!document.body.classList.contains(category)){
 
           all_music_playlists.prepend(list);
           audio_playlists.prepend(cat);
-          cat1 = music_player.loadPlaylist(0);
-          cat2 = music_player.loadPlaylist(1);
-          console.log(cat1, cat2);
+          cat1 = music_player.showPlaylist(category);
+          console.log(cat1);
           music_player.playSpecificTrack(category, track_id);
       }};
       tag_link.send( null );
