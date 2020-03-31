3b81ef02d2e102b7380daf110ac3566f26ddec0f
Serg <ochkarik1983@mail.ru@>
2020-03-31 15:45:53 +0300

    D

--- static/scripts/players_dark.js
+++ static/scripts/players_dark.js
@@ -378,7 +378,6 @@ FWDMSPUtils.onReady(function(){
     });
 
 function music_onReady(){console.log("Аудио плеер готов");}
-function myFunction(){console.log("YYYYYYYYY")};
 function video_onReady(){console.log("Видео плеер готов");}
 
 function video_onPlay(){
@@ -420,7 +419,7 @@ if (!document.body.classList.contains(category)){
             _source=list[i].getAttribute("data-path") + '/stream?client_id=' + 'dce5652caa1b66331903493735ddd64d';
             _title=list[i].getAttribute("data-title");
             _thumbPath=list[i].getAttribute("data-thumbpath");
-            music_player.addTrack(_source, _title, _thumbPath, "0:00", false, false, "myFunction()");
+            music_player.addTrack(_source, _title, _thumbPath, "0:00", true, false, null);
           }
 
           music_player.playSpecificTrack(category, track_id);
