4de7b77d976164ada8980c1baa15647de8220911
Serg <ochkarik1983@mail.ru@>
2020-04-01 12:30:32 +0300

    G

--- static/scripts/music_player.js
+++ static/scripts/music_player.js
@@ -3800,7 +3800,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 						!FWDMSP.hasHTML5Audio && t >= p.mainProgress_do.w && (t = p.mainProgress_do.w);
 						var s = t / p.mainScrubberWidth;
 						p.disable_do && p.addChild(p.disable_do),
-							p.ttm.show(), p.updateMainScrubber(o), .dispatchEvent(n.START_TO_SCRUB), p.dispatchEvent(n.SCRUB_PLAYLIST_ITEM, {
+							p.ttm.show(), p.updateMainScrubber(o), p.dispatchEvent(n.START_TO_SCRUB), p.dispatchEvent(n.SCRUB_PLAYLIST_ITEM, {
 								percent: s
 							}), p.dispatchEvent(n.SCRUB, {
 								percent: o
