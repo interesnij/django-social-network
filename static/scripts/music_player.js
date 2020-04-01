f532a7e17f0557fc376b1e44332d305d3c0cc7e8
Serg <ochkarik1983@mail.ru@>
2020-04-01 08:43:30 +0300

    G

--- static/scripts/music_player.js
+++ static/scripts/music_player.js
@@ -4435,7 +4435,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
                                 if (self.isAPIReady_bl) {
                                     null == pId && (pId = self.id);
                                     var buy = self.data.playlist_ar[pId].buy;
-                                    window.open(buy)
+                                    console.log(pId);
                                 }
                             }, this.playFirstTrack = function() {
                                 self.playSpecificTrack(self.catId, 0)
