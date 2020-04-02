eba212448e477a7fbbb2edb74946a9deec4ba1f6
Serg <ochkarik1983@mail.ru@>
2020-04-02 16:14:45 +0300

    H

--- music/view/get.py
+++ music/view/get.py
@@ -4,7 +4,7 @@ import json
 
 
 class TagMusicGet(TemplateView):
-    template_name="music/get/tag_music.html"
+    template_name="music/load_playlist.html"
 
     def get(self,request,*args,**kwargs):
         self.tag = SoundTags.objects.get(pk=self.kwargs["pk"])
@@ -14,6 +14,33 @@ class TagMusicGet(TemplateView):
 
     def get_context_data(self,**kwargs):
         context = super(TagMusicGet,self).get_context_data(**kwargs)
-        context["tag"] = self.tag
-        context["list"] = self.result
+        context["list"] = self.result[0:300]
+        return context
+
+class GenreMusicGet(TemplateView):
+    template_name="music/load_playlist.html"
+
+    def get(self,request,*args,**kwargs):
+        self.genre = SoundGenre.objects.get(pk=self.kwargs["pk"])
+        self.list_ = SoundcloudParsing.objects.filter(genre=self.genre)
+        self.result = reversed(list(self.list_))
+        return super(GenreMusicGet,self).get(request,*args,**kwargs)
+
+    def get_context_data(self,**kwargs):
+        context = super(GenreMusicGet,self).get_context_data(**kwargs)
+        context["list"] = self.result[0:300]
+        return context
+
+class ListMusicGet(TemplateView):
+    template_name="music/load_playlist.html"
+
+    def get(self,request,*args,**kwargs):
+        self.list = SoundList.objects.get(pk=self.kwargs["pk"])
+        self.list_ = SoundcloudParsing.objects.filter(list=self.list)
+        self.result = reversed(list(self.list_))
+        return super(ListMusicGet,self).get(request,*args,**kwargs)
+
+    def get_context_data(self,**kwargs):
+        context = super(ListMusicGet,self).get_context_data(**kwargs)
+        context["list"] = self.result[0:300]
         return context
--- static/scripts/players_dark.js
+++ static/scripts/players_dark.js
@@ -425,14 +425,30 @@ function save_playlist(suffix, post_link, get_link, track_id){
     };
 
 
-on('#ajax', 'click', '.tag_track', function() {
+on('#ajax', 'click', '.tag_item', function() {
   var track_id = this.getAttribute('data-counter');
   var tag_pk = document.querySelector(".tag_playlist").getAttribute('data-pk');
-
   if (!document.body.classList.contains("tag_" + tag_pk)){
     save_playlist("tag_" + tag_pk, '/music/manage/temp_tag/' + tag_pk, '/music/get/tag/' + tag_pk, track_id)
   }else{music_player.playSpecificTrack("tag_" + tag_pk, track_id)}
   });
+
+on('#ajax', 'click', '.genre_item', function() {
+  var track_id = this.getAttribute('data-counter');
+  var genre_pk = document.querySelector(".genre_playlist").getAttribute('data-pk');
+  if (!document.body.classList.contains("genre_" + genre_pk)){
+    save_playlist("genre_" + genre_pk, '/music/manage/temp_list/' + genre_pk, '/music/get/list/' + genre_pk, track_id)
+  }else{music_player.playSpecificTrack("genre_" + genre_pk, track_id)}
+});
+
+on('#ajax', 'click', '.music_list_item', function() {
+  var track_id = this.getAttribute('data-counter');
+  var list_pk = document.querySelector(".music_playlist").getAttribute('data-pk');
+  if (!document.body.classList.contains("list_" + list_pk)){
+    save_playlist("list_" + list_pk, '/music/manage/temp_list/' + list_pk, '/music/get/list/' + list_pk, track_id)
+  }else{music_player.playSpecificTrack("list_" + list_pk, track_id)}
+});
+
 
 on('#ajax', 'click', '#load_1', function(e) {
   music_player.loadPlaylist(0);
@@ -452,7 +468,7 @@ on('#ajax', 'click', '.track_add', function(e) {
   _link.onreadystatechange = function () {
     if ( _link.readyState == 4 && _link.status == 200 ) {
       block.innerHTML = "";
-      block.append("<span class='track_remove' title='Удалить'><svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' style='width:20px;' class='svg_default' viewBox='0 0 2424'><path fill='none' d='M0 0h24v24H0z'/><path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg></span>")
+      block.innerHTML = "<span class='track_remove' title='Удалить'><svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' style='width:20px;' class='svg_default' viewBox='0 0 2424'><path fill='none' d='M0 0h24v24H0z'/><path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg></span>"
   }};
   _link.send( null );
 });
@@ -465,7 +481,7 @@ on('#ajax', 'click', '.track_remove', function(e) {
   _link.onreadystatechange = function () {
     if ( _link.readyState == 4 && _link.status == 200 ) {
       block.innerHTML = "";
-      block.append("<span class='track_add' title='Добавить'><svg fill='currentColor' style='width:25px;' class='svg_default' xmlns='http://www.w3.org/2000/svg' viewBox='0 024 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/><path d='M0 0h24v24H0z' fill='none'/></svg></span>")
+      block.innerHTML = "<span class='track_add' title='Добавить'><svg fill='currentColor' style='width:25px;' class='svg_default' xmlns='http://www.w3.org/2000/svg' viewBox='0 024 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/><path d='M0 0h24v24H0z' fill='none'/></svg></span>"
   }};
   _link.send( null );
 });
