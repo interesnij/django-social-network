e9fd79570f20486513d10c7fa8f1e9410dc6a096
Serg <ochkarik1983@mail.ru@>
2020-04-02 16:41:13 +0300

    H

--- music/models.py
+++ music/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.postgres.indexes import BrinIndex
 from django.utils import timezone
+from communities.models import Community
 
 
 class SoundGenres(models.Model):
