a570f96e2d224e776a9b78905374d2262ddc086c
Serg <ochkarik1983@mail.ru@>
2020-04-03 20:52:55 +0300

    J

--- static/scripts/music_player.js
+++ static/scripts/music_player.js
@@ -1042,7 +1042,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 										e.preventDefault && e.preventDefault(),
 										self == FWDMSP.keyboardCurInstance)) {
 									if (32 == e.keyCode) {
-										if (self.audioType_str != FWDMSP.VIDEO && self.audioType_str != FWDMSP.HLS || !self.videoScreen_do) {
+										if (self.audioType_str != FWDMSP.VIDEO || !self.videoScreen_do) {
 											if (!self.audioScreen_do.isSafeToBeControlled_bl)
 											  return;
 											self.audioScreen_do.togglePlayPause()
@@ -1281,7 +1281,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 										(void 0 === self.catId || void 0 === self.id || isNaN(self.catId) || isNaN(self.id)) && (self.catId = self.data.startAtPlaylist, self.id = self.data.startAtTrack, e = !0),
                     (self.catId < 0 || self.catId > self.data.totalCategories - 1 && !e) && (self.catId = self.data.startAtPlaylist, self.id = self.data.startAtTrack, e = !0),
 										self.data.playlist_ar && (self.id < 0 && !e ? (self.id = self.data.startAtTrack, e = !0) : self.prevCatId == self.catId && self.id > self.data.playlist_ar.length - 1 && !e && (self.id = self.data.playlist_ar.length - 1, e = !0)),
-                    e ? location.hash = self.instanceName_str + "?catid=" + self.catId + "&trackid=" + self.id : self.prevCatId != self.catId ? (self.loadInternalPlaylist(), self.prevCatId = self.catId) : (self.isPlaylistItemClicked_bl = !0, self.setSource(!1), self.changeHLS_bl = !0, self.isShoutcast_bl || self.isIcecast_bl || self.play())
+                    e ? location.hash = self.instanceName_str + "?catid=" + self.catId + "&trackid=" + self.id : self.prevCatId != self.catId ? (self.loadInternalPlaylist(), self.prevCatId = self.catId) : (self.isPlaylistItemClicked_bl = !0, self.setSource(!1), self.isShoutcast_bl || self.isIcecast_bl || self.play())
 									}
 							}, this.setupPreloader = function() {
 								FWDMSPPreloader.setPrototype(),
@@ -1363,15 +1363,16 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 								e.id == self.id
 								? self.audioType_str == FWDMSP.AUDIO && self.audioScreen_do.isPlaying_bl
 								? self.pause() : self.audioType_str != FWDMSP.AUDIO || self.audioScreen_do.isStopped_bl && !self.audioScreen_do.isStopped_bl
-								? self.audioType_str != FWDMSP.HLS
+								//? self.audioType_str != FWDMSP.HLS
 								? self.audioType_str != FWDMSP.VIDEO
 								? self.ytb_do.isPlaying_bl
-								? self.pause() : self.play() : self.play() : self.pause() : self.play() : self.useDeepLinking_bl &&
+								? self.pause() : self.play() : self.pause() : self.play() : self.useDeepLinking_bl &&
 								self.id != e.id ? (FWDAddress.setValue(self.instanceName_str + "?catid=" + self.catId + "&trackid=" + e.id), self.id = e.id)
 								: (self.id = e.id, self.setSource(!0),
-										self.changeHLS_bl = !0,
-										self.autioType_str != FWDMSP.HLS
-										&& self.play()
+								//		self.changeHLS_bl = !0,
+								//		self.autioType_str != FWDMSP.HLS
+								//		&&
+									self.play()
 									)
 
 							},
@@ -1607,15 +1608,7 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 							this.audioScreenPlayCompleteHandler = function() {
 								self.data.playlist_ar && (self.videoNameGa = self.data.playlist_ar[self.id].titleText,
 									                        self.videoCat = self.data.cats_ar[self.catId]),
-								FWDMSP.hasHTML5Audio && (self.data.loop_bl ? "hls_flash" == self.audioType_str
-																													 ? setTimeout(function() {
-																															self.scrub(0), self.resume()
-																															}, 50)
-																														: (self.scrub(0), self.play())
-																														: self.data.shuffle_bl ? self.playShuffle() : 1 == self.playlist_do.items_ar.length
-																														? (self.stop(), self.playlist_do && self.playlist_do.updateCurItemProgress(0))
-																														: self.playNext()),
-																														self.dispatchEvent(FWDMSP.PLAY_COMPLETE)
+								FWDMSP.hasHTML5Audio && self.dispatchEvent(FWDMSP.PLAY_COMPLETE)
 							},
 							this.loadID3IfPlaylistDisabled = function() {
 								var o = self.data.playlist_ar[self.id].source;
