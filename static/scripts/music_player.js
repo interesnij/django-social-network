9694339032fd4234f7ef6ef1da2ff065c4365f3e
Serg <ochkarik1983@mail.ru@>
2020-04-04 16:16:24 +0300

    H

--- static/scripts/music_player.js
+++ static/scripts/music_player.js
@@ -1352,11 +1352,11 @@ document.write("<script type='text/vbscript'>\r\nFunction IEBinary_getByteAt(str
 								e.id == self.id ? self.audioType_str == FWDMSP.AUDIO && self.audioScreen_do.isPlaying_bl
 																	? self.pause()
 																	: self.audioType_str != FWDMSP.AUDIO || self.audioScreen_do.isStopped_bl && !self.audioScreen_do.isStopped_bl
-																//	? self.audioType_str != FWDMSP.HLS
-																		? self.audioType_str != FWDMSP.VIDEO
+																	? self.audioType_str != FWDMSP.HLS
+																	//	? self.audioType_str != FWDMSP.VIDEO
 
-																		: self.pause()
-															//		: self.play()
+																//		: self.pause()
+														  		: self.play()
 																: self.useDeepLinking_bl && self.id != e.id
 																  ? (FWDAddress.setValue(self.instanceName_str + "?catid=" + self.catId + "&trackid=" + e.id), self.id = e.id)
 																	: (self.id = e.id, self.setSource(!0), self.play()
