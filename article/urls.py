afc395c8ec84357886cd4386966bf0e888adfa01
Serg <ochkarik1983@mail.ru@>
2020-04-05 18:01:54 +0300

    F

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', ArticleView.as_view(), name='articles'),
-    url(r'^add/(?P<pk>\d+)/$', login_required(ArticleUserCreate.as_view()), name="article_add_user"),
+    url(r'^add_user/(?P<pk>\d+)/$', login_required(ArticleUserCreate.as_view()), name="article_add_user"),
     url(r'^add_community/(?P<pk>\d+)/$', login_required(ArticleCommunityCreate.as_view())),
     url(r'^new/(?P<uuid>[0-9a-f-]+)/$', ArticleNewView.as_view(), name='article_new'),
     url(r'^detail/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', ArticleUserDetailView.as_view()),
