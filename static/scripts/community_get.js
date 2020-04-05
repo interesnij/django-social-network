2b901c92afa7017f1b0b449f86bbc970c55eacad
Serg <ochkarik1983@mail.ru@>
2020-04-05 16:10:01 +0300

    F

--- static/scripts/community_get.js
+++ static/scripts/community_get.js
@@ -65,7 +65,7 @@ on('#ajax', 'click', '.c_item_comments.comments_close', function() {
   pk = parent.parentElement.getAttribute("community-pk");
   uuid = parent.getAttribute("item-uuid");
   _this = parent.querySelector(".c_item_comments");
-  this.classList.contains("");
+  this.classList.remove();
   this.classList.add("c_item_comments","comments_open");
   list_load(this.parentElement.parentElement.parentElement.querySelector(".c_load_comments"), "/community/comment/" + uuid + "/" + pk + "/");
 });
@@ -73,6 +73,6 @@ on('#ajax', 'click', '.c_item_comments.comments_open', function() {
   parent = this.parentElement.parentElement.parentElement;
   _this = parent.querySelector(".c_load_comments");
   _this.innerHTML="";
-  this.classList.contains("");
+  this.classList.remove();
   this.classList.add("c_item_comments","comments_close");
 });
