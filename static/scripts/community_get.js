2a47ed3a43f6bad1972b501079ed9c765ce2cc2c
Serg <ochkarik1983@mail.ru@>
2020-04-05 14:43:33 +0300

    F

--- static/scripts/community_get.js
+++ static/scripts/community_get.js
@@ -64,16 +64,16 @@ on('#ajax', 'click', '.c_item_comments.comments_close', function() {
   parent = this.parentElement.parentElement.parentElement.parentElement;
   pk = parent.parentElement.getAttribute("community-pk");
   uuid = parent.getAttribute("item-uuid");
-  _this = parent.querySelector(".c_item_comments");
-  _this.classList.add("comments_open");
-  _this.classList.remove("comments_close");
+  //_this = parent.querySelector(".c_item_comments");
+  this.classList.add("comments_open");
+  this.classList.remove("comments_close");
   url = "/community/comment/" + uuid + "/" + pk + "/";
   list_load(parent.querySelector(".c_load_comments"), url);
 });
 on('#ajax', 'click', '.c_item_comments.comments_open', function() {
   parent = this.parentElement.parentElement.parentElement;
-  _this = parent.querySelector(".c_item_comments");
+  _this = parent.querySelector(".c_load_comments");
   _this.innerHTML="";
-  _this.classList.add("comments_close");
-  _this.classList.remove("comments_open");
+  this.classList.add("comments_close");
+  this.classList.remove("comments_open");
 });
