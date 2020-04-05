1420b6725ba9d927d8db3b9922c078feb7f2829f
Serg <ochkarik1983@mail.ru@>
2020-04-05 12:43:55 +0300

    F

--- static/scripts/community_get.js
+++ static/scripts/community_get.js
@@ -35,6 +35,6 @@ on('#ajax', 'click', '#community_add', function() {
   list_load(loader, url);
   document.querySelector(".community_fullscreen").style.display = "block";
 });
-});
+
 on('#ajax', 'click', '.community_fullscreen_hide', function() {document.querySelector(".community_fullscreen").style.display = "none";document.getElementById("community_loader").innerHTML=""});
 on('#ajax', 'click', '.community_manage_fullscreen_hide', function() {document.querySelector(".manage_window_fullscreen").style.display = "none";document.getElementById("load_staff_window").innerHTML=""});
