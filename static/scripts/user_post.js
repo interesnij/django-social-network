17ef93e4ef501cb36d3b0694911394146cd8e330
Serg <ochkarik1983@mail.ru@>
2020-04-06 11:09:35 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -1,13 +1,13 @@
 
 on('#ajax', 'click', '#form_post_btn', function() {
-  var form_post, formEntries, options, stream, pk
+  var form_post, formEntries, options, lenta_load, pk
 
   form_post = document.getElementById('form_post');
   formEntries = new FormData(form_post);
   options = {method: 'POST',body: formEntries,};
 
-  stream = form_post.parentElement.nextElementSibling;
-  pk = stream.querySelector(".stream").getAttribute("data-pk");
+  lenta_load = form_post.parentElement.nextElementSibling;
+  pk = stream.querySelector(".stream").getAttribute("user-pk");
   fetch("/posts/add_post/" + pk + "/", options).then(data => {
     document.getElementById('id_text').value = "";
     document.getElementById('for_images_upload').innerHTML = "";
@@ -16,8 +16,8 @@ on('#ajax', 'click', '#form_post_btn', function() {
     document.getElementById('for_good').innerHTML = "";
     document.getElementById('for_question').innerHTML = "";
     document.getElementById('for_settings').innerHTML = "";
-    stream.querySelector(".stream").prepend(data);
-    stream.querySelector(".post_empty").style.display = "none";
+    lenta_load.querySelector(".stream").prepend(data);
+    lenta_load.querySelector(".post_empty").style.display = "none";
 
   }).catch(error => {console.log("Все не ОК")})
 });
