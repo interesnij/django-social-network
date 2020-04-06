d0027a9c70c20338be2349c48d6906253ef97d26
Serg <ochkarik1983@mail.ru@>
2020-04-06 17:34:13 +0300

    H

--- main/url/community.py
+++ main/url/community.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^unfixed/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_unfixed)),
 	url(r'^off_comment/(?P<pk>\d+)/$', login_required(community_off_comment)),
     url(r'^on_comment/(?P<pk>\d+)/$', login_required(community_on_comment)),
-    url(r'^delete/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_item_delete))
+    url(r'^delete/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_item_delete)),
 	url(r'^abort_delete/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', login_required(community_item_abort_delete))
 ]
--- main/url/user.py
+++ main/url/user.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^unfixed/(?P<uuid>[0-9a-f-]+)/$', login_required(user_unfixed)),
 	url(r'^off_comment/(?P<uuid>[0-9a-f-]+)/$', login_required(user_off_comment)),
     url(r'^on_comment/(?P<uuid>[0-9a-f-]+)/$', login_required(user_on_comment)),
-    url(r'^delete/(?P<uuid>[0-9a-f-]+)/$', login_required(user_item_delete))
+    url(r'^delete/(?P<uuid>[0-9a-f-]+)/$', login_required(user_item_delete)),
 	url(r'^abort_delete/(?P<uuid>[0-9a-f-]+)/$', login_required(user_item_abort_delete))
 ]
