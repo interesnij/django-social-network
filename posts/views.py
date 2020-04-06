dca44548200fea5d6419dd3c972ba225d492250e
Serg <ochkarik1983@mail.ru@>
2020-04-06 16:41:57 +0300

    H

--- posts/views.py
+++ posts/views.py
@@ -45,10 +45,11 @@ class PostUserCreate(View):
 
         if self.form_post.is_valid() and request.user == self.user:
             post=self.form_post.save(commit=False)
+            ok = "Нужно ввести текст, прикрепить фотографию, аудио/ видеофайлы, статью или товар"
             new_post = post.create_post(creator=request.user, text=post.text, community=None, comments_enabled=post.comments_enabled, status=post.status,)
             return render_to_response('item_user/my_post.html', {'object': new_post,'request': request})
         else:
-            return "ok"
+            return render_to_response('item_user/my_post.html', {'ok': ok})
 
 
 class PostCommunityCreate(View):
