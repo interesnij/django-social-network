34852c50dc971a9390634d1a6a99fd059f66ea8c
Serg <ochkarik1983@mail.ru@>
2020-04-06 18:16:28 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -143,7 +143,7 @@ on('#ajax', 'click', '.item_user_remove_abort', function() {
   link.onreadystatechange = function () {
   if ( link.readyState == 4 && link.status == 200 ) {
     parent = this.parentElement;
-    parent.style.display = "none";
+    this.parentElement.remove();
     item.style.display = "block";
   }};
 
