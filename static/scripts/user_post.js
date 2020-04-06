84e4e5fcb5886ff9818fb201b22ca1c30635cb3f
Serg <ochkarik1983@mail.ru@>
2020-04-06 16:35:47 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -24,7 +24,7 @@ on('#ajax', 'click', '#form_post_btn', function() {
     new_post = document.createElement("span");
     new_post.innerHTML = elem;
     response = new_post.querySelector(".card");
-    if (request.responseText.indexOf("Нужно ввести") != -1){
+    if (response.responseText.indexOf("Нужно ввести") != -1){
       error = form_post.querySelector("#user_post_error");
       error.append(response);
       setTimeout(error.innerHTML = "", 4000);
