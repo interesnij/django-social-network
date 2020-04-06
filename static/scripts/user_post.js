db3bdd205ad5fda93b16d6a5471888192e30cbf3
Serg <ochkarik1983@mail.ru@>
2020-04-06 11:55:41 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -1,14 +1,24 @@
 
 on('#ajax', 'click', '#form_post_btn', function() {
-  var form_post, formEntries, options, lenta_load, pk, response
+  var form_post, lenta_load, pk, link_, elem
 
-  form_post = document.getElementById('form_post');
-  formEntries = new FormData(form_post);
-  options = {method: 'POST',body: formEntries,};
-
+  form_post = new FormData(document.forms.new_post);
   lenta_load = form_post.parentElement.nextElementSibling;
   pk = lenta_load.querySelector(".stream").getAttribute("user-pk");
-  fetch("/posts/add_post/" + pk + "/", options).then(data => {
+
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'POST', "/posts/add_post/" + pk + "/", true );
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    elem = link_.responseText;
+    lenta_load.querySelector(".stream").prepend(elem)
+    lenta_load.querySelector(".post_empty") ? lenta_load.querySelector(".post_empty").style.display = "none" : console.log("post_empty не обнаружен");
+  }};
+  
+  link_.send();
+});
+
     //document.getElementById('id_text').value = "";
     //document.getElementById('for_images_upload').innerHTML = "";
     //document.getElementById('for_gallery').innerHTML = "";
@@ -16,8 +26,3 @@ on('#ajax', 'click', '#form_post_btn', function() {
     //document.getElementById('for_good').innerHTML = "";
     //document.getElementById('for_question').innerHTML = "";
     //document.getElementById('for_settings').innerHTML = "";
-    lenta_load.querySelector(".stream").prepend(data.write(200,{"Content-Type" : "text/plain"}));
-    //lenta_load.querySelector(".post_empty").style.display = "none";
-
-  }).catch(error => {console.log("Все не ОК")})
-});
