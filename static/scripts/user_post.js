83568387fc5553e188a243ae3bd2bd9fa9421dd0
Serg <ochkarik1983@mail.ru@>
2020-04-06 12:05:54 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -13,7 +13,7 @@ on('#ajax', 'click', '#form_post_btn', function() {
   link_.onreadystatechange = function () {
   if ( this.readyState == 4 && this.status == 200 ) {
     elem = link_.responseText;
-    new_post.innerHTML = elem.innerHTML;
+    new_post = document.createElement("span"); new_post.innerHTML = elem;
     lenta_load.querySelector(".stream").prepend(new_post)
     lenta_load.querySelector(".post_empty") ? lenta_load.querySelector(".post_empty").style.display = "none" : console.log("post_empty не обнаружен");
   }};
