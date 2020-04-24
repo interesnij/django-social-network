450fecc300a79ad0dfddddd5c60f6d5d0bf22b98
Serg <ochkarik1983@mail.ru@>
2020-04-24 16:10:50 +0300

    H

--- static/scripts/players_white.js
+++ static/scripts/players_white.js
@@ -382,7 +382,7 @@ function video_onPlay(){
 function music_onPlay(){
     console.log("Воспроизводится трек № : " + music_player.getTrackId());
     title = music_player.getTrackTitle();
-    document.title = title.innerHTML;
+    document.title = title;
     try{video_player.pause();}catch{var a=0}
 };
 
