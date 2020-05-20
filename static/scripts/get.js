5c534b32b448c074326df5421f89bf5206b08e35
Serg <ochkarik1983@mail.ru@>
2020-05-20 19:19:43 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -92,7 +92,7 @@ on('#ajax', 'click', '.community_fullscreen_hide', function() {document.querySel
 on('#ajax', 'click', '.community_manage_fullscreen_hide', function() {document.querySelector(".manage_window_fullscreen").style.display = "none";document.getElementById("load_staff_window").innerHTML=""});
 on('#ajax', 'click', '.good_fullscreen_hide', function() {document.querySelector(".good_fullscreen").style.display = "none";document.getElementById("good_loader").innerHTML=""});
 on('#ajax', 'click', '.stat_fullscreen_hide', function() {document.querySelector(".stat_fullscreen").style.display = "none";document.getElementById("stat_loader").innerHTML=""});
-on('#ajax', 'click', '.video_fullscreen_hide', function() {document.querySelector(".video_fullscreen").style.display = "none";document.getElementById("video_loader").innerHTML=""});
+on('body', 'click', '.video_fullscreen_hide', function() {document.querySelector(".video_fullscreen").style.display = "none";document.getElementById("video_loader").innerHTML=""});
 
 // END FULLSCREENS //
 //--------------------------------------------------------------------//
