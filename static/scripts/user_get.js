5a261037b021be68bd9228697c595660f1e72c28
Serg <ochkarik1983@mail.ru@>
2020-05-20 18:57:44 +0300

    H

--- static/scripts/user_get.js
+++ static/scripts/user_get.js
@@ -14,7 +14,7 @@ on('#ajax', 'click', '.u_video_detail', function() {
   uuid = parent.getAttribute('data-uuid');
   loader = document.getElementById("video_loader");
   open_fullscreen("/video/get_video_playlist/" + pk + "/" + uuid + "/", loader)
-  //load_playlist(counter);
+  load_playlist(counter);
 });
 
 on('#ajax', 'click', '.fullscreen', function() {
