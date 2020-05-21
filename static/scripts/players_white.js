792dae4f3059c64927ce04fbb716ffc5f0162e21
Serg <ochkarik1983@mail.ru@>
2020-05-21 18:46:36 +0300

    H

--- static/scripts/players_white.js
+++ static/scripts/players_white.js
@@ -514,6 +514,7 @@ music_player.pause();
 }
 
 function get_resize_video(){
-  video_player.maxWidth = 500;
+  video_player.maxWidth = 400;
   video_player.maxHeight = 250;
+  video_player.hideplaylist();
 }
