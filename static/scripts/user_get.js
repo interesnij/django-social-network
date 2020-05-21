e6632ebc2ccaee8b9d5f3ec1f36c7db05ce332bb
Serg <ochkarik1983@mail.ru@>
2020-05-21 15:14:02 +0300

    H

--- static/scripts/user_get.js
+++ static/scripts/user_get.js
@@ -19,6 +19,7 @@ on('#ajax', 'click', '.u_video_detail', function() {
   video_saver.setAttribute('data-video', video_player_id + "a");
   setTimeout(function() {
     load_video_playlist(video_player_id + "a", counter);
+    console.log(fwduvPlayer0)
   }, 1000);
 });
 
