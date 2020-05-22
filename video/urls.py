a90a413ab94a3e24a65e074961543ee499ee88cc
Serg <ochkarik1983@mail.ru@>
2020-05-22 12:58:29 +0300

    G

--- video/urls.py
+++ video/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from video.views import AllVideoView, UserVideoPlaylist
+from video.views import AllVideoView, UserBasicVideoList, UserVideoList, 
 
 
 urlpatterns = [
