0ab14a0ae52fd5fcd97cf4c810b42076cfb6bb99
Serg <ochkarik1983@mail.ru@>
2020-05-22 15:30:31 +0300

    G

--- video/view/user.py
+++ video/view/user.py
@@ -19,7 +19,6 @@ class UserBasicVideoList(ListView):
     def get_context_data(self,**kwargs):
         context = super(UserBasicVideoList,self).get_context_data(**kwargs)
         context['user'] = self.user
-        context['playlist'] = self.list
         return context
 
     def get_queryset(self):
