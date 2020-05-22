d2247754629c61283e658d353579fe69bda65238
Serg <ochkarik1983@mail.ru@>
2020-05-22 18:21:28 +0300

    G

--- video/forms.py
+++ video/forms.py
@@ -10,7 +10,8 @@ class AlbumForm(forms.ModelForm):
 
 
 class VideoForm(forms.ModelForm):
-
+	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
+	category = forms.CharField( label="", required=False, widget=forms.Select(attrs={'class': 'form-control'}))
 	class Meta:
 		model = Video
 		fields = ['title', 'description', 'is_public', 'image', 'category', "tag" , 'album', 'is_child', 'comments_enabled', 'uri']
