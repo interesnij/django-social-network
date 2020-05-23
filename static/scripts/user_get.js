311b684fe5fdfc6498b861d3720f75a01439c491
Serg <ochkarik1983@mail.ru@>
2020-05-23 16:19:40 +0300

    G

--- static/scripts/user_get.js
+++ static/scripts/user_get.js
@@ -27,9 +27,7 @@ on('#ajax', 'click', '.u_article_detail', function() {
 on('#ajax', 'click', '#article_add', function() {
   var pk = this.getAttribute('data-pk');
   open_fullscreen("/article/u_article_window/" + pk + "/", document.getElementById("create_loader"));
-  CKEDITOR.on('instanceReady', function(){
-    $('#id_content').css('height','200px');
-});
+  CKEDITOR.instances.id_content.updateElement();
 });
 
 on('#ajax', 'click', '.u_all_likes', function() {
