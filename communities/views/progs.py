042f09fca9d640f94b9efabb1aa7b5f9adf6e6d2
Serg <ochkarik1983@mail.ru@>
2020-05-23 13:10:23 +0300

    G

--- communities/views/progs.py
+++ communities/views/progs.py
@@ -14,7 +14,6 @@ class UserCreateCommunityWindow(TemplateView):
     def get(self,request,*args,**kwargs):
         self.user = User.objects.get(pk=self.kwargs["pk"])
         self.template_name = self.user.get_settings_template(folder="manage/", template="create_community.html", request=request)
-
         return super(UserCreateCommunityWindow,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
