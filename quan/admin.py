c11ae53dbd262f7ea31c224083adfa9ab73e0cf2
Serg <ochkarik1983@mail.ru@>
2020-05-29 09:46:35 +0300

    F

--- quan/admin.py
+++ quan/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog_categories.models import QuestionsCategory, Question, QuestionVote
+from quan.models import QuestionsCategory, Question, QuestionVote
 
 admin.site.register(QuestionsCategory)
 admin.site.register(Question)
