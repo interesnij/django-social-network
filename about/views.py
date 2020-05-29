94df1263fadc9ff2e43b4d13e348aa67aa345408
Serg <ochkarik1983@mail.ru@>
2020-05-29 16:56:35 +0300

    F

--- about/views.py
+++ about/views.py
@@ -5,8 +5,8 @@ class AboutView(TemplateView):
     template_name = None
 
     def get(self,request,*args,**kwargs):
-		if request.user.is_authenticated:
-			self.template_name = "about.html"
-		else:
-			self.template_name = "anon_about.html"
-		return super(AboutView,self).get(request,*args,**kwargs)
+        if request.user.is_authenticated:
+            self.template_name = "about.html"
+        else:
+            self.template_name = "anon_about.html"
+        return super(AboutView,self).get(request,*args,**kwargs)
