02f148e3cceafe9f2a8f2daec4ab1393b5cc8111
Serg <ochkarik1983@mail.ru@>
2020-05-29 18:14:11 +0300

    F

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from about.views import AboutView
+from about.views import AboutView, TermsView, PolicyView, LicenceView
 
 
 urlpatterns = [
     url(r'^$', AboutView.as_view(), name='about'),
+    url(r'^terms/$', TermsView.as_view(), name='terms'),
+    url(r'^policy$', PolicyView.as_view(), name='policy'),
+    url(r'^licence/$', LicenceView.as_view(), name='licence'),
 ]
--- about/views.py
+++ about/views.py
@@ -10,3 +10,36 @@ class AboutView(TemplateView):
         else:
             self.template_name = "anon_about.html"
         return super(AboutView,self).get(request,*args,**kwargs)
+
+
+class TermsView(TemplateView):
+    template_name = None
+
+    def get(self,request,*args,**kwargs):
+        if request.user.is_authenticated:
+            self.template_name = "terms/terms.html"
+        else:
+            self.template_name = "terms/anon_terms.html"
+        return super(TermsView,self).get(request,*args,**kwargs)
+
+
+class PolicyView(TemplateView):
+    template_name = None
+
+    def get(self,request,*args,**kwargs):
+        if request.user.is_authenticated:
+            self.template_name = "policy/policy.html"
+        else:
+            self.template_name = "policy/anon_policy.html"
+        return super(PolicyView,self).get(request,*args,**kwargs)
+
+
+class LicenceView(TemplateView):
+    template_name = None
+
+    def get(self,request,*args,**kwargs):
+        if request.user.is_authenticated:
+            self.template_name = "licence/licence.html"
+        else:
+            self.template_name = "licence/anon_licence.html"
+        return super(AboutView,self).get(request,*args,**kwargs)
