d932560462b3e343c85769861e948928b452c947
Serg <ochkarik1983@mail.ru@>
2020-05-29 11:35:42 +0300

    F

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -1,4 +1,6 @@
 function elementInViewport(el){var bounds = el.getBoundingClientRect();return ((bounds.top + bounds.height > 0) && (window.innerHeight - bounds.top > 0));}
+function on(elSelector,eventName,selector,fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while(el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
+
 function load_chart() {
   try{
 var ctx = document.getElementById('canvas');
