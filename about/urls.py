de70df24eb3d7311188b4d3a2ad7bf5de0944c8f
Serg <ochkarik1983@mail.ru@>
2020-05-30 11:00:58 +0300

    H

--- about/urls.py
+++ about/urls.py
@@ -5,6 +5,6 @@ from about.views import AboutView, TermsView, PolicyView, LicenceView
 urlpatterns = [
     url(r'^$', AboutView.as_view(), name='about'),
     url(r'^terms/$', TermsView.as_view(), name='terms'),
-    url(r'^policy$', PolicyView.as_view(), name='policy'),
+    url(r'^privacy$', PolicyView.as_view(), name='policy'),
     url(r'^licence/$', LicenceView.as_view(), name='licence'),
 ]
