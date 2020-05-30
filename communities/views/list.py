235e6e691e42c9559a9b27c2158e532129f0f949
Serg <ochkarik1983@mail.ru@>
2020-05-30 21:03:27 +0300

    H

--- communities/views/list.py
+++ communities/views/list.py
@@ -51,12 +51,12 @@ class CommunityFriendsView(ListView):
 	def get_queryset(self):
 		import re
 
-		if self.community.is_private() and not self.request.user.is_member_of_community_with_name(self.name):
+		if self.community.is_private() and not self.request.user.is_member_of_community_with_name(self.community.name):
 			frends = None
 			self.template_name = "c_detail/private_community.html"
 		else:
 			frends = self.request.user.get_common_friends_of_community(self.community.pk)
-			self.template_name = "c_detail/friends.html" 
+			self.template_name = "c_detail/friends.html"
 		MOBILE_AGENT_RE = re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
 		if MOBILE_AGENT_RE.match(self.request.META['HTTP_USER_AGENT']):
 			self.template_name = "mob_" + self.template_name
