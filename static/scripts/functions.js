bd9ef26b03ec85ea1feabe7427104c486df5bd2b
Serg <ochkarik1983@mail.ru@>
2020-05-31 15:26:48 +0300

    H

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -675,7 +675,7 @@ function music_onReady(){console.log("Аудио плеер готов");}
             rtr.innerHTML = ajax.innerHTML;
             window.scrollTo(0,0);
             document.title = elem_.querySelector('title').innerHTML;
-            window.history.pushState({}, document.title, url);
+            window.history.pushState(null, document.title, url);
             if_list(rtr);
             load_chart()
           }
