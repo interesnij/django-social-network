86ea0d9e40768d3bad89f73fb6fbf0785c1b1243
Serg <ochkarik1983@mail.ru@>
2020-06-01 17:33:12 +0300

    H

--- static/scripts/user_video.js
+++ static/scripts/user_video.js
@@ -37,7 +37,8 @@ on('#ajax', 'click', '.u_video_detail', function() {
   }, 500);
 });
 function video_onPlay(){
-    video_id = video_player.getVideoId() + 1;
+    video_id = video_player.getVideoId();
+    video_id != 0 ? video_id + 1 : video_id = 1;
     console.log("Воспроизводится ролик № : " + video_id);
     my_playlist = document.body.querySelector("#my_playlist");
     videos = my_playlist.querySelectorAll('li');
