9ef59e193435a0ac4e3e42ae5facaa9abba05a03
Serg <ochkarik1983@mail.ru@>
2020-06-01 14:41:13 +0300

    H

--- users/models.py
+++ users/models.py
@@ -578,7 +578,7 @@ class User(AbstractUser):
         from video.models import Video, VideoAlbum
 
         list = VideoAlbum.objects.get(creator_id=self.id, community=None, is_generic=True)
-        video_query = Q(video_album=list, is_deleted=False)
+        video_query = Q(album=list, is_deleted=False)
         count = Video.objects.filter(video_query).values("pk")
         return count.count()
 
@@ -586,7 +586,7 @@ class User(AbstractUser):
         from video.models import Video, VideoAlbum
         try:
             list = VideoAlbum.objects.get(creator_id=self.id, community=None, is_generic=True)
-            video_query = Q(video_album=list, is_deleted=False)
+            video_query = Q(album=list, is_deleted=False)
             video_list = Video.objects.filter(video_query).order_by("-created")
             return video_list
         except:
