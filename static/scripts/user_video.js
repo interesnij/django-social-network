9d64191401204d9530a996efcdf83dc36dfa8ed3
Serg <ochkarik1983@mail.ru@>
2020-06-01 19:20:41 +0300

    H

--- static/scripts/user_video.js
+++ static/scripts/user_video.js
@@ -3,7 +3,8 @@ function get_video_info(){
   my_playlist = document.body.querySelector("#my_playlist");
   videos = my_playlist.querySelectorAll('li');
   video_id = video_player.getVideoId();
-  video_id != 0 ? (uuid = videos[video_id].getAttribute("data-video-uuid"), video_id += 1): (video_id = 1, uuid = videos[0].getAttribute("data-video-uuid"));
+  uuid = videos[video_id].getAttribute("data-video-uuid");
+  video_id != 0 ? (video_id += 1) : (video_id = 1);
   if (info_video.innerHTML == "" && info_video.getAttribute("data-uuid") != uuid){
     pk = document.body.querySelector("#movies_container").getAttribute("data-pk");
     list_load(info_video, "/video/user/detail/" + pk + "/" + uuid + "/");
