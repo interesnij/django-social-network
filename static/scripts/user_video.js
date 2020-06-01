c7ecc85d1b7c2af4b5d4d644fc8b3cdf931efc65
Serg <ochkarik1983@mail.ru@>
2020-06-01 18:41:50 +0300

    H

--- static/scripts/user_video.js
+++ static/scripts/user_video.js
@@ -32,29 +32,26 @@ on('#ajax', 'click', '.u_video_detail', function() {
     console.log("video player ready");
     video_player.addListener(FWDUVPlayer.PLAY, video_onPlay);
     setTimeout(function() {video_player.playVideo(counter)}, 1000);
-
-    info_video = document.body.querySelector("#info_video");
-    console.log(info_video);
-
-    my_playlist = document.body.querySelector("#my_playlist");
-    videos = my_playlist.querySelectorAll('li');
-    video_id = video_player.getVideoId();
-    video_id != 0 ? (video_id += 1, uuid = videos[video_id].getAttribute("data-video-uuid")) : (video_id = 1, uuid = videos[0].getAttribute("data-video-uuid"));
-    if (info_video.innerHTML == "" && info_video.getAttribute("data-uuid") != uuid){
-      pk = document.body.querySelector("#movies_container").getAttribute("data-pk");
-      list_load(info_video, "/video/user/detail/" + pk + "/" + uuid + "/");
-      info_video.setAttribute("data-uuid", uuid);
+    get_video_info()
     }
     }
   }, 500);
 });
+
+function get_video_info(){
+  info_video = document.body.querySelector("#info_video");
+  my_playlist = document.body.querySelector("#my_playlist");
+  videos = my_playlist.querySelectorAll('li');
+  video_id = video_player.getVideoId();
+  video_id != 0 ? (video_id += 1, uuid = videos[video_id].getAttribute("data-video-uuid")) : (video_id = 1, uuid = videos[0].getAttribute("data-video-uuid"));
+  if (info_video.innerHTML == "" && info_video.getAttribute("data-uuid") != uuid){
+    pk = document.body.querySelector("#movies_container").getAttribute("data-pk");
+    list_load(info_video, "/video/user/detail/" + pk + "/" + uuid + "/");
+    info_video.setAttribute("data-uuid", uuid);
+}
+
 function video_onPlay(){
-    my_playlist = document.body.querySelector("#my_playlist");
-    videos = my_playlist.querySelectorAll('li');
-    video_id = video_player.getVideoId();
-
-    video_id != 0 ? (video_id += 1, uuid = videos[video_id].getAttribute("data-video-uuid")) : (video_id = 1, uuid = videos[0].getAttribute("data-video-uuid"));
-
+    get_video_info()
     console.log("Воспроизводится ролик № : " + video_id)
     console.log("uuid ролика : " + uuid);
     music_player.pause();
