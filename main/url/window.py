4201b0f785d8d32d2d55f34d20d28e4624314092
Serg <ochkarik1983@mail.ru@>
2020-06-02 13:21:09 +0300

    H

--- main/url/window.py
+++ main/url/window.py
@@ -7,8 +7,8 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
 	url(r'^u_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserLikeWindow.as_view()),
 	url(r'^u_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemUserDislikeWindow.as_view()),
-    url(r'^u_comment_like_window/(?P<pk>\d+)/(?P<pk>\d+)/$', ItemUserCommentLikeWindow.as_view()),
-	url(r'^u_comment_dislike_window/(?P<pk>\d+)/(?P<pk>\d+)/$', ItemUserCommentDislikeWindow.as_view()),
+    url(r'^u_comment_like_window/(?P<comment_pk>\d+)/(?P<pk>\d+)/$', ItemUserCommentLikeWindow.as_view()),
+	url(r'^u_comment_dislike_window/(?P<comment_pk>\d+)/(?P<pk>\d+)/$', ItemUserCommentDislikeWindow.as_view()),
 	url(r'^c_like_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityLikeWindow.as_view()),
 	url(r'^c_dislike_window/(?P<uuid>[0-9a-f-]+)/(?P<pk>\d+)/$', ItemCommunityDislikeWindow.as_view()),
     url(r'^c_comment_like_window/(?P<comment_pk>\d+)/(?P<pk>\d+)/$', ItemCommunityCommentLikeWindow.as_view()),
