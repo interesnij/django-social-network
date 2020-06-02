c9e3cfea42a43f76ae37d830cde98c20092a894c
Serg <ochkarik1983@mail.ru@>
2020-06-02 13:38:38 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -83,11 +83,11 @@ on('#ajax', 'click', '#holder_article_image', function() {
   get_image_priview(this, img)
 });
 on('#ajax', 'click', '.comment_photo1', function() {
-  img = this.previousElementSibling.querySelector("#id_item_comment_photo")
+  img = this.previousElementSibling.querySelector(".file1")
   get_image_priview(this, img)
 });
 on('#ajax', 'click', '.comment_photo2', function() {
-  img = this.previousElementSibling.querySelector("#id_item_comment_photo2")
+  img = this.previousElementSibling.querySelector(".file2")
   get_image_priview(this, img)
 });
 
