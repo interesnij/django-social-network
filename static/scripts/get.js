0f77a454f29880ed0540c69ec3a1688a4448811a
Serg <ochkarik1983@mail.ru@>
2020-06-03 12:34:26 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -188,6 +188,7 @@ on('#ajax', 'click', '.upload_photo', function() {
   } else{
     $div.innerHTML = '<div class="comment_photo"><input class="file2 hide_image" type="file" name="photo" accept="image/*" id="id_item_comment_photo"><a href="#" style="display:none;position: absolute;15px;" class="delete_thumb">Удалить</a><h4 class="svg_default"><svg fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/>+<path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg></h4></div>'
   }
+  img_block.append($div);
   add_file_dropdown(dropdown)
   is_full_dropdown(dropdown);
 });
