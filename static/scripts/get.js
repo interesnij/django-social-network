014010524b2f980775681a7ef81f68d4ce800ce6
Serg <ochkarik1983@mail.ru@>
2020-06-04 12:37:44 +0300

    F

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -353,7 +353,7 @@ on('#ajax', 'click', '.music_load_detail', function() {
   $span.setAttribute("flow", "up");
 
   $img.src = _this.querySelector("img").getAttribute('data-src');
-  $div.style.width = "65px";
+  $img.style.width = "50px";
   $figure.append($img);
 
   $media.innerHTML = media_body.innerHTML;
