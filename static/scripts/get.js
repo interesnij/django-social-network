07c2c5d2aa58b773636457ad56aab3d738ddd82a
Serg <ochkarik1983@mail.ru@>
2020-06-04 12:53:40 +0300

    F

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -349,7 +349,7 @@ on('#ajax', 'click', '.music_load_detail', function() {
   $img = document.createElement("img");
   $media = document.createElement("div");
   $figure = document.createElement("figure");
-  $figure.style.cursor = "pointer";
+  $figure.classList.add("music_list_item");
 
   $span.classList.add("item_preview_delete");
   $span.innerHTML = '<svg fill="currentColor" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
