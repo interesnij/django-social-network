98b422157d93bfd73571bce599bcbdb63dea597b
Serg <ochkarik1983@mail.ru@>
2020-06-06 20:55:12 +0300

    F

--- static/scripts/good_get.js
+++ static/scripts/good_get.js
@@ -23,8 +23,8 @@ on('#ajax', 'click', '#good_image5', function() {
 on('#ajax', 'click', '.u_good_detail', function() {
   var container, uuid, pk, loader;
   container = this.parentElement;
-  uuid = container.getAttribute('data-uuid');
-  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid');
+  pk = container.getAttribute('data-pk');
   loader = document.getElementById("good_loader");
   open_fullscreen('/goods/user/good/' + pk + '/' + uuid + '/', loader)
 });
