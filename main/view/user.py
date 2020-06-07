41c892e3e801138dfc821e9204a0505d30d98078
Serg <ochkarik1983@mail.ru@>
2020-06-07 13:45:09 +0300

    D

--- main/view/user.py
+++ main/view/user.py
@@ -77,7 +77,6 @@ class ItemReplyUserCreate(View):
                 if user.is_closed_profile():
                     check_is_connected_with_user_with_id(user=request.user, user_id = user.id)
             new_comment = comment.create_comment(commenter=request.user, parent_comment=parent, item=None, text=comment.text,
-                                                photo=form_post.cleaned_data['photo'], photo2=form_post.cleaned_data['photo2'],
                                                 select_photo = request.POST.get('select_photo'), select_photo2 = request.POST.get('select_photo2'),
                                                 select_video = request.POST.get('select_video'), select_video2 = request.POST.get('select_video2'),
                                                 select_music = request.POST.get('select_music'), select_music2 = request.POST.get('select_music2'))
