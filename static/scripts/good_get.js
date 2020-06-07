2c8073af3aa1d45f7dc702aa1a7f6441a8711eaf
Serg <ochkarik1983@mail.ru@>
2020-06-07 09:36:47 +0300

    D

--- static/scripts/good_get.js
+++ static/scripts/good_get.js
@@ -24,7 +24,8 @@ on('#ajax', 'click', '.u_good_detail', function() {
   uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid');
   pk = this.getAttribute('data-pk');
   loader = document.getElementById("good_loader");
-  open_fullscreen('/goods/user/good/' + pk + '/' + uuid + '/', loader)
+  open_fullscreen('/goods/user/good/' + pk + '/' + uuid + '/', loader);
+  good_gallery()
 });
 
 on('#ajax', 'click', '#c_good_add', function() {
@@ -57,15 +58,16 @@ on('#ajax', 'change', '.goods_category', function() {
   };
 });
 
+function good_gallery(){
+  var galleryThumbnail = document.querySelectorAll(".thumbnails-list li");
+  var galleryFeatured = document.querySelector(".product-gallery-featured");
+  console.log(galleryThumbnail);
+  console.log(galleryFeatured);
 
-var galleryThumbnail = document.querySelectorAll(".thumbnails-list li");
-var galleryFeatured = document.querySelector(".product-gallery-featured");
-console.log(galleryThumbnail);
-console.log(galleryFeatured);
-
-galleryThumbnail.forEach((item) => {
-  item.addEventListener("mouseover", function () {
-  image = item.children[0].src;
-    galleryFeatured.src = image;
+  galleryThumbnail.forEach((item) => {
+    item.addEventListener("mouseover", function () {
+    image = item.children[0].src;
+      galleryFeatured.src = image;
+    });
   });
-});
+}
