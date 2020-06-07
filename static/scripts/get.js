54a77bfca2430719b9e14f256167d1dc1da87a16
Serg <ochkarik1983@mail.ru@>
2020-06-07 14:07:45 +0300

    D

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -525,6 +525,7 @@ on('#ajax', 'click', '.good_load_detail', function() {
 
   $div.setAttribute('good-pk', pk);
   $div.style.cursor = "pointer";
+  $div.style.padding = "5px";
   $div.classList.add("u_good_detail");
 
   $span.innerHTML = '<svg class="svg_default" fill="currentColor" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
