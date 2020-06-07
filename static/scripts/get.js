8f6e9596c31626a8f4dc2479d29011507b8a44fd
Serg <ochkarik1983@mail.ru@>
2020-06-07 22:31:23 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -473,6 +473,7 @@ on('#ajax', 'click', '.good_load_detail', function() {
     $input = document.createElement("span");
     $img = document.createElement("img");
     $info = document.createElement("span");
+    $title = document.createElement("span");
 
     if (img_block.querySelector(".select_good2")){
         is_full_dropdown()}
@@ -494,11 +495,16 @@ on('#ajax', 'click', '.good_load_detail', function() {
   $img.classList.add("image_fit");
   $img.src = _this.querySelector("img").getAttribute('data-src');
 
+  title = _this.querySelector(".article_title").innerHTML;
+  $title.innerHTML = '<span class="badge badge-info mb-2" style="position: absolute;bottom: 24px;"><svg style="padding-bottom: 1px" height="13" fill="#FFFFFF" viewBox="0 0 24 24" width="13"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/></svg>'
+     + title + '</span>'
+
   $info.innerHTML = _this.querySelector(".good_info").innerHTML;
   $div.append(get_delete_span());
   $div.append($input);
   $div.append($img);
   $div.append($info);
+  $div.append($title);
   img_block.append($div);
 
   add_file_dropdown()
