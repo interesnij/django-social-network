70a255db146273af5f62a097207777028d0f24b6
Serg <ochkarik1983@mail.ru@>
2020-06-08 21:30:45 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -17,6 +17,7 @@ on('#ajax', 'click', '#form_post_btn', function() {
     new_post = document.createElement("span");
     new_post.innerHTML = elem;
     new_post.querySelector(".card") ? (lenta_load.querySelector(".stream").prepend(new_post.querySelector(".card")),
+                                       toast_info("Запись опубликована"),
                                        lenta_load.querySelector(".post_empty") ? lenta_load.querySelector(".post_empty").style.display = "none" : null)
                                     :  toast_error("Нужно написать или прикрепить что-нибудь!");
   }};
