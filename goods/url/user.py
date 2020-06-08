f452c69952467bf9c945ac47865c582bb4cea259
Serg <ochkarik1983@mail.ru@>
2020-06-08 13:09:12 +0300

    H

--- goods/url/user.py
+++ goods/url/user.py
@@ -6,5 +6,5 @@ urlpatterns=[
 	url(r'^(?P<pk>\d+)/$', UserGoods.as_view(), name="user_goods"),
 	url(r'^good/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserGood.as_view(), name='user_good'),
 	url(r'^add/(?P<pk>\d+)/$', GoodUserCreate.as_view()),
-	url(r'^add_attach/(?P<pk>\d+)/$', GoodUserCreateAttach.as_view()
+	url(r'^add_attach/(?P<pk>\d+)/$', GoodUserCreateAttach.as_view())
 ]
