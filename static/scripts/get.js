c016429b1f4cbb8fd3e21183af17ee8d9c9ff74f
Serg <ochkarik1983@mail.ru@>
2020-06-10 23:35:02 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -262,9 +262,8 @@ on('#ajax', 'click', '.music_load_detail', function() {
 on('#ajax', 'click', '.photo_preview_delete', function() {
   parent = this.parentElement;
   document.body.querySelector(".img_block") ? (img_block = document.body.querySelector(".img_block"),
-                                               if (img_block.querySelector(".photo")){
-                                                 console.log("Есть и еще фото")
-                                               } else{ console.log("Нет фото")})
+                                               img_block.querySelector(".photo") ? console.log("Есть и еще фото")
+                                                                                 : console.log("Нет фото"))
                                             : null
                                             console.log(img_block);
                                             console.log(img_block.querySelector(".photo"));
