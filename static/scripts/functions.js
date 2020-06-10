55bebd30878e86d8d7d09f1e410e766ebb3189ed
Serg <ochkarik1983@mail.ru@>
2020-06-10 22:56:39 +0300

    H

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -1,6 +1,6 @@
-function get_delete_span_post(){
+function get_delete_span(){
   $span = document.createElement("span");
-  $span.classList.add("post_preview_delete");
+  $span.classList.add("item_preview_delete");
   $span.innerHTML = '<svg fill="currentColor" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
   $span.setAttribute("tooltip", "Не прикреплять");
   $span.setAttribute("flow", "up");
