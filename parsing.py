dd73c3fb16b947fc2a38cb0835955c058712b43a
Serg <ochkarik1983@mail.ru@>
2020-06-13 22:06:24 +0300

    F

--- parsing.py
+++ parsing.py
@@ -76,23 +76,4 @@ for tag in list_1_1:
                     genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
                     new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, description=description, duration=track.duration, genre=genre, title=track.title, uri=track.uri, release_year=track.release_year)
                 count = count + 1
-        while tracks.next_href != None and count < 2000:
-            tracks = client.get(tracks.next_href, limit=page_size, linked_partitioning=1)
-            for track in tracks.collection:
-                created_at = track.created_at
-                created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-                if track.description:
-                    description = track.description[:500]
-                else:
-                    description=None
-                try:
-                    SoundcloudParsing.objects.get(id=track.id)
-                except:
-                    if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
-                        try:
-                            self_tag = SoundTags.objects.get(name=tag, symbol=litera)
-                        except:
-                            self_tag = SoundTags.objects.create(name=tag, symbol=litera)
-                        genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
-                        new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, description=description, duration=track.duration, genre=genre, title=track.title, uri=track.uri, release_year=track.release_year)
-                    count = count + 1
+        
