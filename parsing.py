da89a1e21843e8d7cfabea0a42d890bfddcd01d1
Serg <ochkarik1983@mail.ru@>
2020-06-13 21:05:46 +0300

    F

--- parsing.py
+++ parsing.py
@@ -1771,7 +1771,7 @@ for tag in a_angl_list_1:
             else:
                 description=None
             try:
-                SoundcloudParsing.objects.get(id=track.id)
+                SoundParsing.objects.get(id=track.id)
             except:
                 if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
                     try:
@@ -1779,6 +1779,25 @@ for tag in a_angl_list_1:
                     except:
                         self_tag = SoundTags.objects.create(name=tag, symbol=litera)
                     genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
-                    new_track = SoundcloudParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, description=description, title=track.title, uri=track.uri, release_year=track.release_year)
+                    new_track = SoundParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, description=description, title=track.title, uri=track.uri, release_year=track.release_year)
                 count = count + 1
-        
+        while tracks.next_href != None and count < 2000:
+            tracks = client.get(tracks.next_href, limit=page_size, linked_partitioning=1)
+            for track in tracks.collection:
+                created_at = track.created_at
+                created_at = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+                if track.description:
+                    description = track.description[:500]
+                else:
+                    description=None
+                try:
+                    SoundParsing.objects.get(id=track.id)
+                except:
+                    if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
+                        try:
+                            self_tag = SoundTags.objects.get(name=tag, symbol=litera)
+                        except:
+                            self_tag = SoundTags.objects.create(name=tag, symbol=litera)
+                        genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
+                        new_track = SoundParsing.objects.create(id=track.id, tag=self_tag, artwork_url=track.artwork_url, created_at=created_at, duration=track.duration, genre=genre, description=description, title=track.title, uri=track.uri, release_year=track.release_year)
+                    count = count + 1
