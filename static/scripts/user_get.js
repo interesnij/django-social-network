638f455bc4ed04af0953c62ce796b03da6e9683a
Serg <ochkarik1983@mail.ru@>
2020-06-13 17:42:15 +0300

    F

--- static/scripts/user_get.js
+++ static/scripts/user_get.js
@@ -34,7 +34,7 @@ on('#ajax', 'click', '.u_all_likes', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   uuid = container.getAttribute('item-uuid');
   loader = document.getElementById("votes_loader");
-  open_fullscreen("/item_window/all_user_like/" + uuid + "/" + pk + "/", loader)
+  open_fullscreen("/posts/item_window/all_user_like/" + uuid + "/" + pk + "/", loader)
 });
 on('#ajax', 'click', '.u_all_dislikes', function() {
   var container, uuid, pk, loader;
@@ -42,7 +42,7 @@ on('#ajax', 'click', '.u_all_dislikes', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   uuid = container.getAttribute('item-uuid');
   loader = document.getElementById("votes_loader");
-  open_fullscreen("/item_window/all_user_dislike/" + uuid + "/" + pk + "/", loader)
+  open_fullscreen("/posts/item_window/all_user_dislike/" + uuid + "/" + pk + "/", loader)
 });
 on('#ajax', 'click', '.u_all_reposts', function() {
   var container, uuid, pk, loader;
@@ -50,7 +50,7 @@ on('#ajax', 'click', '.u_all_reposts', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   uuid = container.getAttribute('item-uuid');
   loader = document.getElementById("votes_loader");
-  open_fullscreen("/item_window/all_user_reposts/" + uuid + "/" + pk + "/", loader)
+  open_fullscreen("/posts/item_window/all_user_reposts/" + uuid + "/" + pk + "/", loader)
 });
 
 on('#ajax', 'click', '.u_item_comments', function() {
@@ -59,7 +59,7 @@ on('#ajax', 'click', '.u_item_comments', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   uuid = parent.getAttribute("item-uuid");
   //this.parentElement.parentElement.nextElementSibling.classList.toggle("comments_open");
-  url = "/user/comment/" + uuid + "/" + pk + "/";
+  url = "/posts/user/comment/" + uuid + "/" + pk + "/";
   list_load(parent.querySelector(".u_load_comments"), url);
   this.classList.toggle("comments_open");
 });
