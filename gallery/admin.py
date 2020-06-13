80080097249729f3c5ed6af5547f8e89a6d2e67d
Serg <ochkarik1983@mail.ru@>
2020-06-13 13:44:57 +0300

    F

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
-from gallery.models import Album, Photo
+#from gallery.models import Album, Photo
 
 
-class AlbumAdmin(admin.ModelAdmin):
-    list_display = ['title','creator','created', 'community', 'is_deleted']
-    list_filter = ['creator', ]
+#class AlbumAdmin(admin.ModelAdmin):
+#    list_display = ['title','creator','created', 'community', 'is_deleted']
+#    list_filter = ['creator', ]
 
-    class Meta:
-            model = Album
+#    class Meta:
+#            model = Album
 
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['creator','created', 'community', 'is_deleted']
-    list_filter = ['creator', ]
+#class PhotoAdmin(admin.ModelAdmin):
+#    list_display = ['creator','created', 'community', 'is_deleted']
+#    list_filter = ['creator', ]
 
-    class Meta:
-            model = Photo
+#    class Meta:
+#            model = Photo
 
-admin.site.register(Album, AlbumAdmin)
-admin.site.register(Photo, PhotoAdmin)
+#admin.site.register(Album, AlbumAdmin)
+#admin.site.register(Photo, PhotoAdmin)
