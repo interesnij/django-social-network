9059af7e8eb99e1de3a4f1c529a0a7f444d70e19
Serg <ochkarik1983@mail.ru@>
2020-06-14 20:33:21 +0300

    G

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,9 @@ from rest_framework.exceptions import ValidationError
 from django.db.models import Q
 from django.contrib.postgres.indexes import BrinIndex
 from django.utils import timezone
+from notifications.model.item import item_notification_handler
 from common.model.votes import PostVotes, PostCommentVotes
+
 
 
 class Post(models.Model):
