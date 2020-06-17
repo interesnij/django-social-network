91317bbb27e40bf08d44bee782586679a17b9015
Serg <ochkarik1983@mail.ru@>
2020-06-17 18:21:45 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -464,10 +464,8 @@ on('#ajax', 'click', '.music_list_post', function() {
   var item_pk = item.getAttribute('data-pk');
   if (!document.body.classList.contains("item_" + item_pk)){
     document.querySelector("body").classList.add("item_" + item_pk);
-    list = item.querySelectorAll(".music");
-    arrayNodes = Array.slice.call(list, 0);
-    arrayNodes.reverse();
-    for(i=0; i<arrayNodes.length; i++) {
+    list = [].slice.call(item.querySelectorAll(".music"), 0).reverse();
+    for(i=0; i<list.length; i++) {
       _source=list[i].getAttribute("data-path") + '/stream?client_id=' + 'dce5652caa1b66331903493735ddd64d';
       _title=list[i].querySelector(".music_title").innerHTML;
       _thumbPath=list[i].querySelector(".music_thumb").innerHTML || null;
