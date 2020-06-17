f2646a7ba80d9fea006932331f9ba223bf3afbed
Serg <ochkarik1983@mail.ru@>
2020-06-17 11:09:50 +0300

    H

--- gallery/view/user.py
+++ gallery/view/user.py
@@ -76,8 +76,9 @@ class UserAddAvatar(View):
                 user_profile = UserProfile.objects.get(user=request.user)
             except:
                 user_profile = UserProfile.objects.create(user=request.user)
-            user_profile.b_avatar = request.FILES.get('file')
-            user_profile.s_avatar = request.FILES.get('file')
+            user_profile.b_avatar = photo
+            user_profile.s_avatar = photo
+            user_profile.save()
             return render_to_response('photo_user/my_photo.html',{'object': photo, 'user': request.user, 'request': request})
         else:
             return HttpResponseBadRequest()
