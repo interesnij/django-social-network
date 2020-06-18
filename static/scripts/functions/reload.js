0cb57f630d796f92456d9ac7a9345e462b9fba14
Serg <ochkarik1983@mail.ru@>
2020-06-18 15:14:21 +0300

    H

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -19,36 +19,7 @@ function create_reload_page(form, post_link, history_link) {
     ajax_link.send(form_data);
 }
 
-
-function get_pagination(items, link, items_list) {
-	page = 2;
-	loaded = false;
-	document.addEventListener('scroll', function() {
-	console.log("scroooool");
-		if(items.getElementsByClassName('card').length === (page-1)*3){
-	  var height = document.documentElement.clientHeight-1;
-	  if(window.scrollY+1 >= document.documentElement.scrollHeight-height){
-	    if (loaded){return};
-	    var link_3 = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-	    link_3.open( 'GET', link + '/?page=' + page++, true );
-
-	    link_3.onreadystatechange = function () {
-	    if ( this.readyState == 4 && this.status == 200 ) {
-	      var elem = document.createElement('span');
-	      elem.innerHTML = link_3.responseText;
-	      if(elem.getElementsByClassName('card').length < 3){loaded = true; return};
-				if (elem.querySelector(items_list)){
-					xxx = document.createElement("span");
-					xxx.innerHTML = elem.querySelector(items_list).innerHTML;
-					items.append(xxx)
-				} else {items.append(elem)}
-	      }
-	    }
-			link_3.send();
-	  }}
-});
-}
-function paggg(block, link, block_2){
+function scrolled(block, link, block_2){
 	onscroll = function(){
 		var box = block.querySelector('.last');
 		if(box && box.classList.contains("last")){
@@ -85,22 +56,23 @@ function paginate(block, link, block_2){
 }
 
 function create_pagination(block){
+	// подключаем подгрузкку списков всех страниц с содержимым. Прозванивать придется все страницы со списками.
 	if(block.querySelector('#music_tag_container')){
     music_tag = block.querySelector('#tag_container');
 		_link = music_tag.getAttribute("data-link");
-		paggg(music_tag, _link, '#tag_container')
+		scrolled(music_tag, _link, '#tag_container')
   } else if(block.querySelector('#lenta_load')){
 		lenta_load = block.querySelector('#lenta_load');
 		_link = lenta_load.getAttribute("data-link");
-		get_pagination(lenta_load, _link, '#lenta_load');
+		scrolled(lenta_load, _link, '#lenta_load');
 	} else if(block.querySelector('#music_genre_container')){
 		music_genre = block.querySelector('#genre_container');
 		_link = music_genre.getAttribute("data-link");
-		get_pagination(music_genre, _link, '#genre_container');
+		scrolled(music_genre, _link, '#genre_container');
 	}
 }
 function if_list(block){
-  // проверяем, если ли на странице блок с подгрузкой списка. Если есть, грузим список
+  // проверяем, если ли на странице блок с подгрузкой списка. Если есть, грузим список/С пагинацией сразу
   if(block.querySelector('#news_load')){
     news_load = block.querySelector('#news_load');link = news_load.getAttribute("data-link");
     list_load(block.querySelector("#news_load"), link);
@@ -108,7 +80,7 @@ function if_list(block){
     lenta_load = block.querySelector('#lenta_load');
 		link = lenta_load.getAttribute("data-link");
     list_load(lenta_load, link);
-		paggg(lenta_load, link, '#lenta_load')
+		scrolled(lenta_load, link, '#lenta_load')
 
   }else if(block.querySelector('#lenta_community')){
     lenta_community = block.querySelector('#lenta_community');link = lenta_community.getAttribute("data-link");
@@ -144,11 +116,7 @@ function ajax_get_reload(url) {
         window.history.pushState(null, "vfgffgfgf", url);
         document.title = title;
         if_list(rtr);
-				//create_pagination(rtr);
         load_chart();
-
-				page = 2;
-				loaded = false;
 				create_pagination(rtr);
       }
     }
