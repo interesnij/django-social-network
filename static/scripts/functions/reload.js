b6ba3bac973eb869f6a1bc4fc7673ef1230393aa
Serg <ochkarik1983@mail.ru@>
2020-06-18 12:14:54 +0300

    H

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -24,7 +24,7 @@ function get_pagination(items, link, items_list) {
 	page = 2;
 	loaded = false;
 	ajax = document.querySelector("#ajax");
-	on('body', 'scroll', ajax, function() {
+	on('body', 'onscroll', ajax, function() {
 	console.log("scroooool");
 	//try{
 		if(items.getElementsByClassName('card').length === (page-1)*3){
