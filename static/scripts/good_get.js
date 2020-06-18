06707fe4933439b5a874da6040bdc6fcfdefe166
Serg <ochkarik1983@mail.ru@>
2020-06-18 20:49:44 +0300

    H

--- static/scripts/good_get.js
+++ static/scripts/good_get.js
@@ -128,7 +128,7 @@ on('#ajax', 'click', '#add_good_user_btn', function() {
     } else if (document.querySelector(".attach_block")){
       block = document.body.querySelector(".attach_block");
       is_full_attach();
-      data_pk = new_good.getAttribute('good-pk');
+      data_pk = new_good.querySelector(".new_image").getAttribute('good-pk');
       title = new_good.querySelector(".good_title").innerHTML;
 
       if (block.querySelector( '[good-pk=' + '"' + data_pk + '"]' )){
@@ -137,16 +137,16 @@ on('#ajax', 'click', '#add_good_user_btn', function() {
         return
       };
       new_good.classList.add("attach_toggle");
-      if (!block.querySelector(".good_input")){div = create_preview_good("select_good1", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good2")){div = create_preview_good("select_good2", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good3")){div = create_preview_good("select_good3", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good4")){div = create_preview_good("select_good4", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good5")){div = create_preview_good("select_good5", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good6")){div = create_preview_good("select_good6", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good7")){div = create_preview_good("select_good7", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good8")){div = create_preview_good("select_good8", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good9")){div = create_preview_good("select_good9", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
-      else if (!block.querySelector(".select_good10")){div = create_preview_good("select_good10", new_good.querySelector("img").getAttribute('data-src'), data_pk, title)}
+      if (!block.querySelector(".good_input")){div = create_preview_good("select_good1", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good2")){div = create_preview_good("select_good2", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good3")){div = create_preview_good("select_good3", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good4")){div = create_preview_good("select_good4", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good5")){div = create_preview_good("select_good5", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good6")){div = create_preview_good("select_good6", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good7")){div = create_preview_good("select_good7", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good8")){div = create_preview_good("select_good8", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good9")){div = create_preview_good("select_good9", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
+      else if (!block.querySelector(".select_good10")){div = create_preview_good("select_good10", new_good.querySelector(".new_image").getAttribute('data-src'), data_pk, title)}
     block.append(div);
     block.querySelector(".good_input") ? null : ($good_input = document.createElement("span"), $good_input.innerHTML = '<input type="hidden" class="good_input" name="good" value="1">', block.append($good_input));
 
