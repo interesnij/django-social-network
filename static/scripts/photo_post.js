d9a520d6a67b746253248e8c1f7623737243dcac
Serg <ochkarik1983@mail.ru@>
2020-06-20 23:34:15 +0300

    H

--- static/scripts/photo_post.js
+++ static/scripts/photo_post.js
@@ -26,14 +26,14 @@ on('#ajax', 'click', '.u_photo_like', function() {
   photo = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   uuid = photo.getAttribute("photo-uuid");
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
-  send_like(item, "/gallery/votes/user_like/" + uuid + "/" + pk + "/");
+  send_like(photo, "/gallery/votes/user_like/" + uuid + "/" + pk + "/");
   vote_reload("/gallery/photo_window/u_like_window/" + uuid + "/", "/gallery/photo_window/u_dislike_window/" + uuid + "/", this.nextElementSibling, this.nextElementSibling.nextElementSibling.nextElementSibling)
 });
 on('#ajax', 'click', '.u_photo_dislike', function() {
   photo = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   uuid = photo.getAttribute("photo-uuid");
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
-  send_dislike(item, "/gallery/votes/user_dislike/" + uuid + "/" + pk + "/");
+  send_dislike(photo, "/gallery/votes/user_dislike/" + uuid + "/" + pk + "/");
   vote_reload("/gallery/photo_window/u_like_window/" + uuid + "/", "/gallery/photo_window/u_dislike_window/" + uuid + "/", this.previousElementSibling, this.nextElementSibling)
 });
 on('#ajax', 'click', '.u_photo_dislike2', function() {
@@ -41,7 +41,7 @@ on('#ajax', 'click', '.u_photo_dislike2', function() {
   photo = _this.parentElement;
   comment_pk = photo.getAttribute("data-pk");
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
-  send_like(item, "/gallery/votes/user_comment/" + comment_pk + "/" + pk + "/like/");
+  send_like(photo, "/gallery/votes/user_comment/" + comment_pk + "/" + pk + "/like/");
   vote_reload("/gallery/photo_window/u_comment_like_window/" + comment_pk + "/", "/gallery/photo_window/u_comment_dislike_window/" + comment_pk + "/", _this.nextElementSibling, _this.nextElementSibling.nextElementSibling.nextElementSibling)
 });
 on('#ajax', 'click', '.u_photo_dislike2', function() {
@@ -49,6 +49,6 @@ on('#ajax', 'click', '.u_photo_dislike2', function() {
   photo = _this.parentElement;
   comment_pk = photo.getAttribute("data-pk");
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
-  send_dislike(item, "/gallery/votes/user_comment/" + comment_pk + "/" + pk + "/dislike/");
+  send_dislike(photo, "/gallery/votes/user_comment/" + comment_pk + "/" + pk + "/dislike/");
   vote_reload("/gallery/photo_window/u_comment_like_window/" + comment_pk + "/", "/gallery/photo_window/u_comment_dislike_window/" + comment_pk + "/", _this.previousElementSibling, _this.nextElementSibling)
 });
