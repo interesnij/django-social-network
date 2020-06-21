9465322f8279bec561798e8b466df989acce3cbb
Serg <ochkarik1983@mail.ru@>
2020-06-21 20:29:44 +0300

    F

--- static/scripts/photo_post.js
+++ static/scripts/photo_post.js
@@ -47,12 +47,13 @@ on('#ajax', 'click', '.u_photo_on_votes', function() {
 })
 
 on('#ajax', 'click', '.user_photo_remove', function() {
-  send_photo_change(this, "/gallery/user_progs/delete/", "user_photo_abort_remove btn_danger", "Отмена");
+  send_photo_change(this, "/gallery/user_progs/delete/", "user_photo_abort_remove", "Отмена");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   this.parentElement.nextElementSibling.style.display = "none";
   post.querySelector(".order-2").style.display = "none";
   post.style.opacity = "0.5";
   this.style.opacity = "1";
+  this.style.color = "#FF0000";
 })
 on('#ajax', 'click', '.user_photo_abort_remove', function() {
   send_photo_change(this, "/gallery/user_progs/delete/", "user_photo_remove", "Удалить");
