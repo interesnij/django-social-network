d198a562a1be580aa0a60aead8f32700c20cd97c
Serg <ochkarik1983@mail.ru@>
2020-06-21 19:45:42 +0300

    F

--- static/scripts/photo_post.js
+++ static/scripts/photo_post.js
@@ -16,10 +16,14 @@ on('#ajax', 'click', '.u_replyParentPostComment', function() {
 });
 
 on('#ajax', 'click', '.u_photo_off_comment', function() {
-  send_change(this, "/gallery/user_progs/off_comment/", "u_photo_on_comment", "Вкл. комментарии")
+  send_change(this, "/gallery/user_progs/off_comment/", "u_photo_on_comment", "Вкл. комментарии");
+  post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  post.querySelector(".u_item_comments").style.display = "none"
 })
 on('#ajax', 'click', '.u_photo_on_comment', function() {
-  send_change(this, "/gallery/user_progs/on_comment/", "u_photo_off_comment", "Выкл. комментарии")
+  send_change(this, "/gallery/user_progs/on_comment/", "u_photo_off_comment", "Выкл. комментарии");
+  post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  post.querySelector(".u_item_comments").style.display = "unset"
 })
 
 on('#ajax', 'click', '.u_photo_off_private', function() {
@@ -30,10 +34,16 @@ on('#ajax', 'click', '.u_photo_on_private', function() {
 })
 
 on('#ajax', 'click', '.u_photo_off_votes', function() {
-  send_change(this, "/gallery/user_progs/off_votes/", "u_photo_on_votes", "Вкл. реакции")
+  send_change(this, "/gallery/user_progs/off_votes/", "u_photo_on_votes", "Вкл. реакции");
+  post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  post.querySelector(".like").style.display = "none";
+  post.querySelector(".dislike").style.display = "none";
 })
 on('#ajax', 'click', '.u_photo_on_votes', function() {
-  send_change(this, "/gallery/user_progs/on_votes/", "u_photo_off_votes", "Выкл. реакции")
+  send_change(this, "/gallery/user_progs/on_votes/", "u_photo_off_votes", "Выкл. реакции");
+  post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  post.querySelector(".like").style.display = "unset";
+  post.querySelector(".dislike").style.display = "unset";
 })
 
 on('#ajax', 'click', '.u_photo_like', function() {
