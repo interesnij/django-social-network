3835dcae684b8742ed90c3a26254f50d8b002e2f
Serg <ochkarik1983@mail.ru@>
2020-06-22 22:26:21 +0300

    H

--- gallery/url/community.py
+++ gallery/url/community.py
@@ -9,7 +9,7 @@ urlpatterns=[
 
 	url(r'^add_photo/(?P<pk>\d+)/$', PhotoCommunityCreate.as_view()),
 	url(r'^add_comment_photo/(?P<pk>\d+)/$', PhotoAttachCommunityCreate.as_view()),
-	url(r'^add_album_photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityAlbumUserCreate.as_view()),
+	url(r'^add_album_photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', PhotoAlbumCommunityCreate.as_view()),
 	url(r'^add_album/(?P<pk>\d+)/$', AlbumCommunityCreate.as_view()),
 
 	url(r'^add_avatar/(?P<pk>\d+)/$', CommunityAddAvatar.as_view()),
