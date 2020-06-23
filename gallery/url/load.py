e55a1d7bad1048e34f8f6820b4c77b877c4e238c
Serg <ochkarik1983@mail.ru@>
2020-06-23 19:49:33 +0300

    H

--- gallery/url/load.py
+++ gallery/url/load.py
@@ -9,7 +9,7 @@ urlpatterns=[
     url(r'^avatar_detail/(?P<uuid>[0-9a-f-]+)/$', UserDetailAvatar.as_view(), name="user_avatar"),
     url(r'^community_avatar_detail/(?P<uuid>[0-9a-f-]+)/$', CommunityDetailAvatar.as_view(), name="community_avatar"),
     url(r'^community_photo/(?P<uuid>[0-9a-f-]+)/$', CommunityPhoto.as_view(), name="community_photo"),
-    url(r'^community_album_photo/(?P<pk>\d+)/(?P<album_uuid>[0-9a-f-]+)/$', UserAlbumPhoto.as_view(), name="user_album_photo"),
-    url(r'^community_wall/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserWallPhoto.as_view(), name="u_wall_photo"),
+    url(r'^community_album_photo/(?P<pk>\d+)/(?P<album_uuid>[0-9a-f-]+)/$', CommunityAlbumPhoto.as_view(), name="community_album_photo"),
+    url(r'^community_wall/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityWallPhoto.as_view(), name="c_wall_photo"),
 
 ]
