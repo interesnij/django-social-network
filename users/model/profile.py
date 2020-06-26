2e2a62bfc42e76108c97667e0a42d8fefdc4dbc6
Serg <ochkarik1983@mail.ru@>
2020-06-26 18:30:08 +0300

    H

--- users/model/profile.py
+++ users/model/profile.py
@@ -154,7 +154,7 @@ class UserStaff(models.Model):
         (SUPPORT, 'Поддержка'),
     )
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_staff', verbose_name="Особый пользователь")
-    level = models.CharField(max_length=5, choices=LEVEL, verbose_name="Уровень доступа")
+    level = models.CharField(max_length=5, choices=LEVEL, blank=True, verbose_name="Уровень доступа")
 
     def __str__(self):
         return self.user.get_full_name()
