547015daac3cecb13cc8b9e2f3cd5f487a601044
Serg <ochkarik1983@mail.ru@>
2020-06-28 20:11:10 +0300

    H

--- managers/views.py
+++ managers/views.py
@@ -16,13 +16,13 @@ class ManagersView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user = User.objects.get(pk=self.kwargs["pk"])
-        if self.user.is_administrator() or self.user.is_community_administrator() or self.user.is_post_administrator() or self.user.is_good_administrator() or self.user.is_photo_administrator() or self.user.is_audio_administrator() or self.user.is_video_administrator():
+        if self.user.is_user_administrator() or self.user.is_community_administrator() or self.user.is_post_administrator() or self.user.is_good_administrator() or self.user.is_photo_administrator() or self.user.is_audio_administrator() or self.user.is_video_administrator():
             self.template_name = "manager/admins.html"
-        elif self.user.is_advertiser() or self.user.is_community_advertiser():
+        elif self.user.is_user_advertiser() or self.user.is_community_advertiser():
             self.template_name = "manager/advertisers.html"
-        elif self.user.is_editor() or self.user.is_community_editor() or self.user.is_post_editor() or self.user.is_good_editor() or self.user.is_photo_editor() or self.user.is_audio_editor() or self.user.is_video_editor():
+        elif self.user.is_user_editor() or self.user.is_community_editor() or self.user.is_post_editor() or self.user.is_good_editor() or self.user.is_photo_editor() or self.user.is_audio_editor() or self.user.is_video_editor():
             self.template_name = "manager/editors.html"
-        elif self.user.is_moderator() or self.user.is_community_moderator() or self.user.is_post_moderator() or self.user.is_good_moderator() or self.user.is_photo_moderator() or self.user.is_audio_moderator() or self.user.is_video_moderator():
+        elif self.user.is_user_moderator() or self.user.is_community_moderator() or self.user.is_post_moderator() or self.user.is_good_moderator() or self.user.is_photo_moderator() or self.user.is_audio_moderator() or self.user.is_video_moderator():
             self.template_name = "manager/moderators.html"
         MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
         if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
