fd122963977f83c73a741df38a504f3488d67b1d
Serg <ochkarik1983@mail.ru@>
2020-06-29 09:48:37 +0300

    H

--- managers/urls.py
+++ managers/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', ManagersView.as_view(), name='managers'),
     url(r'^high_officer/(?P<pk>\d+)/$', SuperManagersView.as_view(), name='super_managers'),
 
-    url(r'^user/', include('managers.url.user')),
-    url(r'^community/', include('managers.url.community')),
-    url(r'^post/', include('managers.url.post')),
-    url(r'^good/', include('managers.url.good')),
-    url(r'^photo/', include('managers.url.photo')),
-    url(r'^video/', include('managers.url.video')),
-    url(r'^audio/', include('managers.url.audio')),
+    url(r'^progs_user/', include('managers.url.progs_user')),
+    url(r'^progs_community/', include('managers.url.progs_community')),
+    url(r'^progs_post/', include('managers.url.progs_post')),
+    url(r'^progs_good/', include('managers.url.progs_good')),
+    url(r'^progs_photo/', include('managers.url.progs_photo')),
+    url(r'^progs_video/', include('managers.url.progs_video')),
+    url(r'^progs_audio/', include('managers.url.progs_audio')),
 ]
