62de20d5dac36f34c8402d9e17fc795c52e7f927
Serg <ochkarik1983@mail.ru@>
2020-07-06 21:53:08 +0300

    G

--- static/scripts/manager/post.js
+++ static/scripts/manager/post.js
@@ -55,6 +55,7 @@ on('#ajax', 'click', '.create_post_suspend_btn', function() {
   }};
   link_.send(form_data);
 });
+
 on('#ajax', 'click', '.create_post_delete_btn', function() {
   _this = this;
   form_data = new FormData(document.querySelector("#post_delete_form"));
@@ -65,6 +66,7 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
   } else if (_this.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute){
     div = _this.parentElement.parentElement.parentElement.parentElement.parentElement;
     pk = div.getAttribute("data-uuid");
+    container = document.body.querySelector(".community_stream");
   }
 
   link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
@@ -75,7 +77,10 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
     toast_info("Пост удален!");
     document.querySelector(".create_fullscreen").style.display = "none";
     document.getElementById("create_loader").innerHTML="";
-    div.remove();
+    div.remove(); 
+    if (container){
+      container.querySelector(".card") ? container.innerHTML = '<div class="card mb-3 post_empty centered"><div class="card-body"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></svg></div><h6 style="margin: 20px;text-align: center;"> Пока ничего нет...</h6></div>' : null
+    }
   }};
 
   link_.send(form_data);
