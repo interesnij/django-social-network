462d56eeaec7d13aef4776496ee0c1501fa3143b
Serg <ochkarik1983@mail.ru@>
2020-07-07 20:33:06 +0300

    H

--- posts/models.py
+++ posts/models.py
@@ -58,7 +58,7 @@ class Post(models.Model):
         return self.creator.get_full_name()
 
     def count_comments(self):
-        parent_comments = PostComment.objects.filter(post_id=self.pk, is_deleted=False)
+        parent_comments = PostComment.objects.filter(post_id=self.pk)
         parents_count = parent_comments.count()
         i = 0
         for comment in parent_comments:
