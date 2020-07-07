008c607bc3c8c77b543eb9b65939bb109425bca8
Serg <ochkarik1983@mail.ru@>
2020-07-07 13:14:45 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -58,15 +58,17 @@ on('#ajax', 'click', '.u_itemComment', function() {
 
 on('#ajax', 'click', '.u_replyItemComment', function() {
   form = this.parentElement.parentElement.parentElement.parentElement;
-  send_comment(form, form.parentElement.parentElement.querySelector(".stream_reply_comments"), '/posts/user/reply-comment/')
+  block = form.parentElement.parentElement.querySelector(".stream_reply_comments");
+  send_comment(form, block, '/posts/user/reply-comment/')
   form.parentElement.style.display = "none";
-  console.log(form.parentElement.parentElement.querySelector(".stream_reply_comments"))
+  block.classList.add("replies_open")
 });
 
 on('#ajax', 'click', '.u_replyParentItemComment', function() {
   form = this.parentElement.parentElement.parentElement.parentElement;
   send_comment(form, form.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement, '/posts/user/reply-comment/')
   form.parentElement.style.display = "none";
+  block.classList.add("replies_open")
 });
 
 /*!
