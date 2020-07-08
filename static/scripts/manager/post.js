276e8e5484fd5274f96b1f8dd2820a5642100854
Serg <ochkarik1983@mail.ru@>
2020-07-08 20:14:58 +0300

    H

--- static/scripts/manager/post.js
+++ static/scripts/manager/post.js
@@ -39,8 +39,8 @@ on('#ajax', 'click', '.create_post_suspend_btn', function() {
     div = document.body.querySelector(".changed");
     pk = div.getAttribute("data-uuid");
     moderation_container = document.body.querySelector("#moderation_post_container");
-  } else if (_this.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute){
-    div = _this.parentElement.parentElement.parentElement.parentElement.parentElement;
+  } else if (_this.getAttribute){
+    div = _this;
     pk = div.getAttribute("data-uuid");
     container_object = document.body.querySelector(".post_container");
   }
@@ -77,8 +77,8 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
     div = document.body.querySelector(".changed");
     pk = div.getAttribute("data-uuid");
     moderation_container = document.body.querySelector("#moderation_post_container");
-  } else if (_this.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute){
-    div = _this.parentElement.parentElement.parentElement.parentElement.parentElement;
+  } else if (_this.getAttribute){
+    div = _this;
     pk = div.getAttribute("data-uuid");
     container_object = document.body.querySelector(".post_container");
   }
@@ -97,9 +97,7 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
     } else if (moderation_container && !moderation_container.querySelector(".pag")){
       moderation_container.innerHTML = '<div class="card mb-3 post_empty centered"><div class="card-body"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></svg></div><h6 style="margin: 20px;text-align: center;"> Жалоб пока нет...</h6></div>'
     }
-
   }};
-
   link_.send(form_data);
 });
 
