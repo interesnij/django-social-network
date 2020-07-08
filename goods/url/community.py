aed55d4aac39bbb0c765bfa7b35f0f1e65f843c7
Serg <ochkarik1983@mail.ru@>
2020-07-08 11:09:21 +0300

    H

--- goods/url/community.py
+++ goods/url/community.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 urlpatterns=[
 	url(r'^(?P<pk>\d+)/$', CommunityGoods.as_view(), name="community_goods"),
 	url(r'^good/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityGood.as_view(), name='community_good'),
-	url(r'^add/(?P<pk>\d+)/$', CommunityUserCreate.as_view(), name="good_add_community"),
+	url(r'^add/(?P<pk>\d+)/$', GoodCommunityCreate.as_view()),
+	url(r'^add_attach/(?P<pk>\d+)/$', GoodCommunityCreateAttach.as_view())
 ]
