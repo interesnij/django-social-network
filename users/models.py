ba7f3191e1981808c2ebdbcfe10e506c1a12f7b0
Serg <ochkarik1983@mail.ru@>
2020-07-10 00:25:57 +0300

    G

--- users/models.py
+++ users/models.py
@@ -308,7 +308,7 @@ class User(AbstractUser):
         return self.follows.filter(user__id=user_id, view=True).exists()
 
     def has_blocked_user_with_id(self, user_id):
-        return self.user_blocks.filter(blocker_id=user_id).exists()
+        return self.user_blocks.filter(blocked_user_id=user_id).exists()
 
     def is_blocked_user_with_id(self, user_id):
         return self.blocked_by_users.filter(blocked_user_id=user_id).exists()
