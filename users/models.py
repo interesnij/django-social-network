f1865c105fb45d047ea5012c94f400f984866a14
Serg <ochkarik1983@mail.ru@>
2020-07-10 00:06:47 +0300

    G

--- users/models.py
+++ users/models.py
@@ -1414,7 +1414,7 @@ class User(AbstractUser):
         return community_to_leave
 
     def has_blocked_user_with_id(self, user_id):
-        return self.blocked_by_users.filter(blocker_id=user_id).exists() 
+        return self.user_blocks.filter(blocked_user_id=user_id).exists() 
 
     def get_last_location(self):
         from users.model.profile import OneUserLocation, TwoUserLocation, ThreeUserLocation
