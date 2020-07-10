e1eb4050f803f40f2d6e78538643bb073f07306d
Serg <ochkarik1983@mail.ru@>
2020-07-10 09:57:22 +0300

    H

--- users/views/detail.py
+++ users/views/detail.py
@@ -191,6 +191,8 @@ class ProfileUserView(TemplateView):
             elif self.user.is_closed_profile():
                 if not request.user.is_connected_with_user_with_id(user_id=self.user.pk):
                     self.template_name = "account/close_user.html"
+                elif request.user.is_followers_user_with_id(user_id=self.user.pk):
+                    self.template_name = "account/user.html"
                 else:
                     self.template_name = "account/user.html"
             else:
