fb10b00082c9376b24fd753f7dd11e68ae92f282
Serg <ochkarik1983@mail.ru@>
2020-07-10 10:04:15 +0300

    H

--- users/views/detail.py
+++ users/views/detail.py
@@ -189,7 +189,7 @@ class ProfileUserView(TemplateView):
             elif request.user.is_blocked_with_user_with_id(user_id=self.user.pk):
                 self.template_name = "account/block_user.html"
             elif self.user.is_closed_profile():
-                if request.user.is_followers_user_with_id(user_id=user.pk):
+                if request.user.is_followers_user_with_id(user_id=self.user.pk):
                     self.template_name = "account/user.html"
                 elif not request.user.is_connected_with_user_with_id(user_id=self.user.pk):
                     self.template_name = "account/close_user.html"
