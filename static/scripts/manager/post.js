7de2963c0d4dc6ba858fe4f071207d4954746875
Serg <ochkarik1983@mail.ru@>
2020-07-11 10:00:31 +0300

    H

--- static/scripts/manager/post.js
+++ static/scripts/manager/post.js
@@ -49,7 +49,7 @@ on('#ajax', 'click', '.post_comment_delete_window', function() {
   _this = this;
   if(_this.parentElement.classList.contains("btn_console")){
     div = _this.parentElement.parentElement.parentElement.parentElement;
-    pk = _this.parentElement.parentElement.parentElement.parentElement.getAttribute("data-pk");
+    pk = div.getAttribute("data-pk");
     list = document.querySelectorAll('.pag');
     for (var i = 0; i < list.length; i++) {
       list[i].classList.remove("changed");
@@ -148,9 +148,7 @@ on('#ajax', 'click', '.post_unverify', function() {
     if (!document.body.querySelector(".pag")){
       penalty_container.innerHTML = '<div class="card mb-3 post_empty centered"><div class="card-body"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></svg></div><h6 style="margin: 20px;text-align: center;"> Штраф-лист пуст...</h6></div>'
     }
-
   }};
-
   link_.send();
 });
 
