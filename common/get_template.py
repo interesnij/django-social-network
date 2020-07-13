167f98aac448cb8018cc8ac2b8610b62b4f51864
Serg <ochkarik1983@mail.ru@>
2020-07-13 17:27:59 +0300

    G

--- common/get_template.py
+++ common/get_template.py
@@ -1,6 +1,6 @@
 import re
 from rest_framework.exceptions import PermissionDenied
-from common.checkers import check_is_not_blocked_with_user_with_id, check_is_connected_with_user_with_id
+from common.checkers import check_is_not_blocked_with_user_with_id, check_is_connected_with_user_with_id, check_can_get_posts_for_community_with_name
 
 
 def get_detail_template_user(user, folder, template, request):
@@ -24,34 +24,27 @@ def get_detail_template_user(user, folder, template, request):
 
 def get_detail_template_community(community, folder, template, request_user):
     if community.is_suspended():
-        template_name = "gallery_community/community_suspended.html"
-    elif self.community.is_blocked():
-        template_name = "gallery_community/community_blocked.html"
+        raise PermissionDenied('Ошибка доступа')
+    elif community.is_blocked():
+        raise PermissionDenied('Ошибка доступа')
     elif request_user.is_authenticated:
-        if request_user.is_member_of_community_with_name(community.name):
-            if request.user.is_staff_of_community_with_name(community.name):
-                template_name = folder + "admin_" + template
-            else:
-                template_name = folder + template
+        if request_user.is_staff_of_community_with_name(community.name):
+            template_name = folder + "admin_" + template
         elif request_user.is_community_manager():
             template_name = folder + "staff_" + template
-        elif request_user.is_follow_from_community_with_name(community.pk):
-            template_name = "gallery_community/follow_gallery.html"
-        elif request_user.is_banned_from_community_with_name(community.name):
-            template_name = "gallery_community/block_gallery.html"
+        elif check_can_get_posts_for_community_with_name(request_user, community.name):
+            template_name = folder + template
         elif community.is_public():
             template_name = folder + template
-        elif community.is_closed():
-            template_name = "gallery_community/close_gallery.html"
-        elif community.is_private():
-            template_name = "gallery_community/private_gallery.html"
+        else:
+            raise PermissionDenied('Ошибка доступа')
     elif request_user.is_anonymous:
         if community.is_public():
-            template_name = "c_photo/anon_avatar.html"
+            template_name = folder + "anon_" + template
         elif community.is_closed():
-            template_name = "gallery_community/anon_close_gallery.html"
+            raise PermissionDenied('Ошибка доступа')
         elif community.is_private():
-            template_name = "gallery_community/anon_private_gallery.html"
+            raise PermissionDenied('Ошибка доступа')
     return template_name
 
 
