f1ac603051d74b37ad100e4b307882f9e8b4c84d
Serg <ochkarik1983@mail.ru@>
2020-07-14 18:20:01 +0300

    J

--- goods/models.py
+++ goods/models.py
@@ -128,18 +128,17 @@ class Good(models.Model):
 		return count_reposts
 
 	def get_comments(self):
-        comments_query = Q(photo_comment_id=self.pk)
-        comments_query.add(Q(parent_comment__isnull=True), Q.AND)
-        return GoodComment.objects.filter(comments_query)
-
-    def count_comments(self):
-        parent_comments = GoodComment.objects.filter(good_comment_id=self.pk)
-        parents_count = parent_comments.count()
-        i = 0
-        for comment in parent_comments:
-            i = i + comment.count_replies()
-        i = i + parents_count
-        return i
+		comments_query = Q(photo_comment_id=self.pk)
+		comments_query.add(Q(parent_comment__isnull=True), Q.AND)
+		return GoodComment.objects.filter(comments_query)
+	def count_comments(self):
+		parent_comments = GoodComment.objects.filter(good_comment_id=self.pk)
+		parents_count = parent_comments.count()
+		i = 0
+		for comment in parent_comments:
+			i = i + comment.count_replies()
+		i = i + parents_count
+		return i
 
 
 class GoodComment(models.Model):
