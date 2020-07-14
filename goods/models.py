d75ec0733df0607cac2644b54c7b0524ae052e26
Serg <ochkarik1983@mail.ru@>
2020-07-14 18:21:11 +0300

    J

--- goods/models.py
+++ goods/models.py
@@ -208,17 +208,15 @@ class GoodComment(models.Model):
     def window_dislikes(self):
         dislikes = GoodCommentVotes.objects.filter(good_id=self.pk, vote__lt=0)
         return dislikes[0:6]
-
 	def likes_count(self):
-        likes = GoodCommentVotes.objects.filter(item_id=self.pk, vote__gt=0).values("pk")
-        return likes.count()
+		likes = GoodCommentVotes.objects.filter(item_id=self.pk, vote__gt=0).values("pk")
+		return likes.count()
 
-    def dislikes_count(self):
-        dislikes = GoodCommentVotes.objects.filter(item_id=self.pk, vote__lt=0).values("pk")
-        return dislikes.count()
-
-    def __str__(self):
-        return str(self.good)
+	def dislikes_count(self):
+		dislikes = GoodCommentVotes.objects.filter(item_id=self.pk, vote__lt=0).values("pk")
+		return dislikes.count()
+	def __str__(self):
+		return str(self.good)
 
     @classmethod
     def create_comment(cls, commenter, good=None, parent_comment=None, text=None, created=None ):
