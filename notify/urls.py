4f2ae315e06102b134b627d39eda01d2acc0d90a
Serg <ochkarik1983@mail.ru@>
2020-07-14 22:45:20 +0300

    J

--- notify/urls.py
+++ notify/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 
 
 urlpatterns=[
-	url(r'^user/', include('notifications.url.user')),
-	url(r'^item/', include('notifications.url.item')),
-	url(r'^good/', include('notifications.url.good')),
-	url(r'^photo/', include('notifications.url.photo')),
+	url(r'^user/', include('notify.url.user')),
+	url(r'^item/', include('notify.url.item')),
+	url(r'^good/', include('notify.url.good')),
+	url(r'^photo/', include('notify.url.photo')),
 ]
