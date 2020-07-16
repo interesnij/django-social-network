3951e39dc3a35d830dea7f41a40bc11b706e8ba1
Serg <ochkarik1983@mail.ru@>
2020-07-16 14:17:25 +0300

    G

--- video/urls.py
+++ video/urls.py
@@ -5,6 +5,8 @@ from video.views import AllVideoView
 urlpatterns = [
     url(r'^$', AllVideoView.as_view(), name='all_video'),
 
-    url(r'^progs/', include('video.url.progs')),
+    url(r'^user_progs/', include('video.url.user_progs')),
+    url(r'^community_progs/', include('video.url.community_progs')),
     url(r'^user/', include('video.url.user')),
+    url(r'^community/', include('video.url.community')),
 ]
