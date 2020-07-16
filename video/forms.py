505b88c9eed6ee496c69b543754d325b146de79d
Serg <ochkarik1983@mail.ru@>
2020-07-16 14:46:11 +0300

    G

--- video/forms.py
+++ video/forms.py
@@ -1,4 +1,4 @@
-from video.models import VideoAlbum, Video
+from video.models import VideoAlbum, Video, Video
 from django import forms
 
 
@@ -14,3 +14,10 @@ class VideoForm(forms.ModelForm):
 	class Meta:
 		model = Video
 		fields = ['title', 'description', 'is_public', 'image', 'category', 'album', 'is_child', 'comments_enabled', 'uri']
+
+class CommentForm(forms.ModelForm):
+	text = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control text-comment form-control-rounded'}))
+
+	class Meta:
+		model = VideoComment
+		fields = ['text']
