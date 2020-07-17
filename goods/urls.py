ec5c0507874ca6f70bf32c4f0f1fa217aa630b49
Serg <ochkarik1983@mail.ru@>
2020-07-17 16:35:56 +0300

    H

--- goods/urls.py
+++ goods/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 
 
 urlpatterns=[
+	url(r'^user_progs/', include('goods.url.user_progs')),
+	url(r'^community_progs/', include('goods.url.community_progs')),
 	url(r'^progs/', include('goods.url.progs')),
 	url(r'^user/', include('goods.url.user')),
 	url(r'^community/', include('goods.url.community')),
