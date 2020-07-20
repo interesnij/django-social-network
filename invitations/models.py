f0db92459be53069d1177eb6874e9d18bd5404de
Serg <ochkarik1983@mail.ru@>
2020-07-20 10:58:28 +0300

    H

--- invitations/models.py
+++ invitations/models.py
@@ -10,7 +10,20 @@ class UserInvite(models.Model):
     created = models.DateTimeField(default=timezone.now, null=False, blank=False, verbose_name="Приглашение создано")
     created_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Кто приглашает")
     name = models.CharField(max_length=35, null=True, blank=True)
-    nickname = models.CharField(max_length=35, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True, verbose_name="Емаил")
+    username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
+    token = models.CharField(max_length=255, unique=True)
+    is_invite_email_sent = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return 'UserInvite'
+
+class CommunityInvite(models.Model):
+    invited_by = models.ForeignKey('communities.Community', on_delete=models.CASCADE, related_name='invited_community',null=True, blank=True,verbose_name="В какое сообщество приглашают")
+    created = models.DateTimeField(default=timezone.now, null=False, blank=False, verbose_name="Приглашение создано")
+    created_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Кто приглашает")
+    name = models.CharField(max_length=35, null=True, blank=True)
     email = models.EmailField(null=True, blank=True, verbose_name="Емаил")
     username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
     token = models.CharField(max_length=255, unique=True)
