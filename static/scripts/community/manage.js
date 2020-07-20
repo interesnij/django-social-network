7ee30146205e05372969d08f1ec1f7b8b11bb227
Serg <ochkarik1983@mail.ru@>
2020-07-20 12:33:34 +0300

    H

--- static/scripts/community/manage.js
+++ static/scripts/community/manage.js
@@ -4,7 +4,7 @@ on('#ajax', 'click', '#community_private_post_btn', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   form_data = new FormData(form);
     var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-      ajax_link.open( 'POST', '/communities/manage/private_post/', true );
+      ajax_link.open( 'POST', '/communities/manage/private_post/' + pk + "/", true );
       ajax_link.onreadystatechange = function () {
         if ( this.readyState == 4 && this.status == 200 ) {
             toast_info("Изменения приняты!");
