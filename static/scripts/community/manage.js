b01321dfc7a70cac2c483913abcdd2928f9ab207
Serg <ochkarik1983@mail.ru@>
2020-07-20 18:42:54 +0300

    H

--- static/scripts/community/manage.js
+++ static/scripts/community/manage.js
@@ -51,6 +51,20 @@ on('#ajax', 'click', '#community_private_video_btn', function() {
       ajax_link.send(form_data);
 });
 
+on('#ajax', 'click', '#community_notify_post_btn', function() {
+  form = document.querySelector("#community_notify_post_form");
+  pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
+  form_data = new FormData(form);
+    var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+      ajax_link.open( 'POST', '/communities/manage/notify_post/' + pk + "/", true );
+      ajax_link.onreadystatechange = function () {
+        if ( this.readyState == 4 && this.status == 200 ) {
+            toast_info("Изменения приняты!");
+        }
+      }
+      ajax_link.send(form_data);
+});
+
 on('#ajax', 'click', '.add_staff_options', function() {
     uuid = this.getAttribute("data-uuid");
     status = this.getAttribute("data-status");
