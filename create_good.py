c2a02eda44868384acc137c219528af4cf9c1039
Serg <ochkarik1983@mail.ru@>
2020-07-23 17:53:08 +0300

    H

--- create_good.py
+++ create_good.py
@@ -17,5 +17,5 @@ from goods.models import Good, GoodSubCategory
 from users.models import User
 
 creator = User.objects.get(pk=1)
-sub_category = GoodSubCategory.objects.get(pk=1)
+sub_category = GoodSubCategory.objects.get(order=1)
 new_good = Good.create_good(title="Title", sub_category=sub_category, creator=creator, description="fdkgj ", community=None, price="1000", comments_enabled=True, votes_on=True, status="PG")
