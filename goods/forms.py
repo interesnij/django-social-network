0f40f057433ac325a6b2366e30d9800ccf5e2aff
Serg <ochkarik1983@mail.ru@>
2020-07-23 18:14:34 +0300

    H

--- goods/forms.py
+++ goods/forms.py
@@ -6,18 +6,7 @@ class GoodForm(forms.ModelForm):
 
 	class Meta:
 		model = Good
-		fields = [	'image',
-					'image2',
-					'image3',
-					'image4',
-					'image5',
-					'price',
-					'description',
-					'comments_enabled',
-					'votes_on',
-					'sub_category',
-					'title',
-				]
+		exclude = ['creator']
 
 class CommentForm(forms.ModelForm):
 	text=forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control text-comment form-control-rounded'}))
