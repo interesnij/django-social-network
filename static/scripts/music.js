b559142d2bfde2839fe74d651c5878ad727b0b36
Serg <ochkarik1983@mail.ru@>
2020-07-23 23:21:07 +0300

    H

--- static/scripts/music.js
+++ static/scripts/music.js
@@ -2,5 +2,5 @@ on('#ajax', 'click', '.u_soundcloud_set', function() {
   uuid = this.parentElement.getAttribute('data-uuid');
   pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
   loader = document.getElementById("item_loader");
-  open_fullscreen("/music/manage/user_progs/" + pk, loader)
+  open_fullscreen("/music/user_progs/create_soundcloud_set/" + pk, loader)
 });
