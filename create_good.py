5f9c0e02b0b7124c642ab55fb416eaae387584b4
Serg <ochkarik1983@mail.ru@>
2020-07-23 17:43:39 +0300

    H

---
+++ create_good.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from locale import *
+import csv,sys,os
+
+project_dir = '../tr/tr/'
+
+sys.path.append(project_dir)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django, json, requests
+
+django.setup()
+
+
+from goods.models import Good, GoodSubCategory
+from users.models import User
+
+creator = User.objects.get(pk=1)
+sub_category = GoodSubCategory.objects.get(pk=1)
+new_good = Good.create_good(title="Title", sub_category=sub_category, creator=creator, description="fdkgj ", community=None, price="1000", comments_enabled=True, votes_on=True, status="PG")
