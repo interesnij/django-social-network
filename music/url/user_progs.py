26362e7ac7e6419c89cfb44a0d8f71ee910e7446
Serg <ochkarik1983@mail.ru@>
2020-07-23 23:20:22 +0300

    H

--- music/url/user_progs.py
+++ music/url/user_progs.py
@@ -4,5 +4,5 @@ from music.view.user_progs import *
 
 urlpatterns = [
     url(r'^souncloud_set_window/(?P<pk>\d+)/$', UserSoundcloudSetWindow.as_view()),
-    url(r'^create_soundcloud_set/(?P<pk>\d+)/$', UserSoundcloudSetCreate.as_view()),
+    url(r'^create_soundcloud_set/(?P<pk>\d+)/$', UserSoundcloudSetCreate.as_view()), 
 ]
