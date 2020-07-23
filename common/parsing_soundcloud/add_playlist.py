e754de8f7a4285cbaf42f8fd8c965003c1fe71cf
Serg <ochkarik1983@mail.ru@>
2020-07-23 23:44:28 +0300

    H

--- common/parsing_soundcloud/add_playlist.py
+++ common/parsing_soundcloud/add_playlist.py
@@ -4,7 +4,6 @@ from datetime import datetime, date, time
 
 
 client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
-page_size = 200
 genres_list = SoundGenres.objects.values('name')
 genres_list_names = [name['name'] for name in genres_list]
 
@@ -24,7 +23,6 @@ def load_playlist(permalink_url, request_user, list):
             if track.genre and track.release_year and track.duration > 90000 and track.genre in genres_list_names:
                 genre =SoundGenres.objects.get(name=track.genre.replace("'", '') )
                 new_track = SoundcloudParsing.objects.create(id=track.id,
-                                                            tag=self_tag,
                                                             artwork_url=track.artwork_url,
                                                             created_at=created_at,
                                                             description=description,
