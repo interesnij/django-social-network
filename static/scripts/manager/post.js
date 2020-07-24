f573ce202664debdb32373c919fcdbc5ee85213d
Serg <ochkarik1983@mail.ru@>
2020-07-24 12:47:41 +0300

    H

--- static/scripts/manager/post.js
+++ static/scripts/manager/post.js
@@ -37,9 +37,9 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
     uuid = div.getAttribute("data-uuid");
     moderation_container = document.body.querySelector("#moderation_post_container");
   } else if (_this.getAttribute){
-    div = _this;
-    uuid = div.getAttribute("data-uuid");
+    uuid = _this.getAttribute("data-uuid");
     container_object = document.body.querySelector(".post_container");
+    div = _this.parentElement.parentElement.parentElement.parentElement.parentElement
   }
 
   link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
@@ -51,11 +51,12 @@ on('#ajax', 'click', '.create_post_delete_btn', function() {
     document.querySelector(".worker_fullscreen").style.display = "none";
     document.getElementById("worker_loader").innerHTML="";
     div.remove();
+    try{
     if (container_object && !container_object.querySelector(".card")){
       container_object.innerHTML = '<div class="card mb-3 post_empty centered"><div class="card-body"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></svg></div><h6 style="margin: 20px;text-align: center;"> Пока ничего нет...</h6></div>'
     } else if (moderation_container && !moderation_container.querySelector(".pag")){
       moderation_container.innerHTML = '<div class="card mb-3 post_empty centered"><div class="card-body"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></svg></div><h6 style="margin: 20px;text-align: center;"> Жалоб пока нет...</h6></div>'
-    }
+    }} catch { null }
   }};
   link_.send(form_data);
 });
