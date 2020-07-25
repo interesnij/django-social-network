9e306d7b1027be110e1cf867e5c9c90f16729515
Serg <ochkarik1983@mail.ru@>
2020-07-25 21:14:55 +0300

    H

--- common/parsing_soundcloud/add_playlist.py
+++ common/parsing_soundcloud/add_playlist.py
@@ -27,16 +27,13 @@ def add_playlist(url, request_user, list):
                 genre = SoundGenres.objects.get(name=track_genre)
             except:
                 genre = SoundGenres.objects.create(name=track_genre, order=SoundGenres.get_new_order())
-            try:
-                new_track = SoundcloudParsing.objects.get(id=track['id'])
-            except:
-                new_track = SoundcloudParsing.objects.create(id=track['id'],
-                                                        artwork_url=track['artwork_url'],
+
+            new_track = SoundcloudParsing.objects.create(artwork_url=track['artwork_url'],
                                                         created_at=created_at,
                                                         description=description,
                                                         duration=track['duration'],
                                                         genre=genre,
                                                         title=track['title'],
                                                         uri=track['uri'],
-                                                    release_year=track['release_year'])
+                                                        release_year=track['release_year'])
             list.players.add(new_track)
