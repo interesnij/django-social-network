1eb1c67037b82041b95ea3fbc61c69e763930fa7
Serg <ochkarik1983@mail.ru@>
2020-07-26 20:14:23 +0300

    H

--- static/scripts/music.js
+++ static/scripts/music.js
@@ -111,9 +111,12 @@ on('#ajax', 'click', '.u_add_track_in_list', function() {
   link.open( 'GET', '/music/user_progs/u_add_track_in_list/' + pk + "/" + uuid + "/", true );
   link.onreadystatechange = function () {
   if ( link.readyState == 4 && link.status == 200 ) {
-    _this.classList.add("u_remove_track_in_list");
-    _this.classList.remove("u_add_track_in_list");
-    _this.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>' + parent.innerHTML;
+    span = document.createElement("span");
+    span.classList.add("u_remove_track_in_list");
+    span.classList.remove("u_add_track_in_list");
+    span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>' + parent.innerHTML;
+    parent.innerHTML = "";
+    parent.append(span)
   }};
   link.send( null );
 })
@@ -126,9 +129,11 @@ on('#ajax', 'click', '.u_remove_track_in_list', function() {
   link.open( 'GET', '/music/user_progs/u_remove_track_in_list/' + pk + "/" + uuid + "/", true );
   link.onreadystatechange = function () {
   if ( link.readyState == 4 && link.status == 200 ) {
-    _this.classList.add("u_add_track_in_list");
-    _this.classList.remove("u_remove_track_in_list");
-    _this.querySelector("svg").remove()
+    span = document.createElement("span");
+    span.classList.add("u_add_track_in_list");
+    span.classList.remove("u_remove_track_in_list");
+    parent.innerHTML = "";
+    parent.append(span)
   }};
   link.send( null );
 })
