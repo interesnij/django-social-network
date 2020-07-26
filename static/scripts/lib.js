43e13e2a1d4971b0a41eb9a035b9b0f3fc63582a
Serg <ochkarik1983@mail.ru@>
2020-07-26 15:09:41 +0300

    H

--- static/scripts/lib.js
+++ static/scripts/lib.js
@@ -52,8 +52,6 @@ function loadScripts( src ) {
 
 loadScripts('/static/scripts/lib/lazysizes.min.js')
 loadScripts('/static/scripts/lib/Chart.min.js')
-loadScripts('/static/scripts/lib/plugin_for_player_1.js')
-loadScripts('/static/scripts/lib/music_player.js')
 
 loadScripts('/static/scripts/lib/video_player.js')
 loadScripts('/static/scripts/lib/video_init.js')
