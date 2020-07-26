134672e7e8a122dac5bc6bc3f69afc329efd0cf3
Serg <ochkarik1983@mail.ru@>
2020-07-26 12:26:06 +0300

    H

--- music/url/user_progs.py
+++ music/url/user_progs.py
@@ -5,11 +5,10 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^souncloud_create_list_window/(?P<pk>\d+)/$', UserSoundcloudSetPlaylistWindow.as_view()),
-    url(r'^souncloud_list_window_main/(?P<pk>\d+)/$', UserSoundcloudSetMainWindow.as_view()),
     url(r'^souncloud_list_window/(?P<pk>\d+)/$', UserSoundcloudSetWindow.as_view()),
 
     url(r'^create_soundcloud_set/(?P<pk>\d+)/$', UserSoundcloudSetCreate.as_view()),
-    url(r'^soundcloud_set_main/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserSoundcloudSetMain.as_view()),
+    url(r'^soundcloud_set/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserSoundcloudSet.as_view()),
 
     url(r'^u_add_track/(?P<pk>\d+)/$', login_required(UserTrackAdd.as_view())),
     url(r'^u_remove_track/(?P<pk>\d+)/$', login_required(UserTrackRemove.as_view())),
--- static/scripts/music.js
+++ static/scripts/music.js
@@ -49,29 +49,6 @@ on('#ajax', 'click', '#soundcloud_set_create_btn', function() {
     ajax_link.send(form_data);
 });
 
-on('#ajax', 'click', '#main_soundcloud_set_btn', function() {
-  this.disabled = true;
-  form = document.body.querySelector("#soundcloud_set_form");
-  form_data = new FormData(form);
-  if (!form.querySelector("#id_permalink").value){
-    form.querySelector("#id_permalink").style.border = "1px #FF0000 solid";
-    toast_error("Ссылка - обязательное поле!");
-  }
-  saver = document.body.querySelector(".pk_saver");
-  pk = saver.getAttribute("data-pk");
-  uuid = saver.getAttribute("data-uuid");
-
-  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-    ajax_link.open( 'POST', "/music/user_progs/soundcloud_set_main/" + pk + "/" + uuid + "/", true );
-    ajax_link.onreadystatechange = function () {
-      if ( this.readyState == 4 && this.status == 200 ) {
-        this_page_reload(document.location.href)
-        document.querySelector(".create_fullscreen").style.display = "none";
-        document.getElementById("create_loader").innerHTML="";
-      }
-    }
-    ajax_link.send(form_data);
-});
 on('#ajax', 'click', '#soundcloud_set_btn', function() {
   this.disabled = true;
   form = document.body.querySelector("#soundcloud_set_form");
@@ -88,9 +65,9 @@ on('#ajax', 'click', '#soundcloud_set_btn', function() {
     ajax_link.open( 'POST', "/music/user_progs/soundcloud_set/" + pk + "/" + uuid + "/", true );
     ajax_link.onreadystatechange = function () {
       if ( this.readyState == 4 && this.status == 200 ) {
-        this_page_reload('/users/detail/music_list/' + pk + "/" + uuid + "/")
         document.querySelector(".create_fullscreen").style.display = "none";
         document.getElementById("create_loader").innerHTML="";
+        this_page_reload(document.location.href);
       }
     }
     ajax_link.send(form_data);
