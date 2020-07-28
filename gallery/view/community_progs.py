f15e63509afb52c47c8b3a8582aacb0956f7b81c
Serg <ochkarik1983@mail.ru@>
2020-07-28 20:41:52 +0300

    F

--- gallery/view/community_progs.py
+++ gallery/view/community_progs.py
@@ -93,7 +93,7 @@ class PhotoReplyCommunityCreate(View):
         elif form_post.is_valid() and parent.photo_comment.comments_enabled:
             comment = form_post.save(commit=False)
 
-            check_can_get_posts_for_community_with_name(request.user, community.name)
+            check_can_get_lists(request.user, community)
             if request.POST.get('text') or  request.POST.get('photo') or request.POST.get('video') or request.POST.get('music'):
                 from common.comment_attacher import get_comment_attach
                 new_comment = comment.create_comment(commenter=request.user, parent_comment=parent, photo_comment=None, text=comment.text)
