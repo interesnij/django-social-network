4934717e9726eb56bc7e6c8e9650f1c69b5e23d5
Serg <ochkarik1983@mail.ru@>
2020-07-30 14:39:07 +0300

    H

--- communities/forms.py
+++ communities/forms.py
@@ -15,7 +15,7 @@ class GeneralCommunityForm(forms.ModelForm):
 	status = forms.CharField( label="",required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
 	class Meta:
 		model = Community
-		fields = ['name', 'description', 'status',]
+		fields = ['name', 'description', 'status', 'category',]
 
 
 class CoverCommunityForm(forms.ModelForm):
