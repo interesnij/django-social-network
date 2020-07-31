7854f65c166344841462b397c83d9513392603f4
Serg <ochkarik1983@mail.ru@>
2020-07-31 20:10:41 +0300

    G

--- communities/url/manage.py
+++ communities/url/manage.py
@@ -10,13 +10,13 @@ urlpatterns = [
     url(r'^notify_photo/(?P<pk>[0-9]+)/$', CommunityNotifyPhotoView.as_view(), name='community_photo_notify'),
     url(r'^notify_good/(?P<pk>[0-9]+)/$', CommunityNotifyGoodView.as_view(), name='community_good_notify'),
     url(r'^notify_video/(?P<pk>[0-9]+)/$', CommunityNotifyVideoView.as_view(), name='community_video_notify'),
-    url(r'^notify_music/(?P<pk>[0-9]+)/$', CommunityMusicVideoView.as_view(), name='community_music_notify'),
+    url(r'^notify_music/(?P<pk>[0-9]+)/$', CommunityNotifyMusicView.as_view(), name='community_music_notify'),
 
     url(r'^private_post/(?P<pk>[0-9]+)/$', CommunityPrivatePostView.as_view(), name='community_post_private'),
     url(r'^private_photo/(?P<pk>[0-9]+)/$', CommunityPrivatePhotoView.as_view(), name='community_photo_private'),
     url(r'^private_good/(?P<pk>[0-9]+)/$', CommunityPrivateGoodView.as_view(), name='community_good_private'),
     url(r'^private_video/(?P<pk>[0-9]+)/$', CommunityPrivateVideoView.as_view(), name='community_video_private'),
-    url(r'^private_music/(?P<pk>[0-9]+)/$', CommunityMusicVideoView.as_view(), name='community_music_private'),
+    url(r'^private_music/(?P<pk>[0-9]+)/$', CommunityPrivateMusicView.as_view(), name='community_music_private'),
 
     url(r'^(?P<pk>[0-9]+)/admins/$', CommunityAdminView.as_view(), name='community_admins'),
     url(r'^(?P<pk>[0-9]+)/moders/$', CommunityModersView.as_view(), name='community_moders'),
