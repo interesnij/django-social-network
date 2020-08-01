3ae8a328f50ff35e412a4d663880b85bd05d2c17
Serg <ochkarik1983@mail.ru@>
2020-08-01 14:53:35 +0300

    H

--- video/view/user_progs.py
+++ video/view/user_progs.py
@@ -240,9 +240,9 @@ class UserVideoInListCreate(View):
 
         if form_post.is_valid() and request.user == user:
             try:
-                album = VideoAlbum.objects.get(creator_id=self.user.pk, community=None, is_generic=True, title="Все видео")
+                album = VideoAlbum.objects.get(creator_id=user.pk, community=None, is_generic=True, title="Все видео")
             except:
-                album = VideoAlbum.objects.create(creator_id=self.user.pk, community=None, is_generic=True, title="Все видео")
+                album = VideoAlbum.objects.create(creator_id=user.pk, community=None, is_generic=True, title="Все видео")
             new_video = form_post.save(commit=False)
             new_video.creator = request.user
             albums = form_post.cleaned_data.get("album")
