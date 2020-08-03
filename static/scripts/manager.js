210f218f90618f7048d7f69830cbc7e20aa2e2db
Serg <ochkarik1983@mail.ru@>
2020-08-03 15:27:16 +0300

    G

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -1,3 +1,4 @@
 loadScripts('/static/scripts/manager/user.js')
 loadScripts('/static/scripts/manager/community.js')
 loadScripts('/static/scripts/manager/post.js')
+loadScripts('/static/scripts/manager/photo.js')
