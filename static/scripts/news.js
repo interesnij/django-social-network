7b1feb37421d6423fce9fc2e62c98c88b409b957
Serg <ochkarik1983@mail.ru@>
2020-08-04 14:57:30 +0300

    H

--- static/scripts/news.js
+++ static/scripts/news.js
@@ -11,4 +11,24 @@ on('#ajax', 'click', '.c_news_fullscreen', function() {
   pk = this.parentElement.getAttribute('data-pk');
   loader = document.getElementById("item_loader");
   open_fullscreen("/communities/item/" + pk + "/" + uuid + "/", loader)
-}); 
+});
+
+on('#ajax', 'click', '.u_wall_item_comments', function() {
+  clear_comment_dropdown();
+  parent = this.parentElement.parentElement.parentElement.parentElement;
+  pk = parent.getAttribute("data-pk");
+  uuid = parent.getAttribute("data-uuid");
+  url = "/posts/user/comment/" + uuid + "/" + pk + "/";
+  list_load(parent.querySelector(".u_load_comments"), url);
+  this.classList.toggle("comments_open");
+});
+
+on('#ajax', 'click', '.c_wall_item_comments', function() {
+  clear_comment_dropdown();
+  parent = this.parentElement.parentElement.parentElement.parentElement;
+  pk = parent.getAttribute("data-pk");
+  uuid = parent.getAttribute("data-uuid");
+  url = "/posts/community/comment/" + uuid + "/" + pk + "/";
+  list_load(parent.querySelector(".c_load_comments"), url);
+  this.classList.toggle("comments_open");
+});
