51d29fd5ee733526e0b5fb99079d6e7dc5748303
Serg <ochkarik1983@mail.ru@>
2020-08-04 15:33:50 +0300

    H

--- static/scripts/news.js
+++ static/scripts/news.js
@@ -1,34 +1,5 @@
-on('#ajax', 'click', '.u_news_fullscreen', function() {
-  container = this.parentElement;
-  uuid = container.getAttribute('data-uuid');
-  pk = container.getAttribute("data-pk");
-  loader = document.getElementById("item_loader");
-  open_fullscreen("/users/detail/post/" + pk + "/" + uuid + "/", loader)
-})
 
-on('#ajax', 'click', '.c_news_fullscreen', function() {
-  uuid = this.parentElement.getAttribute('data-uuid');
-  pk = this.parentElement.getAttribute('data-pk');
-  loader = document.getElementById("item_loader");
-  open_fullscreen("/communities/item/" + pk + "/" + uuid + "/", loader)
-});
-
-on('#ajax', 'click', '.u_wall_item_comments', function() {
-  clear_comment_dropdown();
-  parent = this.parentElement.parentElement.parentElement.parentElement;
-  pk = parent.getAttribute("data-pk");
-  uuid = parent.getAttribute("data-uuid");
-  url = "/posts/user/comment/" + uuid + "/" + pk + "/";
-  list_load(parent.querySelector(".u_load_comments"), url);
-  this.classList.toggle("comments_open");
-});
-
-on('#ajax', 'click', '.c_wall_item_comments', function() {
-  clear_comment_dropdown();
-  parent = this.parentElement.parentElement.parentElement.parentElement;
-  pk = parent.getAttribute("data-pk");
-  uuid = parent.getAttribute("data-uuid");
-  url = "/posts/community/comment/" + uuid + "/" + pk + "/";
-  list_load(parent.querySelector(".c_load_comments"), url);
-  this.classList.toggle("comments_open");
-});
+loadScripts('/static/scripts/news/user_get.js')
+loadScripts('/static/scripts/news/community_get.js')
+loadScripts('/static/scripts/news/user_post.js')
+loadScripts('/static/scripts/news/community_post.js')
