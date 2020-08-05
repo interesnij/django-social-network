5668b790bc61631b9dc831b66153fc06e3290b69
Serg <ochkarik1983@mail.ru@>
2020-08-05 20:19:09 +0300

    H

--- users/models.py
+++ users/models.py
@@ -1092,15 +1092,18 @@ class User(AbstractUser):
 
             community_posts_query = Q(community__memberships__user__id=user.pk, is_deleted=False, status=Post.STATUS_PUBLISHED)
             community_posts_query.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk)), Q.AND)
+            community_posts_queryset = Post.objects.only('created').filter(community_posts_query)
 
             followed_users = user.follows.values('followed_user_id')
             followed_users_ids = [followed_user['followed_user_id'] for followed_user in followed_users]
             followed_users_query = Q(creator__in=followed_users_ids, creator__user_private__is_private=False, is_deleted=False, status=Post.STATUS_PUBLISHED)
+            followed_users_queryset = Post.objects.only('created').filter(followed_users_query)
 
             frends = user.connections.values('target_user_id')
             frends_ids = [target_user['target_user_id'] for target_user in frends]
             frends_query = Q(creator__in=frends_ids, is_deleted=False, status=Post.STATUS_PUBLISHED)
             frends_queryset = Post.objects.only('created').filter(frends_query)
+
             final_queryset = own_posts_queryset.union(community_posts_queryset, followed_users_queryset, frends_queryset)
             query.union(final_queryset)
         return query
