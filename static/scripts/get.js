4f01c2b5980a16326448e44c28e9f4c327fb072c
Serg <ochkarik1983@mail.ru@>
2020-08-05 19:53:02 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -40,7 +40,7 @@ on('body', 'click', '.ajax', function(event) {
 window.addEventListener('popstate', function (e) {
   e.preventDefault();
   ajax_get_reload(history.back());
-  console.log(history.back())
+  console.log(document.referrer)
 });
 
 on('body', 'click', '.next_item', function(event) {
