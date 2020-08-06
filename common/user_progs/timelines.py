5921f7707131b0bec1bb6cbf67b543c445bf362b
Serg <ochkarik1983@mail.ru@>
2020-08-06 18:24:34 +0300

    H

--- common/user_progs/timelines.py
+++ common/user_progs/timelines.py
@@ -100,39 +100,47 @@ def get_timeline_goods_for_possible_users(user):
 
 
 def get_timeline_videos_for_user(user):
+    empty_list_exclude = Q(video_album__isnull=True)
     own_videos_query = Q(creator_id=user.pk, community__isnull=True, is_deleted=False, is_public=True)
+    own_videos_query.add(~Q(empty_list_exclude), Q.AND)
     own_videos_queryset = user.video_user_creator.only('id').filter(own_videos_query)
 
     community_videos_query = Q(community__memberships__user__id=user.pk, is_deleted=False, is_public=True)
     community_videos_query.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk)), Q.AND)
+    community_videos_query.add(~Q(empty_list_exclude), Q.AND)
     community_videos_queryset = VideoAlbum.objects.only('id').filter(community_videos_query)
 
     followed_users = user.follows.values('followed_user_id')
     followed_users_ids = [followed_user['followed_user_id'] for followed_user in followed_users]
     followed_users_query = Q(creator__in=followed_users_ids, creator__user_private__is_private=False, is_deleted=False, is_public=True)
+    followed_users_query.add(~Q(empty_list_exclude), Q.AND)
     followed_users_queryset = VideoAlbum.objects.only('id').filter(followed_users_query)
 
     frends = user.connections.values('target_user_id')
     frends_ids = [target_user['target_user_id'] for target_user in frends]
     frends_query = Q(creator__in=frends_ids, is_deleted=False, is_public=True)
+    frends_query.add(~Q(empty_list_exclude), Q.AND)
     frends_queryset = VideoAlbum.objects.only('id').filter(frends_query)
     final_queryset = own_videos_queryset.union(community_videos_queryset, followed_users_queryset, frends_queryset)
     return final_queryset
 
 def get_timeline_videos_for_possible_users(user):
     possible_users = user.get_possible_friends_ids()
+    empty_list_exclude = Q(video_album__isnull=True)
     videos_query = Q(creator_id__in=possible_users, community__isnull=True, is_deleted=False, is_public=True)
+    videos_query.add(~Q(empty_list_exclude), Q.AND)
     videos_queryset = VideoAlbum.objects.only('id').filter(videos_query)
     community_query = Q(community__memberships__user__id__in=possible_users, is_deleted=False, is_public=True)
     community_query.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk)), Q.AND)
+    community_query.add(~Q(empty_list_exclude), Q.AND)
     community_queryset = VideoAlbum.objects.only('id').filter(community_query)
     final_queryset = videos_queryset.union(community_queryset)
     return final_queryset
 
 
 def get_timeline_audios_for_user(user):
-    own_audios_query = Q(creator_id=user.pk, community__isnull=True, is_deleted=False)
     empty_list_exclude = Q(players__isnull=True)
+    own_audios_query = Q(creator_id=user.pk, community__isnull=True, is_deleted=False)
     own_audios_query.add(~Q(empty_list_exclude), Q.AND)
     own_audios_queryset = user.user_playlist.only('id').filter(own_audios_query)
 
@@ -157,10 +165,13 @@ def get_timeline_audios_for_user(user):
 
 def get_timeline_audios_for_possible_users(user):
     possible_users = user.get_possible_friends_ids()
+    empty_list_exclude = Q(players__isnull=True)
     audios_query = Q(creator_id__in=possible_users, community__isnull=True, is_deleted=False)
+    audios_query.add(~Q(empty_list_exclude), Q.AND)
     audios_queryset = SoundList.objects.only('id').filter(audios_query)
     community_query = Q(community__memberships__user__id__in=possible_users, is_deleted=False)
     community_query.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk)), Q.AND)
+    community_query.add(~Q(empty_list_exclude), Q.AND)
     community_queryset = SoundList.objects.only('id').filter(community_query)
     final_queryset = audios_queryset.union(community_queryset)
     return final_queryset
