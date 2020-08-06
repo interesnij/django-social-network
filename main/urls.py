daf5e117d6101a235d23e73ef6a1a03a3f4222ca
Serg <ochkarik1983@mail.ru@>
2020-08-06 13:12:35 +0300

    H

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from main.views import *
 
 urlpatterns = [
 	url(r'^$', ComingView.as_view(), name="coming"),
-	url(r'^main/$', MainPageView.as_view(), name="main"),
 	url(r'^phone_send/$', MainPhoneSend.as_view(), name="phone_send"),
+	
 	url(r'^posts/$', PostListView.as_view(), name="post_list"),
 	url(r'^featured_posts/$', FeaturedPostsView.as_view(), name="featured_post_list"),
 ]
