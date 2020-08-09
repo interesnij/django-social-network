334cdbad545641fbe1149af1b327c0d90765f8bd
Serg <ochkarik1983@mail.ru@>
2020-08-09 12:34:23 +0300

    H

--- delete.py
+++ delete.py
@@ -18,11 +18,11 @@ from music.models import SoundList
 from video.models import VideoAlbum
 from gallery.models import Album
 
-user = User.objects.get(pk=9)
+#user = User.objects.get(pk=9)
 
-SoundList.objects.create(creator=user, community=None, type=SoundList.MAIN, name="Основной плейлист")
-VideoAlbum.objects.create(creator=user, community=None, type=VideoAlbum.MAIN, title="Основной список")
+#SoundList.objects.create(creator=user, community=None, type=SoundList.MAIN, name="Основной плейлист")
+#VideoAlbum.objects.create(creator=user, community=None, type=VideoAlbum.MAIN, title="Основной список")
 
-Album.objects.create(creator=user, community=None, type=Album.AVATAR, title="Фото со страницы")
-Album.objects.create(creator=user, community=None, type=Album.MAIN, title="Основной альбом")
-Album.objects.create(creator=user, community=None, type=Album.WALL, title="Фото со стены")
+#Album.objects.create(creator=user, community=None, type=Album.AVATAR, title="Фото со страницы")
+#Album.objects.create(creator=user, community=None, type=Album.MAIN, title="Основной альбом")
+#Album.objects.create(creator=user, community=None, type=Album.WALL, title="Фото со стены")
