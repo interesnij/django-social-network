6aeb752627df2851261baced69e812abf2d835fd
Serg <ochkarik1983@mail.ru@>
2020-08-10 15:44:31 +0300

    H

--- common/user_progs/timelines.py
+++ common/user_progs/timelines.py
@@ -28,7 +28,7 @@ def get_timeline_posts_for_user(user):
 
 def get_timeline_posts_for_possible_users(user):
     possible_users = user.get_possible_friends_ids()
-    posts_query = Q(creator_id__in=possible_users, community__isnull=True, is_deleted=False, status=Post.STATUS_PUBLISHED)
+    posts_query = Q(creator_id__in=possible_users, creator__user_private__is_private=False, community__isnull=True, is_deleted=False, status=Post.STATUS_PUBLISHED)
     posts_queryset = Post.objects.only('created').filter(posts_query)
     community_query = Q(community__memberships__user__id__in=possible_users, is_deleted=False, status=Post.STATUS_PUBLISHED)
     community_query.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk)), Q.AND)
