879ae0185fbc722386243a68a12bf1722d809c33
Serg <ochkarik1983@mail.ru@>
2020-08-11 15:44:32 +0300

    H

--- goods/forms.py
+++ goods/forms.py
@@ -6,7 +6,7 @@ class GoodForm(forms.ModelForm):
 
 	class Meta:
 		model = Good
-		fields = ['title', 'image', 'image2', 'image3', 'image4', 'image5', 'sub_category', 'description', 'price', 'comments_enabled', 'votes_on', ]
+		fields = ['title', 'image', 'image2', 'image3', 'image4', 'image5', 'price', ]
 
 class CommentForm(forms.ModelForm):
 	text=forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control text-comment form-control-rounded'}))
