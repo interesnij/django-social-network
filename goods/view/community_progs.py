249e6b89b11ebe2e82e9c868e57d0d57a7adaef0
Serg <ochkarik1983@mail.ru@>
2020-08-11 20:35:11 +0300

    H

--- goods/view/community_progs.py
+++ goods/view/community_progs.py
@@ -204,6 +204,7 @@ class GoodCommunityCreate(TemplateView):
         if request.is_ajax() and self.form.is_valid():
             new_good = self.form.save(commit=False)
             new_good.creator = request.user
+            new_good.community = self.community
             new_good = self.form.save()
             html = render(request,'good_base/u_new_good.html',{'object': new_good})
             return HttpResponse()
