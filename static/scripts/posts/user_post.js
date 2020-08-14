2a8a6312dc3d40a40f66db1734ef4598eacfa455
Serg <ochkarik1983@mail.ru@>
2020-08-14 12:30:48 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -214,8 +214,8 @@ function like_reload(like_block, dislike_block, _class){
   if (!like_block.querySelector('figure')){
     console.log("создаем блок лайков");
     div = document.createElement("div");
-    div.innerHTML = block_vote_create(_class, user_pk);
-    like_block.innerHTML = div.innerHTML
+    div = block_vote_create(_class, user_pk);
+    like_block.innerHTML = div
   }
   else if (like_block.querySelector( '[data-pk=' + '"' + user_pk + '"' + ']' )){
       if (like_block.querySelector('figure')){
