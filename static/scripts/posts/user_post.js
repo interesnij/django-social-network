41891947e771e1ece25c20ef6835598c94603172
Serg <ochkarik1983@mail.ru@>
2020-08-14 11:31:23 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -205,7 +205,8 @@ function like_reload(like_block, dislike_block){
   userpic.querySelector(".img") ? user_img = userpic.querySelector(".img") : user_img = '<svg fill="currentColor" class="svg_default svg_info" style="margin-bottom:40px;" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
   user_pk = userpic.getAttribute("data-pk");
   user_name = userpic.getAttribute("data-name");
-
+  console.log(like_block);
+  console.log(dislike_block);
   if (like_block.firstChild){
     if (like_block.querySelector( '[data-pk=' + '"' + user_pk + '"' + ']' )){
       like_block.querySelector( '[data-pk=' + '"' + user_pk + '"' + ']' ).remove()
