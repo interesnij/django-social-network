4142f197e87bf2060acf9208499823d651161887
Serg <ochkarik1983@mail.ru@>
2020-08-14 14:26:59 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -247,14 +247,15 @@ function like_reload(like_block, dislike_block, _class){
   }
   else if (like_block.querySelector( '[data-pk=' + '"' + user_pk + '"' + ']' )){
         like_block.querySelector( '[data-pk=' + '"' + user_pk + '"' + ']' ).remove()
+      if (!like_block.querySelector('figure')) {
+        like_block.innerHTML = ""
+        console.log("удаляем блок лайков");
+      } else {
         value = like_block.querySelector('[data-count=like]').innerHTML;
         value = value*1;
         value -= 1;
         like_block.querySelector('[data-count=like]').innerHTML = value;
         console.log("удаляем пользователя из лайков");
-      if (!like_block.querySelector('figure')) {
-        like_block.innerHTML = ""
-        console.log("удаляем блок лайков");
       }
   }
   else {
