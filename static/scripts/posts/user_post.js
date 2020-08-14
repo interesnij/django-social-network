9a8f12386c4751adb5fb6513babbc858b2f9eacc
Serg <ochkarik1983@mail.ru@>
2020-08-14 14:08:21 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -196,6 +196,7 @@ function like_block_create(_class, user_pk){
   userpic = document.body.querySelector(".userpic");
   userpic.querySelector("img") ? user_img = userpic.querySelector("img") : user_img = '<svg fill="currentColor" class="svg_default svg_default_50" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
   user_name = userpic.getAttribute("data-name");
+  $div = document.createElement("div");
   $div.style.paddingRight = "div";
   $a = document.createElement("a");
   $a.style.paddingRight = "10px";
