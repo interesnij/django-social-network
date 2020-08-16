91962c4064cf397cd063061d1604048da28dc6e8
Serg <ochkarik1983@mail.ru@>
2020-08-16 11:59:57 +0300

    H

--- posts/url/repost.py
+++ posts/url/repost.py
@@ -5,5 +5,7 @@ urlpatterns = [
 # u_ucm_post - запись пользователя(u) к себе на стену(u), в сообщество (c) или в сообщения(m)
     url(r'^u_ucm_post_window/(?P<uuid>[0-9a-f-]+)/$', UUCMPostWindow.as_view()),
     url(r'^c_ucm_post_window/(?P<uuid>[0-9a-f-]+)/$', CUCMPostWindow.as_view()),
-    url(r'^u_u_post_repost/(?P<uuid>[0-9a-f-]+)/$', UUPostRepost.as_view(), name='u_u_post_repost'),
+    
+    url(r'^u_u_post_repost/(?P<uuid>[0-9a-f-]+)/$', UUPostRepost.as_view()),
+    url(r'^c_u_post_repost/(?P<uuid>[0-9a-f-]+)/$', CUPostRepost.as_view()),
 ]
