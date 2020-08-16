51c0eeef25a250117bed5ae4f137343d3a216599
Serg <ochkarik1983@mail.ru@>
2020-08-16 19:01:47 +0300

    H

--- posts/view/repost.py
+++ posts/view/repost.py
@@ -118,7 +118,7 @@ class UCPostRepost(View):
                     new_post = post.create_post(creator=request.user, is_signature=False, text=post.text, community=community, comments_enabled=post.comments_enabled, parent = self.parent, status="PG")
                     get_post_attach(request, new_post)
                     get_post_processing(new_post)
-                    return HttpResponse()
+            return HttpResponse()
         else:
             return HttpResponseBadRequest()
 
