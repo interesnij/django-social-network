9ce84d7f9745a27e7713bd0c18fedc90cd71d300
Serg <ochkarik1983@mail.ru@>
2020-08-18 13:57:55 +0300

    H

--- gallery/view/repost.py
+++ gallery/view/repost.py
@@ -72,7 +72,7 @@ class UUPhotoRepost(View):
         form_post = PostForm(request.POST)
         if request.is_ajax() and form_post.is_valid():
             post = form_post.save(commit=False)
-            parent = Post.create_parent_post(creator=request.user, community=None)
+            parent = Post.create_parent_post(creator=photo.creator, community=None)
             new_post = post.create_post(creator=request.user, is_signature=False, text=post.text, community=None, comments_enabled=post.comments_enabled, parent = parent, status="PG")
             get_post_attach(request, new_post)
             get_post_processing(new_post)
