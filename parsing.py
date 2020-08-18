8871178ea94ddb633987b508ebb3ff5d27f6e599
Serg <ochkarik1983@mail.ru@>
2020-08-18 20:45:42 +0300

    H

--- parsing.py
+++ parsing.py
@@ -1,26 +1,18 @@
-# -*- coding: utf-8 -*-
-from locale import *
-import sys,os
-
-project_dir = '../tr/tr/'
-
-sys.path.append(project_dir)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-import django
-django.setup()
-
-import soundcloud
-from music.models import *
-from datetime import datetime, date, time
-import json, requests
-
-
-client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
-
-response = requests.get(url= "https://api.soundcloud.com/resolve?url=https://soundcloud.com/rehan-khan-55/sets/meditation&client_id=dce5652caa1b66331903493735ddd64d")
-data = response.json()
-
-if data:
-    for track in data['tracks']:
-        track_genre = track['genre'].replace("'", '')
-        print(track_genre)
+{% if object.is_photo_repost %}
+  {{ object.get_u_photo_repost|safe }}
+{% elif object.is_photo_album_repost %}
+  Пользователь поделился фотоальбомом!
+{% elif object.get_good_repost %}
+  {{ object.get_u_good_repost|safe }}
+{% elif object.get_music_repost %}
+  {% include 'generic/attach/repost_track.html' %}
+{% elif object.is_music_list_repost %}
+  Пользователь поделился плейлистом!
+{% elif object.is_video_repost %}
+  {{ object.get_u_video_repost|safe }}
+{% elif object.is_video_list_repost %}
+  Пользователь поделился видеоальбомом!
+{% elif object.is_user_repost %}
+  Пользователь поделился пользователем!
+{% elif object.is_community_repost %}
+  Пользователь поделился сообществом!
