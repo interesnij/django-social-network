1633b1837bc3b864f6623b93622de7476be38884
Serg <ochkarik1983@mail.ru@>
2020-08-19 20:06:54 +0300

    H

--- static/scripts/music/user_post.js
+++ static/scripts/music/user_post.js
@@ -1,4 +1,98 @@
+on('#ajax', 'click', '#u_ucm_music_repost_btn', function() {
+  form_post = document.body.querySelector("#u_uсm_music_repost_form");
+  form_data = new FormData(form_post);
+  track_pk = this.getAttribute("track-pk");
+  pk = this.getAttribute("data-pk");
 
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+
+  if (form_post.querySelector('#repost_radio_wall').checked) {
+    link_.open( 'POST', "/music/repost/u_u_music_repost/" + pk + "/" + track_pk + "/", true );
+    link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    link_.send(form_data);
+    toast_info("Репост аудиозаписи на стену сделан")
+  }
+
+  else if(form_post.querySelector('#repost_radio_community').checked){
+    staff_communities = form_post.querySelector("#id_staff_communities");
+    selectedOptions = staff_communities.selectedOptions;
+    val = false;
+    for (var i = 0; i < selectedOptions.length; i++) {if(selectedOptions[i].value) {val = true}}
+    if(val){
+      link_.open( 'POST', "/music/repost/u_c_music_repost/" + pk + "/" + uuid + "/", true );
+      link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      link_.send(form_data);
+      toast_info("Репост аудиозаписи в сообщества сделан")
+    }else{toast_error("Выберите сообщества для репоста")}
+  }
+
+  else if(form_post.querySelector('#repost_radio_message').checked){
+    user_connections = form_post.querySelector("#id_user_connections");
+    selectedOptions = user_connections.selectedOptions;
+    val = false;
+    for (var i = 0; i < selectedOptions.length; i++) {if(selectedOptions[i].value) {val = true}}
+    if(val){
+      link_.open( 'POST', "/music/repost/u_m_music_repost/" + pk + "/" + uuid + "/", true );
+      link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      link_.send(form_data);
+      toast_info("Репост аудиозаписи в сообщения сделан")
+    }else{toast_error("Выберите пользователя для репоста")}
+  };
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    document.querySelector(".votes_fullscreen").style.display = "none";
+    document.getElementById("votes_loader").innerHTML="";
+  }}
+});
+
+on('#ajax', 'click', '#u_ucm_music_list_repost_btn', function() {
+  form_post = document.body.querySelector("#u_uсm_music_list_repost_form");
+  form_data = new FormData(form_post);
+  uuid = this.getAttribute("data-uuid");
+  pk = this.getAttribute("data-pk");
+
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+
+  if (form_post.querySelector('#repost_radio_wall').checked) {
+    link_.open( 'POST', "/music/repost/u_u_music_list_repost/" + pk + "/" + uuid + "/", true );
+    link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    link_.send(form_data);
+    toast_info("Репост плейлиста на стену сделан")
+  }
+
+  else if(form_post.querySelector('#repost_radio_community').checked){
+    staff_communities = form_post.querySelector("#id_staff_communities");
+    selectedOptions = staff_communities.selectedOptions;
+    val = false;
+    for (var i = 0; i < selectedOptions.length; i++) {if(selectedOptions[i].value) {val = true}}
+    if(val){
+      link_.open( 'POST', "/music/repost/u_c_music_list_repost/" + pk + "/" + uuid + "/", true );
+      link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      link_.send(form_data);
+      toast_info("Репост плейлиста в сообщества сделан")
+    }else{toast_error("Выберите сообщества для репоста")}
+  }
+
+  else if(form_post.querySelector('#repost_radio_message').checked){
+    user_connections = form_post.querySelector("#id_user_connections");
+    selectedOptions = user_connections.selectedOptions;
+    val = false;
+    for (var i = 0; i < selectedOptions.length; i++) {if(selectedOptions[i].value) {val = true}}
+    if(val){
+      link_.open( 'POST', "/music/repost/u_m_music_list_repost/" + pk + "/" + uuid + "/", true );
+      link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      link_.send(form_data);
+      toast_info("Репост плейлиста в сообщения сделан")
+    }else{toast_error("Выберите пользователя для репоста")}
+  };
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    document.querySelector(".votes_fullscreen").style.display = "none";
+    document.getElementById("votes_loader").innerHTML="";
+  }}
+});
 
 on('#ajax', 'click', '#soundcloud_set_create_btn', function() {
   this.disabled = true;
