c66598d34dca06921caf036a71295fbf943d7b7a
Serg <ochkarik1983@mail.ru@>
2020-08-19 19:16:21 +0300

    H

---
+++ static/scripts/music/user_get.js
@@ -0,0 +1,27 @@
+on('#ajax', 'click', '.u_soundcloud_set_create', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/user_progs/souncloud_create_list_window/" + pk + "/", loader)
+});
+on('#ajax', 'click', '.u_soundcloud_set_list_main', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/user_progs/souncloud_list_window_main/" + pk + "/", loader)
+});
+on('#ajax', 'click', '.u_soundcloud_set_list', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/user_progs/souncloud_list_window/" + pk + "/", loader)
+});
+
+on('#ajax', 'click', '.u_ucm_music_repost', function() {
+  parent = this.parentElement.parentElement.parentElement.parentElement.parentElement
+  track_pk = parent.getAttribute("data-pk");
+  document.body.querySelector(".pk_saver") ? pk = document.body.querySelector(".pk_saver").getAttribute('data-pk') : pk = parent.getAttribute('user-pk');
+  loader = document.getElementById("votes_loader");
+  open_fullscreen("/music/repost/u_ucm_music_window/" + pk + "/" + track_pk + "/", loader);
+  clear_attach_block();
+})
---
+++ static/scripts/music/user_post.js
@@ -0,0 +1,129 @@
+
+
+on('#ajax', 'click', '#soundcloud_set_create_btn', function() {
+  this.disabled = true;
+  form = document.body.querySelector("#soundcloud_set_create_form");
+  form_data = new FormData(form);
+  if (!form.querySelector("#id_name").value){
+    form.querySelector("#id_name").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!");
+  } else if (!form.querySelector("#id_permalink").value){
+    form.querySelector("#id_permalink").style.border = "1px #FF0000 solid";
+    toast_error("Ссылка - обязательное поле!");
+  }
+  pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
+
+  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+    ajax_link.open( 'POST', "/music/user_progs/create_soundcloud_set/" + pk + "/", true );
+    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax_link.onreadystatechange = function () {
+      if ( this.readyState == 4 && this.status == 200 ) {
+        elem_ = document.createElement('span');
+        elem_.innerHTML = ajax_link.responseText;
+        ajax = elem_.querySelector("#reload_block");
+        rtr = document.getElementById('ajax');
+        rtr.innerHTML = ajax.innerHTML;
+        window.scrollTo(0,0);
+        document.title = elem_.querySelector('title').innerHTML;
+
+        uuid = rtr.querySelector(".pk_saver").getAttribute("data-uuid");
+        window.history.pushState(null, "vfgffgfgf", '/users/detail/music_list/' + pk + "/" + uuid + '/');
+      }
+    }
+    ajax_link.send(form_data);
+});
+
+on('#ajax', 'click', '#soundcloud_set_btn', function() {
+  this.disabled = true;
+  form = document.body.querySelector("#soundcloud_set_form");
+  form_data = new FormData(form);
+  if (!form.querySelector("#id_permalink").value){
+    form.querySelector("#id_permalink").style.border = "1px #FF0000 solid";
+    toast_error("Ссылка - обязательное поле!");
+  }
+  saver = document.body.querySelector(".pk_saver");
+  pk = saver.getAttribute("data-pk");
+  uuid = saver.getAttribute("data-uuid");
+
+  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+    ajax_link.open( 'POST', "/music/user_progs/soundcloud_set/" + pk + "/" + uuid + "/", true );
+    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax_link.onreadystatechange = function () {
+      if ( this.readyState == 4 && this.status == 200 ) {
+        document.querySelector(".create_fullscreen").style.display = "none";
+        document.getElementById("create_loader").innerHTML="";
+        this_page_reload(document.location.href);
+      }
+    }
+    ajax_link.send(form_data);
+});
+
+
+on('#ajax', 'click', '.u_track_add', function(e) {
+  block = this.parentElement;
+  pk = block.parentElement.getAttribute("data-pk");
+  uuid = document.body.querySelector(".pk_saver").getAttribute("data-uuid");
+  var _link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  _link.open( 'GET', "/music/user_progs/u_add_track/" + pk + "/" + uuid + "/", true );
+  _link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  _link.onreadystatechange = function () {
+    if ( _link.readyState == 4 && _link.status == 200 ) {
+      block.innerHTML = "";
+      block.innerHTML = "<span class='u_track_remove btn_default pointer' title='Удалить'><svg fill='currentColor' style='width:22px;height:22px;' class='svg_default'><path fill='none' d='M0 0h24v24H0z'/><path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg></span>"
+  }};
+  _link.send( null );
+});
+
+on('#ajax', 'click', '.u_track_remove', function(e) {
+  block = this.parentElement;
+  pk = block.parentElement.getAttribute("data-pk");
+  uuid = document.body.querySelector(".pk_saver").getAttribute("data-uuid");
+  var _link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  _link.open( 'GET', "/music/user_progs/u_remove_track/" + pk + "/" + uuid + "/", true );
+  _link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  _link.onreadystatechange = function () {
+    if ( _link.readyState == 4 && _link.status == 200 ) {
+      block.innerHTML = "";
+      block.innerHTML = "<span class='u_track_add btn_default pointer' title='Добавить'><svg fill='currentColor' style='width:22px;height:22px;' class='svg_default'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/><path d='M0 0h24v24H0z' fill='none'/></svg></span>"
+  }};
+  _link.send( null );
+});
+
+on('#ajax', 'click', '.u_add_track_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = _this.parentElement.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/music/user_progs/u_add_track_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_add_track_in_list");
+    list.style.paddingLeft = "14px";
+    list.classList.add("u_remove_track_in_list");
+    list.classList.remove("u_add_track_in_list");
+    span = document.createElement("span");
+    span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg> ';
+    list.prepend(span)
+  }};
+  link.send( null );
+})
+on('#ajax', 'click', '.u_remove_track_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = _this.parentElement.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/music/user_progs/u_remove_track_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_remove_track_in_list");
+    list.style.paddingLeft = "30px";
+    list.classList.add("u_add_track_in_list");
+    list.classList.remove("u_remove_track_in_list");
+    list.querySelector("svg").remove();
+  }};
+  link.send( null );
+})
