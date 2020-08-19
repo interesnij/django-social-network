8451e3b9618fe6fd0f13a79273da1ef1be44219c
Serg <ochkarik1983@mail.ru@>
2020-08-19 13:41:45 +0300

    H

--- gallery/view/repost.py
+++ gallery/view/repost.py
@@ -92,7 +92,7 @@ class CUPhotoRepost(View):
         check_can_get_lists(request.user, community)
         if request.is_ajax() and form_post.is_valid():
             post = form_post.save(commit=False)
-            parent = Post.create_parent_post(creator=photo.creator, community=None, status=Post.PHOTO_REPOST)
+            parent = Post.create_parent_post(creator=photo.creator, community=community, status=Post.PHOTO_REPOST)
             photo.item.add(parent)
             new_post = post.create_post(creator=request.user, is_signature=False, text=post.text, community=None, comments_enabled=post.comments_enabled, parent=parent, status="PG")
             get_post_attach(request, new_post)
