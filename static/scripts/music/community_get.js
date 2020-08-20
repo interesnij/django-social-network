815238d2e61a7356c48e53fd3f400f441e82df2c
Serg <ochkarik1983@mail.ru@>
2020-08-20 20:03:54 +0300

    H

--- static/scripts/music/community_get.js
+++ static/scripts/music/community_get.js
@@ -6,3 +6,11 @@ on('#ajax', 'click', '.c_ucm_music_repost', function() {
   open_fullscreen("/music/repost/c_ucm_music_window/" + pk + "/" + track_pk + "/", loader);
   clear_attach_block();
 })
+on('#ajax', 'click', '.c_ucm_music_list_repost', function() {
+  parent = this.parentElement;
+  parent.getAttribute("data-pk") ? pk = parent.getAttribute('data-pk') : document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  parent.getAttribute("data-uuid") ? uuid = parent.getAttribute('data-uuid') : document.body.querySelector(".pk_saver").getAttribute('data-uuid');
+  loader = document.getElementById("votes_loader");
+  open_fullscreen("/music/repost/c_ucm_list_window/" + pk + "/" + uuid + "/", loader);
+  clear_attach_block();
+})
