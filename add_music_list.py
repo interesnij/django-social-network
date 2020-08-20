a630a4837d55e4204334fe5b9e9bc6810c08f920
Serg <ochkarik1983@mail.ru@>
2020-08-20 16:57:55 +0300

    H

--- add_music_list.py
+++ add_music_list.py
@@ -14,10 +14,15 @@ django.setup()
 import soundcloud
 from music.models import *
 from PIL import Image
+import io
 
 
 client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
-
+byteImgIO = io.BytesIO()
+byteImg = Image.open("some/location/to/a/file/in/my/directories.png")
+byteImg.save(byteImgIO, "JPG")
+byteImgIO.seek(0)
+byteImg = byteImgIO.read()
 
 response = requests.get("https://api.soundcloud.com/resolve?url=https://soundcloud.com/yeonkkot/sets/s12&client_id=dce5652caa1b66331903493735ddd64d")
 data = response.json()
@@ -25,8 +30,10 @@ data = response.json()
 if data:
     playlist_url = data['artwork_url']
     print(playlist_url)
-    #playlist_url.replace("large.jpg", "crop.jpg")
+    playlist_url.replace("large.jpg", "crop.jpg")
+    print(playlist_url)
     img_response = requests.get(url=playlist_url).raw
-    print(img_response)
-    img = Image.open(img_response)
-    print(img)
+    byteImg = Image.open(img_response)
+    byteImg.save(byteImgIO, "JPG")
+    byteImgIO.seek(0)
+    print(byteImg)
