a5ce355e1b9a2fec73ddd150269b5d48f6f735c9
Serg <ochkarik1983@mail.ru@>
2020-08-20 16:54:06 +0300

    H

--- add_music_list.py
+++ add_music_list.py
@@ -19,13 +19,14 @@ from PIL import Image
 client = soundcloud.Client(client_id='dce5652caa1b66331903493735ddd64d')
 
 
-response = requests.get("https://api.soundcloud.com/resolve?url=https://soundcloud.com/ilyanaazman/sets/tsyn&client_id=dce5652caa1b66331903493735ddd64d")
+response = requests.get("https://api.soundcloud.com/resolve?url=https://soundcloud.com/yeonkkot/sets/s12&client_id=dce5652caa1b66331903493735ddd64d")
 data = response.json()
 
 if data:
     playlist_url = data['artwork_url']
+    print(playlist_url)
     #playlist_url.replace("large.jpg", "crop.jpg")
     img_response = requests.get(url=playlist_url).raw
+    print(img_response)
     img = Image.open(img_response)
-    print(playlist_url)
     print(img)
