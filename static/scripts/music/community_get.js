41d4b70e69c2c70f0de78985f76270a8b78714f6
Serg <ochkarik1983@mail.ru@>
2020-08-20 20:05:29 +0300

    H

--- static/scripts/music/community_get.js
+++ static/scripts/music/community_get.js
@@ -8,8 +8,8 @@ on('#ajax', 'click', '.c_ucm_music_repost', function() {
 })
 on('#ajax', 'click', '.c_ucm_music_list_repost', function() {
   parent = this.parentElement;
-  parent.getAttribute("data-pk") ? pk = parent.getAttribute('data-pk') : document.body.querySelector(".pk_saver").getAttribute('data-pk');
-  parent.getAttribute("data-uuid") ? uuid = parent.getAttribute('data-uuid') : document.body.querySelector(".pk_saver").getAttribute('data-uuid');
+  parent.getAttribute("data-pk") ? pk = parent.getAttribute('data-pk') : pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  parent.getAttribute("data-uuid") ? uuid = parent.getAttribute('data-uuid') : uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid');
   loader = document.getElementById("votes_loader");
   open_fullscreen("/music/repost/c_ucm_list_window/" + pk + "/" + uuid + "/", loader);
   clear_attach_block();
--- static/scripts/music/user_get.js
+++ static/scripts/music/user_get.js
@@ -27,8 +27,8 @@ on('#ajax', 'click', '.u_ucm_music_repost', function() {
 })
 on('#ajax', 'click', '.u_ucm_music_list_repost', function() {
   parent = this.parentElement;
-  parent.getAttribute("data-pk") ? pk = parent.getAttribute('data-pk') : document.body.querySelector(".pk_saver").getAttribute('data-pk');
-  parent.getAttribute("data-uuid") ? uuid = parent.getAttribute('data-uuid') : document.body.querySelector(".pk_saver").getAttribute('data-uuid');
+  parent.getAttribute("data-pk") ? pk = parent.getAttribute('data-pk') : pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  parent.getAttribute("data-uuid") ? uuid = parent.getAttribute('data-uuid') : uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid');
   loader = document.getElementById("votes_loader");
   open_fullscreen("/music/repost/u_ucm_list_window/" + pk + "/" + uuid + "/", loader);
   clear_attach_block();
