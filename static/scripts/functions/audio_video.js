7fe8b39de3b12a497e9aeb915c79f5082322f99c
Serg <ochkarik1983@mail.ru@>
2020-08-21 11:17:38 +0300

    J

--- static/scripts/functions/audio_video.js
+++ static/scripts/functions/audio_video.js
@@ -486,7 +486,7 @@ function music_onReady(){console.log("Аудио плеер готов");}
         };
       }
 
-      var list_pk = this.parentElement.parentElement.parentElement.parentElement.getAttribute('data-pk');
+      list_pk = this.parentElement.parentElement.parentElement.parentElement.getAttribute('data-pk');
       if (!document.body.classList.contains("list_" + list_pk)){
         save_playlist("list_" + list_pk, '/music/manage/temp_list/' + list_pk, '/music/get/list/' + list_pk + "/", id)
       }else{
