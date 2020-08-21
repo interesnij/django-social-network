aa977d9009bc0407608128307066cd0eb87e92da
Serg <ochkarik1983@mail.ru@>
2020-08-21 20:20:46 +0300

    J

--- static/scripts/music/community_get.js
+++ static/scripts/music/community_get.js
@@ -14,3 +14,21 @@ on('#ajax', 'click', '.c_ucm_music_list_repost', function() {
   open_fullscreen("/music/repost/c_ucm_list_window/" + pk + "/" + uuid + "/", loader);
   clear_attach_block();
 })
+on('#ajax', 'click', '.c_soundcloud_set_create', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/community_progs/souncloud_create_list_window/" + pk + "/", loader)
+});
+on('#ajax', 'click', '.c_soundcloud_set_list_main', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/community_progs/souncloud_list_window_main/" + pk + "/", loader)
+});
+on('#ajax', 'click', '.c_soundcloud_set_list', function() {
+  uuid = this.parentElement.getAttribute('data-uuid');
+  pk = document.body.querySelector(".pk_saver").getAttribute('data-pk');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/music/community_progs/souncloud_list_window/" + pk + "/", loader)
+});
