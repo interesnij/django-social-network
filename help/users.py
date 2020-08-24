00b8f7b0a3ef4a1ff05d127be01199ce7979cceb
Serg <ochkarik1983@mail.ru@>
2020-08-24 18:27:46 +0300

    J

--- help/users.py
+++ help/users.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import six, timezone, translation
+from django.utils import timezone, translation
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 from imagekit.models import ProcessedImageField
--- invitations/models.py
+++ invitations/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.validators import UnicodeUsernameValidator, ASCIIUserna
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from django.utils import six
+from django.utils.six import six
 
 
 class UserInvite(models.Model):
