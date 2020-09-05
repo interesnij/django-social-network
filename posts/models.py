c44cc1c3f448a595db2b6aeca7997d2f4431048b
Serg <ochkarik1983@mail.ru@>
2020-09-05 16:27:17 +0300

    H

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from rest_framework.exceptions import ValidationError
 from django.db.models import Q
 from django.contrib.postgres.indexes import BrinIndex
 from django.utils import timezone
-from notify.model.item import *
+from notify.model.post import *
 from common.model.votes import PostVotes, PostCommentVotes
 from common.utils import try_except
 
