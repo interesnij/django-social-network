4a6a28054550262cb8ae36ac35523d61e96ee159
Serg <ochkarik1983@mail.ru@>
2020-09-06 17:18:37 +0300

    h

--- users/views/progs.py
+++ users/views/progs.py
@@ -14,8 +14,8 @@ class GetUserGender(View):
         else:
             import pandas as pd
 
-            ru_url = "http://раса.рус/static/scripts/csv/rus2.csv"
-            en_url = "http://раса.рус/static/scripts/csv/en2.csv"
+            ru_url = "http://раса.рус/static/scripts/csv/rus.csv"
+            en_url = "http://раса.рус/static/scripts/csv/en.csv"
             ru_s=requests.get(ru_url).text
             en_s=requests.get(en_url).text
 
