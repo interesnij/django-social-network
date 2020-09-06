2e9f04aee7f8e11ce076689be01f5fd27fd65c30
Serg <ochkarik1983@mail.ru@>
2020-09-06 16:08:30 +0300

    h

--- users/views/progs.py
+++ users/views/progs.py
@@ -12,8 +12,8 @@ class GetUserGender(View):
         else:
             import pandas as pd
 
-            dfru = pd.read_csv('http://раса.рус/static/scripts/csv/FNru.csv', engine='python')
-            dfen = pd.read_csv('http://раса.рус/static/scripts/csv/FNen.csv', engine='python')
+            dfru = pd.read_csv('http://раса.рус/static/scripts/csv/FNru.csv', encoding = "ISO-8859-1")
+            dfen = pd.read_csv('http://раса.рус/static/scripts/csv/FNen.csv', encoding = "ISO-8859-1")
 
             rumalenames = set(dfru[dfru['Gender'] == 'male']['GivenName'])
             rumalesurnames = set(dfru[dfru['Gender'] == 'male']['Surname'])
