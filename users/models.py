13f1b0366149fc178ae1fdc6aeae48fa08655e4c
Serg <ochkarik1983@mail.ru@>
2020-09-07 14:31:19 +0300

    H

--- users/models.py
+++ users/models.py
@@ -53,7 +53,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=17, unique=True, verbose_name='Телефон')
     perm = models.CharField(max_length=5, choices=PERM, default=PHONE_NO_VERIFIED, verbose_name="Уровень доступа")
     gender = models.CharField(max_length=5, choices=GENDER, blank=True, verbose_name="Пол")
-    birthday = models.DateField(default=timezone.now, blank=True, verbose_name='День рождения')
+    birthday = models.DateField(blank=True, verbose_name='День рождения')
     USERNAME_FIELD = 'phone'
 
     class Meta:
