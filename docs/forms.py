b6830cc92d67c9b37fac8091e6fc663ba7bebafd
Serg <ochkarik1983@mail.ru@>
2020-09-09 17:45:49 +0300

    J

--- docs/forms.py
+++ docs/forms.py
@@ -13,22 +13,16 @@ class DocForm(forms.ModelForm):
 	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
 
 	def clean_file(self):
-        file = self.cleaned_data['file']
-        try:
-            if file:
-                file_type = file.content_type.split('/')[0]
-                print file_type
-
-                if len(file.name.split('.')) == 1:
-                    raise forms.ValidationError(_('File type is not supported'))
-
-                if file._size > settings.DOC_UPLOAD_FILE_MAX_SIZE:
-                    raise forms.ValidationError(_('Максимальный размер файла: %s. Ваш документ: %s') % (filesizeformat(settings.TASK_UPLOAD_FILE_MAX_SIZE), filesizeformat(file._size)))
-                else:
-                    raise forms.ValidationError(_('File type is not supported'))
-        except:
-            pass
-        return file
+		file = self.cleaned_data['file']
+		try:
+			if file:
+				if len(file.name.split('.')) == 1:
+					raise forms.ValidationError('File type is not supported')
+				if file._size > settings.DOC_UPLOAD_FILE_MAX_SIZE:
+					raise forms.ValidationError(_('Максимальный размер файла: %s. Ваш документ: %s') % (filesizeformat(settings.TASK_UPLOAD_FILE_MAX_SIZE), filesizeformat(file._size)))
+		except:
+			pass
+		return file
 
 	class Meta:
 		model = Doc
