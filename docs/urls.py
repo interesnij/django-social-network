e65f6d83acaf51523dbd6fb61d2f65de022b2bc4
Serg <ochkarik1983@mail.ru@>
2020-09-09 12:26:48 +0300

    J

--- docs/urls.py
+++ docs/urls.py
@@ -4,4 +4,8 @@ from docs.views import DocsView
 
 urlpatterns = [
     url(r'^$', DocsView.as_view(), name='docs'),
+
+    url(r'^user_progs/', include('music.url.user_progs')),
+    url(r'^community_progs/', include('music.url.community_progs')),
+    url(r'^repost/', include('music.url.repost')),
 ]
