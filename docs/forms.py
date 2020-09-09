807cddeae3d959460dda594506ddb0a5e9326851
Serg <ochkarik1983@mail.ru@>
2020-09-09 17:43:38 +0300

    J

--- docs/forms.py
+++ docs/forms.py
@@ -1,5 +1,6 @@
 from docs.models import DocList, Doc
 from django import forms
+from django.conf import settings
 
 
 class DoclistForm(forms.ModelForm):
@@ -10,6 +11,25 @@ class DoclistForm(forms.ModelForm):
 
 class DocForm(forms.ModelForm):
 	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
+
+	def clean_file(self):
+        file = self.cleaned_data['file']
+        try:
+            if file:
+                file_type = file.content_type.split('/')[0]
+                print file_type
+
+                if len(file.name.split('.')) == 1:
+                    raise forms.ValidationError(_('File type is not supported'))
+
+                if file._size > settings.DOC_UPLOAD_FILE_MAX_SIZE:
+                    raise forms.ValidationError(_('Максимальный размер файла: %s. Ваш документ: %s') % (filesizeformat(settings.TASK_UPLOAD_FILE_MAX_SIZE), filesizeformat(file._size)))
+                else:
+                    raise forms.ValidationError(_('File type is not supported'))
+        except:
+            pass
+        return file
+
 	class Meta:
 		model = Doc
 		fields = ['title', 'file', 'type', 'list', ]
