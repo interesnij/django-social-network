9228432f5aa0d7338e3cf0c22f95123e5da73c85
Serg <ochkarik1983@mail.ru@>
2020-09-11 16:00:46 +0300

    G

--- music/view/user_progs.py
+++ music/view/user_progs.py
@@ -16,7 +16,7 @@ class UserSoundcloudSetPlaylistWindow(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user = User.objects.get(pk=self.kwargs["pk"])
-        self.template_name = get_settings_template("music_create/", "u_soundcloud_add_playlist.html", request)
+        self.template_name = get_settings_template("music_create/u_soundcloud_add_playlist.html", request)
         return super(UserSoundcloudSetPlaylistWindow,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
@@ -29,7 +29,7 @@ class UserSoundcloudSetWindow(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user = User.objects.get(pk=self.kwargs["pk"])
-        self.template_name = get_settings_template("music_create/", "u_soundcloud_set_playlist.html", request)
+        self.template_name = get_settings_template("music_create/u_soundcloud_set_playlist.html", request)
         return super(UserSoundcloudSetWindow,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
@@ -128,7 +128,7 @@ class UserCreatePlaylistWindow(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.user = User.objects.get(pk=self.kwargs["pk"])
-        self.template_name = get_settings_template("music_create/", "u_create_list.html", request)
+        self.template_name = get_settings_template("music_create/u_create_list.html", request)
         return super(UserCreatePlaylistWindow,self).get(request,*args,**kwargs)
 
 
