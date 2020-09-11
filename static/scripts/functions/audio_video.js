8e72784c5987d10afe7d35cc1349b6dc7b358666
Serg <ochkarik1983@mail.ru@>
2020-09-11 16:16:42 +0300

    G

--- static/scripts/functions/audio_video.js
+++ static/scripts/functions/audio_video.js
@@ -489,7 +489,7 @@ function music_onReady(){console.log("Аудио плеер готов");}
 
       list_pk = parents.getAttribute('data-pk');
       console.log(list_pk);
-      if (!document.body.classList.contains("list_" + list_pk)){
+      if (!document.body.classList.contains("list_" + list_pk) && list_pk){
         save_playlist("list_" + list_pk, '/music/manage/temp_list/' + list_pk, '/music/get/list/' + list_pk + "/", id)
       }else{
         music_player.loadPlaylist(0);
