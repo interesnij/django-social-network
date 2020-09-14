5a02fb16137f4160754361167078608fd62f892b
Serg <ochkarik1983@mail.ru@>
2020-09-14 17:39:35 +0300

    H

--- users/views/load.py
+++ users/views/load.py
@@ -118,7 +118,7 @@ class UserLoadGood(ListView):
 
 	def get(self,request,*args,**kwargs):
 		from goods.models import GoodAlbum
-		self.album = GoodAlbum.objects.get(type=GoodAlbum.MAIN, creator=request.user)
+		self.album = GoodAlbum.objects.get(type=GoodAlbum.MAIN, creator=request.user, community=None)
 		self.template_name = get_settings_template("load/u_good_list_load.html", request)
 		return super(UserLoadGood,self).get(request,*args,**kwargs)
 
