20660009f2fa3399badbc8b88656468f05b8bb9a
Serg <ochkarik1983@mail.ru@>
2020-09-14 11:51:47 +0300

    H

--- static/scripts/posts/user_get.js
+++ static/scripts/posts/user_get.js
@@ -141,8 +141,7 @@ on('#ajax', 'click', '.u_select_music', function() {
   this.parentElement.parentElement.previousElementSibling.classList.add("attach_block");
   clear_comment_dropdown();
   loader = document.getElementById("create_loader");
-  open_fullscreen('/users/load/u_music_load/', loader)
-  create_load_pagination(document.getElementById("create_loader"))
+  open_load_fullscreen('/users/load/u_music_load/', loader) 
 });
 on('#ajax', 'click', '.u_select_good', function() {
   this.parentElement.parentElement.previousElementSibling.classList.add("attach_block");
