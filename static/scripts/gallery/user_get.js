55b6516bd88fba21f3bd0295848b24d4548f0296
Serg <ochkarik1983@mail.ru@>
2020-09-15 11:27:45 +0300

    H

--- static/scripts/gallery/user_get.js
+++ static/scripts/gallery/user_get.js
@@ -74,27 +74,9 @@ on('#ajax', 'click', '#u_create_album_btn', function() {
     toast_error("Название - обязательное поле!");
   } else { null }
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
-
-  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-    ajax_link.open( 'POST', "/gallery/user_progs/add_album/" + pk + "/", true );
-    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    ajax_link.onreadystatechange = function () {
-      if ( this.readyState == 4 && this.status == 200 ) {
-        elem_ = document.createElement('span');
-        elem_.innerHTML = ajax_link.responseText;
-        ajax = elem_.querySelector("#reload_block");
-        rtr = document.getElementById('ajax');
-        rtr.innerHTML = ajax.innerHTML;
-        uuid = rtr.querySelector(".pk_saver").getAttribute("album-uuid");
-        window.scrollTo(0,0);
-        document.title = elem_.querySelector('title').innerHTML;
-        window.history.pushState(null, "vfgffgfgf", '/gallery/user/album/' + pk + '/' + uuid + '/');
-        toast_info("Альбом изображений создан!");
-        album_photo_load =  rtr.querySelector("#album_photo_load");
-        list_load(album_photo_load, album_photo_load.getAttribute("data-link"));
-      }
-    } 
-    ajax_link.send(form_data);
+  post_and_load_object_page(form, "/gallery/user_progs/add_album/", "/users/", "/album/");
+  reload = document.body.querySelector("#album_photo_load")
+  list_load(reload, reload.getAttribute("data-link"));
 });
 
 on('#ajax', 'click', '.u_all_photo_likes', function() {
