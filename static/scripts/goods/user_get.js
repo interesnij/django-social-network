e7974ce3612b60b648452da6a5056416d2043570
Serg <ochkarik1983@mail.ru@>
2020-09-15 17:54:13 +0300

    H

--- static/scripts/goods/user_get.js
+++ static/scripts/goods/user_get.js
@@ -1,5 +1,6 @@
 on('#ajax', 'click', '.u_good_detail', function() {
-  document.body.querySelector(".pk_saver") ? uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid') : uuid = this.getAttribute('data-uuid');
+
+  this.getAttribute('data-uuid') ? uuid = this.getAttribute('data-uuid') : uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid')
   pk = this.getAttribute('good-pk');
   loader = document.getElementById("good_loader");
   open_fullscreen('/goods/user/good/' + pk + '/' + uuid + '/', loader);
