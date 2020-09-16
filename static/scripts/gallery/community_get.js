32980fd4db9581516fe2f8ca15ad9b9c1d183990
Serg <ochkarik1983@mail.ru@>
2020-09-16 12:02:17 +0300

    H

--- static/scripts/gallery/community_get.js
+++ static/scripts/gallery/community_get.js
@@ -37,7 +37,7 @@ on('#ajax', 'click', '.c_AV_photo', function() {
 
 on('#ajax', 'click', '.c_AL_photo', function() {
   container = this.parentElement;
-  document.body.querySelector(".pk_saver") ? uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid') : uuid = this.getAttribute('data-uuid');
+  document.body.querySelector(".pk_saver").getAttribute('data-uuid') ? uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid') : uuid = this.getAttribute('data-uuid');
   pk = this.getAttribute('photo-pk');
   loader = document.getElementById("photo_loader");
   open_fullscreen("/gallery/community/album_photo/" + pk + "/" + uuid + "/", loader)
