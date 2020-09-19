0ef2072f6453c590fe614ce80ee657b75c353025
Serg <ochkarik1983@mail.ru@>
2020-09-19 19:32:21 +0300

    H

--- music/url/community_progs.py
+++ music/url/community_progs.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^souncloud_create_list_window/(?P<pk>\d+)/$', CommunitySoundcloudSetPlaylistWindow.as_view()),
     url(r'^souncloud_list_window/(?P<pk>\d+)/$', CommunitySoundcloudSetWindow.as_view()),
     url(r'^create_list_window/(?P<pk>\d+)/$', CommunityCreatePlaylistWindow.as_view()),
-    url(r'^create_edit_window/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityEditPlaylistWindow.as_view()),
+    url(r'^edit_edit_window/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityEditPlaylistWindow.as_view()),
 
     url(r'^create_soundcloud_set/(?P<pk>\d+)/$', CommunitySoundcloudSetCreate.as_view()),
     url(r'^soundcloud_set/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunitySoundcloudSet.as_view()),
