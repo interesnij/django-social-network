20e2d6584de55884d4f8e21c4f273e2b640db50c
Serg <ochkarik1983@mail.ru@>
2020-09-22 20:08:21 +0300

    J

--- common/check/community.py
+++ common/check/community.py
@@ -6,24 +6,28 @@ def check_user_not_banned(user, community_pk):
         raise PermissionDenied('Вы заблокированы в этом сообществе')
 
 def check_can_get_lists(user, community):
-    check_user_not_banned(user=user, community_pk=community.pk)
-    if community.is_private() and not user.is_member_of_community_with_name(community_name=community.name):
-        raise ValidationError(
-            'Сообщество является приватным. Вы должны стать участником, чтобы видеть его записи.',
-        )
-    elif community.is_closed() and not user.is_member_of_community_with_name(community_name=community.name):
-        raise ValidationError(
-            'Сообщество является закрытым. Вы должны стать участником, чтобы видеть его записи.',
-        )
-    elif not community.is_verified() and user.is_child():
-        raise ValidationError(
-            'Сообщество не проверено. Вы должны стать участником, чтобы видеть его записи.',
-        )
+    if user.is_authenticated:
+        check_user_not_banned(user=user, community_pk=community.pk)
+        if community.is_private() and not user.is_member_of_community_with_name(community_name=community.name):
+            raise ValidationError(
+                'Сообщество является приватным. Вы должны стать участником, чтобы видеть его записи.',
+            )
+        elif community.is_closed() and not user.is_member_of_community_with_name(community_name=community.name):
+            raise ValidationError(
+                'Сообщество является закрытым. Вы должны стать участником, чтобы видеть его записи.',
+            )
+        elif not community.is_verified() and user.is_child():
+            raise ValidationError(
+                'Сообщество не проверено. Вы должны стать участником, чтобы видеть его записи.',
+            )
+    elif user.is_anonymous:
+        if community.is_private() or community.is_closed() or not community.is_verified():
+            raise ValidationError('Ошибка доступа.')
     else:
         return True
 
 def check_anon_can_get_list(community):
-    if community.is_public() and community.is_verified():
-        return True
+    if community.is_private() or community.is_closed() or not community.is_verified():
+        raise ValidationError('Ошибка доступа.')
     else:
-        raise ValidationError('Ошибка доступа',)
+        return True
