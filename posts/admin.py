00d3bbe11dd5a88e6d5184c9bc2e8fa532e77687
Serg <ochkarik1983@mail.ru@>
2020-09-22 22:39:30 +0300

    J

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,7 @@ from posts.models import Post, PostComment
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('name',)
-    list_display = ['creator','created', 'is_deleted']
+    list_display = ['creator','created', 'is_fixed', 'is_deleted']
     list_filter = ['is_deleted', 'created',]
 
 
