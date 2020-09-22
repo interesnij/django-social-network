e5a9171d654469477ed7d5997725b543caad1fb2
Serg <ochkarik1983@mail.ru@>
2020-09-22 11:39:34 +0300

    J

--- static/scripts/gallery/user_get.js
+++ static/scripts/gallery/user_get.js
@@ -1,6 +1,6 @@
 on('#ajax', 'click', '.u_photo_detail', function() {
   pk = this.getAttribute('photo-pk');
-  this.parentElement.parentElement.parentElement.getAttribute('data-uuid') ? uuid = this.parentElement.parentElement.parentElement.getAttribute('data-uuid') : document.body.querySelector(".pk_saver")
+  this.parentElement.parentElement.parentElement.getAttribute('data-uuid') ? uuid = this.parentElement.parentElement.parentElement.getAttribute('data-uuid') : uuid = document.body.querySelector(".pk_saver").getAttribute('data-uuid')
   loader = document.getElementById("photo_loader");
   open_fullscreen("/gallery/user/photo/" + pk + "/" + uuid + "/", loader)
 });
