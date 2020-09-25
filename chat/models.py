827f3e4db96e483e73240bf84987f0d5bdaa0a26
Serg <ochkarik1983@mail.ru@>
2020-09-25 17:05:38 +0300

    H

--- chat/models.py
+++ chat/models.py
@@ -73,7 +73,7 @@ class Chat(models.Model):
         return self.chat_relation.only("pk")
 
     def get_first_message(self):
-        return self.chat_message.only("pk")[0]
+        return self.chat_message.all()[0]
 
     def get_preview(self):
         return self.get_first_message().text
