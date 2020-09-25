0bc17f7d87477cfcddbdf869ca1fda068700405e
Serg <ochkarik1983@mail.ru@>
2020-09-25 13:52:23 +0300

    H

--- static/scripts/anon.js
+++ static/scripts/anon.js
@@ -293,31 +293,32 @@ var ready = (callback) => {
   else document.addEventListener("DOMContentLoaded", callback);
 }
 on('body', 'click', '#register_ajax', function() {
-  if (!document.body.querySelector("#id_first_name").value){
-    document.body.querySelector("#id_first_name").style.border = "1px #FF0000 solid";
+  form = document.querySelector("#signup");
+  if (!form.querySelector("#id_first_name").value){
+    form.querySelector("#id_first_name").style.border = "1px #FF0000 solid";
     toast_error("Имя - обязательное поле!");
-  } else if (!document.body.querySelector("#id_last_name").value){
-    document.body.querySelector("#id_last_name").style.border = "1px #FF0000 solid";
+  } else if (!form.querySelector("#id_last_name").value){
+    form.querySelector("#id_last_name").style.border = "1px #FF0000 solid";
     toast_error("Фамилия - обязательное поле!")
-  } else if (!document.body.querySelector("#password1").value){
-    document.body.querySelector("#password1").style.border = "1px #FF0000 solid";
+  } else if (!form.querySelector("#password1").value){
+    form.querySelector("#password1").style.border = "1px #FF0000 solid";
     toast_error("Пароль - обязательное поле!")
-  }else if (!document.body.querySelector("#password2").value){
-    document.body.querySelector("#password2").style.border = "1px #FF0000 solid";
+  }else if (!form.querySelector("#password2").value){
+    form.querySelector("#password2").style.border = "1px #FF0000 solid";
     toast_error("Введите пароль еще раз!")
   }
 
-  else if (!document.body.querySelector("#date_day").value){
-      document.body.querySelector("#date_day").style.border = "1px #FF0000 solid";
+  else if (!form.querySelector("#date_day").value){
+      form.querySelector("#date_day").style.border = "1px #FF0000 solid";
       toast_error("День рождения - обязательное поле!")
-  } else if (!document.body.querySelector("#password2").value){
-      document.body.querySelector("#password2").style.border = "1px #FF0000 solid";
+  } else if (!form.querySelector("#password2").value){
+      form.querySelector("#password2").style.border = "1px #FF0000 solid";
       toast_error("Месяц рождения - обязательное поле!")
-  } else if (!document.body.querySelector("#password2").value){
-      document.body.querySelector("#password2").style.border = "1px #FF0000 solid";
+  } else if (!form.querySelector("#password2").value){
+      form.querySelector("#password2").style.border = "1px #FF0000 solid";
       toast_error("Год рождения - обязательное поле!")
   } else {this.disabled = true}
-  form_data = new FormData(document.querySelector("#signup"));
+  form_data = new FormData(form);
   reg_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
   reg_link.open( 'POST', "/rest-auth/registration/", true );
   reg_link.onreadystatechange = function () {
@@ -327,17 +328,18 @@ on('body', 'click', '#register_ajax', function() {
   reg_link.send(form_data);
 })
 on('body', 'click', '#logg', function() {
-  if (!document.body.querySelector("#id_username").value){
-    document.body.querySelector("#id_username").style.border = "1px #FF0000 solid";
+  form = document.querySelector("#login_form");
+  if (!form.querySelector("#id_username").value){
+    form.querySelector("#id_username").style.border = "1px #FF0000 solid";
     toast_error("Введите телефон!")}
-  else if (!document.body.querySelector("#id_password").value){
-    document.body.querySelector("#id_password").style.border = "1px #FF0000 solid";
+  else if (!form.querySelector("#id_password").value){
+    form.querySelector("#id_password").style.border = "1px #FF0000 solid";
     toast_error("Введите пароль!")}
   else {this.disabled = true}
-  if (document.body.querySelector("#id_username").value){document.body.querySelector("#id_username").style.border = "rgba(0, 0, 0, 0.2)";}
-  if (document.body.querySelector("#id_password").value){document.body.querySelector("#id_password").style.border = "rgba(0, 0, 0, 0.2)";}
+  if (form.querySelector("#id_username").value){form.querySelector("#id_username").style.border = "rgba(0, 0, 0, 0.2)";}
+  if (form.querySelector("#id_password").value){form.querySelector("#id_password").style.border = "rgba(0, 0, 0, 0.2)";}
 
-  form_data = new FormData(document.querySelector("#login_form"));
+  form_data = new FormData(form);
   link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
   link.open( 'POST', "/rest-auth/login/", true );
 
