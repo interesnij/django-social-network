11be03d4b264efe2933b406f7893e0870175d9f3
Serg <ochkarik1983@mail.ru@>
2020-09-25 16:58:17 +0300

    H

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from chat.models import Message
+from chat.models import *
 
 
-@admin.register(Message)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("creator", "type", "is_deleted")
+    list_filter = ("creator", )
+
+class ChatUsersAdmin(admin.ModelAdmin):
+    list_display = ("user", "chat", "is_administrator")
+    list_filter = ("user", )
+
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("creator", "created")
     list_filter = ("creator", )
+
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(ChatUsers, ChatUsers)
+admin.site.register(Message, MessageAdmin)
