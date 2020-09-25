19dbc08632a3478d6809b50fe96a4e0951fc4161
Serg <ochkarik1983@mail.ru@>
2020-09-25 21:43:52 +0300

    H

--- chat/models.py
+++ chat/models.py
@@ -165,7 +165,7 @@ class Message(models.Model):
     def send_message(chat, creator, parent, text):
         # программа для отсылки сообщения, когда чат известен
         sender = ChatUsers.objects.get(user=creator)
-        new_message = Message.objects.create(chat=chat, creator__pk=creator.pk, parent=parent, text=text)
+        new_message = Message.objects.create(chat=chat, creator_pk=sender.pk, parent=parent, text=text)
         channel_layer = get_channel_layer()
         payload = {'type': 'receive', 'key': 'text', 'message_id': new_message.uuid, 'creator': creator}
         async_to_sync(channel_layer.group_send)(creator.username, payload)
