d513bb8ab187c1fcfd6639b927762571106d119c
Serg <ochkarik1983@mail.ru@>
2020-09-26 16:23:49 +0300

    G

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -375,13 +375,13 @@ on('#ajax', 'click', '.photo_load_one', function() {
 
 on('#votes_loader', 'click', '.commmunty_load_one', function() {
   _this = this;
-  block = _this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  block = _this.parentElement.parentElement.parentElement.parentElement;
   document.querySelector(".create_fullscreen")
   commmunity_form_selected(_this, block.querySelector(".selected_message_target_items"))
 });
 on('#votes_loader', 'click', '.chat_item_load_one', function() {
   _this = this;
-  block = _this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+  block = _this.parentElement.parentElement.parentElement.parentElement;
   document.querySelector(".create_fullscreen")
   chat_item_form_selected(_this, block.querySelector(".selected_message_target_items"))
 });
