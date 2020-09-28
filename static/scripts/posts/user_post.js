5f7a7628e07b93e6134f33d4e0474e080fbcfba4
Serg <ochkarik1983@mail.ru@>
2020-09-28 12:29:13 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -254,7 +254,7 @@ on('#ajax', 'change', '#u_photo_post_attach', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
   form = document.body.querySelector("#add_photos");
   form_data = new FormData(form);
-  input = form.querySelector("#u_photo_post_comment_attach")
+  input = form.querySelector(".upload_for_post_attach")
   if (input.files.length > 10) {
       toast_error("Не больше 10 фотографий");
       return;
