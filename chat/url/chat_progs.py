3f88c176304affa7f831258687bc939adc31f17c
Serg <ochkarik1983@mail.ru@>
2020-09-29 13:03:36 +0300

    H

--- chat/url/chat_progs.py
+++ chat/url/chat_progs.py
@@ -3,9 +3,9 @@ from chat.view.chat_progs import *
 
 
 urlpatterns = [
-    url(r'^create_chat/$', login_required(CreateChat.as_view())),
-    #url(r'^edit/(?P<pk>\d+)/$', login_required(ChatEdit.as_view())),
-    url(r'^delete/(?P<pk>\d+)/$', login_required(ChatDelete.as_view())),
-	url(r'^abort_delete/(?P<pk>\d+)/$', login_required(ChatAbortDelete.as_view())),
-    url(r'^exit/(?P<pk>\d+)/$', login_required(ChatExit.as_view())),
+    url(r'^create_chat/$', CreateChat.as_view()),
+    #url(r'^edit/(?P<pk>\d+)/$', ChatEdit.as_view()),
+    url(r'^delete/(?P<pk>\d+)/$', ChatDelete.as_view()),
+	url(r'^abort_delete/(?P<pk>\d+)/$', ChatAbortDelete.as_view()),
+    url(r'^exit/(?P<pk>\d+)/$', ChatExit.as_view()),
 ]
