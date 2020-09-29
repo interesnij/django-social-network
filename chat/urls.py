7f5fecdc32745de65a0bfc04692f4b902da4ae6a
Serg <ochkarik1983@mail.ru@>
2020-09-29 12:53:51 +0300

    H

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', ChatDetailView.as_view(), name='chat_detali'),
 
     url(r'^message_progs/', include('chat.url.message_progs')),
-	#url(r'^chat_progs/', include('chat.url.chat_progs')),
+	url(r'^chat_progs/', include('chat.url.chat_progs')),
     url(r'^member_progs/', include('chat.url.member_progs')),
 ]
