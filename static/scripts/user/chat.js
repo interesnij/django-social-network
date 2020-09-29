11e37661dceba2ee21103865ab880b3ec905f35a
Serg <ochkarik1983@mail.ru@>
2020-09-29 20:00:57 +0300

    H

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -8,3 +8,28 @@ on('#ajax', 'click', '.user_add_members', function() {
   block = this.parentElement.parentElement.parentElement.querySelector("#chat_members");
   list_load(block, "/users/load/friends/")
 })
+
+on('#ajax', 'click', '#add_chat_btn', function() {
+  form = document.querySelector("#add_chat_form");
+  this.disabled = true;
+  form_data = new FormData(form);
+
+    var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+      ajax_link.open( 'POST', '/chat/chat_progs/create_chat/', true );
+      ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      ajax_link.onreadystatechange = function () {
+        if ( this.readyState == 4 && this.status == 200 ) {
+            elem_ = document.createElement('span');
+            elem_.innerHTML = ajax_link.responseText;
+            ajax = elem_.querySelector("#reload_block");
+            rtr = document.getElementById('ajax');
+            rtr.innerHTML = ajax.innerHTML;
+            pk = rtr.querySelector(".pk_saver").getAttribute("data-pk");
+            window.scrollTo(0,0);
+            document.title = elem_.querySelector('title').innerHTML;
+            if_list(rtr);
+            window.history.pushState(null, "vfgffgfgf", "/chat/" + pk + "/");
+        }
+      }
+      ajax_link.send(form_data);
+});
