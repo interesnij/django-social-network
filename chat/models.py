2133ed98ccbe1efe6faf43257d45636aaa5e9876
Serg <ochkarik1983@mail.ru@>
2020-09-30 17:14:08 +0300

    H

--- chat/models.py
+++ chat/models.py
@@ -143,9 +143,9 @@ class Chat(models.Model):
         if count == 2:
             member = self.get_chat_member(user_id)
             if self.image:
-                figure = '<figure><img src=' + self.image.url + 'style="border-radius:50px;width:50px;" alt="image"></figure>'
+                figure = '<figure><img src="' + self.image.url + '" style="border-radius:50px;width:50px;" alt="image"></figure>'
             if member.get_avatar():
-                figure = '<figure><img src=' + member.get_avatar() + 'style="border-radius:50px;width:50px;" alt="image"></figure>'
+                figure = '<figure><img src="' + member.get_avatar() + '" style="border-radius:50px;width:50px;" alt="image"></figure>'
             else:
                 figure = '<figure><svg fill="currentColor" class="svg_default svg_default_50" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></figure>'
             if self.name:
