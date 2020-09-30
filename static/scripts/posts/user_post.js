a66e1a74895a7429a38df706a022bdc5b46c7d61
Serg <ochkarik1983@mail.ru@>
2020-09-30 21:12:16 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -348,6 +348,7 @@ on('#ajax', 'click', '.chat_item_load_one', function() {
 on('#ajax', 'click', '.chat_friends_load_one', function() {
   _this = this;
   block = this.nextElementSibling;
+  console.log(this.nextElementSibling);
   chat_item_form_selected(_this, block)
 });
 
