69291e08b9218179dc206d48d434642e0a255c6e
Serg <ochkarik1983@mail.ru@>
2020-10-01 20:45:49 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -163,8 +163,8 @@ function scrollToBottom (id) {
 	document.querySelector(id).scrollIntoView(false);
 }
 
-function minus_one_chat(){ 
-	document.body.querySelector(".new_unread_chats") ? (count = document.body.querySelector(".new_unread_chats").innerHTML, count*1, count -= 1, )
+function minus_one_chat(){
+	document.body.querySelector(".new_unread_chats") ? (count = document.body.querySelector(".new_unread_chats").innerHTML, count*1, count -= 1 ) : null
 }
 
 
