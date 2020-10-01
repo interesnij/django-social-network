1ecc263b3c8aa8dd74a5d7344f00976a98aeaffa
Serg <ochkarik1983@mail.ru@>
2020-10-01 14:58:48 +0300

    H

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -191,7 +191,7 @@ function if_list(block){
     list_load(block.querySelector("#c_album_photo_load"), link);
 		scrolled(link, '#c_album_photo_load', target=0)
   }else if(block.querySelector('.chatlist')){
-    window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0,block.scrollHeight);
   };
 }
 if_list(document.getElementById('ajax'));
