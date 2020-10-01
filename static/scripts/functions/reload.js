7a1965ea96f5f8eba2674e90334e4aaad489dce4
Serg <ochkarik1983@mail.ru@>
2020-10-01 16:03:04 +0300

    H

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -199,6 +199,7 @@ function if_list(block){
 		scrolled(link, '#c_album_photo_load', target=0)
   }else if(block.querySelector('.chatlist')){
 		scrollToBottom (".chatlist");
+		document.getElementById(".text").focus();
   };
 }
 if_list(document.getElementById('ajax'));
