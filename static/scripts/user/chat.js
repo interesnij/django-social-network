c789be42a662a2794ea202a9bde14ad4db5eb1e9
Serg <ochkarik1983@mail.ru@>
2020-10-02 08:59:50 +0300

    H

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -101,7 +101,7 @@ on('#ajax', 'click', '.chat_ajax', function(e) {
         ajax = elem_.querySelector("#reload_block");
         rtr = document.querySelector('.chat_load_container');
         rtr.innerHTML = ajax.innerHTML;
-				document.querySelector(".pk_saver").scrollIntoView(false); 
+				document.querySelector("#scrolled").scrollIntoView(false); 
         window.history.pushState(null, "vfgffgfgf", url);
 				page = 2;
 				loaded = false;
