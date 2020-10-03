3158b7eedf7579c3131d7f041c835801eba12875
Serg <ochkarik1983@mail.ru@>
2020-10-03 14:20:27 +0300

    H

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -349,7 +349,7 @@ on('#ajax', 'click', '.photo_load_one', function() {
     check_photo_in_block(document.body.querySelector(".current_file_dropdown"), pk)
     photo_comment_attach(_this, document.body.querySelector(".current_file_dropdown").parentElement.parentElement, pk)
   } else if (document.body.querySelector(".attach_block")){
-    check_photo_in_block(document.body.querySelector(".attach_block"), _this.parentElement.parentElement, pk) ? null : (photo_post_attach(_this, document.body.querySelector(".attach_block"), pk), close_create_window())
+    check_photo_in_block(document.body.querySelector(".attach_block"), _this.parentElement, pk) ? null : (photo_post_attach(_this, document.body.querySelector(".attach_block"), pk), close_create_window())
   } else if (document.body.querySelector(".message_attach_block")){
     check_photo_in_block(document.body.querySelector(".message_attach_block"), pk)
     photo_message_attach(_this, document.body.querySelector(".message_attach_block"), pk)
