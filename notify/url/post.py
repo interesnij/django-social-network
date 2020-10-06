e223aa75a3aa0b815968ffd15fcf00835dd81150
Serg <ochkarik1983@mail.ru@>
2020-10-06 15:23:48 +0300

    H

--- notify/url/post.py
+++ notify/url/post.py
@@ -3,7 +3,7 @@ from notify.view.post import *
 
 
 urlpatterns = [
-    url(r'^', PostNotificationListView.as_view()),
+    url(r'', PostNotificationListView.as_view()),
     url(r'^c/$', PostCommunityNotificationListView.as_view()),
     url(r'^user_all_read/$', post_user_all_read, name='post_user_all_read'),
     url(r'^community_all_read/$', post_community_all_read, name='post_community_all_read'),
