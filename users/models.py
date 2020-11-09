db0d7b68ae136f7d7bcf509001fa776d971ec529
Serg <ochkarik1983@mail.ru@>
2020-11-09 16:31:50 +0300

    J

--- users/models.py
+++ users/models.py
@@ -817,7 +817,7 @@ class User(AbstractUser):
         return self.communities_memberships.values('pk').count()
 
     def count_albums(self):
-        return self.photo_album_creator.values('pk').count() 
+        return self.photo_album_creator.filter(community=None, is_deleted=False).values('pk').count() 
 
     def count_goods(self):
         return self.good_creator.values('pk').count()
