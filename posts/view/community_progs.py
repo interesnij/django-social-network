e8f1355af183ab348c912c805c9b8d923331abb1
Serg <ochkarik1983@mail.ru@>
2020-11-10 18:18:06 +0300

    J

--- posts/view/community_progs.py
+++ posts/view/community_progs.py
@@ -37,9 +37,9 @@ class PostCommunityCreate(View):
                 get_post_processing(new_post)
                 return render_for_platform(request, 'posts/post_community/admin_post.html', {'object': new_post})
             else:
-                return HttpResponseBadRequest()
+                return HttpResponse("1")
         else:
-            return HttpResponseBadRequest()
+            return HttpResponse("2")
 
 
 class PostOfferCommunityCreate(View):
