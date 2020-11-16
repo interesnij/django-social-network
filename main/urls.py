e2d5afe9b90fdc8d2253ee5637dd59cc131bc038
Serg <ochkarik1983@mail.ru@>
2020-11-16 17:28:08 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -5,15 +5,6 @@ from main.views import *
 urlpatterns = [
 	url(r'^$', PostsListView.as_view(), name="post_list"),
 	url(r'^phone_verify/$', MainPhoneSend.as_view(), name="phone_send"),
-	url(r'^featured_posts/$', FeaturedPostsView.as_view(), name="featured_posts_list"),
-	url(r'^photos/$', PhotosListView.as_view(), name="photos_list"),
-	url(r'^featured_photos/$', FeaturedPhotosView.as_view(), name="featured_photos_list"),
-	url(r'^goods/$', GoodsListView.as_view(), name="goods_list"),
-	url(r'^featured_goods/$', FeaturedGoodsView.as_view(), name="featured_goods_list"),
-	url(r'^videos/$', VideosListView.as_view(), name="videos_list"),
-	url(r'^featured_videos/$', FeaturedVideosView.as_view(), name="featured_videos_list"),
-	url(r'^audios/$', AudiosListView.as_view(), name="audios_list"),
-	url(r'^featured_audios/$', FeaturedAudiosView.as_view(), name="featured_audios_list"),
 
 	url(r'^news/', include('main.url.news')),
 	url(r'^featured/', include('main.url.featured')),
