da73c564581d465be544be94786378ce2c83cbc9
Serg <ochkarik1983@mail.ru@>
2020-11-16 18:16:24 +0300

    J

--- main/url/featured.py
+++ main/url/featured.py
@@ -3,7 +3,7 @@ from main.view.featured import *
 
 
 urlpatterns = [
-	url(r'^$', FeaturedPostsView.as_view(), name="featured_post_list"),
+	url(r'^$', FeaturedPostsView.as_view(), name="featured_posts_list"),
 	url(r'^photos/$', FeaturedPhotosView.as_view(), name="featured_photos_list"),
 	url(r'^goods/$', FeaturedGoodsView.as_view(), name="featured_goods_list"),
 	url(r'^videos/$', FeaturedVideosView.as_view(), name="featured_videos_list"),
