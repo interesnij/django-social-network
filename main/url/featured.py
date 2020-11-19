9a2e480c4acd19bace6b94455cdea2dcfe48c88d
Serg <ochkarik1983@mail.ru@>
2020-11-19 12:32:39 +0300

    J

--- main/url/featured.py
+++ main/url/featured.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	url(r'^photos/$', FeaturedPhotosView.as_view(), name="featured_photos_list"),
 	url(r'^goods/$', FeaturedGoodsView.as_view(), name="featured_goods_list"),
 	url(r'^videos/$', FeaturedVideosView.as_view(), name="featured_videos_list"),
-	url(r'^audios/$', FeaturedAudiosView.as_view(), name="featured_audios_list"),
+	url(r'^music/$', FeaturedAudiosView.as_view(), name="featured_music_list"),
 ]
--- main/url/news.py
+++ main/url/news.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	url(r'^photos/$', PhotosListView.as_view(), name="photos_list"),
 	url(r'^goods/$', GoodsListView.as_view(), name="goods_list"),
 	url(r'^videos/$', VideosListView.as_view(), name="videos_list"),
-	url(r'^audios/$', AudiosListView.as_view(), name="audios_list"),
+	url(r'^music/$', AudiosListView.as_view(), name="music_list"),
 ]
