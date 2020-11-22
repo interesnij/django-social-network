bd3e3b0cf6f18edd00827ef1e95fbb763578cc2b
Serg <ochkarik1983@mail.ru@>
2020-11-22 16:57:08 +0300

    J

--- common/processing/message.py
+++ common/processing/message.py
@@ -1,5 +1,4 @@
 from users.models import User
-from chat.models import Message, Chat
 from communities.models import Community
 from django.http import HttpResponse, HttpResponseBadRequest
 from common.attach.post_attacher import get_post_attach
@@ -7,6 +6,8 @@ from common.check.community import check_user_is_staff
 
 
 def get_message_processing(message):
+    from chat.models import Message
+    
     message.status = Message.STATUS_DRAFT
     message.save(update_fields=['status'])
     return message
