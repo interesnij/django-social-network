5d2652fc5f3e2df06a75b02a92ce2fcbbcf3196f
Serg <ochkarik1983@mail.ru@>
2020-11-23 15:15:24 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -7,10 +7,15 @@ webSocket.connect(ws_path);
 
 function plus_notify(){
   notify = document.body.querySelector(".new_unread_notify");
+  console.log(notify);
   notify.innerHTML ? (notify_count = notify.innerHTML) : notify_count = 0;
+  console.log(notify_count);
   notify_count = int(notify_count) += 1;
+  console.log(notify_count);
   notify.innerHTML = notify_count;
   notify.classList.add("badge", "badge-danger");
+  console.log(notify);
+
 }
 
 webSocket.socket.onclose = function () {
