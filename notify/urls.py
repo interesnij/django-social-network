894d528046177050afbcd4b0c6b6208a9cdb01b5
Serg <ochkarik1983@mail.ru@>
2020-11-24 13:48:11 +0300

    J

--- notify/urls.py
+++ notify/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from notify.views import MainNotifyView
+from notify.views import *
 
 urlpatterns=[
 	url(r'^$', AllNotifyView.as_view(), name='all_notify'),
