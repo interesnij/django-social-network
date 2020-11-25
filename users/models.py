8739225f0176ab476b384d7c9347e8fc8ad182e6
Serg <ochkarik1983@mail.ru@>
2020-11-25 18:39:15 +0300

    J

--- users/models.py
+++ users/models.py
@@ -1537,9 +1537,7 @@ class User(AbstractUser):
         video_notify = VideoNotify.objects.only('created').filter(recipient_id=self.pk)
         VideoNotify.notify_unread(self.pk)
 
-        #result_list = sorted(chain(user_notify, post_notify, photo_notify, good_notify, video_notify), key=lambda instance: instance.created, reverse = True)
         result_list = list(chain(user_notify, post_notify, photo_notify, good_notify, video_notify))
-
         return result_list
 
 
