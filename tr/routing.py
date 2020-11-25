e38eb7b574a67ffbd01fcc8ad5a4117bc54792f0
Serg <ochkarik1983@mail.ru@>
2020-11-25 10:34:58 +0300

    J

--- tr/routing.py
+++ tr/routing.py
@@ -12,7 +12,7 @@ application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
-                url(r"^ws://раса.рус/notify/$", NotificationsConsumer),
+                url(r"/notify/$", NotificationsConsumer),
                 url('ws://раса.рус/(?P<username>[^/]+)/$', MessagerConsumer),
             ])
         ),
