fa96a5a75798a5504bf8a6c4e7d686fc2a662ad6
Serg <ochkarik1983@mail.ru@>
2020-12-01 19:45:52 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -630,13 +630,13 @@ class Post(models.Model):
     def __str__(self):
         return self.creator.get_full_name()
 
-	def notification_user_like(self, user, community):
+    def notification_user_like(self, user, community):
 	    post_notification_handler(creator=user, self.creator, None, self, PostNotify.LIKE)
     def notification_user_dislike(self, user, community):
 	    post_notification_handler(creator=user, self.creator, None, self, PostNotify.DISLIKE)
-	def notification_user_user_repost(self, user, community):
-	    post_notification_handler(user, self.creator, None, self, PostNotify.REPOST)
-	def notification_community_user_repost(self, user, community):
+    def notification_user_user_repost(self, user, community):
+        post_notification_handler(user, self.creator, None, self, PostNotify.REPOST)
+    def notification_community_user_repost(self, user, community):
 	    post_notification_handler(user, self.creator, community, self, PostNotify.COMMUNITY_REPOST)
 
     def notification_community_user_repost(self, user, community):
