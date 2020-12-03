3aed046a5826c9326c06f7c97ecec28045fee5c5
Serg <ochkarik1983@mail.ru@>
2020-12-03 15:12:21 +0300

    J

--- static/scripts/functions/socket.js
+++ static/scripts/functions/socket.js
@@ -24,7 +24,7 @@ function case_u_post_repost_notify(uuid) {
     if (document.body.querySelector( '[data-uuid=' + '"' + uuid + '"' + ']' )){
       post = document.body.querySelector( '[data-uuid=' + '"' + uuid + '"' + ']' );
       block = post.querySelector(".repost_count");
-      block.innerHTML ? (count = block.innerHTML.replace(/\s+/g, ''), count = count*1) : count = 0;
+      post.querySelector(".repost_count").innerHTML ? (count = post.querySelector(".repost_count").innerHTML.replace(/\s+/g, ''), count = count*1) : count = 0;
       count += 1;
       block.innerHTML = count;
     }//}catch{null};
