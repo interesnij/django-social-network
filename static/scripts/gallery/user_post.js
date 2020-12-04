e98120b199b360e4781547145778b54cd441f69a
Serg <ochkarik1983@mail.ru@>
2020-12-04 14:08:12 +0300

    J

--- static/scripts/gallery/user_post.js
+++ static/scripts/gallery/user_post.js
@@ -92,12 +92,12 @@ on('#ajax', 'click', '.u_photo_on_comment', function() {
   post.querySelector(".u_photo_comments").style.display = "unset"
 })
 on('#ajax', 'click', '.mob_u_photo_off_comment', function() {
-  send_change(this, "/gallery/user_progs/off_comment/", "u_photo_on_comment", "Вкл. комментарии");
+  mob_send_change(this, "/gallery/user_progs/off_comment/", "mob_u_photo_on_comment", "Вкл. комментарии");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   post.querySelector(".u_photo_comments").style.display = "none"
 })
 on('#ajax', 'click', '.mob_u_photo_on_comment', function() {
-  send_change(this, "/gallery/user_progs/on_comment/", "u_photo_off_comment", "Выкл. комментарии");
+  mob_send_change(this, "/gallery/user_progs/on_comment/", "mob_u_photo_off_comment", "Выкл. комментарии");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   post.querySelector(".u_photo_comments").style.display = "unset"
 })
@@ -116,10 +116,10 @@ on('#ajax', 'click', '.u_photo_on_private', function() {
   send_photo_change(this, "/gallery/user_progs/on_private/", "u_photo_off_private", "Выкл. приватность")
 })
 on('#ajax', 'click', '.mob_u_photo_off_private', function() {
-  send_change(this, "/gallery/user_progs/off_private/", "u_photo_on_private", "Вкл. приватность")
+  mob_send_change(this, "/gallery/user_progs/off_private/", "mob_u_photo_on_private", "Вкл. приватность")
 })
 on('#ajax', 'click', '.mob_u_photo_on_private', function() {
-  send_change(this, "/gallery/user_progs/on_private/", "u_photo_off_private", "Выкл. приватность")
+  mob_send_change(this, "/gallery/user_progs/on_private/", "mob_u_photo_off_private", "Выкл. приватность")
 })
 
 on('#ajax', 'click', '.u_photo_edit', function() {
@@ -161,13 +161,13 @@ on('#ajax', 'click', '.u_photo_on_votes', function() {
   post.querySelector(".dislike").style.display = "unset";
 })
 on('#ajax', 'click', '.mob_u_photo_off_votes', function() {
-  send_photo_change(this, "/gallery/user_progs/off_votes/", "u_photo_on_votes", "Вкл. реакции");
+  mob_send_change(this, "/gallery/user_progs/off_votes/", "mob_u_photo_on_votes", "Вкл. реакции");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   post.querySelector(".like").style.display = "none";
   post.querySelector(".dislike").style.display = "none";
 })
 on('#ajax', 'click', '.mob_u_photo_on_votes', function() {
-  send_photo_change(this, "/gallery/user_progs/on_votes/", "u_photo_off_votes", "Выкл. реакции");
+  mob_send_change(this, "/gallery/user_progs/on_votes/", "mob_u_photo_off_votes", "Выкл. реакции");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
   post.querySelector(".like").style.display = "unset";
   post.querySelector(".dislike").style.display = "unset";
