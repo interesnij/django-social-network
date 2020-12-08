77720142a78381da8cf5f4b6d3a4bd89205e132c
Serg <ochkarik1983@mail.ru@>
2020-12-08 14:19:56 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -182,7 +182,7 @@ function list_block_load(target_block, response_block, link) {
     if ( request.readyState == 4 && request.status == 200 ){
         elem_ = document.createElement('span');
         elem_.innerHTML = request.responseText;
-       target_block.innerHTML = elem_.querySelector(response_block)
+       target_block.innerHTML = elem_.querySelector(response_block).innerHTML
     }};
     request.send( null );
 }
