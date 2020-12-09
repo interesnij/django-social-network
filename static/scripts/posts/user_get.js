c3359c23fa290603017d8c3b4d6b2b8f0a39aeb1
Serg <ochkarik1983@mail.ru@>
2020-12-09 14:31:33 +0300

    J

--- static/scripts/posts/user_get.js
+++ static/scripts/posts/user_get.js
@@ -29,7 +29,7 @@ on('#ajax', 'click', '.post_list_select', function() {
   span = document.createElement("span");
   span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg> ';
   list.prepend(span)
-})
+});
 
 on('#ajax', 'click', '.post_list_unselect', function() {
   pk = this.parentElement.getAttribute("data-pk");
