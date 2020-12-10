14be9810a0d63df9e7697f4b08cef32220ad095c
Serg <ochkarik1983@mail.ru@>
2020-12-10 20:03:54 +0300

    J

--- static/scripts/anon.js
+++ static/scripts/anon.js
@@ -441,6 +441,10 @@ function open_fullscreen(link, block) {
 }
 function if_list(block) {
     if (block.querySelector('.is_profile_post_paginate')) {
+        link = "/users/detail/list/" + document.body.querySelector(".pk_saver").getAttribute("data-pk") + "/" + document.body.querySelector(".is_main_post_list").getAttribute("list-pk") + "/";
+        list_block_load(block.querySelector('.is_profile_post_paginate'), ".post_stream", link);
+        scrolled(link, '.post_stream', target = 1)
+    } else if (block.querySelector('.is_community_post_paginate')) {
         link = "/communities/detail/list/" + document.body.querySelector(".pk_saver").getAttribute("data-pk") + "/" + document.body.querySelector(".is_main_post_list").getAttribute("list-pk") + "/";
         list_block_load(block.querySelector('.is_profile_post_paginate'), ".post_stream", link);
         scrolled(link, '.post_stream', target = 1)
