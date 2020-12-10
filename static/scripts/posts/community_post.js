77f59b75de7429a4e1326349d513c0279f40196b
Serg <ochkarik1983@mail.ru@>
2020-12-10 17:08:02 +0300

    J

--- static/scripts/posts/community_post.js
+++ static/scripts/posts/community_post.js
@@ -34,9 +34,9 @@ on('#ajax', 'click', '#c_add_post_btn', function() {
     new_post = document.createElement("span");
     new_post.innerHTML = elem;
     (new_post.querySelector('.span1').classList.contains(list_pk) && new_post.querySelector(".card")) ? (lenta_load.querySelector(".community_stream").prepend(new_post),
-                                       toast_info('Запись опубликована в списке "' + list_name + '"'),
+                                       toast_info('Запись опубликована'),
                                        lenta_load.querySelector(".post_empty") ? lenta_load.querySelector(".post_empty").style.display = "none" : null)
-                                     : toast_info('Запись опубликована в списке "' + list_name + '"');
+                                     : toast_info('Запись опубликована');
   }};
 
   link_.send(form_data);
