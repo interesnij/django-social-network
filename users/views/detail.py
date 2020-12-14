fd808fc25a0f13435ef5cfb0b964792afc53ec4a
Serg <ochkarik1983@mail.ru@>
2020-12-14 16:44:54 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -251,7 +251,7 @@ class ProfileUserView(TemplateView):
         if request.user.is_authenticated:
             if request.user.is_no_phone_verified():
                 self.template_name = "main/phone_verification.html"
-            elif user_pk == r_user_pk:
+            elif int(user_pk) == int(r_user_pk):
                 if self.user.is_suspended():
                     self.template_name = "generic/u_template/you_suspended.html"
                 elif self.user.is_blocked():
