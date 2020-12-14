deca39feccb7daecc844f2e2f017784969479a17
Serg <ochkarik1983@mail.ru@>
2020-12-14 16:43:37 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -241,16 +241,17 @@ class ProfileUserView(TemplateView):
         from stst.models import UserNumbers
         import re
 
-        self.user, user_agent, MOBILE_AGENT_RE, user_pk, r_user_pk = User.objects.get(pk=self.kwargs["pk"]), \
-                                                                    request.META['HTTP_USER_AGENT'], \
-                                                                    re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE), \
-                                                                    self.kwargs["pk"], \
-                                                                    request.user.pk
+        user_pk = self.kwargs["pk"]
+
+        self.user, user_agent, MOBILE_AGENT_RE, r_user_pk = User.objects.get(pk=user_pk), \
+                                                                request.META['HTTP_USER_AGENT'], \
+                                                                re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE), \
+                                                                request.user.pk
 
         if request.user.is_authenticated:
             if request.user.is_no_phone_verified():
                 self.template_name = "main/phone_verification.html"
-            elif request.user.pk == r_user_pk:
+            elif user_pk == r_user_pk:
                 if self.user.is_suspended():
                     self.template_name = "generic/u_template/you_suspended.html"
                 elif self.user.is_blocked():
