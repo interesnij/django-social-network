af8430793faa910313985ab8c403c5be7afd9def
Serg <ochkarik1983@mail.ru@>
2020-12-14 19:01:53 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -299,10 +299,10 @@ class ProfileUserView(TemplateView):
             self.template_name = "mobile/" + self.template_name
         else:
             self.template_name = "mobile/" + self.template_name
-        self.data = {c['user']:self.user}
         return super(ProfileUserView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
         c = super(ProfileUserView, self).get_context_data(**kwargs)
+        self.data = {c['user']:self.user}
         c['user'], c['photo_album'], c['video_album'], c['playlist'], c['docs_list'], c['good_album'],c['get_buttons_block'], c['common_frends'] = self.user,self.user.get_or_create_photo_album(),self.user.get_or_create_video_album(),self.user.get_or_create_playlist(),self.user.get_or_create_doc_list(),self.user.get_or_create_good_album(),self.get_buttons_block,self.common_frends
         return self.data
