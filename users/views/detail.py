5a00cc682179dc7de88db032cf02568fb4471929
Serg <ochkarik1983@mail.ru@>
2020-12-14 18:57:33 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -303,14 +303,7 @@ class ProfileUserView(TemplateView):
 
     def get_context_data(self, **kwargs):
         c = super(ProfileUserView, self).get_context_data(**kwargs)
-        c['user'],
-        c['photo_album'],
-        c['video_album'],
-        c['playlist'],
-        c['docs_list'],
-        c['good_album'],
-        c['get_buttons_block'],
-        c['common_frends'] = self.user,
+        c['user'], c['photo_album'], c['video_album'], c['playlist'], c['docs_list'], c['good_album'],c['get_buttons_block'], c['common_frends'] = self.user,
         self.user.get_or_create_photo_album(),
         self.user.get_or_create_video_album(),
         self.user.get_or_create_playlist(),
