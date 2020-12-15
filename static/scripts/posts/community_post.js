9231517e8e907a0f47e0f40d33e69557bf1aab27
Serg <ochkarik1983@mail.ru@>
2020-12-15 14:43:27 +0300

    J

--- static/scripts/posts/community_post.js
+++ static/scripts/posts/community_post.js
@@ -85,8 +85,6 @@ on('#ajax', 'click', '#c_add_post_list_btn', function() {
     date_list = document.body.querySelector(".date-list");
     list = date_list.querySelectorAll(".list");
     for (var i = 0; i < list.length; i++) {list[i].classList.remove("tab_active");list[i].classList.add("pointer", "c_post_list_change");};
-    date_list.querySelector(".main_list").classList.remove("tab_active");
-    date_list.querySelector(".main_list").classList.add("pointer", "c_posts_change");
 
     elem = link_.responseText;
     new_post = document.createElement("span");
