61e0119c2753038950c0907a75c5aa3972182540
Serg <ochkarik1983@mail.ru@>
2020-12-16 20:49:47 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -1,3 +1,21 @@
+function on(elSelector, eventName, selector, fn) {
+    var element = document.querySelector(elSelector);
+    element.addEventListener(eventName, function(event) {
+        var possibleTargets = element.querySelectorAll(selector);
+        var target = event.target;
+        for (var i = 0, l = possibleTargets.length; i < l; i++) {
+            var el = target;
+            var p = possibleTargets[i];
+            while (el && el !== element) {
+                if (el === p) {
+                    return fn.call(p, event);
+                }
+                el = el.parentNode;
+            }
+        }
+    });
+};
+
 function check_photo_in_block(block, _this, pk) {
     if (block.querySelector('[photo-pk=' + '"' + pk + '"' + ']')) {
         _this.parentElement.parentElement.setAttribute("tooltip", "Изображение уже выбрано");
@@ -441,24 +459,6 @@ function elementInViewport(el) {
     var bounds = el.getBoundingClientRect();
     return ((bounds.top + bounds.height > 0) && (window.innerHeight - bounds.top > 0));
 }
-
-function on(elSelector, eventName, selector, fn) {
-    var element = document.querySelector(elSelector);
-    element.addEventListener(eventName, function(event) {
-        var possibleTargets = element.querySelectorAll(selector);
-        var target = event.target;
-        for (var i = 0, l = possibleTargets.length; i < l; i++) {
-            var el = target;
-            var p = possibleTargets[i];
-            while (el && el !== element) {
-                if (el === p) {
-                    return fn.call(p, event);
-                }
-                el = el.parentNode;
-            }
-        }
-    });
-};
 
 function send_comment(form, block, link) {
     form_comment = new FormData(form);
