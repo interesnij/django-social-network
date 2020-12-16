6203bd6f881163880ed481cb1204eac63bc6e846
Serg <ochkarik1983@mail.ru@>
2020-12-16 21:01:29 +0300

    J

--- static/scripts/functions/audio_video.js
+++ static/scripts/functions/audio_video.js
@@ -1,20 +1,3 @@
-function on(elSelector, eventName, selector, fn) {
-    var element = document.querySelector(elSelector);
-    element.addEventListener(eventName, function(event) {
-        var possibleTargets = element.querySelectorAll(selector);
-        var target = event.target;
-        for (var i = 0, l = possibleTargets.length; i < l; i++) {
-            var el = target;
-            var p = possibleTargets[i];
-            while (el && el !== element) {
-                if (el === p) {
-                    return fn.call(p, event);
-                }
-                el = el.parentNode;
-            }
-        }
-    });
-};
 
 function msToTime(duration) {
   var milliseconds = parseInt((duration % 1000) / 100),
--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -1,3 +1,21 @@
+function on(elSelector, eventName, selector, fn) {
+    var element = document.querySelector(elSelector);
+    element.addEventListener(eventName, function(event) {
+        var possibleTargets = element.querySelectorAll(selector);
+        var target = event.target;
+        for (var i = 0, l = possibleTargets.length; i < l; i++) {
+            var el = target;
+            var p = possibleTargets[i];
+            while (el && el !== element) {
+                if (el === p) {
+                    return fn.call(p, event);
+                }
+                el = el.parentNode;
+            }
+        }
+    });
+};
+
 function close_fullscreen() {
   if (document.body.querySelector(".create_fullscreen").style.display == "block") {
     document.body.querySelector(".create_fullscreen").style.display = "none";
