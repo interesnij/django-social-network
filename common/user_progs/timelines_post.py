6c447d3d7081dde1aad8b957b51d76b74848c385
Serg <ochkarik1983@mail.ru@>
2020-12-21 14:31:26 +0300

    J

--- common/user_progs/timelines_post.py
+++ common/user_progs/timelines_post.py
@@ -8,21 +8,22 @@ from music.models import SoundList
 
 def get_timeline_posts(user):
     """ лента записей, которые публикуются друзьями, источниками подписки, сообществами пользователя """
-    my_posts = user.post_creator.only('created').filter(Q(creator_id=user.pk, community__isnull=True, is_deleted=False, status="P"))
+    u_select_related, c_select_related = ('creator', 'parent'), ('creator', 'community', 'parent')
+    my_posts = user.post_creator.only('created').select_related(*u_select_related).filter(Q(creator_id=user.pk, community__isnull=True, is_deleted=False, status="P"))
 
     _community_posts = Q(community__memberships__user__id=user.pk, is_deleted=False, status="P")
     _community_posts.add(~Q(Q(creator__blocked_by_users__blocker_id=user.pk) | Q(creator__user_blocks__blocked_user_id=user.pk) |Q(list__type="DE") |Q(list__type="PR")), Q.AND)
-    community_posts = Post.objects.only('created').filter(_community_posts)
+    community_posts = Post.objects.only('created').select_related(*c_select_related).filter(_community_posts)
 
     followeds = user.follows.values('followed_user_id')
     _followed_users = Q(creator_id__in=[i['followed_user_id'] for i in followeds], creator__user_private__is_private=False, is_deleted=False, status=Post.STATUS_PUBLISHED)
     _followed_users.add(~Q(Q(list__type="DE") |Q(list__type="PR")), Q.AND)
-    followed_users = Post.objects.only('created').filter(_followed_users)
+    followed_users = Post.objects.only('created').select_related(*u_select_related).filter(_followed_users)
 
     frends = user.connections.values('target_user_id')
     _frends_query = Q(creator_id__in=[i['target_user_id'] for i in frends], is_deleted=False, status="P")
     _frends_query.add(~Q(Q(list__type="DE") |Q(list__type="PR")), Q.AND)
-    frends = Post.objects.only('created').filter(_frends_query)
+    frends = Post.objects.only('created').select_related(*u_select_related).filter(_frends_query)
     return my_posts.union(community_posts, followed_users, frends)
 
 def get_timeline_featured_posts(user):
