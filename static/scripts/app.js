6f25b32f067142801366be33da1f32ae1019c97f
Serg <ochkarik1983@mail.ru@>
2020-12-22 19:46:05 +0300

    J

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,7 +3,7 @@ loadScripts('/static/scripts/functions/preview.js')
 loadScripts('/static/scripts/functions/comment_attach.js')
 loadScripts('/static/scripts/functions/post_attach.js')
 loadScripts('/static/scripts/functions/message_attach.js')
-loadScripts('/static/scripts/functions/reload.js')
+//loadScripts('/static/scripts/functions/reload.js')
 loadScripts('/static/scripts/functions/socket.js')
 
 loadScripts('/static/scripts/community/get.js')
