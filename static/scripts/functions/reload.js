77db91895825bc13b38a37040bbf40e47b699b35
Serg <ochkarik1983@mail.ru@>
2020-12-22 18:50:38 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -31,7 +31,7 @@ function scrolled(link, block_id, target) {
     // 2. id блока, куда нужно грузить следующие страницы
     // 3. Указатель на нужность работы просмотров элементов в ленте. Например, target=1 - просмотры постов в ленте
     onscroll = function() {
-        try {
+      //  try {
             if (document.body.querySelector(link)){
               _block = document.body.querySelector(block_id);
               box = _block.querySelector('.first');
@@ -49,7 +49,7 @@ function scrolled(link, block_id, target) {
                     }
                   };
                   if (target == 1) {get_post_view()}}
-                } catch {return}
+        //        } catch {return}
     }
 }
 page = 2;
