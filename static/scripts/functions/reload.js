c2487e8be1d9b42c1fd87ed65677bbc9a1c3a7d9
Serg <ochkarik1983@mail.ru@>
2020-12-22 19:26:54 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -32,7 +32,7 @@ function scrolled(link, block_id, target) {
     // 3. Указатель на нужность работы просмотров элементов в ленте. Например, target=1 - просмотры постов в ленте
     onscroll = function() {
       //  try {
-            if (document.body.querySelector(".is_chat_paginate")){
+            if (document.body.querySelector(".chat_container")){
               console.log("is_chat_paginate");
               _block = document.body.querySelector(".is_chat_paginate");
               box = _block.querySelector('.first');
