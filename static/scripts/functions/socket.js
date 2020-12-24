ef4a749c42a7f6b124a49310954f6b26cc9ee7ed
Serg <ochkarik1983@mail.ru@>
2020-12-24 11:20:09 +0300

    J

--- static/scripts/functions/socket.js
+++ static/scripts/functions/socket.js
@@ -126,9 +126,7 @@ notify.querySelector(".tab_badge") ? (notify_count = notify.querySelector(".tab_
 tab_span = document.createElement("span");
 tab_span.classList.add("tab_badge", "badge-success");
 
-
-ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-ws_path = ws_scheme + '://' + "раса.рус/";
+ws_path = 'wss://' + "раса.рус/notify/";
 webSocket = new channels.WebSocketBridge();
 webSocket.connect(ws_path);
 
