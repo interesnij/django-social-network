8d5472d120b2f0829784a9232cd8b17fa6d2f081
Serg <ochkarik1983@mail.ru@>
2020-12-26 19:36:17 +0300

    K

--- survey/urls.py
+++ survey/urls.py
@@ -4,6 +4,6 @@ from survey.views import SurveyView
 
 urlpatterns = [
     url(r'^$', SurveyView.as_view(), name='survey'),
-    url(r'^user_progs/', include('goods.url.user_progs')),
+    url(r'^user_progs/', include('survey.url.user_progs')),
 	#url(r'^community_progs/', include('goods.url.community_progs')),
 ]
