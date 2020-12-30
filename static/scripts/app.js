00da9184e918eba72c7403c213c11e9248c1578a
Serg <ochkarik1983@mail.ru@>
2020-12-30 20:00:52 +0300

    J

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -49,3 +49,8 @@ loadScripts('/static/scripts/video/user_get.js')
 loadScripts('/static/scripts/video/community_get.js')
 loadScripts('/static/scripts/video/user_post.js')
 loadScripts('/static/scripts/video/community_post.js')
+
+loadScripts('/static/scripts/survey/user_get.js')
+loadScripts('/static/scripts/survey/community_get.js')
+loadScripts('/static/scripts/survey/user_post.js')
+loadScripts('/static/scripts/survey/community_post.js')
