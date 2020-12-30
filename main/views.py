36a02f4a86ab8d5ead149ef814de363a5e2a7781
Serg <ochkarik1983@mail.ru@>
2020-12-30 12:25:01 +0300

    J

--- main/views.py
+++ main/views.py
@@ -57,7 +57,7 @@ class SwitchView(TemplateView):
 		self.custom_link = CustomLink.objects.get(link=self.kwargs["link"])
 
 		try:
-			self.user, user_agent, r_user_pk = self.custom_link.user, request.META['HTTP_USER_AGENT'], request.user.pk
+			self.user, user_agent, r_user_pk = self.custom_link.user.user, request.META['HTTP_USER_AGENT'], request.user.pk
 			user_pk = self.user.pk
 			if request.user.is_authenticated:
 				if request.user.is_no_phone_verified():
