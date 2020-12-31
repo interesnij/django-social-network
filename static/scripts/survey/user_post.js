194e2924d4ef26535ab9df19a2abc38f4277a1d1
Serg <ochkarik1983@mail.ru@>
2020-12-31 12:02:50 +0300

    J

--- static/scripts/survey/user_post.js
+++ static/scripts/survey/user_post.js
@@ -12,7 +12,9 @@ on('#create_loader', 'click', '#u_add_survey_btn', function() {
     document.body.querySelector("#id_title").style.border = "1px #FF0000 solid";
     toast_error("Название - обязательное поле!");
   } else if (!val){
-    answers.style.border = "1px #FF0000 solid";
+    for (var i = 0; i < selectedOptions.length; i++) {
+      if(selectedOptions[i].style.border = "1px #FF0000 solid"}
+    };
     toast_error("Напишите варианты ответов!");
     return
   } else {this.disabled = true}
