3c1b3e7ae710071a7fded002989a89c7dd4c014e
Serg <ochkarik1983@mail.ru@>
2020-12-31 11:11:38 +0300

    J

--- static/scripts/survey/user_get.js
+++ static/scripts/survey/user_get.js
@@ -12,11 +12,9 @@ on('#ajax', 'click', '.remove_answer', function() {
 });
 on('#ajax', 'click', '.add_answer', function() {
   container = this.parentElement.parentElement
-  answers = container.querySelectorAll("answer");
+  answers = container.querySelectorAll(".answer");
   answers.length > 9 ? toast_error("Допустимо не больше 10 вариантов!") :
   (div = document.createElement("div"), div.classList.add("form-group"),
   div.innerHTML = '<div class="input-group pointer remove_answer"><input type="text" name="answers" placeholder="Вариант ответа" class="form-control answer"><div class="input-group-append"><span class="input-group-text custom_color">x</span></div></div>',
   container.append(div));
-  console.log(container);
-  console.log(answers.length)
 });
