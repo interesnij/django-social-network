713379cdec9666ed8f64c54d08950d4a9132c1d9
Serg <ochkarik1983@mail.ru@>
2020-12-31 12:15:12 +0300

    J

--- survey/admin.py
+++ survey/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from survey.models import *
 
 
-admin.site.register(Survey)
-admin.site.register(Answer)
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'community', 'is_anonymous', 'is_multiple', 'is_no_edited', 'time_end']
+    inlines = [
+        AnswerInline,
+    ]
+    search_fields = ('title',)
+
+
+admin.site.register(Survey, SurveyAdmin)
 admin.site.register(SurveyVote)
