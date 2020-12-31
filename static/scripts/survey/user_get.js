4aee07f18920ca98655a35985b66fd06a7197faf
Serg <ochkarik1983@mail.ru@>
2020-12-31 11:27:35 +0300

    J

--- static/scripts/survey/user_get.js
+++ static/scripts/survey/user_get.js
@@ -15,6 +15,6 @@ on('#ajax', 'click', '.add_answer', function() {
   answers = container.querySelectorAll(".answer");
   answers.length > 9 ? toast_error("Допустимо не больше 10 вариантов!") :
   (div = document.createElement("div"), div.classList.add("form-group"),
-  div.innerHTML = '<div class="input-group pointer remove_answer"><input type="text" name="answers" placeholder="Вариант ответа" class="form-control answer"><div class="input-group-append"><span class="input-group-text custom_color">x</span></div></div>',
+  div.innerHTML = '<div class="input-group"><input type="text" name="answers" placeholder="Вариант ответа" class="form-control answer"><div class="input-group-append"><span class="input-group-text custom_color pointer remove_answer">x</span></div></div>',
   container.append(div));
 });
