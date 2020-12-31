2a21445c9863ab42fe1572fe88d0283b80802809
Serg <ochkarik1983@mail.ru@>
2020-12-31 14:56:12 +0300

    J

--- survey/models.py
+++ survey/models.py
@@ -81,7 +81,7 @@ class Answer(models.Model):
         return self.user_voter.filter(answer_id=self.pk, user_id=user_id).exists()
 
     def get_answers(self):
-        return self.surveyvote.all()
+        return SurveyVote.objects.filter(answer_id=self.pk)
 
 
 class SurveyVote(models.Model):
