5ea76499df364fc532c46eb6450ef877b5af5919
Serg <ochkarik1983@mail.ru@>
2020-12-31 19:24:01 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -679,7 +679,7 @@ function get_image_priview(ggg, img) {
                         $img.id = "targetImageCrop";
                         $img.src = e.target.result;
                         $img.class = "thumb-image";
-                        ggg.innerHTML = '<a href="#" style="position: absolute;right:15px;top: 0;" class="delete_thumb">Удалить</a>'
+                        ggg.innerHTML = '<a href="#" style="right:15px;top: 0;" class="delete_thumb">Удалить</a>'
                         ggg.append($img)
                     };
                     reader.readAsDataURL(img.files[0])
