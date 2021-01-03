761e75e3d2aed7fc341461c5e70102dfd9bd2492
Serg <ochkarik1983@mail.ru@>
2021-01-03 11:50:35 +0300

    J

--- static/scripts/docs/user_post.js
+++ static/scripts/docs/user_post.js
@@ -17,6 +17,45 @@ on('#ajax', 'click', '#u_ucm_doc_list_repost_btn', function() {
                      "Репост списка документов в сообщения сделан")
 });
 
+on('#ajax', 'click', '.u_add_doc_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = _this.parentElement.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/docs/user_progs/u_add_doc_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_add_doc_in_list");
+    list.style.paddingLeft = "14px";
+    list.classList.add("u_remove_doc_in_list");
+    list.classList.remove("u_add_doc_in_list");
+    span = document.createElement("span");
+    span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg> ';
+    list.prepend(span)
+  }};
+  link.send( null );
+})
+on('#ajax', 'click', '.u_remove_doc_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = _this.parentElement.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/docs/user_progs/u_remove_doc_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_remove_doc_in_list");
+    list.style.paddingLeft = "30px";
+    list.classList.add("u_add_doc_in_list");
+    list.classList.remove("u_remove_doc_in_list");
+    list.querySelector("svg").remove();
+  }};
+  link.send( null );
+})
+
 on('#ajax', 'click', '#u_create_doc_btn', function() {
   form = document.querySelector("#u_doc_create");
   form_data = new FormData(form);
@@ -65,6 +104,34 @@ on('#ajax', 'click', '#u_create_doc_btn', function() {
   link_.send(form_data);
 });
 
+on('#ajax', 'click', '#u_create_doc_list_btn', function() {
+  form = document.body.querySelector("#c_doc_list_create");
+  form_data = new FormData(form);
+  if (!form.querySelector("#id_name").value){
+    form.querySelector("#id_name").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!");
+  } else { this.disabled = true }
+  pk = document.body.querySelector(".pk_saver").getAttribute("data-pk");
+
+  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+    ajax_link.open( 'POST', "/docs/user_progs/create_list/" + pk + "/", true );
+    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax_link.onreadystatechange = function () {
+      if ( this.readyState == 4 && this.status == 200 ) {
+        elem_ = document.createElement('span');
+        elem_.innerHTML = ajax_link.responseText;
+        ajax = elem_.querySelector("#reload_block");
+        rtr = document.getElementById('ajax');
+        rtr.innerHTML = ajax.innerHTML;
+        window.scrollTo(0,0);
+        document.title = elem_.querySelector('title').innerHTML;
+
+        uuid = rtr.querySelector(".pk_saver").getAttribute("data-uuid");
+        window.history.pushState(null, "vfgffgfgf", '/users/' + pk + "/" + 'doc_list/' + uuid + '/');
+      }
+    }
+    ajax_link.send(form_data);
+});
 
 on('#ajax', 'click', '#u_edit_doclist_btn', function() {
   form = document.body.querySelector("#u_edit_doclist_form");
