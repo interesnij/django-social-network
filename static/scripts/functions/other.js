b663f75895d112dc3c4aa4ae36c2539ecce9e3e2
Serg <ochkarik1983@mail.ru@>
2021-01-03 19:14:09 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -381,8 +381,7 @@ function send_good_change(span, _link, new_class, html) {
 }
 function send_mob_good_change(span, _link, new_class, html) {
     parent = span.parentElement;
-    item = span.parentElement.parentElement.parentElement.parentElement;
-    console.log(item);
+    item = parent.parentElement.parentElement.parentElement.parentElement;
     pk = item.getAttribute("good-pk");
     link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     link.open('GET', _link + pk + "/", true);
