08e932e46a0d74d91d6b36c97b8839dd06d77097
Serg <ochkarik1983@mail.ru@>
2021-01-05 14:08:05 +0300

    J

--- users/models.py
+++ users/models.py
@@ -229,9 +229,9 @@ class User(AbstractUser):
                 return '<i>Онлайн</i>&nbsp;<svg style="width: 17px;" class="svg_default" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z"/></svg>'
         else:
             if self.is_women():
-                '<i>Была' + self.get_last_activity + '</i>'
+                '<i>Была' + self.get_last_activity() + '</i>'
             else:
-                '<i>Был' + self.get_last_activity + '</i>'
+                '<i>Был' + self.get_last_activity() + '</i>'
 
 
     def get_request_ip(request):
