f9457e40ee5d376dec2e3bf7e53f9224ed5946fd
Serg <ochkarik1983@mail.ru@>
2021-01-06 14:09:01 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -570,7 +570,7 @@ class Post(models.Model):
         for item in self.attach:
             #if item[:3] == "pho":
             from gallery.models import Photo
-            photo = Photo.objects.get(pk=item[3:], is_public=True)
+            photo = Photo.objects.get(pk=int(item[3:]), is_public=True)
             block = ''.join([block, '<div class="photo"><div class="progressive replace image_fit u_post_photo pointer" data-href="', photo.file.url, '" photo-pk="', photo.pk, '"><img class="preview image_fit" width="20" height="15" loading="lazy" src="', photo.preview.url,'" alt="img"></div></div>'])
         return ''.join(['<div class="">', block, '</div>'])
 
