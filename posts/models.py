8641f20b02b1fd017c239a938d32df4d073f5b01
Serg <ochkarik1983@mail.ru@>
2021-01-06 19:21:16 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -579,7 +579,7 @@ class Post(models.Model):
                 from music.models import SoundcloudParsing
                 music = SoundcloudParsing.objects.get(pk=item[3:])
                 if music.artwork_url:
-                    figure = '<figure><a class="music_list_post music_thumb pointer"><img style="width:30px;heigth:auto" src="', music.artwork_url, '" alt="img" /></a></figure>'
+                    figure = '<figure><a class="music_list_post music_thumb pointer"><img style="width:30px;heigth:auto" src="', '" alt="img" /></a></figure>'
                 else:
                     figure = '<figure><a class="music_list_post music_thumb pointer"><svg fill="currentColor" style="width:30px;heigth:30px" class="svg_default" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 5h-3v5.5c0 1.38-1.12 2.5-2.5 2.5S10 13.88 10 12.5s1.12-2.5 2.5-2.5c.57 0 1.08.19 1.5.51V5h4v2zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z"/></svg></a></figure>'
                 lists = ''
@@ -592,7 +592,7 @@ class Post(models.Model):
                 '<div class="music" data-path="', \
                 music.uri, '" data-duration="', \
                 music.duration, '" style="flex-basis: 100%;position: relative;"><div class="media" music-counter="0">', \
-                 '<div class="media-body" style="display: flex;"><h6 class="music_list_post music_title"><a>', \
+                figure, '<div class="media-body" style="display: flex;"><h6 class="music_list_post music_title"><a>', \
                 music.title, '</a></h6><span class="span_btn" style="margin-left:auto;display:flex" data-pk="', \
                 str(music.pk), '" user-pk="', str(self.creator.pk), \
                 '"><span class="dropdown" style="position: inherit;"><span class="btn_default pointer drop"><svg fill="currentColor" \
