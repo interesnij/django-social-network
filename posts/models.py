46426c17859daed7a30b424e9c33924c3bcc09ab
Serg <ochkarik1983@mail.ru@>
2021-01-06 13:53:56 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -572,7 +572,7 @@ class Post(models.Model):
                 from gallery.models import Photo
                 photo = Photo.objects.get(pk=item[3:], is_public=True)
                 block = ''.join([block, '<div class="photo"><div class="progressive replace image_fit u_post_photo pointer" data-href="', photo.file.url, '" photo-pk="', photo.pk, '"><img class="preview image_fit" width="20" height="15" loading="lazy" src="', photo.preview.url,'" alt="img"></div></div>'])
-        return block
+        return ''.join(['<div class="">', block, '</div>']) 
 
 
     def get_created(self):
