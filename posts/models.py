0b68d3664e7ac03fb23382bb97b523aa81c89c00
Serg <ochkarik1983@mail.ru@>
2021-01-10 16:15:51 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -647,7 +647,7 @@ class Post(models.Model):
                 if survey.is_time_end():
                     time = "<p>Время голосования вышло</p>"
                 else:
-                    time = "<p>До " + survey.time_end + "</p>"
+                    time = "<p>До " + str(survey.time_end) + "</p>"
                     if user.is_authenticated and not survey.is_user_voted(user.pk):
                         _class = "pointer u_survey_vote" + survey.is_multiple
                 if survey.image:
