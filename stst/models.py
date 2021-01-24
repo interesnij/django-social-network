2ad4bbc5aebc27f28f7a1f1ad9311757f1b81a22
Serg <ochkarik1983@mail.ru@>
2021-01-24 19:22:01 +0300

    J

--- stst/models.py
+++ stst/models.py
@@ -4,9 +4,12 @@ from django.contrib.postgres.indexes import BrinIndex
 
 
 class UserNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     visitor = models.PositiveIntegerField(default=0, verbose_name="Кто заходит")
     target = models.PositiveIntegerField(default=0, verbose_name="К кому заходит")
-    platform = models.PositiveIntegerField(default=0, verbose_name="0 Комп, 1 Телефон")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
 
     class Meta:
@@ -16,9 +19,12 @@ class UserNumbers(models.Model):
 
 
 class CommunityNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     user = models.PositiveIntegerField(default=0, verbose_name="Кто заходит")
     community = models.PositiveIntegerField(default=0, verbose_name="В какое сообщество заходил")
-    platform = models.PositiveIntegerField(default=0, verbose_name="0 Комп, 1 Телефон")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
 
     class Meta:
@@ -32,9 +38,12 @@ class CommunityNumbers(models.Model):
 
 
 class GoodNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     user = models.PositiveIntegerField(default=0, verbose_name="Кто смотрит")
     good = models.PositiveIntegerField(default=0, verbose_name="Какой товар смотрит")
-    platform = models.PositiveIntegerField(default=0, verbose_name="0 Комп, 1 Телефон")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
 
     class Meta:
@@ -44,9 +53,12 @@ class GoodNumbers(models.Model):
 
 
 class VideoNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     user = models.PositiveIntegerField(default=0, verbose_name="Кто смотрит")
     video = models.PositiveIntegerField(default=0, verbose_name="Какой ролик смотрит")
-    platform = models.PositiveIntegerField(default=0, verbose_name="0 Комп, 1 Телефон")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
 
     class Meta:
@@ -56,9 +68,13 @@ class VideoNumbers(models.Model):
 
 
 class MusicNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     user = models.PositiveIntegerField(default=0, verbose_name="Кто слушает")
     music = models.PositiveIntegerField(default=0, verbose_name="Какой трек слушает")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
 
     class Meta:
         indexes = (BrinIndex(fields=['created']),)
@@ -67,9 +83,13 @@ class MusicNumbers(models.Model):
 
 
 class PostNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+
     user = models.PositiveIntegerField(default=0, verbose_name="Кто смотрит")
     post = models.PositiveIntegerField(default=0, verbose_name="Какую запись смотрит")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
 
     class Meta:
         indexes = (BrinIndex(fields=['created']),)
@@ -77,9 +97,13 @@ class PostNumbers(models.Model):
         verbose_name_plural = "Охват записей на стене и в лентах"
 
 class PostAdNumbers(models.Model):
+    DESCTOP, PHONE = 'De', 'Ph'
+    DEVICE = ((DESCTOP, 'Комп'),(PHONE, 'Телефон'),)
+    
     user = models.PositiveIntegerField(default=0, verbose_name="Кто смотрит")
     post = models.PositiveIntegerField(default=0, verbose_name="Какую запись смотрит")
     created = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Создано")
+    device = models.CharField(max_length=5, choices=DEVICE, default=DESCTOP, blank=True, verbose_name="Оборудование")
 
     class Meta:
         indexes = (BrinIndex(fields=['created']),)
