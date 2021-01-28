d7f7c816f879ae3cdad40692aea61ff94610f5c0
Serg <ochkarik1983@mail.ru@>
2021-01-28 15:12:39 +0300

    J

--- goods/models.py
+++ goods/models.py
@@ -385,9 +385,9 @@ class GoodComment(models.Model):
 			return str(count) + " ответов"
 
 	def get_u_attach(self, user):
-        from common.attach.comment_attach import get_u_comment_attach
-        return get_c_comment_attach(self, user)
+		from common.attach.comment_attach import get_u_comment_attach
+		return get_c_comment_attach(self, user)
 
-    def get_c_attach(self, user):
-        from common.attach.comment_attach import get_c_comment_attach
-        return get_c_comment_attach(self, user)
+	def get_c_attach(self, user):
+		from common.attach.comment_attach import get_c_comment_attach
+		return get_c_comment_attach(self, user)
