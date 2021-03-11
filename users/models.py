39325e98fa60b964519a4d7c8277a3f0b9d283f6
Serg <ochkarik1983@mail.ru@>
2021-03-11 17:13:03 +0300

    J

--- users/models.py
+++ users/models.py
@@ -1593,7 +1593,12 @@ class User(AbstractUser):
         from notify.model.video import VideoNotify
         from itertools import chain
 
-        return sorted(chain(UserNotify.objects.only('created').filter(recipient_id=self.pk), PostNotify.objects.only('created').filter(recipient_id=self.pk), PhotoNotify.objects.only('created').filter(recipient_id=self.pk), GoodNotify.objects.only('created').filter(recipient_id=self.pk), VideoNotify.objects.only('created').filter(recipient_id=self.pk)), key=lambda instance: instance.created, reverse=True)
+        return sorted(chain(UserNotify.objects.only('created').filter(recipient_id=self.pk, ), \
+                            PostNotify.objects.only('created').filter(recipient_id=self.pk, user_set__isnull=False, object_set__isnull=False), \
+                            PhotoNotify.objects.only('created').filter(recipient_id=self.pk), \
+                            GoodNotify.objects.only('created').filter(recipient_id=self.pk), \
+                            VideoNotify.objects.only('created').filter(recipient_id=self.pk)), \
+                            key=lambda instance: instance.created, reverse=True)
 
     def read_user_notify(self):
         from notify.model.good import GoodNotify
