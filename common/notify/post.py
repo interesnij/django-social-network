8c88e0f1b347a2a2168deaa5e3b8dde63a75b6b8
Serg <ochkarik1983@mail.ru@>
2021-03-14 17:56:55 +0300

    J

--- common/notify/post.py
+++ common/notify/post.py
@@ -51,8 +51,8 @@ def user_post_notify(creator, recipient_id, action_community_id, post_id, commen
         elif PostNotify.objects.filter(creator=creator, recipient_id=recipient_id, community__isnull=True, created__gt=today, verb=current_verb).exists():
             notify = PostNotify.objects.filter(creator=creator, recipient_id=recipient_id, community__isnull=True, created__gt=today, verb=current_verb).last()
             PostNotify.objects.create(creator=creator, recipient_id=recipient_id, post_id=post_id, verb=current_verb, user_set=notify)
-        elif PostNotify.objects.filter(recipient_id=recipient_id, community__isnull=True, created__gt=today, post_id=post_id, verb__in=verb).exists():
-            notify = PostNotify.objects.get(recipient_id=recipient_id, community__isnull=True, post_id=post_id, created__gt=today, verb__in=verb)
+        elif PostNotify.objects.filter(recipient_id=recipient_id, community__isnull=True, created__gt=today, post_id=post_id, Q(verb=verb)|Q(verb="W"+verb)).exists():
+            notify = PostNotify.objects.get(recipient_id=recipient_id, community__isnull=True, post_id=post_id, created__gt=today, Q(verb=verb)|Q(verb="W"+verb))
             PostNotify.objects.create(creator=creator, recipient_id=recipient_id, post_id=post_id, verb="G"+verb, object_set=notify)
         else:
             PostNotify.objects.create(creator=creator, recipient_id=recipient_id, post_id=post_id, verb=current_verb)
