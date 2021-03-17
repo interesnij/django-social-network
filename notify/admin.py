b4d6b4922ff2a235ceb98ccb48b3439ce82311ed
Serg <ochkarik1983@mail.ru@>
2021-03-17 17:16:09 +0300

    J

--- notify/admin.py
+++ notify/admin.py
@@ -1,27 +1,12 @@
 from django.contrib import admin
-from notify.model.user import UserNotify, UserCommunityNotify
-from notify.model.post import PostNotify, PostCommunityNotify
-from notify.model.photo import PhotoNotify, PhotoCommunityNotify
-from notify.model.good import GoodNotify, GoodCommunityNotify
-from notify.model.video import VideoNotify, VideoCommunityNotify
-from notify.model.survey import SurveyNotify, SurveyCommunityNotify
+from notify.models import Notify
 
 
-class PostNotifyAdmin(admin.ModelAdmin):
+class NotifyAdmin(admin.ModelAdmin):
     list_display = ['creator','recipient','created']
     list_filter = ['created']
     class Meta:
-        model = PostNotify
+        model = Notify
 
-admin.site.register(UserNotify)
-admin.site.register(UserCommunityNotify)
-admin.site.register(PostNotify, PostNotifyAdmin)
-admin.site.register(PostCommunityNotify)
-admin.site.register(PhotoNotify)
-admin.site.register(PhotoCommunityNotify)
-admin.site.register(GoodNotify)
-admin.site.register(GoodCommunityNotify)
-admin.site.register(VideoNotify)
-admin.site.register(VideoCommunityNotify)
-admin.site.register(SurveyNotify)
-admin.site.register(SurveyCommunityNotify)
+
+admin.site.register(Notify, NotifyAdmin)
