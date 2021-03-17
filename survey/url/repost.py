8ffebf19eeb9c80b6d4a875d62eb33731529ccfe
Serg <ochkarik1983@mail.ru@>
2021-03-17 15:29:36 +0300

    J

---
+++ survey/url/repost.py
@@ -0,0 +1,14 @@
+from posts.view.repost import *
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^u_ucm_survey_window/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UUCMSurveyWindow.as_view()),
+    url(r'^c_ucm_survey_window/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CUCMSurveyWindow.as_view()),
+
+    url(r'^u_u_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UUSurveyRepost.as_view()),
+    url(r'^c_u_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CUSurveyRepost.as_view()),
+    url(r'^u_c_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UCSurveyRepost.as_view()),
+    url(r'^c_c_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CCSurveyRepost.as_view()),
+    url(r'^u_m_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UMSurveyRepost.as_view()),
+    url(r'^c_m_survey_repost/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CMSurveyRepost.as_view()),
+]
