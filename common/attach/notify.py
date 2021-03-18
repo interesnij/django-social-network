bf44a4753ea164bef3c472c4dfac5d7849fc0fb4
Serg <ochkarik1983@mail.ru@>
2021-03-18 19:23:28 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -1,5 +1,6 @@
 def linebreaks(value, autoescape=None):
     from django.utils.html import linebreaks
+    from django.utils.safestring import mark_safe
     autoescape = autoescape and not isinstance(value, SafeData)
     return mark_safe(linebreaks(value, autoescape))
 
