32120a04f2249fc22b935d49d006bb8b33189014
Serg <ochkarik1983@mail.ru@>
2021-03-19 16:18:52 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -2,4 +2,7 @@
 def get_notify(user, verb, attach):
     if attach[:3] == "pos":
         from common.items.post import get_post
-        return get_post(user, attach[3:])
+        if verb == "ITE":
+            return get_post(user, attach[3:])
+        else:
+            return '<p><a href="' + user.get_link() + '" class="ajax">' + user.get_full_name() + '</a>' + verb + 'запись</P>' + get_post(user, attach[3:])
