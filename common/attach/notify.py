951f8ed6073e8644a6f02db48337ea709891723f
Serg <ochkarik1983@mail.ru@>
2021-03-19 10:37:20 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -80,7 +80,7 @@ def get_notify(user, verb, attach):
             </svg><span class="repost_count">', str(post.count_reposts()), '</span></span></div><span class="col-auto" title="Просмотры" \
             style="font-size:12px"><svg fill="currentColor" class="svg_info svg_default" style="width:17px;padding-bottom: 3px;" \
             viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>\
-            </svg>', str(post.all_visits_count()), '</span></div><div class="u_load_comments"></div></div></span>'])
+            </svg>', str(post.text), '</span></div><div class="u_load_comments"></div></div></span>'])
         #except:
         #    pass
     if block:
