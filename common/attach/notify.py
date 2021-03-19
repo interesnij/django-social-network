c024f97253ca9c56492966dba8ad192a883f8f99
Serg <ochkarik1983@mail.ru@>
2021-03-19 17:42:42 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -8,11 +8,11 @@ def get_notify(user, notify):
         else:
             if notify.is_have_user_set():
                 first_notify = notify.get_first_user_set()
-                return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax">' + first_notify.creator.get_full_name() + '</a> '\
+                return '<p style="padding-left: 7px;"><a href="' + first_notify.creator.get_link() + '" class="ajax">' + first_notify.creator.get_full_name() + '</a> '\
                 + first_notify.get_verb_display() + str(notify.count_user_set()) + '</p>' + get_post(user, attach[3:])
             elif notify.is_have_object_set():
                 first_notify = notify.get_first_object_set()
-                return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
+                return '<p style="padding-left: 7px;"><a href="' + first_notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
                 first_notify.creator.get_full_name() + '</a> и ещё ' + str(notify.count_object_set()) + first_notify.get_verb_display()\
                  + ' запись </p>' + get_post(user, attach[3:])
             else:
