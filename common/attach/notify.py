4c3b73e934cd6dd37fb048602430262b39e0cc46
Serg <ochkarik1983@mail.ru@>
2021-03-19 13:58:57 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -2,8 +2,4 @@
 def get_notify(user, verb, attach):
     if attach[:3] == "pos":
         from common.items.post import get_u_post
-        return get_u_post(user, attach[3:])
-    if block:
-        return block
-    else:
-        return "None"
+        return get_post(user, attach[3:])
