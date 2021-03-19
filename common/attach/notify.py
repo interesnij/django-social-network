9f32d820f6b14af1adf933b7acb5a3769b5f7f8d
Serg <ochkarik1983@mail.ru@>
2021-03-19 11:11:10 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -67,7 +67,7 @@ def get_notify(user, verb, attach):
             <span class="dropdown-item u_all_posts_dislikes pointer">Не оценили</span></span>', card_drop, '</div></div></div></div>\
              <div class="fullscreen pointer">', linebreaks(post.text),'\
             </div>', attach, '<div class="card-footer border-top py-2"><div class="row"><div class="col interaction" id="interaction">\
-            <span ', votes_on, ' class="like u_like ', 'user_like" title="Нравится"><svg class="svg_info" fill="currentColor"viewBox="0 0 24 24">\
+            <span ', votes_on, ' class="like u_like ', user_like, 'title="Нравится"><svg class="svg_info" fill="currentColor"viewBox="0 0 24 24">\
             <path fill="none" d="M0 0h24v24H0V0z"/><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z"/>\
             </svg><span class="likes_count" data-count="like">', str(post.likes_count()), '</span></span><span class="like_window">\
             <div class="like_pop"><span class="u_all_posts_likes pointer">Оценили: ', post.likes_count_ru(), '</span>\
