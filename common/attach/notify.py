5f57fc2a227c33994b0736177c1d6ec370dbbf23
Serg <ochkarik1983@mail.ru@>
2021-03-19 17:37:51 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -11,7 +11,7 @@ def get_notify(user, notify):
                 return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax">' + first_notify.creator.get_full_name() + '</a> '\
                 + first_notify.get_verb_display() + str(notify.count_user_set()) + '</p>' + get_post(user, attach[3:])
             elif notify.is_have_object_set():
-                 first_notify = notify.get_first_object_set()
+                first_notify = notify.get_first_object_set()
                 return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
                 first_notify.creator.get_full_name() + '</a> и ещё ' + str(notify.count_object_set()) + first_notify.get_verb_display()\
                  + 'запись</p>' + get_post(user, attach[3:])
