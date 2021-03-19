281e9424d4a574de4dd44b30cc25eda26886fa59
Serg <ochkarik1983@mail.ru@>
2021-03-19 17:37:18 +0300

    J

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -4,5 +4,16 @@ def get_notify(user, notify):
     if attach[:3] == "pos":
         from common.items.post import get_post
         if notify.verb == "ITE":
-            get_post(user, attach[3:])
-        
+            return get_post(user, attach[3:])
+        else:
+            if notify.is_have_user_set():
+                first_notify = notify.get_first_user_set()
+                return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax">' + first_notify.creator.get_full_name() + '</a> '\
+                + first_notify.get_verb_display() + str(notify.count_user_set()) + '</p>' + get_post(user, attach[3:])
+            elif notify.is_have_object_set():
+                 first_notify = notify.get_first_object_set()
+                return '<p><a href="' + first_notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
+                first_notify.creator.get_full_name() + '</a> и ещё ' + str(notify.count_object_set()) + first_notify.get_verb_display()\
+                 + 'запись</p>' + get_post(user, attach[3:])
+            else:
+                return get_post(user, attach[3:])
