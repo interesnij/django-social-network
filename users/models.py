074e61c64631167ae85cc609fabf34c823f0f1d0
Serg <ochkarik1983@mail.ru@>
2021-03-30 10:42:00 +0300

    J

--- users/models.py
+++ users/models.py
@@ -1086,7 +1086,7 @@ class User(AbstractUser):
         from gallery.models import Album
 
         albums_query = Q(is_deleted=False, is_public=True, community__isnull=True)
-        lists_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
+        albums_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
         albums_query.add(~Q(type=Album.MAIN), Q.AND)
         return Album.objects.filter(albums_query).order_by("order")
 
