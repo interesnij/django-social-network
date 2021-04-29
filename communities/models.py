ee34d99829a141f26b56443802c71e6f199fdca1
Serg <ochkarik1983@mail.ru@>
2021-04-29 17:08:06 +0300

    J

--- communities/models.py
+++ communities/models.py
@@ -48,8 +48,8 @@ class Community(models.Model):
         (CLOSED, 'Закрытый'),(PRIVATE, 'Приватный'),(MANAGER, 'Созданный персоналом'),(PUBLIC, 'Открытый'), (THIS_PROCESSING, 'Обработка'),
         (THIS_OPEN_DELETED, 'Открытый удалённый'),(THIS_PRIVATE_DELETED, 'Приватный удалённый'),(THIS_CLOSED_DELETED, 'Закрытый удалённый'),(THIS_MANAGER_DELETED, 'Менеджерский удалённый'),
         (THIS_BANNER_OPEN, 'Открытый баннер'),(THIS_BANNER_PRIVATE, 'Приватный баннер'),(THIS_BANNER_CLOSED, 'Закрытый баннер'),(THIS_BANNER_MANAGER, 'Менеджерский баннер'),
-        (THIS_SUSPENDED_OPEN, 'Открытый замороженный'),(THIS_SUSPENDED_PRIVATE, 'Приватный замороженный'), (THIS_SUSPENDED_BANNER, 'Закрытый замороженный'),(THIS_SUSPENDED_BANNER, 'Менеджерский замороженный'),
-        (THIS_BLOCKED_OPEN, 'Открытый блокнутый'),(THIS_BLOCKED_PRIVATE, 'Приватный блокнутый'), (THIS_BLOCKED_BANNER, 'Закрытый блокнутый'),(THIS_BLOCKED_BANNER, 'Менеджерский блокнутый'),
+        (THIS_SUSPENDED_OPEN, 'Открытый замороженный'),(THIS_SUSPENDED_PRIVATE, 'Приватный замороженный'), (THIS_SUSPENDED_CLOSED, 'Закрытый замороженный'),(THIS_SUSPENDED_MANAGER, 'Менеджерский замороженный'),
+        (THIS_BLOCKED_OPEN, 'Открытый блокнутый'),(THIS_BLOCKED_PRIVATE, 'Приватный блокнутый'), (THIS_BLOCKED_CLOSED, 'Закрытый блокнутый'),(THIS_BLOCKED_MANAGER, 'Менеджерский блокнутый'),
     )
 
     CHILD, STANDART, VERIFIED_SEND, VERIFIED = 'CH', 'ST', 'VS', 'VE'
