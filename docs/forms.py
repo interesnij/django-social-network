4367b63a7a70693ff5aca8236d353830c43e1af4
Serg <ochkarik1983@mail.ru@>
2021-04-29 18:36:46 +0300

    J

--- docs/forms.py
+++ docs/forms.py
@@ -12,18 +12,6 @@ class DoclistForm(forms.ModelForm):
 class DocForm(forms.ModelForm):
 	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
 
-	def clean_file(self):
-		file = self.cleaned_data['file']
-		try:
-			if file:
-				if len(file.name.split('.')) == 1:
-					raise forms.ValidationError('File type is not supported')
-				if file._size > settings.DOC_UPLOAD_FILE_MAX_SIZE:
-					raise forms.ValidationError(_('Максимальный размер файла: %s. Ваш документ: %s') % (filesizeformat(settings.TASK_UPLOAD_FILE_MAX_SIZE), filesizeformat(file._size)))
-		except:
-			pass
-		return file
-
 	class Meta:
 		model = Doc
-		fields = ['title', 'file', 'type', 'list', ]
+		fields = ['title', 'file', 'list', ]
