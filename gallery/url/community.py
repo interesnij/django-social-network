6633ef9b0b969c6b4568a888dc14c705c5a39100
Serg <ochkarik1983@mail.ru@>
2021-04-29 18:11:31 +0300

    J

--- gallery/url/community.py
+++ gallery/url/community.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 urlpatterns=[
 	url(r'^photos/(?P<pk>\d+)/$', CommunityPhotosList.as_view()),
-	url(r'^photo_list_photos/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityPhotoListPhotos.as_view(), name="community_photos"),
+	url(r'^photo_list_photos/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityAlbumPhotoList.as_view(), name="community_photos"),
 
 	url(r'^avatar_photo/(?P<pk>\d+)/(?P<photo_pk>\d+)/$', CommunityDetailAvatar.as_view(), name="community_avatar"),
     url(r'^photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', CommunityPhoto.as_view(), name="community_photo"),
