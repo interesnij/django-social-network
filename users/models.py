54b26537b397f2761365f92e9480156623f85773
Serg <ochkarik1983@mail.ru@>
2021-05-01 17:22:02 +0300

    J

--- users/models.py
+++ users/models.py
@@ -915,13 +915,13 @@ class User(AbstractUser):
     def get_post_lists(self):
         from posts.models import PostList
         query = Q(creator_id=self.id, community__isnull=True)
-        query.add(~Q(type__contains="!"), Q.AND)
+        query.add(~Q(type__contains="_"), Q.AND)
         return PostList.objects.filter(query).order_by("order")
 
     def get_survey_lists(self):
         from survey.models import SurveyList
         query = Q(creator_id=self.id, community__isnull=True)
-        query.add(~Q(status__contains="!"), Q.AND)
+        query.add(~Q(type__contains="_") Q.AND)
         return SurveyList.objects.filter(query).order_by("order")
 
     def get_post_categories(self):
