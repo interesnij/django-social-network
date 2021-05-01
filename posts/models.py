f3feb1262cbb131953401d823375a226391b2acb
Serg <ochkarik1983@mail.ru@>
2021-05-01 12:33:19 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -114,8 +114,8 @@ class PostList(models.Model):
 
     @classmethod
     def get_user_staff_lists(cls, user_pk):
-        query = ~Q(type__contains="_")
-        query.add(Q(Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)), Q.AND)
+        query = Q(Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk))
+        query.add(~Q(type__contains="_"), Q.AND)
         return cls.objects.filter(query)
     @classmethod
     def get_user_lists(cls, user_pk):
