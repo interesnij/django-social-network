6bfebf0bf8451a6b10af25110eb3b31a1aa5855f
Serg <ochkarik1983@mail.ru@>
2021-05-01 14:23:13 +0300

    J

--- users/models.py
+++ users/models.py
@@ -943,7 +943,7 @@ class User(AbstractUser):
 
     def get_good_list(self):
         from goods.models import GoodList
-        GoodList.objects.get(creator_id=self.pk)
+        GoodList.objects.get(creator=self, community__isnull=True, type=GoodList.MAIN)
     def get_playlist(self):
         from music.models import SoundList
         return SoundList.objects.get(creator_id=self.pk, community__isnull=True, type=SoundList.MAIN)
