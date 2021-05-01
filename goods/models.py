29f2562d0e52ae05864a0121b3aa4166b1d5008d
Serg <ochkarik1983@mail.ru@>
2021-05-01 14:02:14 +0300

    J

--- goods/models.py
+++ goods/models.py
@@ -134,53 +134,53 @@ class GoodList(models.Model):
 	@classmethod
 	def get_user_staff_lists(cls, user_pk):
 		query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type__contains="_"), Q.AND)
+		query.add(Q(type__contains="_"), Q.AND)
 		return cls.objects.filter(query)
 	@classmethod
 	def is_have_user_staff_lists(cls, user_pk):
 		query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type__contains="_"), Q.AND)
+		query.add(Q(type__contains="_"), Q.AND)
 		return cls.objects.filter(query).exists()
 	@classmethod
 	def get_user_lists(cls, user_pk):
 		query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type="LIS"), Q.AND)
+		query.add(Q(type="LIS"), Q.AND)
 		return cls.objects.filter(query).order_by("order")
 	@classmethod
 	def is_have_user_lists(cls, user_pk):
 		query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type="LIS"), Q.AND)
+		query.add(Q(type="LIS"), Q.AND)
 		return cls.objects.filter(query).exists()
 	@classmethod
 	def get_user_lists_count(cls, user_pk):
 		query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type="LIS"), Q.AND)
+		query.add(Q(type="LIS"), Q.AND)
 		return cls.objects.filter(query).values("pk").count()
 
 	@classmethod
 	def get_community_staff_lists(cls, community_pk):
 		query = Q(community_id=user_pk)|Q(communities__id=user_pk)
-        query.add(~Q(type__contains="_")), Q.AND)
+		query.add(~Q(type__contains="_")), Q.AND)
 		return cls.objects.filter(query)
 	@classmethod
 	def is_have_community_staff_lists(cls, community_pk):
 		query = Q(community_id=user_pk)|Q(communities__id=user_pk)
-        query.add(~Q(type__contains="_")), Q.AND)
+		query.add(~Q(type__contains="_")), Q.AND)
 		return cls.objects.filter(query).exists()
 	@classmethod
 	def get_community_lists(cls, community_pk):
 		query = Q(community_id=user_pk)|Q(communities__id=user_pk)
-        query.add(Q(type="LIS")), Q.AND)
+		query.add(Q(type="LIS")), Q.AND)
 		return cls.objects.filter(query).order_by("order")
 	@classmethod
 	def is_have_community_lists(cls, community_pk):
 		query = Q(community_id=user_pk)|Q(communities__id=user_pk)
-        query.add(Q(type="LIS")), Q.AND)
+		query.add(Q(type="LIS")), Q.AND)
 		return cls.objects.filter(query).exists()
 	@classmethod
 	def get_community_lists_count(cls, community_pk):
 		query = Q(community_id=user_pk)|Q(communities__id=user_pk)
-        query.add(Q(type="LIS")), Q.AND)
+		query.add(Q(type="LIS")), Q.AND)
 		return cls.objects.filter(query).values("pk").count()
 
 	@classmethod
