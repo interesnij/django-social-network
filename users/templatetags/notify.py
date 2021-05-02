a948c347648328fa2b711f8284976f4ee349eb20
Serg <ochkarik1983@mail.ru@>
2021-05-02 09:55:03 +0300

    H

--- users/templatetags/notify.py
+++ users/templatetags/notify.py
@@ -5,7 +5,3 @@ register=template.Library()
 @register.filter
 def get_notify(notify, user):
     return notify.get_notify(user)
-
-@register.filter
-def get__notify(notify, user):
-    return notify.get__notify(user)
