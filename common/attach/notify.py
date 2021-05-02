be2249bbaaedc8d8880713e26fb1bd66418e82ee
Serg <ochkarik1983@mail.ru@>
2021-05-02 10:03:20 +0300

    H

--- common/attach/notify.py
+++ common/attach/notify.py
@@ -1,9 +1,8 @@
-
 def get_notify(user, notify):
-    attach = notify.attach
-    if attach[:3] == "pos":
+    type = notify.type
+    if type == "POS":
         from common.items.post import get_post
         return get_post(user, notify)
-    elif attach[:3] == "pho":
+    elif type == "PHO":
         from common.items.photo import get_photo
         return get_photo(user, notify)
