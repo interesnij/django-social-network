e1303ab5bacef933887f1bbaad3d431164b93fd9
Serg <ochkarik1983@mail.ru@>
2021-05-03 15:32:27 +0300

    J

--- common/notify/notify.py
+++ common/notify/notify.py
@@ -49,7 +49,7 @@ def community_notify(creator, community, action_community_id, object_id, type, s
             Notify.objects.create(creator_id=creator.pk, recipient_id=user_id, action_community_id=action_community_id, community_id=community.pk, object_id=object_id, type=type, verb="G"+verb, object_set=notify)
         else:
             Notify.objects.create(creator_id=creator.pk, recipient_id=user_id, action_community_id=action_community_id, community_id=community.pk, object_id=object_id, type=type, verb=current_verb)
-        community_send_notify(attach[3:], creator.pk, user_id, community, action_community_id, socket_name)
+        community_send_notify(object_id, creator.pk, user_id, community, action_community_id, socket_name)
 
 
 def user_wall(creator, action_community_id, object_id, type, socket_name, verb):
@@ -57,11 +57,11 @@ def user_wall(creator, action_community_id, object_id, type, socket_name, verb):
 
     current_verb = creator.get_verb_gender(verb)
 
-    if Wall.objects.filter(creator_id=creator.pk, action_community_id=action_community_id, attach=attach, verb=verb).exists():
+    if Wall.objects.filter(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb).exists():
         pass
-    #elif Wall.objects.filter(action_community_id=action_community_id, created__gt=today, attach__contains=attach[:3], verb=current_verb).exists():
-    #    notify = Wall.objects.get(action_community_id=action_community_id, attach__contains=attach[:3], created__gt=today, verb=current_verb)
-    #    Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, attach=attach, verb=current_verb, user_set=notify)
+    #elif Wall.objects.filter(action_community_id=action_community_id, created__gt=today, object_id, verb=current_verb).exists():
+    #    notify = Wall.objects.get(action_community_id=action_community_id, object_id, created__gt=today, verb=current_verb)
+    #    Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=current_verb, user_set=notify)
     elif Wall.objects.filter(object_id=object_id, type=type, created__gt=today, verb=verb).exists():
         notify = Wall.objects.get(object_id=object_id, type=type, created__gt=today, verb=verb)
         Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb="G"+verb, object_set=notify)
@@ -76,9 +76,9 @@ def community_wall(creator, community, action_community_id, object_id, type, soc
 
     if Wall.objects.filter(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb).exists():
         pass
-    #elif Wall.objects.filter(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, created__gt=today, attach__in=attach[:3], verb=verb).exists():
-    #    notify = Wall.objects.get(creator_id=creator.pk, community_id=community.pk, created__gt=today, action_community_id=action_community_id, attach=attach, verb=verb)
-    #    Wall.objects.create(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, attach=attach, verb=verb, user_set=notify)
+    #elif Wall.objects.filter(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, created__gt=today, object_id=object_id, verb=verb).exists():
+    #    notify = Wall.objects.get(creator_id=creator.pk, community_id=community.pk, created__gt=today, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb)
+    #    Wall.objects.create(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb, user_set=notify)
     elif Wall.objects.filter(community_id=community.pk, object_id=object_id, type=type, created__gt=today, verb=verb).exists():
         notify = Wall.objects.get(community_id=community.pk, object_id=object_id, type=type, created__gt=today, verb=verb)
         Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, community_id=community.pk, object_id=object_id, type=type, verb="G"+verb, object_set=notify)
