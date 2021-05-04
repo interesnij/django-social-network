6f37612095c909e4f513fb9d7a51fa0d006a2a6c
Serg <ochkarik1983@mail.ru@>
2021-05-04 19:11:05 +0300

    J

--- users/views/load.py
+++ users/views/load.py
@@ -9,7 +9,7 @@ class UserLoadPhoto(ListView):
 		from gallery.models import PhotoList
 
 		self.list, self.template_name = request.user.get_photo_list(), get_settings_template("users/load/u_photo_load.html", request.user, request.META['HTTP_USER_AGENT'])
-		self.get_lists = PhotoList.get_user_lists(self.user.pk)
+		self.get_lists = PhotoList.get_user_lists(request.user.pk)
 		return super(UserLoadPhoto,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
