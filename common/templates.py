2dc4d0f2ed5d4cdf2bc25b48b21bba1dbbb969a7
Serg <ochkarik1983@mail.ru@>
2021-05-07 16:03:56 +0300

    J

--- common/templates.py
+++ common/templates.py
@@ -30,23 +30,23 @@ def get_template_community_list(list, folder, template, request_user, user_agent
                 template_name = "generic/c_template/admin_community_closed.html"
             else:
                 template_name = "generic/c_template/community_closed.html"
-    elif playlist.type[0] == "_":
-        if playlist.is_deleted():
-            if request_user.is_administrator_of_community(community.pk) and playlist.community.pk == community.pk:
+    elif list.type[0] == "_":
+        if list.is_deleted():
+            if request_user.is_administrator_of_community(community.pk) and list.community.pk == community.pk:
                 template_name = folder + "admin_deleted_" + template
             else:
                 template_name = "generic/c_template/list_deleted.html"
-        elif playlist.is_closed():
+        elif list.is_closed():
             if staff:
                 template_name = folder + "staff_closed_" + template
-            if request_user.is_administrator_of_community(community.pk) and playlist.community.pk == community.pk:
+            if request_user.is_administrator_of_community(community.pk) and list.community.pk == community.pk:
                 template_name = "generic/c_template/admin_list_closed.html"
             else:
                 template_name = "generic/c_template/list_closed.html"
-        elif playlist.is_suspended():
+        elif list.is_suspended():
             if staff:
                 template_name = folder + "staff_suspended_" + template
-            if request_user.is_administrator_of_community(community.pk) and playlist.community.pk == community.pk:
+            if request_user.is_administrator_of_community(community.pk) and list.community.pk == community.pk:
                 template_name = "generic/c_template/admin_list_suspended.html"
             else:
                 template_name = "generic/c_template/list_suspended.html"
@@ -55,7 +55,7 @@ def get_template_community_list(list, folder, template, request_user, user_agent
             template_name = folder + "admin_" + template
         elif staff:
             template_name = folder + "staff_member_" + template
-        elif playlist.is_private():
+        elif list.is_private():
             template_name = folder + "private_" + template
         else:
             template_name = folder + "member_" + template
@@ -71,7 +71,7 @@ def get_template_community_list(list, folder, template, request_user, user_agent
     elif request_user.is_banned_from_community(community.pk):
         template_name = "generic/c_template/block_community.html"
     elif community.is_public():
-        if playlist.is_private():
+        if list.is_private():
             template_name = folder + "private_" + template
         elif request_user.is_child() and not community.is_verified():
             template_name = "generic/c_template/no_child_safety.html"
@@ -88,16 +88,18 @@ def get_anon_template_community(list, template, request_user, user_agent):
             template_name = "generic/c_template/anon_community_deleted.html"
         elif community.is_closed():
             template_name = "generic/c_template/anon_community_closed.html"
-    elif playlist.type[0] == "_":
-        if playlist.is_deleted():
+    elif list.type[0] == "_":
+        if list.is_deleted():
             template_name = "generic/c_template/anon_list_deleted.html"
-        elif playlist.is_closed():
+        elif list.is_closed():
             template_name = "generic/c_template/anon_list_closed.html"
-        elif playlist.is_suspended():
+        elif list.is_suspended():
             template_name = "generic/c_template/anon_list_suspended.html"
     elif community.is_public():
         if not community.is_verified():
             template_name = "generic/c_template/anon_no_child_safety.html"
+        elif list.is_private():
+            template_name = "generic/c_template/anon_private_list.html"
         else:
             template_name = template
     elif community.is_close():
@@ -107,8 +109,8 @@ def get_anon_template_community(list, template, request_user, user_agent):
     return get_folder(user_agent) + template_name
 
 
-def get_template_user(playlist, folder, template, request_user, user_agent, staff):
-    user = playlist.creator
+def get_template_user(list, folder, template, request_user, user_agent, staff):
+    user = list.creator
     update_activity(request_user, user_agent)
     if request_user.type[0] == "_":
         if request_user.is_no_phone_verified():
@@ -119,6 +121,13 @@ def get_template_user(playlist, folder, template, request_user, user_agent, staf
             template_name = "generic/u_template/you_closed.html"
         elif request_user.is_suspended():
             template_name = "generic/u_template/you_suspended.html"
+    elif list.type[0] == "_":
+        if list.is_deleted():
+            template_name = "generic/u_template/list_deleted.html"
+        elif list.is_closed():
+            template_name = "generic/u_template/list_closed.html"
+        elif list.is_suspended():
+            template_name = "generic/u_template/list_suspended.html"
     elif user.pk == request_user.pk:
             template_name = folder + "my_" + template
     elif request_user.pk != user.pk:
@@ -129,10 +138,12 @@ def get_template_user(playlist, folder, template, request_user, user_agent, staf
                 template_name = "generic/u_template/user_deleted.html"
             elif user.is_blocked():
                 template_name = "generic/u_template/user_global_block.html"
-            elif staff or request_user.is_superuser:
-                template_name = folder + "staff_" + template
-            elif request_user.is_blocked_with_user_with_id(user_id=user.pk):
-                template_name = "generic/u_template/block_user.html"
+        elif staff or request_user.is_superuser:
+            template_name = folder + "staff_" + template
+        elif request_user.is_blocked_with_user_with_id(user_id=user.pk):
+            template_name = "generic/u_template/block_user.html"
+        elif list.is_private():
+            template_name = "generic/u_template/private_list.html"
         elif user.is_closed_profile():
             if request_user.is_followers_user_with_id(user_id=user.pk) or request_user.is_connected_with_user_with_id(user_id=user.pk):
                 template_name = folder + template
@@ -152,11 +163,20 @@ def get_template_anon_user(list, template, request_user, user_agent):
         elif user.is_deleted():
             template_name = "generic/u_template/anon_user_deleted.html"
         elif user.is_closed():
-            template_name = "generic/u_template/anon_closed.html"
+            template_name = "generic/u_template/anon_user_closed.html"
+    elif list.type[0] == "_":
+        if list.is_deleted():
+            template_name = "generic/u_template/anon_list_deleted.html"
+        elif list.is_closed():
+            template_name = "generic/u_template/anon_list_closed.html"
+        elif list.is_suspended():
+            template_name = "generic/u_template/anon_list_suspended.html"
     elif user.is_closed_profile():
         template_name = "generic/u_template/anon_close_user.html"
     elif not user.is_child_safety():
         template_name = "generic/u_template/anon_no_child_safety.html"
+    elif list.is_private():
+        template_name = "generic/u_template/anon_private_list.html"
     else:
         template_name = template
     return get_folder(user_agent) + template_name
