f374cda5e792329f5aee010127ca6a22d978d2ce
Serg <ochkarik1983@mail.ru@>
2021-05-09 12:49:37 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -448,7 +448,7 @@ class Post(models.Model):
                 from common.notify.progs import community_send_notify, community_send_wall
                 from notify.models import Notify, Wall
 
-                Wall.objects.create(creator_id=creator.pk, community_id=community.pk, recipient_id=user_id, type="POS", object_id=post.pk, verb="ITE")
+                Wall.objects.create(creator_id=creator.pk, community_id=community.pk, type="POS", object_id=post.pk, verb="ITE")
                 community_send_wall(post.pk, creator.pk, community.pk, None, "create_c_post_wall")
                 for user_id in community.get_member_for_notify_ids():
                     Notify.objects.create(creator_id=creator.pk, community_id=community.pk, recipient_id=user_id, type="POS", object_id=post.pk, verb="ITE")
