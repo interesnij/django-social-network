b926515ab25cceaa7eef257b4a7e5ee470e5a3b2
Serg <ochkarik1983@mail.ru@>
2021-05-09 18:41:06 +0300

    J

--- music/view/community_progs.py
+++ music/view/community_progs.py
@@ -104,6 +104,7 @@ class CommunityCreatePlaylistWindow(TemplateView):
     template_name = None
 
     def get(self,request,*args,**kwargs):
+        self.community = Community.objects.get(pk=self.kwargs["pk"])
         self.template_name = get_community_manage_template("music/music_create/c_create_list.html", request.user, self.community, request.META['HTTP_USER_AGENT'])
         return super(CommunityCreatePlaylistWindow,self).get(request,*args,**kwargs)
 
