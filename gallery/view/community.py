9779e03d363e17d45aaaba436dba725dbf06ad07
Serg <ochkarik1983@mail.ru@>
2021-05-09 17:06:28 +0300

    J

--- gallery/view/community.py
+++ gallery/view/community.py
@@ -17,10 +17,10 @@ class CommunityLoadPhotoList(ListView):
 	def get(self,request,*args,**kwargs):
 		self.c, self.list = Community.objects.get(pk=self.kwargs["pk"]), PhotoList.objects.get(uuid=self.kwargs["uuid"])
 		if self.list.community:
-			self.template_name = get_permission_community_photo(self.list, "gallery/community/", "list.html", request.user, request.META['HTTP_USER_AGENT'])
+			self.template_name = get_permission_community_photo(self.list, "communities/photos/main_list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
 		else:
 			from common.template.photo import get_permission_user_photo
-			self.template_name = get_permission_user_photo(self.list, "gallery/user/", "list.html", request.user, request.META['HTTP_USER_AGENT'])
+			self.template_name = get_permission_user_photo(self.list, "users/photos/main_list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
 		if request.user.is_authenticated and request.user.is_staff_of_community(self.c.pk):
 			self.photo_list = self.list.get_staff_items()
 		else:
@@ -45,7 +45,7 @@ class CommunityPhotosList(ListView):
         self.community = Community.objects.get(pk=self.kwargs["pk"])
         self.list = PhotoList.objects.get(community_id=self.community.pk, type=PhotoList.MAIN)
         if request.is_ajax():
-            self.template_name = get_permission_community_photo(self.list, "communities/gallery/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
+            self.template_name = get_permission_community_photo(self.list, "communities/photos/main_list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
         else:
             raise Http404
         if request.user.is_authenticated and request.user.is_staff_of_community(self.community.pk):
@@ -71,7 +71,7 @@ class CommunityAlbumPhotoList(ListView):
         self.community = Community.objects.get(pk=self.kwargs["pk"])
         self.list = PhotoList.objects.get(uuid=self.kwargs["uuid"])
         if request.is_ajax():
-            self.template_name = get_permission_community_photo(self.list, "communities/photo_list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
+            self.template_name = get_permission_community_photo(self.list, "communities/photos/list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
         else:
             raise Http404
         if request.user.is_authenticated and request.user.is_staff_of_community(self.community.pk):
