db9b8e980e93d980120718e00463018e041a795f
Serg <ochkarik1983@mail.ru@>
2021-05-09 18:53:35 +0300

    J

--- static/scripts/music/user_post.js
+++ static/scripts/music/user_post.js
@@ -165,50 +165,11 @@ on('body', 'click', '.u_track_restore', function() {
   link.send();
 });
 
-//on('#ajax', 'click', '.u_add_track_in_list', function() {
-//  add_item_in_list(this, '/music/user_progs/add_track_in_list/', ".u_add_track_in_list", "u_remove_track_from_list")
-//})
-//on('#ajax', 'click', '.u_remove_track_from_list', function() {
-//  remove_item_from_list(this, '/music/user_progs/remove_track_from_list/', ".u_remove_track_from_list", "u_add_track_in_list")
-//})
-
-on('body', 'click', '.u_add_track_in_list', function() {
-  _this = this;
-  parent = _this.parentElement;
-  uuid = parent.getAttribute("data-uuid");
-  pk = parent.parentElement.parentElement.parentElement.getAttribute("data-pk");
-  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-  link.open( 'GET', '/music/user_progs/add_track_in_list/' + pk + "/" + uuid + "/", true );
-  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  link.onreadystatechange = function () {
-  if ( link.readyState == 4 && link.status == 200 ) {
-    list = parent.querySelector(".u_add_track_in_list");
-    list.style.paddingLeft = "14px";
-    list.classList.add("u_remove_track_from_list");
-    list.classList.remove("u_add_track_in_list");
-    span = document.createElement("span");
-    span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg> ';
-    list.prepend(span)
-  }};
-  link.send( null );
+on('#ajax', 'click', '.u_add_track_in_list', function() {
+  add_item_in_list(this, '/music/user_progs/add_track_in_list/', "u_add_track_in_list", "u_remove_track_from_list")
 })
-on('body', 'click', '.u_remove_track_from_list', function() {
-  _this = this;
-  parent = _this.parentElement;
-  uuid = parent.getAttribute("data-uuid");
-  pk = parent.parentElement.parentElement.parentElement.getAttribute("data-pk");
-  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-  link.open( 'GET', '/music/user_progs/remove_track_from_list/' + pk + "/" + uuid + "/", true );
-  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  link.onreadystatechange = function () {
-  if ( link.readyState == 4 && link.status == 200 ) {
-    list = parent.querySelector(".u_remove_doc_in_list");
-    list.style.paddingLeft = "30px";
-    list.classList.add("u_add_track_in_list");
-    list.classList.remove("u_remove_track_from_list");
-    list.querySelector("svg").remove();
-  }};
-  link.send( null );
+on('#ajax', 'click', '.u_remove_track_from_list', function() {
+  remove_item_from_list(this, '/music/user_progs/remove_track_from_list/', "u_remove_track_from_list", "u_add_track_in_list")
 })
 
 on('#ajax', 'click', '#u_soundcloud_set_create_btn', function() {
