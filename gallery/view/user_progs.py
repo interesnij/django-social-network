149c4882167a7e3446634d64cefd521aedafe296
Serg <ochkarik1983@mail.ru@>
2021-05-29 16:30:04 +0300

    J

--- gallery/view/user_progs.py
+++ gallery/view/user_progs.py
@@ -76,7 +76,7 @@ class UserCreatePhotosInPhotoList(View):
             for p in request.FILES.getlist('file'):
                 photo = Photo.create_photo(creator=user, image=p, list=list, type="PHLIS", community=None)
                 photos += [photo,]
-            return render_for_platform(request, 'gallery/u_photo/new_list_photos.html',{'object_list': photos, 'list': _list, 'user': request.user})
+            return render_for_platform(request, 'gallery/u_photo/new_album_photos.html',{'object_list': photos, 'list': _list, 'user': request.user})
         else:
             raise Http404
 
@@ -88,7 +88,7 @@ class PhotoAttachUserCreate(View):
             for p in request.FILES.getlist('file'):
                 photo = Photo.create_photo(creator=request.user, image=p, list=list, type="PHWAL", community=None)
                 photos += [photo,]
-            return render_for_platform(request, 'gallery/u_photo/new_list_photos.html',{'object_list': photos, 'list': list})
+            return render_for_platform(request, 'gallery/u_photo/new_album_photos.html',{'object_list': photos, 'list': list})
         else:
             raise Http404
 
