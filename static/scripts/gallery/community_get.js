97b1a4225a27ce545866b478492c305069ef2803
Serg <ochkarik1983@mail.ru@>
2021-06-01 18:25:04 +0300

    J

--- static/scripts/gallery/community_get.js
+++ static/scripts/gallery/community_get.js
@@ -1,10 +1,10 @@
 on('body', 'click', '.c_load_profile_photo_list', function() {
   pk = document.body.querySelector(".pk_saver").getAttribute('data-pk')
-  profile_list_block_load(this, ".load_block", "/communities/" + pk + "/photo_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_profile_photo_list");
+  profile_list_block_load(this, ".load_block", "/communities/" + pk + "/photo_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "c_load_profile_photo_list");
 });
 
 on('body', 'click', '.c_load_attach_photo_list', function() {
-  profile_list_block_load(this, ".load_block", "/users/load/u_photo_list_load/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_attach_photo_list");
+  profile_list_block_load(this, ".load_block", "/users/load/u_photo_list_load/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "c_load_attach_photo_list");
 });
 
 on('#ajax', 'click', '.c_MAI_photo', function() {
