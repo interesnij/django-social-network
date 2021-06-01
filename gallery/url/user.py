8a1fd680d6b32c0322079535d9c0727185f3c57c
Serg <ochkarik1983@mail.ru@>
2021-06-01 19:50:53 +0300

    J

--- gallery/url/user.py
+++ gallery/url/user.py
@@ -7,7 +7,7 @@ urlpatterns=[
 	url(r'^list_photos/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserPhotosAlbumList.as_view(), name="user_photos"),
 
 	url(r'^photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserPhoto.as_view(), name="user_photo"),
-    url(r'^list_photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserPhotoAlbumList.as_view(), name="user_list_photo"),
+    url(r'^list_photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserPhotoAlbumList.as_view(), name="user_list_photo"), 
     url(r'^wall_photo/(?P<pk>\d+)/(?P<photo_pk>\d+)/$', UserWallPhoto.as_view(), name="u_wall_photo"),
     url(r'^avatar_photo/(?P<photo_pk>\d+)/$', UserDetailAvatar.as_view(), name="user_avatar"),
 	url(r'^post_photo/(?P<uuid>[0-9a-f-]+)/(?P<photo_pk>\d+)/$', UserPostPhoto.as_view(), name="user_post_photo"),
