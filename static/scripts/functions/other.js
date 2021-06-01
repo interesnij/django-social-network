0759a036bbf2ac766d7264dd46376312df48c235
Serg <ochkarik1983@mail.ru@>
2021-06-01 17:23:54 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -15,7 +15,6 @@ function profile_list_block_load(_this, block, link, actions_class) {
         elem_ = document.createElement('span');
         elem_.innerHTML = request.responseText;
        document.body.querySelector(block).innerHTML = elem_.querySelector(block).innerHTML;
-       init_music(document.body.querySelector(block));
        class_to_add = _this.parentElement.parentElement.parentElement.parentElement.parentElement.querySelectorAll(".list_toggle")
        for (var i = 0; i < class_to_add.length; i++) {
          class_to_add[i].classList.add(actions_class, "pointer");
