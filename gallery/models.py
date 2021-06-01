e76b9e77f4a990c7ed00d02dcbf90840220398dd
Serg <ochkarik1983@mail.ru@>
2021-06-01 14:47:06 +0300

    J

--- gallery/models.py
+++ gallery/models.py
@@ -133,7 +133,7 @@ class PhotoList(models.Model):
     @classmethod
     def get_user_lists(cls, user_pk):
         query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(type="LIS"), Q.AND)
+        query.add(~Q(type__contains="_"), Q.AND)
         return cls.objects.filter(query)
     @classmethod
     def get_user_lists_count(cls, user_pk):
