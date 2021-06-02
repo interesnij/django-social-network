d044cbdaa3865bf597687594f4f99ac5ebb78e47
Serg <ochkarik1983@mail.ru@>
2021-06-02 11:41:08 +0300

    J

--- goods/models.py
+++ goods/models.py
@@ -87,7 +87,7 @@ class GoodList(models.Model):
 	def is_open(self):
 		return self.type[0] != "_"
 	def is_have_edit(self):
-        return self.is_list() or self.is_private()
+		return self.is_list() or self.is_private()
 
 	def get_items(self):
 		return self.good_list.filter(type="PUB")
