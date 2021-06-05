5f9ef4cfe31d559de463f69d713dfce1400cc43b
Serg <ochkarik1983@mail.ru@>
2021-06-05 12:41:00 +0300

    J

--- gallery/view/repost.py
+++ gallery/view/repost.py
@@ -79,7 +79,7 @@ class CUCMPhotoListWindow(TemplateView):
         from common.template.user import get_detect_platform_template
 
         self.list, self.c, self.template_name = PhotoList.objects.get(uuid=self.kwargs["uuid"]), Community.objects.get(pk=self.kwargs["pk"]), get_detect_platform_template("gallery/repost/c_ucm_photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
-        check_can_get_lists(request.user, self.community)
+        check_can_get_lists(request.user, self.c)
         return super(CUCMPhotoPhotoListWindow,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
