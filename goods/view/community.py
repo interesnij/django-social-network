a3414da7e359ffb8f29a943ca1e2321d2b66e6e5
Serg <ochkarik1983@mail.ru@>
2021-06-12 14:04:35 +0300

    J

--- goods/view/community.py
+++ goods/view/community.py
@@ -31,29 +31,29 @@ class CommunityLoadGoodList(ListView):
 
 
 class CommunityGood(TemplateView):
-    template_name = None
+	template_name = None
 
-    def get(self,request,*args,**kwargs):
+	def get(self,request,*args,**kwargs):
 		from common.templates import get_template_community_item, get_template_anon_community_item
 
-        self.list, self.good = GoodList.objects.get(uuid=self.kwargs["uuid"]), Good.objects.get(pk=self.kwargs["pk"])
-        self.goods = self.list.get_goods()
-        check_can_get_lists(self.request.user, self.list.community)
+		self.list, self.good = GoodList.objects.get(uuid=self.kwargs["uuid"]), Good.objects.get(pk=self.kwargs["pk"])
+		self.goods = self.list.get_goods()
+		check_can_get_lists(self.request.user, self.list.community)
 
 		if request.user.is_authenticated:
-            self.template_name = get_template_community_item(self.post, "goods/c_good/", "good.html", request.user, request.META['HTTP_USER_AGENT'])
-        else:
-            self.template_name = get_template_anon_community_item(self.post, "goods/c_good/anon_good.html", request.user, request.META['HTTP_USER_AGENT'])
-        return super(CommunityGood,self).get(request,*args,**kwargs)
+			self.template_name = get_template_community_item(self.post, "goods/c_good/", "good.html", request.user, request.META['HTTP_USER_AGENT'])
+		else:
+			self.template_name = get_template_anon_community_item(self.post, "goods/c_good/anon_good.html", request.user, request.META['HTTP_USER_AGENT'])
+		return super(CommunityGood,self).get(request,*args,**kwargs)
 
-    def get_context_data(self,**kwargs):
-        context = super(CommunityGood,self).get_context_data(**kwargs)
-        context["object"] = self.good
-        context["list"] = self.list
-        context["community"] = self.list.community
-        context["next"] = self.goods.filter(pk__gt=self.good.pk).order_by('pk').first()
-        context["prev"] = self.goods.filter(pk__lt=self.good.pk).order_by('-pk').first()
-        return context
+	def get_context_data(self,**kwargs):
+		context = super(CommunityGood,self).get_context_data(**kwargs)
+		context["object"] = self.good
+		context["list"] = self.list
+		context["community"] = self.list.community
+		context["next"] = self.goods.filter(pk__gt=self.good.pk).order_by('pk').first()
+		context["prev"] = self.goods.filter(pk__lt=self.good.pk).order_by('-pk').first()
+		return context
 
 
 class GoodCommunityCommentList(ListView):
@@ -81,19 +81,19 @@ class GoodCommunityCommentList(ListView):
 
 
 class GoodCommunityDetail(TemplateView):
-    template_name = None
+	template_name = None
 
-    def get(self,request,*args,**kwargs):
+	def get(self,request,*args,**kwargs):
 		from common.templates import get_template_community_item, get_template_anon_community_item
 
-        self.good, self.c = Good.objects.get(pk=self.kwargs["good_pk"]), Community.objects.get(pk=self.kwargs["pk"])
+		self.good, self.c = Good.objects.get(pk=self.kwargs["good_pk"]), Community.objects.get(pk=self.kwargs["pk"])
 		if request.user.is_authenticated:
-            self.template_name = get_template_community_item(self.post, "goods/c_good/", "detail.html", request.user, request.META['HTTP_USER_AGENT'])
-        else:
-            self.template_name = get_template_anon_community_item(self.post, "goods/c_good/anon_detail.html", request.user, request.META['HTTP_USER_AGENT'])
-        return super(GoodCommunityDetail,self).get(request,*args,**kwargs)
+			self.template_name = get_template_community_item(self.post, "goods/c_good/", "detail.html", request.user, request.META['HTTP_USER_AGENT'])
+		else:
+			self.template_name = get_template_anon_community_item(self.post, "goods/c_good/anon_detail.html", request.user, request.META['HTTP_USER_AGENT'])
+		return super(GoodCommunityDetail,self).get(request,*args,**kwargs)
 
-    def get_context_data(self,**kwargs):
-        context = super(GoodCommunityDetail,self).get_context_data(**kwargs)
-        context["object"], context["community"] = self.good, self.c
-        return context
+	def get_context_data(self,**kwargs):
+		context = super(GoodCommunityDetail,self).get_context_data(**kwargs)
+		context["object"], context["community"] = self.good, self.c
+		return context
--- goods/view/user.py
+++ goods/view/user.py
@@ -28,31 +28,31 @@ class UserLoadGoodList(ListView):
 
 
 class UserGood(TemplateView):
-    template_name = None
+	template_name = None
 
-    def get(self,request,*args,**kwargs):
+	def get(self,request,*args,**kwargs):
 		from common.templates import get_template_user_item, get_template_anon_user_item
 
-        self.good, self.list = Good.objects.get(pk=self.kwargs["pk"]), GoodList.objects.get(uuid=self.kwargs["uuid"])
-        self.goods, self.user, user_agent = self.list.get_goods(), self.list.creator, request.META['HTTP_USER_AGENT']
-        try:
-            GoodNumbers.objects.get(user=request.user.pk, good=self.good.pk)
-        except:
-            GoodNumbers.objects.create(user=request.user.pk, good=self.good.pk, device=request.user.get_device())
+		self.good, self.list = Good.objects.get(pk=self.kwargs["pk"]), GoodList.objects.get(uuid=self.kwargs["uuid"])
+		self.goods, self.user, user_agent = self.list.get_goods(), self.list.creator, request.META['HTTP_USER_AGENT']
+		try:
+			GoodNumbers.objects.get(user=request.user.pk, good=self.good.pk)
+		except:
+			GoodNumbers.objects.create(user=request.user.pk, good=self.good.pk, device=request.user.get_device())
 		if request.user.is_authenticated:
-            self.template_name = get_template_user_item(self.post, "goods/u_good/", "good.html", request.user, request.META['HTTP_USER_AGENT'])
-        else:
-            self.template_name = get_template_anon_user_item(self.post, "goods/u_good/anon_good.html", request.user, request.META['HTTP_USER_AGENT'])
-        return super(UserGood,self).get(request,*args,**kwargs)
+			self.template_name = get_template_user_item(self.post, "goods/u_good/", "good.html", request.user, request.META['HTTP_USER_AGENT'])
+		else:
+			self.template_name = get_template_anon_user_item(self.post, "goods/u_good/anon_good.html", request.user, request.META['HTTP_USER_AGENT'])
+		return super(UserGood,self).get(request,*args,**kwargs)
 
-    def get_context_data(self,**kwargs):
-        context = super(UserGood,self).get_context_data(**kwargs)
-        context["object"] = self.good
-        context["list"] = self.list
-        context["user"] = self.user
-        context["next"] = self.goods.filter(pk__gt=self.good.pk).order_by('pk').first()
-        context["prev"] = self.goods.filter(pk__lt=self.good.pk).order_by('-pk').first()
-        return context
+	def get_context_data(self,**kwargs):
+		context = super(UserGood,self).get_context_data(**kwargs)
+		context["object"] = self.good
+		context["list"] = self.list
+		context["user"] = self.user
+		context["next"] = self.goods.filter(pk__gt=self.good.pk).order_by('pk').first()
+		context["prev"] = self.goods.filter(pk__lt=self.good.pk).order_by('-pk').first()
+		return context
 
 
 class GoodUserCommentList(ListView):
@@ -79,19 +79,19 @@ class GoodUserCommentList(ListView):
 
 
 class GoodUserDetail(TemplateView):
-    template_name = None
+	template_name = None
 
-    def get(self,request,*args,**kwargs):
+	def get(self,request,*args,**kwargs):
 		from common.templates import get_template_user_item, get_template_anon_user_item
-		
-        self.good, self.user = Good.objects.get(pk=self.kwargs["good_pk"]), User.objects.get(pk=self.kwargs["pk"])
-        if request.user.is_authenticated:
-            self.template_name = get_template_user_item(self.post, "goods/u_good/", "detail.html", request.user, request.META['HTTP_USER_AGENT'])
-        else:
-            self.template_name = get_template_anon_user_item(self.post, "goods/u_good/anon_detail.html", request.user, request.META['HTTP_USER_AGENT'])
-        return super(GoodUserDetail,self).get(request,*args,**kwargs)
 
-    def get_context_data(self,**kwargs):
-        context = super(GoodUserDetail,self).get_context_data(**kwargs)
-        context["object"], context["user"] = self.good, self.user
-        return context
+		self.good, self.user = Good.objects.get(pk=self.kwargs["good_pk"]), User.objects.get(pk=self.kwargs["pk"])
+		if request.user.is_authenticated:
+			self.template_name = get_template_user_item(self.post, "goods/u_good/", "detail.html", request.user, request.META['HTTP_USER_AGENT'])
+		else:
+			self.template_name = get_template_anon_user_item(self.post, "goods/u_good/anon_detail.html", request.user, request.META['HTTP_USER_AGENT'])
+		return super(GoodUserDetail,self).get(request,*args,**kwargs)
+
+	def get_context_data(self,**kwargs):
+		context = super(GoodUserDetail,self).get_context_data(**kwargs)
+		context["object"], context["user"] = self.good, self.user
+		return context
