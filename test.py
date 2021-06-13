3b630daafb6f64a0d07f3c96c2bafdb8f526dbb1
Serg <ochkarik1983@mail.ru@>
2021-06-13 13:58:35 +0300

    J

--- test.py
+++ test.py
@@ -21,24 +21,4 @@ from gallery.models import PhotoList
 from survey.models import SurveyList
 from goods.models import GoodList
 
-for list in SoundList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in VideoList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in PostList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in DocList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in PhotoList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in SurveyList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
-for list in GoodList.objects.all():
-    list.count=list.count_items()
-    list.save(update_fields=["count"])
+PostList.objects.filter(name="Основной список").update(order=0)
