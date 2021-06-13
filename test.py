f09a724f659fdd06d378902e695a2324d9d0b754
Serg <ochkarik1983@mail.ru@>
2021-06-13 14:45:21 +0300

    J

--- test.py
+++ test.py
@@ -15,10 +15,18 @@ django.setup()
 
 from music.models import SoundList
 from video.models import VideoList
-from posts.models import PostList
+from posts.models import PostList, Post
 from docs.models import DocList
 from gallery.models import PhotoList
 from survey.models import SurveyList
 from goods.models import GoodList
 
-PostList.objects.filter(name="Основной список").update(order=0)
+
+post_lists = PostList.objects.all()
+for list in post_lists:
+    order = 0
+    posts = list.get_staff_items().order_by("created")
+    for post in posts:
+        order += 1
+        post.order = order
+        post.save(update_fields=["order"])
