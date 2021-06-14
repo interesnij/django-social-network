afee69a6e30486a7ec1c9e361dca414cf749742d
Serg <ochkarik1983@mail.ru@>
2021-06-14 17:33:03 +0300

    J

--- video/forms.py
+++ video/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class VideoListForm(forms.ModelForm):
 	class Meta:
 		model = VideoList
-		fields = ['name', 'description', 'order']
+		fields = ['name', 'description']
 
 
 class VideoForm(forms.ModelForm):
