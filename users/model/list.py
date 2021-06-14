fa1716e84d08b4aa80d5b75064a73920fa9ebf06
Serg <ochkarik1983@mail.ru@>
2021-06-14 14:58:57 +0300

    J

--- users/model/list.py
+++ users/model/list.py
@@ -50,3 +50,95 @@ class UserPopulateCommunity(models.Model):
         verbose_name = 'Популярность сообществ'
         verbose_name_plural = 'Популярность сообществ'
         ordering = ['-count']
+
+
+class PhotoListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения фотоальбома'
+        verbose_name_plural = 'Порядки отображения фотоальбомов'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class PostListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения списка записей'
+        verbose_name_plural = 'Порядки отображения списков записей'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class PlayListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения плейлиста'
+        verbose_name_plural = 'Порядки отображения плейлистов'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class GoodListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения списка товаров'
+        verbose_name_plural = 'Порядки отображения списков товаров'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class VideoListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения видеольбома'
+        verbose_name_plural = 'Порядки отображения видеоальбомов'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class SurveyListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения списка опросов'
+        verbose_name_plural = 'Порядки отображения списков опросов'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
+
+class DocListPosition(models.Model):
+    user = models.PositiveIntegerField(default=0, verbose_name="Пользователь")
+    list = models.PositiveIntegerField(default=0, verbose_name="Фотоальбом")
+    position = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок отображения")
+
+    class Meta:
+        verbose_name = 'Порядок отображения списка документов'
+        verbose_name_plural = 'Порядки отображения списков документов'
+        ordering = ['-position']
+
+    def __str__(self):
+        return '{} - {} :{}'.format(self.user, self.list, self.position)
