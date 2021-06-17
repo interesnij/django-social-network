ebbd73a96b1181b4b498acba3d2c2c4caa92e211
Serg <ochkarik1983@mail.ru@>
2021-06-17 14:25:19 +0300

    J

--- common/templates.py
+++ common/templates.py
@@ -235,6 +235,35 @@ def get_template_user(list, folder, template, request_user, user_agent, staff):
             template_name = folder + template
     return get_folder(user_agent) + template_name
 
+def get_owner_template_user(list, folder, template, owner_user, request_user, user_agent, staff):
+    user = list.creator
+    update_activity(request_user, user_agent)
+    if request_user.type[0] == "_":
+        template_name = get_fine_request_user(request_user)
+    elif list.type[0] == "_":
+        template_name = get_fine_user_item(request_user, list, folder, template)
+    elif owner_user.pk == request_user.pk:
+            template_name = folder + "my_" + template
+    elif request_user.pk != user.pk:
+        if user.type[0] == "_":
+            template_name = get_fine_user(user)
+        elif staff:
+            template_name = folder + "staff_" + template
+        elif request_user.is_blocked_with_user_with_id(user_id=user.pk):
+            template_name = "generic/u_template/block_user.html"
+        elif list.is_private():
+            template_name = "generic/u_template/list_private.html"
+        elif user.is_closed_profile():
+            if request_user.is_followers_user_with_id(user_id=user.pk) or request_user.is_connected_with_user_with_id(user_id=user.pk):
+                template_name = folder + template
+            else:
+                template_name = "generic/u_template/close_user.html"
+        elif request_user.is_child() and not user.is_child_safety():
+            template_name = "generic/u_template/no_child_safety.html"
+        else:
+            template_name = folder + template
+    return get_folder(user_agent) + template_name
+
 def get_template_anon_user(list, template, request_user, user_agent):
     user = list.creator
     if user.type[0] == "_":
--- users/views/lists.py
+++ users/views/lists.py
@@ -212,7 +212,7 @@ class UserPostsListView(ListView):
 
 	def get(self,request,*args,**kwargs):
 		from posts.models import PostList
-		from common.templates import get_template_user, get_template_anon_user
+		from common.templates import get_owner_template_user, get_template_anon_user
 
 		self.user, user_pk, self.post_list = User.objects.get(pk=self.kwargs["pk"]), int(self.kwargs["pk"]), PostList.objects.get(pk=self.kwargs["list_pk"])
 		if user_pk == request.user.pk:
@@ -222,7 +222,7 @@ class UserPostsListView(ListView):
 			self.list = self.post_list.get_items()
 			self.post_lists = PostList.get_user_lists(user_pk)
 		if request.user.is_authenticated:
-			self.template_name = get_template_user(self.post_list, "users/lenta/", "list.html", request.user, request.META['HTTP_USER_AGENT'], request.user.is_post_manager())
+			self.template_name = get_owner_template_user(self.post_list, "users/lenta/", "list.html", self.user, request.user, request.META['HTTP_USER_AGENT'], request.user.is_post_manager())
 		else:
 			self.template_name = get_template_anon_user(self.post_list, "users/lenta/anon_list.html", request.user, request.META['HTTP_USER_AGENT'])
 		return super(UserPostsListView,self).get(request,*args,**kwargs)
