30f89f9b8b7ed1292d7d5ecdcee55f3ed2c8ffb0
Serg <ochkarik1983@mail.ru@>
2021-06-19 17:00:08 +0300

    J

--- static/scripts/posts/user_get.js
+++ static/scripts/posts/user_get.js
@@ -116,6 +116,7 @@ on('#ajax', 'click', '.u_post_edit', function() {
     return
   } else {
     block.querySelector(".fullscreen") ? block.querySelector(".fullscreen").style.display = "none" : null;
+    block.querySelector(".attach_container") ? block.querySelector(".attach_container").style.display = "none" : null;
     block.querySelector(".card-footer").style.display = "none";
     div = document.createElement("div");
     block.append(div);
