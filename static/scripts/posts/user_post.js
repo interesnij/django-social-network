9389b21f772758ad93c82936b3bf19cfd6b2abff
Serg <ochkarik1983@mail.ru@>
2021-06-19 18:21:18 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -77,7 +77,7 @@ on('#ajax', 'click', '#u_edit_post_btn', function() {
     new_post = document.createElement("span");
     new_post.innerHTML = elem;
     card_header = block.querySelector(".card-header");
-    form_post.parentElement.revove();
+    form_post.parentElement.remove();
     if (new_post.querySelector(".fullscreen")) {
       card_header.append(new_post.querySelector(".fullscreen"))
     }
