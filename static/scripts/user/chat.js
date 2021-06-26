ed96a96e4a609cf732f9d15447163627767ffe86
Serg <ochkarik1983@mail.ru@>
2021-06-26 19:37:07 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -8,8 +8,7 @@ on('#ajax', 'click', '.message', function() {
                                              btn_display.querySelector(".settings_btn").style.display = "block")
 });
 
-on('#ajax', 'mouseover', '.message_dropdown', function() {this.querySelector(".message_dropdown_menu").style.display = "block"})
-on('#ajax', 'mouseout', '.message_dropdown', function() {this.querySelector(".message_dropdown_menu").style.display = "none"})
+on('#ajax', 'click', '.message_dropdown', function() {this.classList.toggle("show")})
 
 on('#ajax', 'input', '.message_text', function() {
   btn_block = this.nextElementSibling.nextElementSibling;
