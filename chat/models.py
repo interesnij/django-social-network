2d660ef4c2714bfb93b74c52fc116d303d1bded5
Serg <ochkarik1983@mail.ru@>
2021-06-27 13:33:30 +0300

    H

--- chat/models.py
+++ chat/models.py
@@ -172,7 +172,7 @@ class Chat(models.Model):
              chat_name = self.name
         else:
             chat_name = "Групповой чат"
-        media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, + '</h5><p class="mb-0 target_display"><span class="type_display">', self.get_type_display(), '</span>', buttons, '</p></div>'])
+        media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, '</h5><p class="mb-0 target_display"><span class="type_display">', self.get_type_display(), '</span>', buttons, '</p></div>'])
         return ''.join([avatars, media_body])
 
     def is_not_empty(self):
