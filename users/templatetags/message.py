88cfed85ef3e7ef70d4fc87fae4cab4b5f40a47a
Serg <ochkarik1983@mail.ru@>
2021-06-27 13:23:40 +0300

    H

--- users/templatetags/message.py
+++ users/templatetags/message.py
@@ -11,8 +11,11 @@ def get_preview_message(chat, user_id):
     return chat.get_preview_message(user_id)
 
 @register.filter
-def get_chat_header(chat, user_id):
-    return chat.get_header_chat(user_id)
+def get_header_private_chat(chat, user_id):
+    return chat.get_header_private_chat(user_id)
+@register.filter
+def get_header_group_chat(chat, user_id):
+    return chat.get_header_group_chat(user_id)
 
 @register.filter
 def get_user_attach(message, request_user):
