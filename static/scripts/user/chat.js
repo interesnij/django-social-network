b66d3fe134db1e89c58a6f7fefbe2761944d8698
Serg <ochkarik1983@mail.ru@>
2021-06-28 11:59:01 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -158,7 +158,7 @@ on('#ajax', 'click', '.toggle_message', function(e) {
         is_toggle = true
       }
     }
-    is_toggle ? null : (_console.style.display = "none", _console.previousElementSibling.style.display = "block";_console.parentElement.parentElement.querySelector("h5").style.display = "none")
+    is_toggle ? null : (_console.style.display = "none", _console.previousElementSibling.style.display = "block",_console.parentElement.parentElement.querySelector("h5").style.display = "none")
   } else {
     message.classList.add("custom_color");
     _console.style.display = "block";
