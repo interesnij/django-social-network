705fda49220e5c988e9eb2f34e440a8b64e18517
Serg <ochkarik1983@mail.ru@>
2021-06-29 14:26:41 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -1,13 +1,9 @@
 function get_toggle_messages() {
   list = document.body.querySelectorAll(".custom_color");
   query = [];
-  console.log("begin");
-  console.log("list" + list);
   for (var i = 0; i < list.length; i++){
       query.push(list[i])
-      console.log(list[i]);
   };
-  console.log(query);
   return query
 }
 function show_chat_console(is_favourite) {
