3eed7b8d1cc0c05d7314535f50aa77fc6029145a
Serg <ochkarik1983@mail.ru@>
2021-06-30 16:18:22 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -91,7 +91,7 @@ class Chat(models.Model):
         return Photo.objects.filter(message__uuid__in=self.get_messages_uuids())
 
     def get_unread_count_message(self, user_id):
-        count = self.chat_message.exclude(creator_id=user_id, type__contains="_").filter(unread=True, recipient_id=user_id).values("pk").count()
+        count = self.chat_message.exclude(type__contains="_").filter(creator_id=user_id, unread=True, recipient_id=user_id).values("pk").count()
         if count:
             return ''.join(['<span style="font-size: 80%;" class="tab_badge badge-success">', str(count), '</span>'])
         else:
