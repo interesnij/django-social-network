999f002635b2cc9d9643d2da43a6b5c005f5d4fc
Serg <ochkarik1983@mail.ru@>
2021-07-02 13:57:05 +0300

    J

--- static/scripts/functions/socket.js
+++ static/scripts/functions/socket.js
@@ -119,7 +119,7 @@ function case_u_message_create(request_user_id, chat_id, message_uuid) {
       chats = document.body.querySelector(".new_unread_chats");
       chats.querySelector(".tab_badge") ? (count = chats.querySelector(".tab_badge").innerHTML.replace(/\s+/g, ''), count = count*1) : count = 0;
       tab_span = document.createElement("span");
-      console.log(count);
+      count += 1;
       chats.classList.add("tab_badge", "badge-success");
       chats.innerHTML = "";tab_span.append(count);chats.append(count);
   }
