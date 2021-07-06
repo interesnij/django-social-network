3b20c4df4efb8c343843e02274bc6960f2867962
Serg <ochkarik1983@mail.ru@>
2021-07-06 12:48:32 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -38,7 +38,7 @@ on('#ajax', 'click', '.smile_dropdown', function() {
   block.classList.toggle("show");
 })
 on('#ajax', 'input', '.message_text', function() {
-  if (document.body.querySelector("#chatlist")) {
+  if (document.body.querySelector(".chatlist")) {
   check_message_form_btn()
   }
 })
