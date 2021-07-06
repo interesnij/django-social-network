23ad3cd8cbb5ded9efec9c4724d320942a466ea3
Serg <ochkarik1983@mail.ru@>
2021-07-06 13:17:17 +0300

    J

--- test.py
+++ test.py
@@ -29,5 +29,6 @@ from chat.models import Message
 message = Message.objects.get(uuid="9edf1918-7661-442a-bdad-12bc59f08e17")
 import re
 
-ids = print(re.findall(r'(pk="(?P<pk>\d+)" st)', message.text))
-return ids
+ids = print(re.findall(r'(pk="(?P<pk>\d+)")', message.text))
+for id in ids:
+    print(id[:5])
