1b7d0613c40bb4937505aac81341d3316d2ac54d
Serg <ochkarik1983@mail.ru@>
2021-07-06 13:41:30 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -403,12 +403,12 @@ class Message(models.Model):
         else:
             if text:
                 import re
-                #images = re.findall(r'(<img.+?>)', text)
-                #ids = re.findall(r'data-pk="(?P<pk>\d+)"', text)
-                #if ids:
-                #    from common.model.other import UserPopulateSmiles
-                #    for id in ids:
-                #        UserPopulateSmiles.get_plus_or_create(creator.pk, id)
+                images = re.findall(r'(<img.+?>)', text)
+                ids = re.findall(r'data-pk="(?P<pk>\d+)"', text)
+                if ids:
+                    from common.model.other import UserPopulateSmiles
+                    for id in ids:
+                        UserPopulateSmiles.get_plus_or_create(creator.pk, id)
             creator_message = Message.objects.create(chat=chat, creator=creator, recipient_id=creator.pk, repost=repost, text=text, attach=Message.get_format_attach(attach), type=Message.PROCESSING)
         get_message_processing(creator_message, 'PUB')
 
