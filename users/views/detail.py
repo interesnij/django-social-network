979bfffa979697000d83b8de2cdb089972944e9a
Serg <ochkarik1983@mail.ru@>
2021-07-21 15:20:18 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -257,11 +257,14 @@ class ProfileUserView(TemplateView):
         from stst.models import UserNumbers
         import re
 
-        self.user, user_agent, MOBILE_AGENT_RE, user_pk, r_user_pk = User.objects.get(pk=self.kwargs["pk"]), request.META['HTTP_USER_AGENT'], re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE), int(self.kwargs["pk"]), request.user.pk
+        user_pk, r_user_pk = int(self.kwargs["pk"]), request.user.pk
+
+        user_agent, MOBILE_AGENT_RE = request.META['HTTP_USER_AGENT'], re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
         if request.user.is_authenticated:
             if request.user.is_no_phone_verified():
                 self.template_name = "main/phone_verification.html"
             elif user_pk == r_user_pk:
+                self.user = User.objects.get(pk=self.kwargs["pk"])
                 if self.user.is_suspended():
                     self.template_name = "generic/u_template/you_suspended.html"
                 elif self.user.is_closed():
@@ -271,6 +274,7 @@ class ProfileUserView(TemplateView):
                 else:
                     self.template_name = "users/account/my_user.html"
             elif r_user_pk != user_pk:
+                self.user = User.objects.select_related('profile').get(pk=self.kwargs["pk"])
                 self.get_buttons_block, self.common_frends = request.user.get_buttons_profile(user_pk), self.user.get_common_friends_of_user(self.request.user)[0:5]
                 if self.user.is_suspended():
                     self.template_name = "generic/u_template/user_suspended.html"
@@ -299,6 +303,7 @@ class ProfileUserView(TemplateView):
                 else:
                     UserNumbers.objects.create(visitor=r_user_pk, target=user_pk, device=UserNumbers.DESCTOP)
         elif request.user.is_anonymous:
+            self.user = User.objects.select_related('profile').get(pk=self.kwargs["pk"])
             if self.user.is_suspended():
                 self.template_name = "generic/u_template/anon_user_suspended.html"
             elif self.user.is_closed():
