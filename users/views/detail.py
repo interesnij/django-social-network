c52fa6455a3d223f0f09b0bbf7f5395dd52e08e0
Serg <ochkarik1983@mail.ru@>
2021-07-22 18:26:18 +0300

    J

--- users/views/detail.py
+++ users/views/detail.py
@@ -264,6 +264,7 @@ class ProfileUserView(TemplateView):
             if request.user.is_no_phone_verified():
                 self.template_name = "main/phone_verification.html"
             elif user_pk == r_user_pk:
+                self.is_community_open, self.is_photo_open, self.is_post_open, self.is_video_open, self.is_music_open, self.is_friend_open, self.is_good_open  = True,True,True,True,True,True,True
                 if self.user.is_suspended():
                     self.template_name = "generic/u_template/you_suspended.html"
                 elif self.user.is_closed():
