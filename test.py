c45c3c5496f738db2768ba6189cc0aedd91656ef
Serg <ochkarik1983@mail.ru@>
2021-08-30 18:50:41 +0300

    J

--- test.py
+++ test.py
@@ -34,8 +34,9 @@ import json, requests
 response = requests.get(url= "https://api.soundcloud.com/playlists?url=https://soundcloud.com/eliana-cogine/sets/musicas-relaxantes&client_id=dce5652caa1b66331903493735ddd64d")
 #data = "   " + str(response.json()) + "   "
 #data.replace("   [", "").replace("   ]", "")
-response.replace("[", "").replace("]", "")
+
 data = response.json()
+data.replace("[", "").replace("]", "")
 
 if data:
     for track in data['tracks']:
