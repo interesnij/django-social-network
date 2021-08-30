13865a48eb008f49f91ed72c47d9ce98747ebaa7
Serg <ochkarik1983@mail.ru@>
2021-08-30 18:45:07 +0300

    J

--- test.py
+++ test.py
@@ -32,8 +32,8 @@ from datetime import datetime, date, time
 import json, requests
 
 response = requests.get(url= "https://api.soundcloud.com/playlists?url=https://soundcloud.com/eliana-cogine/sets/musicas-relaxantes&client_id=dce5652caa1b66331903493735ddd64d")
-data = "  " + response.json() + "  "
-data.replace("[  ", "").replace("]  ", "")
+data = "   " + str(response.json()) + "   "
+data.replace("   [", "").replace("   ]", "")
 
 
 if data:
