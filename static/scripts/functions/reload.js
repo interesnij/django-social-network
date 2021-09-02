9c945e9ed08133b0b84b33a3a6c615cf27e66b53
Serg <ochkarik1983@mail.ru@>
2021-09-02 14:41:46 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -214,7 +214,7 @@ function paginate(block, target) {
                   block.parentElement.insertAdjacentHTML('beforeend', elem.querySelector(".is_post_paginate").innerHTML)
                 } else if (elem.querySelector(".is_paginate")){
                   block.parentElement.insertAdjacentHTML('beforeend', elem.querySelector(".is_paginate").innerHTML)
-                } else if (document.body.querySelector(".is_block_paginate")){
+                } else if (elem.innerHTML){
                   block.parentElement.insertAdjacentHTML('beforeend', block.innerHTML)
                 }
                 block.remove()
