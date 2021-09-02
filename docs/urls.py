5602e460c6875542d553082db6f13508934de0da
Serg <ochkarik1983@mail.ru@>
2021-09-02 20:17:02 +0300

    J

--- docs/urls.py
+++ docs/urls.py
@@ -4,7 +4,7 @@ from docs.views import DocsView, LoadDocList
 
 urlpatterns = [
     url(r'^$', DocsView.as_view()),
-    url(r'^load_list/(?P<uuid>[0-9a-f-]+)/$', LoadGoodList.as_view(), name="load_good_list"),
+    url(r'^load_list/(?P<uuid>[0-9a-f-]+)/$', LoadDocList.as_view(), name="load_doc_list"),
 
     url(r'^user_progs/', include('docs.url.user_progs')),
     url(r'^community_progs/', include('docs.url.community_progs')),
