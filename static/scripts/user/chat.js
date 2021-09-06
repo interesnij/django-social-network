f87d54bf97162540ab33e15e9f763ded19a800e3
Serg <ochkarik1983@mail.ru@>
2021-09-06 17:47:57 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -366,7 +366,7 @@ on('#ajax', 'click', '.toggle_message', function(e) {
   is_toggle = false, is_favourite = false;
   if (message.classList.contains("custom_color")) {
     message.classList.remove("custom_color");
-    message.querySelector(".message_checkbox").setAttribute("checked", "false");
+    message.querySelector(".message_checkbox").addAttr("checked");
     list = message.parentElement.querySelectorAll(".message");
     for (var i = 0; i < list.length; i++){
       if (list[i].classList.contains("custom_color")) {
@@ -379,7 +379,7 @@ on('#ajax', 'click', '.toggle_message', function(e) {
     is_toggle ? null : hide_chat_console(is_favourite)
   } else {
     message.classList.add("custom_color");
-    message.querySelector(".message_checkbox").setAttribute("checked", "true");
+    message.querySelector(".message_checkbox").removeAttr("checked");
     show_chat_console(is_favourite)
   };
   if (get_toggle_messages().length > 1) {
