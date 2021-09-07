66f4c6fd3a746569c65fe2b2141f9089ddc08014
Serg <ochkarik1983@mail.ru@>
2021-09-07 11:14:03 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -484,7 +484,7 @@ on('#ajax', 'click', '.u_message_reply', function() {
   checkbox.checked = false;
   checkbox.style.display = "none";
   message.classList.remove("target_message", "custom_color");
-  if (message.querySelector(".text").firstChild) {
+  if (message.querySelector(".text").innerHTML) {
     parent = message.querySelector(".text").innerHTML
   } else if(message.querySelector(".message_sticker")) {
       parent = "Наклейка"
