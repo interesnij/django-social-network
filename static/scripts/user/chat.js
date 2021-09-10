998e75aff40ee0ac95345052acf62cccdb35e6a3
Serg <ochkarik1983@mail.ru@>
2021-09-10 19:32:08 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -257,25 +257,6 @@ on('#ajax', 'click', '#add_chat_btn', function() {
       ajax_link.send(form_data);
 });
 
-on('#ajax', 'click', '#send_page_message_btn', function() {
-  form = this.parentElement.parentElement.parentElement;
-  this.disabled = true;
-  form.querySelector(".type_hidden").value = form.querySelector(".page_message_text").innerHTML;
-  form_data = new FormData(form);
-
-    var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-      ajax_link.open( 'POST', '/chat/user_progs/send_page_message/' + this.getAttribute("data-pk") + '/', true );
-      ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      ajax_link.onreadystatechange = function () {
-        if ( this.readyState == 4 && this.status == 200 ) {
-            toast_success("Сообщение отправлено");
-            document.querySelector(".item_fullscreen").style.display = "none";
-            document.getElementById("item_loader").innerHTML="";
-        } else {this.disabled = false}
-      }
-      ajax_link.send(form_data);
-});
-
 function send_message (form_post, url) {
   if (!form_post.querySelector(".message_text").innerHTML && !form_post.querySelector(".special_block").innerHTML){
     toast_error("Напишите или прикрепите что-нибудь");
