194b2e818549acc4fe3777fddde8f0cc3f75a69b
Serg <ochkarik1983@mail.ru@>
2021-09-11 14:11:51 +0300

    H

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -50,7 +50,7 @@ on('#ajax', 'input', '.message_text', function() {
         remove_class_timeout(_this);
         setTimeout(function(){
           form = _this.parentElement.parentElement;
-          if (!form.querySelector(".message_text").innerHTML && !form.querySelector(".special_block").innerHTML){
+          if (form.querySelector(".message_text").innerHTML || form.querySelector(".special_block").innerHTML){
             send_message (form, "/chat/user_progs/save_draft_message/" + form.parentElement.parentElement.getAttribute("chat-pk") + "/");
             form.nextElementSibling.innerHTML = "Черновик сохранен"
           }
