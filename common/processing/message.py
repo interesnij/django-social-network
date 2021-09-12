deceab9c2796ee4707ea65fda5efa6601eb67e38
Serg <ochkarik1983@mail.ru@>
2021-09-12 12:05:32 +0300

    J

--- common/processing/message.py
+++ common/processing/message.py
@@ -2,7 +2,7 @@ def get_message_processing(message, type):
     message.type = type
     message.save(update_fields=['type'])
     return message
-def get_edit_message_processing(message, type):
+def get_edit_message_processing(message):
     return message
 def get_chat_processing(list, type):
     list.type = type
