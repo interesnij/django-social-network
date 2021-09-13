0be5325b76cf6cd708ddddf32f900385a6ca931a
Serg <ochkarik1983@mail.ru@>
2021-09-13 15:32:39 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -274,6 +274,8 @@ on('#ajax', 'click', '#add_chat_btn', function() {
 function send_message (form_post, url) {
   _text = form_post.querySelector(".message_text").innerHTML;
   __text = _text.replace("<br>","");
+  console.log(_text)
+  console.log(__text)
   if (!__text && !form_post.querySelector(".special_block").innerHTML){
     toast_error("Напишите или прикрепите что-нибудь");
     form_post.querySelector(".message_text").classList.add("border_red");
