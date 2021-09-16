7538cd15951000153611e2b42b3f4d9e8cac5ef4
Serg <ochkarik1983@mail.ru@>
2021-09-16 17:25:45 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -31,7 +31,7 @@ on('#ajax', 'click', '#u_add_post_btn', function() {
   form_post = this.parentElement.parentElement.parentElement.parentElement;
   _text = form_post.querySelector(".smile_supported").innerHTML;
   if (_text.replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g,'').trim() == "" && form_post.querySelector(".files_0")) {
-    toast_error("Напишите или прикрепите что-нибудь")
+    toast_error("Напишите или прикрепите что-нибудь"); return
   };
 
   $input = document.createElement("input");
