11a960865e60223bc769cea32b605b80025b73fd
Serg <ochkarik1983@mail.ru@>
2021-09-17 19:24:48 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -46,9 +46,7 @@ on('#ajax', 'input', '.chat_message_text', function() {
         remove_class_timeout(_this);
         setTimeout(function(){
           form = _this.parentElement.parentElement;
-          if ( !_this.innerHTML.replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g,'').trim() == ""){
-            send_draft_message (form, "/chat/user_progs/save_draft_message/" + form.parentElement.parentElement.getAttribute("chat-pk") + "/");
-          }
+          send_draft_message (form, "/chat/user_progs/save_draft_message/" + form.parentElement.parentElement.getAttribute("chat-pk") + "/");
       }, 1000)
   };
 });
