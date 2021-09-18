ad8514af188bb3c1abb4db78f64ee48cd3376d99
Serg <ochkarik1983@mail.ru@>
2021-09-18 14:51:29 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -154,9 +154,7 @@ class Chat(models.Model):
                  chat_name = self.name
             else:
                 chat_name = "Групповой чат"
-            if first_message.creator.id == user_id:
-                creator_figure = '<span class="underline">Вы:</span> '
-            media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, '<small class="float-right text-muted">', first_message.get_created(), '</small></h5><p class="mb-0" style="white-space: nowrap;">', creator_figure, first_message.get_preview_text(user_id), '</p></div>'])
+            media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, '<small class="float-right text-muted">', first_message.get_created(), '</small></h5><p class="mb-0" style="white-space: nowrap;">', first_message.get_preview_text(user_id), '</p></div>'])
             return ''.join(['<div class="media">', figure, media_body, self.get_unread_count_message(user_id), '</div>'])
 
     def get_avatars(self):
