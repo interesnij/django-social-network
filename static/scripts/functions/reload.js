251dab5157dcb9f61b873b54df10436a69b4d77a
Serg <ochkarik1983@mail.ru@>
2021-09-18 19:02:59 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -246,7 +246,8 @@ function open_fullscreen(url, block) {
                     if (this.readyState == 4 && this.status == 200) {
                         var elem = document.createElement('span');
                         elem.innerHTML = link_3.responseText;
-                        box.parentElement.insertAdjacentHTML('beforeend', elem.querySelector(".is_block_paginate").innerHTML)
+                        box.parentElement.insertAdjacentHTML('beforeend', elem.querySelector(".is_block_paginate").innerHTML);
+                        box.remove()
                       }
                 }
                 link_3.send();
