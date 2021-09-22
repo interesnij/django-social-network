65712b8c3fba4f80291b1f6269132ba6c8358486
Serg <ochkarik1983@mail.ru@>
2021-09-22 15:13:16 +0300

    J

--- docs/forms.py
+++ docs/forms.py
@@ -7,7 +7,7 @@ class DoclistForm(forms.ModelForm):
 
 	class Meta:
 		model = DocList
-		fields = ['name', 'description','can_see_item','can_see_comment','add_item','add_comment','can_copy_item','can_copy_list',]
+		fields = ['name', 'description','can_see_item','can_see_comment','add_item','add_comment','can_copy']
 
 class DocForm(forms.ModelForm):
 	description = forms.CharField( label="", required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
