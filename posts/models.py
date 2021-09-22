f4e69e10f1b0c826181dfcf37eef2608f0f29b0b
Serg <ochkarik1983@mail.ru@>
2021-09-22 19:11:48 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -37,12 +37,7 @@ class PostList(models.Model):
     can_see_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит комментарии")
     create_item = models.PositiveSmallIntegerField(choices=PERM, default=4, verbose_name="Кто создает записи и потом с этими документами работает")
     create_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
-
-    #can_copy = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто копирует записи и списки")
-    #can_see_item = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит записи")
-    #can_see_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит комментарии")
-    #create_item = models.PositiveSmallIntegerField(choices=PERM, default=4, verbose_name="Кто создает записи и потом с этими документами работает")
-    #create_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
+    create_copy = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто может копировать")
 
     def __str__(self):
         return self.name + " - " + self.creator.get_full_name()
