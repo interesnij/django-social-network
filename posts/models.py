7de4994ad6492ffe5123be96fba16e62c18066a7
Serg <ochkarik1983@mail.ru@>
2021-09-22 15:51:55 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -39,10 +39,10 @@ class PostList(models.Model):
     create_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
 
     can_copy = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто копирует записи и списки")
-    can_see_item_2 = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит записи")
-    can_see_comment_2 = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит комментарии")
-    can_create_item_2 = models.PositiveSmallIntegerField(choices=PERM, default=4, verbose_name="Кто создает записи и потом с этими документами работает")
-    can_create_comment_2 = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
+    can_see_item = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит записи")
+    can_see_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит комментарии")
+    can_create_item = models.PositiveSmallIntegerField(choices=PERM, default=4, verbose_name="Кто создает записи и потом с этими документами работает")
+    can_create_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
 
     def __str__(self):
         return self.name + " - " + self.creator.get_full_name()
