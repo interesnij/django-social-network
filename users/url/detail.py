83e6b850f661df74721e9c9adbb7b4c26a8248b0
Serg <ochkarik1983@mail.ru@>
2021-09-26 15:52:11 +0300

    J

--- users/url/detail.py
+++ users/url/detail.py
@@ -3,8 +3,6 @@ from users.views.detail import *
 from users.views.lists import *
 
 urlpatterns = [
-    url(r'^post/(?P<uuid>[0-9a-f-]+)/$', UserPostView.as_view(), name='user_post'),
-    url(r'^fix_post/(?P<uuid>[0-9a-f-]+)/$', UserFixPostView.as_view(), name='user_fix_post'),
     url(r'^visit_communities/$', UserVisitCommunities.as_view(), name='visited_communities'),
     url(r'^list/(?P<pk>\d+)/(?P<list_pk>\d+)/$', UserPostsListView.as_view(), name='user_post_list'),
     url(r'^possible/$', AllPossibleUsersList.as_view(), name='possible_users'),
