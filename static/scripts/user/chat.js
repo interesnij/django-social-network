1fe63c721c1cc813a436d4f56eb771823ffc583d
Serg <ochkarik1983@mail.ru@>
2021-09-26 13:49:40 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -288,10 +288,9 @@ function send_message_sticker(url, value) {
 };
 
 on('#ajax', 'click', '.user_create_chat', function() {
-  create_fullscreen("/chat/user_progs/create_chat/" + pk + "/", "item_fullscreen");
+  create_fullscreen("/chat/user_progs/create_chat/" + this.getAttribute("data-pk") + "/", "item_fullscreen");
 });
 on('#ajax', 'click', '.user_send_page_message', function() {
-  pk = this.getAttribute("data-pk");
   create_fullscreen("/chat/user_progs/send_page_message/" + this.getAttribute("data-pk") + "/", "item_fullscreen");
 });
 
