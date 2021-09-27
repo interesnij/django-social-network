a23080e726f3fc05de2cdd8db44f3afa54fdda36
Serg <ochkarik1983@mail.ru@>
2021-09-27 14:40:14 +0300

    J

--- docs/models.py
+++ docs/models.py
@@ -28,9 +28,7 @@ class DocList(models.Model):
     count = models.PositiveIntegerField(default=0)
 
     can_see_item = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит документы")
-    can_see_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто видит комментарии")
     create_item = models.PositiveSmallIntegerField(choices=PERM, default=4, verbose_name="Кто создает документы и потом с этими документами работает")
-    create_comment = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто пишет комментарии")
     can_copy = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Кто копирует документы")
 
     users = models.ManyToManyField("users.User", blank=True, related_name='+')
