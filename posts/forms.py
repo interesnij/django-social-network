61823eff13d348580873fdb28a260360a09648a4
Serg <ochkarik1983@mail.ru@>
2021-09-27 19:41:23 +0300

    J

--- posts/forms.py
+++ posts/forms.py
@@ -18,4 +18,4 @@ class CommentForm(forms.ModelForm):
 class PostListForm(forms.ModelForm):
 	class Meta:
 		model = PostList
-		fields = ['name', 'description','can_see_item','can_see_comment','create_item','create_comment', 'create_copy']
+		fields = ['name', 'description','can_see_el','can_see_comment','create_el','create_comment', 'copy_el']
