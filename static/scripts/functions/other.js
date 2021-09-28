39eaa7ad53393aec42a5c2326ad7dab508d3bd93
Serg <ochkarik1983@mail.ru@>
2021-09-28 16:31:06 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -883,7 +883,7 @@ function elementInViewport(el) {
 
 function send_comment(form, block, link) {
   _text = form.querySelector(".comment_text").innerHTML;
-  if (_text.replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g,'').trim() == "" || form.querySelector(".img_block").firstChild) {
+  if (_text.replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g,'').trim() == "" || form.querySelector(".files_0")) {
     toast_error("Напишите или прикрепите что-нибудь"); return
   };
 
