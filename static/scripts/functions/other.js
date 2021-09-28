89c30557bc3157e3b3211969bcc9d4ad35fd67e0
Serg <ochkarik1983@mail.ru@>
2021-09-28 12:58:20 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -90,10 +90,15 @@ function change_this_fullscreen(_this, type_class) {
 on('body', 'click', '.this_fullscreen_hide', function() {
   get_document_opacity_1(document.body.querySelector(".main-container"));
   this.parentElement.remove();
+  if (!document.body.querySelector("#fullscreens_container").innerHTML) {
+    get_document_opacity_1(document.body.querySelector(".main-container"));
+  }
 });
 on('body', 'click', '.this_mob_fullscreen_hide', function() {
-  get_document_opacity_1(document.body.querySelector(".main-container"));
   this.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+  if (!document.body.querySelector("#fullscreens_container").innerHTML) {
+    get_document_opacity_1(document.body.querySelector(".main-container"));
+  }
 });
 
 function check_message_form_btn() {
