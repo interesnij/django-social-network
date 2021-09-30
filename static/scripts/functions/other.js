24a92cc4deca4a3deceafb343c75fa2759e25f2f
Serg <ochkarik1983@mail.ru@>
2021-09-30 13:42:05 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -121,11 +121,11 @@ function change_this_fullscreen(_this, type_class) {
           $parent_div = $loader.parentElement
           if (height < 500){
             $loader.style.height = height + "px";
-            top_height = (window.innerHeight - height - 50) / 2 + "px";
-            $parent_div.style.top = top_height;
+            //top_height = (window.innerHeight - height - 50) / 2 + "px";
+            //$parent_div.style.top = top_height;
           } else {
             $loader.style.height = "100%";
-            $parent_div.style.top = "15px";
+            //$parent_div.style.top = "15px";
           }
       }
   };
