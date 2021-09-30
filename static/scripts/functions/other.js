d27303236e839939a3d815ac68b2993f0411d450
Serg <ochkarik1983@mail.ru@>
2021-09-30 12:15:33 +0300

    J

--- static/scripts/functions/other.js
+++ static/scripts/functions/other.js
@@ -66,7 +66,7 @@ function create_fullscreen(url, type_class) {
           span = document.createElement("span");
           span.innerHTML = elem;
           height = span.querySelector(".card").scrollHeight*1 + 30;
-          console.log(span);
+          console.log(span.querySelector(".card").scrollHeight);
           if (height < 500) {
             $parent_div.style.height = height + "px";
           } else { $parent_div.style.height = "100%"};
