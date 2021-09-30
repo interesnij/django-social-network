3dd3d0bcf437ec3d68b47a566e19aa29ec69e1cb
Serg <ochkarik1983@mail.ru@>
2021-09-30 18:57:25 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -146,7 +146,7 @@ on('#ajax', 'input', '.smile_supported', function() {
       img_list[i].remove()
     };
   };
-  get_caret();
+  get_caret(_this);
 
   if (_this.classList.contains("chat_message_text")){
     if (document.body.querySelector(".chatlist")) {
