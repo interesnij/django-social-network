b581cb3d1edc5b1c95886815f3c87f8a6d70b31f
Serg <ochkarik1983@mail.ru@>
2021-10-03 11:50:22 +0300

    J

--- common/processing_2.py
+++ common/processing_2.py
@@ -67,7 +67,7 @@ def is_have_bad_words(text):
         for part in range(len(text)):
             fragment = text[part: part+len(word)]
             if distance(fragment, word) <= len(word)*0.25:
-                raise PermissionDenied("bad words")
+                raise PermissionDenied("Исправьте слова", "Найдено", word, "Похоже на", fragment)
     return False
 
 def get_links_in_text(text):
