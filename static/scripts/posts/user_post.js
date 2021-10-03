6b257da343093ee320a6d13fc0b4cb4347cb482d
Serg <ochkarik1983@mail.ru@>
2021-10-03 12:21:31 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -65,7 +65,7 @@ on('#ajax', 'click', '#u_add_post_btn', function() {
       elem = link_.responseText;
       new_post = document.createElement("span");
       new_post.innerHTML = elem;
-      cknsole.log(new_post);
+      console.log(new_post);
       toast_info(new_post.querySelector(".exception_value").innerHTML)
     }
   };
