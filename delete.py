1370b8cac8a43169d80e34631ecfe7b6c488e683
Serg <ochkarik1983@mail.ru@>
2021-10-04 15:09:57 +0300

    J

--- delete.py
+++ delete.py
@@ -20,29 +20,9 @@ from docs.models import Doc
 from chat.models import Message
 from gallery.models import PhotoList
 from chat.models import Message
+from communities.models import Community
+from communities.model.settings import CommunityPrivate
 import re
 
-text = "https://www.youtube.com/1111111 https://www.youtube.com/2222222222 https://www.youtube.com/3333333333"
-
-http = re.findall(r'(https?://[^\s]+)', text)
-_loop = []
-_loop.append(text)
-
-if http:
-    this = -1
-    next = 0
-    for p in http:
-        a = ""
-        this += 1
-        next += 1
-        print ("Ссылка", p)
-        print ("=========")
-        _loop.append(a)
-        if "трезвый.рус" in p:
-            _loop[next] = _loop[this].replace(p, '<a class="ajax underline" href="' + p + '">' + p + '</a>')
-        else:
-            _loop[next] = _loop[this].replace(p, '<a class="underline" target="_blank" href="' + p + '">' + p + '</a>')
-        print ("Старая переменная", _loop[this])
-        print ("---------")
-        print ("Новая переменная", _loop[next])
-        print ("---------")
+for c in Community.objects.all():
+    CommunityPrivate.objects.create(community=c)
