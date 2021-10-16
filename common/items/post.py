04ec3438045cca62ec559ce570b0aba9fe66760f
Serg <ochkarik1983@mail.ru@>
2021-10-16 17:24:46 +0300

    J

--- common/items/post.py
+++ common/items/post.py
@@ -52,7 +52,7 @@ def post(user, value):
             else:
                 card_drop = '<span class="dropdown-item post_claim">Пожаловаться</span>'
             return ''.join([block, '<span class="post" data-pk="', str(community.pk), '"list-pk="', str(post.list.pk), '" data-uuid="', str(post.uuid), '"><div class="card-header">\
-            <div class="media"><a href="', community.get_link(), '" class="ajax"><figure>', community.get_community_avatar(), '</figure></a><div class="media-body"><h6 class="mb-0"><a href="', community.get_link(), '" class="ajax">\
+            <div class="media"><a href="', community.get_link(), '" class="ajax"><figure>', community.get_community_avatar(), '</figure></a><div class="media-body"><h6 class="mb-0 fullscreen_2"><a href="', community.get_link(), '" class="ajax">\
             ', community.name, '</a></h6><p class="mb-0">', post.get_created(), '</p></div><div class="dropdown"><a style="cursor:pointer" class="icon-circle icon-30 btn_default drop">\
             <svg class="svg_info" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>\
             </a><div class="dropdown-menu dropdown-menu-right"><span><span class="dropdown-item u_all_posts_likes pointer">Оценили</span>\
@@ -100,7 +100,7 @@ def post(user, value):
             else:
                 card_drop = '<span class="dropdown-item post_claim">Пожаловаться</span>'
             return ''.join([block, '<span class="post" data-pk="', str(creator.pk), '"list-pk="', str(post.list.pk), '" data-uuid="', str(post.uuid), '"><div class="card-header">\
-            <div class="media"><a href="', creator.get_link(), '" class="ajax"><figure>', creator.get_my_avatar(), '</figure></a><div class="media-body"><h6 class="mb-0"><a href="', creator.get_link(), '" class="ajax">\
+            <div class="media"><a href="', creator.get_link(), '" class="ajax"><figure>', creator.get_my_avatar(), '</figure></a><div class="media-body"><h6 class="mb-0 fullscreen_2"><a href="', creator.get_link(), '" class="ajax">\
             ', creator.get_full_name(), '</a></h6><p class="mb-0">', post.get_created(), '</p></div><div class="dropdown"><a style="cursor:pointer" class="icon-circle icon-30 btn_default drop">\
             <svg class="svg_info" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>\
             </a><div class="dropdown-menu dropdown-menu-right"><span><span class="dropdown-item u_all_posts_likes pointer">Оценили</span>\
