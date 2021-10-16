2eaf1b7fc1d6d866efe5a78efddece42f7252b49
Serg <ochkarik1983@mail.ru@>
2021-10-16 14:05:19 +0300

    J

--- delete.py
+++ delete.py
@@ -113,7 +113,7 @@ if words:
             if word[0] == "#":
                 _p = word.strip(".,:;!_*-+()/@#¤%&)")
                 tag = "#" + _p
-                _loop[next] = _loop[this].replace(indent + tag, indent + '<a class="ajax action"href="/search/?tag=' + _p + '">' + tag + '</a>')
+                _loop[next] = _loop[this].replace(tag + indent, '<a class="ajax action"href="/search/?tag=' + _p + '">' + tag + '</a>' + indent)
             else:
                 _p = word.strip(".,:;!_*-+()/@#¤%&)")
                 p_2 = _p[_p.find("#") + 1:]
