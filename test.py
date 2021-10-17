a3e8d4b1d16e77122f597304da163b1ec054e494
Serg <ochkarik1983@mail.ru@>
2021-10-17 17:44:44 +0300

    J

--- test.py
+++ test.py
@@ -14,20 +14,6 @@ django.setup()
 
 
 response = requests.get(url= "http://api.sypexgeo.net/J5O6d/json/91.239.184.244")
-print(response)
 data = response.json()
-
-sity = data['city']
-region = data['region']
-country = data['country']
-loc.city_ru = sity['name_ru']
-loc.city_en = sity['name_en']
-loc.city_lat = sity['lat']
-loc.city_lon = sity['lon']
-loc.region_ru = region['name_ru']
-loc.region_en = region['name_en']
-loc.country_ru = country['name_ru']
-loc.country_en = country['name_en']
-loc.phone = country['phone']
 
 print(data)
