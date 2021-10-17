f37e7e06e366932f95c421ec96842cbf2815e9a5
Serg <ochkarik1983@mail.ru@>
2021-10-17 17:44:11 +0300

    J

--- test.py
+++ test.py
@@ -12,19 +12,6 @@ import django, json, requests
 
 django.setup()
 
-# -*- coding: utf-8 -*-
-from locale import *
-import csv,sys,os
-
-project_dir = '../tr/tr/'
-
-sys.path.append(project_dir)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import django, json, requests
-
-django.setup()
 
 response = requests.get(url= "http://api.sypexgeo.net/J5O6d/json/91.239.184.244")
 print(response)
