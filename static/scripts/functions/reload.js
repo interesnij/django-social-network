213a07aac4c92eea10752140bcc9c2751d5d45e4
Serg <ochkarik1983@mail.ru@>
2021-10-18 11:03:52 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -412,7 +412,7 @@ function ajax_get_reload(url) {
   console.log($serf_history);
   $serf_history.push(document.title + "," + document.location.href);
   console.log($serf_history);
-  console.log($serf_history[-1]);
+  console.log($serf_history.slice(-1));
     var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     ajax_link.open('GET', url, true);
     ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
