0d09cb806e5fc71fe37d215c543faeb642d66078
Serg <ochkarik1983@mail.ru@>
2021-10-19 12:49:26 +0300

    J

--- static/scripts/gallery/community_post.js
+++ static/scripts/gallery/community_post.js
@@ -69,12 +69,12 @@ on('#ajax', 'click', '.c_photo_edit_comment_btn', function() {
 on('#ajax', 'click', '.c_photo_off_comment', function() {
   send_photo_change(this, "/gallery/community_progs/off_comment/", "c_photo_on_comment", "Вкл. комментарии");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement;
-  post.querySelector(".c_photo_comments").style.display = "none"
+  post.querySelector(".load_photo_comments").style.display = "none"
 })
 on('#ajax', 'click', '.c_photo_on_comment', function() {
   send_photo_change(this, "/gallery/community_progs/on_comment/", "c_photo_off_comment", "Выкл. комментарии");
   post = this.parentElement.parentElement.parentElement.parentElement.parentElement;
-  post.querySelector(".c_photo_comments").style.display = "unset"
+  post.querySelector(".load_photo_comments").style.display = "unset"
 })
 
 on('#ajax', 'click', '.c_photo_comment_delete', function() {
