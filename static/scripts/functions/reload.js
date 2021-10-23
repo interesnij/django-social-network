68b6ce8002c6173ccc83738e8191a201c4d377d9
Serg <ochkarik1983@mail.ru@>
2021-10-23 17:26:30 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -252,9 +252,9 @@ var scrollStopper = delayedExec(3000, function() {
                       get_el_view_time(120)
                     };
                     if ($main_container.querySelector(".is_paginate") && !$list_stat.length) {
+                      console.log($list_stat);
                       pag_list = $main_container.querySelector(".is_paginate");
                       $list_stat = [pag_list.getAttribute("data-type"), 0, 0, pag_list.getAttribute("data-pk"), $main_container.getAttribute("data-type"),$request_user_id];
-                      console.log($list_stat);
                     };
                     pk = list[i].getAttribute('data-pk');
                     type = list[i].getAttribute('data-type');
