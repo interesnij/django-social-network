709f4e764cc883ecd1bf599bde203bf10d7e882c
Serg <ochkarik1983@mail.ru@>
2021-10-23 15:55:41 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -246,10 +246,9 @@ var scrollStopper = delayedExec(3000, function() {
           if ($new_elements) {
             for (var i = 0; i < $new_elements.length; i++){
               $new_elements[i][2] = 3 + $new_time;
-              console.log($new_elements[i]);
               el = $new_elements[i][0] + " " + $new_elements[i][1] + " " + $new_elements[i][2] + " " + $new_elements[i][3] + " " + $new_elements[i][4]
-              $all_stat.push(el);
-              console.log(el);
+              $all_stat.push($new_elements[i]);
+              console.log($new_elements[i]);
               console.log("Добавлен элемент в общий список со временем", el);
             };
             $new_elements = [];
