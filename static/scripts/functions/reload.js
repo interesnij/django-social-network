caf7e0ac5ea498c2dd91a5fc320de3eef042fc72
Serg <ochkarik1983@mail.ru@>
2021-10-26 14:53:56 +0300

    J

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -42,7 +42,7 @@ function get_window_page_view_time(count, time_total) {
     setInterval(() => $new_window_elements[2] += 1, 1000);
     i += 1
   } else {
-    block = container.querySelectorAll(".card_fullscreen");
+    block = container.querySelector(".card_fullscreen");
     block.classList.add("count_done");
   };
 };
@@ -118,7 +118,7 @@ function create_fullscreen(url, type_class) {
               _page_time = false;
               time_total += 120;
               get_window_page_view_time(120, time_total);
-            }
+            };
             if ($new_window_elements.length) {
                 el = $new_window_elements[0] + " " + $new_window_elements[1] + " " + $new_window_elements[2] + " " + $new_window_elements[3] + " " + $new_window_elements[4] + " " + $new_window_elements[5] + " " + $new_window_elements[6]
                 $all_stat.push(el);
