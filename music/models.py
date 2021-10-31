1ac6b9e823c74193d5a540365a71973b31f46969
Serg <ochkarik1983@mail.ru@>
2021-10-31 16:42:53 +0300

    J

--- music/models.py
+++ music/models.py
@@ -450,20 +450,26 @@ class Music(models.Model):
     def get_s_image(self):
         if self.image:
             return '<img style="width:30px;" alt="image" src="' + self.image.url + '" />'
-        elif self.album.image:
-            return '<img style="width:30px;" alt="image" src="' + self.album.image.url + '" />'
-        elif self.album.artist.image:
-            return '<img style="width:30px;" alt="image" src="' + self.album.artist.image.url + '" />'
+        elif self.album:
+            if self.album.image:
+                return '<img style="width:30px;" alt="image" src="' + self.album.image.url + '" />'
+            elif self.album.artist and self.album.artist.image:
+                return '<img style="width:30px;" alt="image" src="' + self.album.artist.image.url + '" />'
+            else:
+                return '<svg fill="currentColor" class="svg_default" width="30" height="30" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path></svg>'
         else:
             return '<svg fill="currentColor" class="svg_default" width="30" height="30" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path></svg>'
 
     def get_image(self):
         if self.image:
             return '<img style="width:40px;" alt="image" src="' + self.image.url + '" />'
-        elif self.album.image:
-            return '<img style="width:40px;" alt="image" src="' + self.album.image.url + '" />'
-        elif self.album.artist.image:
-            return '<img style="width:40px;" alt="image" src="' + self.album.artist.image.url + '" />'
+        elif self.album:
+            if self.album.image:
+                return '<img style="width:40px;" alt="image" src="' + self.album.image.url + '" />'
+            elif self.album.artist and self.album.artist.image:
+                return '<img style="width:40px;" alt="image" src="' + self.album.artist.image.url + '" />'
+            else:
+                return '<svg fill="currentColor" class="svg_default" width="40" height="40" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path></svg>'
         else:
             return '<svg fill="currentColor" class="svg_default" width="40" height="40" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path></svg>'
 
