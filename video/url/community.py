560c8231b414ce93b706c2bbf65ef5a1fe50f013
Serg <ochkarik1983@mail.ru@>
2021-11-01 14:20:02 +0300

    J

--- video/url/community.py
+++ video/url/community.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^list/(?P<pk>\d+)/$', CommunityVideoList.as_view()),
     url(r'^list_post/(?P<pk>\d+)/$', CommunityPostVideoList.as_view()),
     url(r'^list_post_comment/(?P<pk>\d+)/$', CommunityPostCommentVideoList.as_view()),
-    url(r'^detail/(?P<pk>\d+)/(?P<pk>\d+)/$', CommunityVideoDetail.as_view(), name="video_detail"),
+    url(r'^detail/(?P<pk>\d+)/(?P<video_pk>\d+)/$', CommunityVideoDetail.as_view(), name="video_detail"),
     url(r'^info/(?P<pk>\d+)/(?P<video_pk>\d+)/$', CommunityVideoInfo.as_view()),
 ]
