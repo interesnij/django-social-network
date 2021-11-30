6235ef1921801bdb7cb34792024fff40a3fc64a7
Serg <ochkarik1983@mail.ru@>
2021-11-30 16:27:10 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.conf import settings
 from django.db import models
 from common.utils import try_except
-from pilkit.processors import ResizeToFill, ResizeToFit
+from pilkit.processors import ResizeToFill, ResizeToFit, Transpose
 from chat.helpers import upload_to_chat_directory, validate_file_extension
 from imagekit.models import ProcessedImageField
 from django.contrib.postgres.indexes import BrinIndex
@@ -27,7 +27,7 @@ class Chat(models.Model):
 
     name = models.CharField(max_length=100, blank=True, verbose_name="Название")
     type = models.CharField(blank=False, null=False, choices=TYPE, max_length=6, verbose_name="Тип чата")
-    image = ProcessedImageField(blank=True, format='JPEG',options={'quality': 100},upload_to=upload_to_chat_directory,processors=[Transpose(), ResizeToFit(width=200, height=200)]) 
+    image = ProcessedImageField(blank=True, format='JPEG',options={'quality': 100},upload_to=upload_to_chat_directory,processors=[Transpose(), ResizeToFit(width=200, height=200)])
     description = models.CharField(max_length=200, blank=True, verbose_name="Описание")
     community = models.ForeignKey('communities.Community', related_name='community_chat', on_delete=models.CASCADE, null=True, blank=True, verbose_name="Сообщество")
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='chat_creator', null=True, blank=False, verbose_name="Создатель")
