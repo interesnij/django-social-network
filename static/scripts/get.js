179bfd23c0cd05936d267b8ebaabe4de342fcf7a
Serg <ochkarik1983@mail.ru@>
2021-12-06 15:46:59 +0300

    J

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -461,3 +461,20 @@ on('#ajax', 'change', '.cool_private_select', function() {
       }
   }
 });
+
+on('#ajax', 'click', '#add_list_selected_users_btn', function() {
+  form = this.parentElement.parentElement;
+  form.querySelector(".form_btn").disabled = true;
+  collector = document.body.querySelector(".collector_active");
+  users_block = form.querySelector(".card-header");
+  users_list = users_block.querySelectorAll(".custom_color");
+  final_list = "";
+  for (var i = 0; i < users_list.length; i++){
+    a = users_list[i].querySelector("a");
+    final_list += '<a href="' + a.getAttribute("href") + '" target="_blank">' + a.innerHTML + '</a>'
+    final_list += '<input type="hidden" name="' + collector.previousElementSibling.getAttribute("data-action") + '_users" value="' + users_list[i].getAttribute("data-pk") + '" />'
+  };
+  collector.innerHTML = final_list;
+  form.classList.remove("cool_private_form");
+  close_work_fullscreen();
+});
