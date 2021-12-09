caac6736196658dd8fb289d24815f72b11c627bc
Serg <ochkarik1983@mail.ru@>
2021-12-09 11:20:02 +0300

    J

--- common/templates.py
+++ common/templates.py
@@ -3,16 +3,9 @@ import re
 MOBILE_AGENT_RE = re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
 
 
-def update_activity(user, user_agent, stat=None):
+def update_activity(user, user_agent):
     from datetime import datetime, timedelta
-    if stat:
-        _stat = stat.split(",")
-        if len(_stat) > 3:
-            profile = user.profile
-            flo = profile.height + float(_stat[2])
-            profile.height = round(flo,2)
-            profile.time += timedelta(seconds = float(_stat[3]))
-            profile.save(update_fields=['height', 'time'])
+
     if MOBILE_AGENT_RE.match(user_agent):
         user.last_activity, user.device = datetime.now(), "Ph"
         user.save(update_fields=['last_activity', 'device'])
@@ -25,7 +18,7 @@ def get_folder(user_agent):
     if MOBILE_AGENT_RE.match(user_agent):
         return "mobile/"
     else:
-        return "mobile/"
+        return "desctop/"
 
 def get_fine_request_user(request_user):
     if request_user.is_no_phone_verified():
