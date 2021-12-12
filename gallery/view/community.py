612e5948eca4729baa07975bf8d349aecb89942e
Serg <ochkarik1983@mail.ru@>
2021-12-12 18:15:56 +0300

    J

--- gallery/view/community.py
+++ gallery/view/community.py
@@ -48,35 +48,37 @@ class CommunityPhotosList(ListView):
     def get_queryset(self):
         return self.list.get_items()
 
-
-class CommunityAlbumPhotoList(TemplateView):
+class CommunityAlbumPhotosList(ListView):
     template_name = None
+    paginate_by = 15
 
     def get(self,request,*args,**kwargs):
-        from common.templates import get_template_community_item, get_template_anon_community_item
+        from common.templates import get_template_community_list, get_template_anon_community_list
 
-        self.photo = Photo.objects.get(pk=self.kwargs["pk"])
+        self.community = Community.objects.get(pk=self.kwargs["pk"])
         self.list = PhotoList.objects.get(pk=self.kwargs["list_pk"])
-        if request.user.is_authenticated:
-            self.template_name = get_template_community_item(self.photo, "gallery/c_photo/list_photo/", "photo.html", request.user, request.META['HTTP_USER_AGENT'])
+        if request.is_ajax():
+            if request.user.is_authenticated:
+                self.template_name = get_template_community_list(self.list, "communities/photos/list/", "photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
+            else:
+                self.template_name = get_template_anon_community_list(self.list, "communities/photos/list/anon_photo_list.html", request.user, request.META['HTTP_USER_AGENT'])
         else:
-            self.template_name = get_template_anon_community_item(self.photo, "gallery/c_photo/list_photo/anon_photo.html", request.user, request.META['HTTP_USER_AGENT'])
-        if request.user.is_authenticated and request.user.is_administrator_of_community(self.list.community.pk):
-            self.photos = self.list.get_staff_items()
+            raise Http404
+        if request.user.is_authenticated and request.user.is_staff_of_community(self.community.pk):
+            self.photo_list = self.list.get_staff_items()
         else:
-            self.photos = self.list.get_items()
-        return super(CommunityAlbumPhotoList,self).get(request,*args,**kwargs)
+            self.photo_list = self.list.get_items()
+        return super(CommunityAlbumPhotosList,self).get(request,*args,**kwargs)
 
     def get_context_data(self,**kwargs):
-        context = super(CommunityAlbumPhotoList,self).get_context_data(**kwargs)
-        context["object"] = self.photo
-        context["community"] = self.list.community
-        context["list"] = self.list
-        context["next"] = self.photos.filter(pk__gt=self.photo.pk).order_by('pk').first()
-        context["prev"] = self.photos.filter(pk__lt=self.photo.pk).order_by('-pk').first()
-        context["avatar"] = self.photo.is_avatar(self.request.user)
-        context["user_form"] = PhotoDescriptionForm(instance=self.photo)
+        context = super(CommunityAlbumPhotosList,self).get_context_data(**kwargs)
+        context['community'] = self.community
+        context['list'] = self.list
         return context
+
+    def get_queryset(self):
+        return self.photo_list
+
 
 class GetCommunityPhoto(TemplateView):
     template_name = None
