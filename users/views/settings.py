40bb4181f9ba566d4084ac522ce38e4289553b4f
Serg <ochkarik1983@mail.ru@>
2021-12-14 16:23:38 +0300

    J

--- users/views/settings.py
+++ users/views/settings.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from users.model.profile import UserProfile
 from users.model.settings import *
 from users.forms import *
