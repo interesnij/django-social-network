b40fd7bc297582441308b4b202cfb5229b7e2770
Serg <ochkarik1983@mail.ru@>
2021-12-15 16:19:22 +0300

    J

--- delete.py
+++ delete.py
@@ -20,6 +20,7 @@ from communities.models import Community
 from chat.models import Message, MessageFixed, MessageVersion
 
 
+ProfilePrivate.objects.all().delete()
 Message.objects.all().delete()
 MessageFixed.objects.all().delete()
 MessageVersion.objects.all().delete()
