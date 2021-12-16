b6a4ba42d212feaf770399d66ba1821f6d3fa194
Serg <ochkarik1983@mail.ru@>
2021-12-16 18:07:07 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -63,7 +63,7 @@ on('#ajax', 'click', '.create_favourite_messages', function() {
   for (var i = 0; i < get_toggle_messages().length; i++){
     if (!list[i].querySelector(".toggle_message_favourite")) {
         messages.push(list[i].getAttribute("data-uuid"));
-        list[i].querySelector(".favourite_icon").innerHTML = "            <svg width="18" height="18" class="toggle_message_favourite pointer" fill="currentColor" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><path d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/></svg>"
+        list[i].querySelector(".favourite_icon").innerHTML = '<svg width="18" height="18" class="toggle_message_favourite pointer" fill="currentColor" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><path d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/></svg>'
     };
     list[i].classList.remove("custom_color", "target_message")
   };
@@ -73,7 +73,7 @@ on('#ajax', 'click', '.create_favourite_messages', function() {
 		ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     ajax_link.onreadystatechange = function () {
       if ( this.readyState == 4 && this.status == 200 ) {
-
+        edit_favourite_count(len(messages), "plus")
       }
     }
     ajax_link.send();
