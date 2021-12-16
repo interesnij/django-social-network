b419e895a104bb67067d63190161b35ede1c0284
Serg <ochkarik1983@mail.ru@>
2021-12-16 10:56:22 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -587,6 +587,10 @@ on('#ajax', 'click', '.toggle_message', function(e) {
       if (list[i].classList.contains("custom_color")) {
         is_toggle = true
       }
+      if (list[i].querySelector("toggle_message_favourite")) {
+          favourite_btn.innerHTML = '<path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>'
+        } else { favourite_btn.innerHTML = '<path d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/>' };
+      }
     };
     is_toggle ? null : hide_chat_console();
   } else {
