82d2592c6efd75d10f80ebc11aaf9e6fe871bee6
Serg <ochkarik1983@mail.ru@>
2021-12-16 12:00:43 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -598,9 +598,11 @@ on('#ajax', 'click', '.toggle_message', function(e) {
   };
   custom_color_list = message.parentElement.querySelectorAll(".custom_color");
   for (var i = 0; i < custom_color_list.length; i++){
-    if (list[i].querySelector(".toggle_message_favourite") && !list[i].getAttribute("data-uuid") == message.getAttribute("data-uuid")) {
-      is_favourite = true;
-    }
+    if (list[i].getAttribute("data-uuid") != message.getAttribute("data-uuid")) {
+      if (list[i].querySelector(".toggle_message_favourite")) {
+        is_favourite = true;
+      }
+    } else {console.log("Совпадение")}
   };
   if (message.querySelector(".toggle_message_favourite")) {
     is_favourite = true;
