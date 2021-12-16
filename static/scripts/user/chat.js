92153be9eff493a7400d3055b3b5f3be8ebfaf66
Serg <ochkarik1983@mail.ru@>
2021-12-16 19:47:48 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -15,7 +15,7 @@ function show_chat_console(message) {
   favourite_btn = _console.querySelector(".toggle_messages_favourite");
   list_not_have_favourite_messages = true;
   for (var i = 0; i < get_toggle_messages().length; i++){
-    if (!list[i].querySelector(".toggle_message_favourite")) {
+    if (!list[i].querySelector(".delete_favourite_message")) {
         list_not_have_favourite_messages = false;
       }
     };
@@ -76,9 +76,9 @@ on('#ajax', 'click', '.create_favourite_messages', function() {
   messages = [];
   list = get_toggle_messages();
   for (var i = 0; i < list.length; i++){
-    if (!list[i].querySelector(".toggle_message_favourite")) {
+    if (!list[i].querySelector(".delete_favourite_message")) {
         messages.push(list[i].getAttribute("data-uuid"));
-        list[i].querySelector(".favourite_icon").innerHTML = '<svg width="18" height="18" class="toggle_message_favourite pointer" fill="currentColor" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><path d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/></svg>'
+        list[i].querySelector(".favourite_icon").innerHTML = '<svg width="18" height="18" class="delete_favourite_message pointer" fill="currentColor" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><path d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/></svg>'
     };
     list[i].classList.remove("custom_color", "target_message")
   };
@@ -98,7 +98,7 @@ on('#ajax', 'click', '.remove_favourite_messages', function() {
   messages = [];
   list = get_toggle_messages();
   for (var i = 0; i < list.length; i++){
-    if (list[i].querySelector(".toggle_message_favourite")) {
+    if (list[i].querySelector(".delete_favourite_message")) {
         messages.push(list[i].getAttribute("data-uuid"));
         list[i].querySelector(".favourite_icon").innerHTML = ''
     };
