ab18d606233732b3330ad44938b1e786c177f208
Serg <ochkarik1983@mail.ru@>
2021-12-17 16:53:28 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -68,9 +68,11 @@ on('#ajax', 'click', '.u_message_unfixed', function() {
       if ( this.readyState == 4 && this.status == 200 ) {
         fix_span = message.parentElement.parentElement.parentElement.querySelector(".count_fixed_messages")
         fix_count = fix_span.innerHTML;
+        console.log("до", fix_count);
         fix_count *= 1;
         fix_count -= 1;
         fix_span.innerHTML = fix_count;
+        console.log("после", fix_count);
         message.remove();
       }
     }
