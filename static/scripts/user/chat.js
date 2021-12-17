965fc4dd60e302bb8f045eb5274610bf578d5af3
Serg <ochkarik1983@mail.ru@>
2021-12-17 16:59:56 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -66,13 +66,11 @@ on('#ajax', 'click', '.u_message_unfixed', function() {
 		ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     ajax_link.onreadystatechange = function () {
       if ( this.readyState == 4 && this.status == 200 ) {
-        fix_span = message.parentElement.parentElement.parentElement.querySelector(".count_fixed_messages")
-        fix_count = fix_span.innerHTML;
-        console.log("до", fix_count);
-        fix_count *= 1;
-        fix_count -= 1;
-        fix_span.innerHTML = fix_count;
-        console.log("после", fix_count);
+        //fix_span = message.parentElement.parentElement.parentElement.querySelector(".count_fixed_messages")
+        //fix_count = fix_span.innerHTML;
+        //fix_count *= 1;
+        //fix_count -= 1;
+        //fix_span.innerHTML = fix_count;
         message.remove();
       }
     }
@@ -564,7 +562,6 @@ on('#ajax', 'click', '.u_message_fixed', function() {
         parent = "Наклейка"
     };
     creator_p = '<p><svg style="width: 17px;vertical-align: bottom;" fill="currentColor" viewBox="0 0 24 24"><g><rect fill="none" height="16" width="16"/></g><g><path d="M16,9V4l1,0c0.55,0,1-0.45,1-1v0c0-0.55-0.45-1-1-1H7C6.45,2,6,2.45,6,3v0 c0,0.55,0.45,1,1,1l1,0v5c0,1.66-1.34,3-3,3h0v2h5.97v7l1,1l1-1v-7H19v-2h0C17.34,12,16,10.66,16,9z" fill-rule="evenodd"/></g></svg>' + message.querySelector(".creator_name").innerHTML + '</p>';
-    message.remove();
 
     block = document.body.querySelector(".fixed_messages");
     block.innerHTML = "<div class='pointer show_chat_fixed_messages'>" + creator_p + "<div class='border-bottom' style='position:relative;padding-bottom: 5px;'><div style='overflow: hidden;text-overflow:ellipsis;padding-right:5px;'><span style='white-space: nowrap;'>" + parent + "</span></div></div></div>";
