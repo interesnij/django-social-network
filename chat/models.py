9dc14687573691789e543f7b7f76dc9faea43664
Serg <ochkarik1983@mail.ru@>
2021-12-19 16:20:42 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -330,13 +330,16 @@ class Chat(models.Model):
         if self.attach:
             ids = []
             for i in self.attach.split(","):
-                if i[:3] == "pho" or i[:3] == "lph":
+                if i[:3] == "pho":
                     ids.append(int(i[3:]))
             return ids
         return []
     def get_attach_photos(self):
         if self.attach:
-            ids = self.get_attach_photos_ids()
+            ids = []
+            for i in self.attach.split(","):
+                if i[:3] == "pho" or i[:3] == "lph":
+                    ids.append(int(i[3:]))
         else:
             return []
         from gallery.models import Photo
