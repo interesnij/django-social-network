29fe8403441596c31be97955b66aa5f1e768f4af
Serg <ochkarik1983@mail.ru@>
2022-01-04 14:30:15 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -134,6 +134,32 @@ async function get_record_stream() {
     TIMER_VALUE = 183;
   }
 
+  function downsampleBuffer(buffer, rate) {
+    if (rate == sampleRate) {
+        return buffer;
+    }
+    if (rate > sampleRate) {
+        throw "downsampling rate show be smaller than original sample rate";
+    }
+    var sampleRateRatio = sampleRate / rate;
+    var newLength = Math.round(buffer.length / sampleRateRatio);
+    var result = new Float32Array(newLength);
+    var offsetResult = 0;
+    var offsetBuffer = 0;
+    while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+        var accum = 0, count = 0;
+        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+            accum += buffer[i];
+            count++;
+        }
+        result[offsetResult] = accum / count;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+    }
+    return result;
+}
+
   function stop() {
     console.log('Stop');
     recording = false;
