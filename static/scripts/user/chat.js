b125cf9af786020b07335427a789235a14012d76
Serg <ochkarik1983@mail.ru@>
2022-01-04 19:39:47 +0300

    J

--- static/scripts/user/chat.js
+++ static/scripts/user/chat.js
@@ -305,7 +305,7 @@ async function get_record_stream() {
     link = user.getAttribute("data-pk");
     if (user.querySelector("img")) {
       src = user.querySelector("img").getAttribute("src");
-      img = '<img src="' + src + '" />'
+      img = '<img style="border-radius:40px;width:40px;" src="' + src + '" />'
     } else {
       img = '<svg fill="currentColor" class="svg_default svg_default_30" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>'
     };
