1e3f9d97dcd6094c2473b93c53697b43a2648af9
Serg <ochkarik1983@mail.ru@>
2022-01-06 16:22:05 +0300

    J

--- chat/admin.py
+++ chat/admin.py
@@ -11,7 +11,7 @@ class ChatUsersAdmin(admin.ModelAdmin):
     list_filter = ("user", )
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ("chat",  "text", "unread")
+    list_display = ("chat", "created", "text", "type", "unread")
     list_filter = ("creator", )
 
 
