44875b99210a5adbc1c20255d1ca211e3ae8b8d7
Serg <ochkarik1983@mail.ru@>
2022-01-14 17:01:59 +0300

    j

--- users/models.py
+++ users/models.py
@@ -260,7 +260,7 @@ class User(AbstractUser):
 
     def get_b_avatar(self):
         if self.get_avatar_pk():
-            return '<img src="' + self.b_avatar.url + '" class="detail_photo pointer" photo-pk="' + self.get_avatar_pk() + '">'
+            return '<img src="' + self.b_avatar.url + '" class="detail_photo pointer" photo-pk="' + str(self.get_avatar_pk()) + '">'
         else:
             return '/static/images/no_img/b_avatar.png'
 
