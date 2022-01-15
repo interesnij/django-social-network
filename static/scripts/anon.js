6b7892ba3d4d2e67b37ffba5268a9b9db3b734d4
Serg <ochkarik1983@mail.ru@>
2022-01-15 12:18:32 +0300

    J

--- static/scripts/anon.js
+++ static/scripts/anon.js
@@ -625,10 +625,11 @@ on('body', 'click', '#register_ajax', function() {
   if ( reg_link.readyState == 4 && reg_link.status == 200 ) {
     elem = reg_link.responseText;
     _span = document.createElement("span");
+    _span.innerHTML = elem.innerHTML;
     _span.querySelector(".user_name").innerHTML = first_name.value + " " + last_name.value;
     container = document.body.querySelector(".main-container");
     container.innerHTML = _span.innerHTML;
-    
+
     final_form = container.querySelector("form");
     final_form.append(create_hide_input ("first_name", first_name.value));
     final_form.append(create_hide_input ("last_name", last_name.value));
