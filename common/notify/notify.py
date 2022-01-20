55c2846653c965fd3527b4fb298161f8e6d418f7
Serg <ochkarik1983@mail.ru@>
2022-01-20 18:52:26 +0300

    J

--- common/notify/notify.py
+++ common/notify/notify.py
@@ -23,8 +23,8 @@ def user_notify(creator, action_community_id, object_id, type, socket_name, verb
 
     for user_id in creator.get_member_for_notify_ids():
         if Notify.objects.filter(creator_id=creator.pk, action_community_id=action_community_id, recipient_id=user_id, object_id=object_id, type=type, verb=verb).exists():
-            pass
-        elif Notify.objects.filter(recipient_id=user_id, action_community_id=action_community_id, created__gt=today, type=type, verb=current_verb).exists():
+            return
+        if Notify.objects.filter(recipient_id=user_id, action_community_id=action_community_id, created__gt=today, type=type, verb=current_verb).exists():
             notify = Notify.objects.get(recipient_id=user_id, action_community_id=action_community_id, type=type, created__gt=today, verb=current_verb)
             Notify.objects.create(creator_id=creator.pk, action_community_id=action_community_id, recipient_id=user_id, object_id=object_id, type=type, verb=current_verb, user_set=notify)
         elif Notify.objects.filter(recipient_id=user_id, object_id=object_id, type=type, created__gt=today, verb=verb).exists():
@@ -40,8 +40,8 @@ def community_notify(creator, community, action_community_id, object_id, type, s
     current_verb = creator.get_verb_gender(verb)
     for user_id in community.get_member_for_notify_ids():
         if Notify.objects.filter(creator_id=creator.pk, recipient_id=user_id, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb).exists():
-            pass
-        elif Notify.objects.filter(creator_id=creator.pk, recipient_id=user_id, community_id=community.pk, action_community_id=action_community_id, created__gt=today, type=type, verb=verb).exists():
+            return
+        if Notify.objects.filter(creator_id=creator.pk, recipient_id=user_id, community_id=community.pk, action_community_id=action_community_id, created__gt=today, type=type, verb=verb).exists():
             notify = Notify.objects.get(creator_id=creator.pk, recipient_id=user_id, community_id=community.pk, created__gt=today, action_community_id=action_community_id, type=type, verb=verb)
             Notify.objects.create(creator_id=creator.pk, recipient_id=user_id, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb, user_set=notify)
         elif Notify.objects.filter(community_id=community.pk, recipient_id=user_id, object_id=object_id, type=type, created__gt=today, verb=verb).exists():
@@ -58,11 +58,11 @@ def user_wall(creator, action_community_id, object_id, type, socket_name, verb):
     current_verb = creator.get_verb_gender(verb)
 
     if Wall.objects.filter(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb).exists():
-        pass
+        return
     #elif Wall.objects.filter(action_community_id=action_community_id, created__gt=today, object_id, verb=current_verb).exists():
     #    notify = Wall.objects.get(action_community_id=action_community_id, object_id, created__gt=today, verb=current_verb)
     #    Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=current_verb, user_set=notify)
-    elif Wall.objects.filter(object_id=object_id, type=type, created__gt=today, verb=verb).exists():
+    if Wall.objects.filter(object_id=object_id, type=type, created__gt=today, verb=verb).exists():
         notify = Wall.objects.get(object_id=object_id, type=type, created__gt=today, verb=verb)
         Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb="G"+verb, object_set=notify)
     else:
@@ -75,11 +75,11 @@ def community_wall(creator, community, action_community_id, object_id, type, soc
     current_verb = creator.get_verb_gender(verb)
 
     if Wall.objects.filter(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb).exists():
-        pass
+        return
     #elif Wall.objects.filter(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, created__gt=today, object_id=object_id, verb=verb).exists():
     #    notify = Wall.objects.get(creator_id=creator.pk, community_id=community.pk, created__gt=today, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb)
     #    Wall.objects.create(creator_id=creator.pk, community_id=community.pk, action_community_id=action_community_id, object_id=object_id, type=type, verb=verb, user_set=notify)
-    elif Wall.objects.filter(community_id=community.pk, object_id=object_id, type=type, created__gt=today, verb=verb).exists():
+    if Wall.objects.filter(community_id=community.pk, object_id=object_id, type=type, created__gt=today, verb=verb).exists():
         notify = Wall.objects.get(community_id=community.pk, object_id=object_id, type=type, created__gt=today, verb=verb)
         Wall.objects.create(creator_id=creator.pk, action_community_id=action_community_id, community_id=community.pk, object_id=object_id, type=type, verb="G"+verb, object_set=notify)
     else:
