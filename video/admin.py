013003d0b1e835bf15007fe4f5aec4fe3cb1fc60
Serg <ochkarik1983@mail.ru@>
2022-01-20 11:47:29 +0300

    J

--- video/admin.py
+++ video/admin.py
@@ -5,7 +5,7 @@ from video.models import VideoCategory, VideoList, Video, VideoComment
 
 class VideoListAdmin(admin.ModelAdmin):
     search_fields = ('creator',)
-    list_display = ['name',type','creator',]
+    list_display = ['name','type','creator',]
 
 admin.site.register(VideoList, VideoListAdmin)
 admin.site.register(VideoCategory)
