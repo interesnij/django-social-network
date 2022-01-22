4c2d898ba490c3b3dfa167c734561e562060de92
Serg <ochkarik1983@mail.ru@>
2022-01-22 12:40:11 +0300

    J

--- static/scripts/posts/user_get.js
+++ static/scripts/posts/user_get.js
@@ -134,16 +134,22 @@ on('#ajax', 'click', '.u_ucm_post_repost', function() {
 });
 
 on('#ajax', 'click', '#repost_for_wall', function() {
+  this.querySelector("#repost_radio_wall").setAttribute("checked", "true");
+  //parent = this.parentElement;
+  //parent.querySelector("#repost_radio_community").setAttribute("checked", "false");
+  //parent.querySelector("#repost_radio_message").setAttribute("checked", "true");
   current_block = this.parentElement.nextElementSibling;
   current_block.querySelector(".collector").innerHTML = "";
   create_fullscreen("/users/load/post_lists/", "worker_fullscreen")
 });
 on('#ajax', 'click', '#u_repost_for_community', function() {
+  this.querySelector("#repost_radio_community").setAttribute("checked", "true");
   current_block = this.parentElement.nextElementSibling;
   current_block.querySelector(".collector").innerHTML = "";
   create_fullscreen("/users/load/communities_post_lists/", "worker_fullscreen");
 });
 on('#ajax', 'click', '#repost_for_message', function() {
+  this.querySelector("#repost_radio_message").setAttribute("checked", "true");
   current_block = this.parentElement.nextElementSibling;
   current_block.querySelector(".collector").innerHTML = "";
   create_fullscreen("/users/load/chat_items/", "worker_fullscreen");
