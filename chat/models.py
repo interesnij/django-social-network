978302cb13483ac4b4b83a9e8e0adc38427f8fd4
Serg <ochkarik1983@mail.ru@>
2022-02-02 19:10:02 +0300

    J

--- chat/models.py
+++ chat/models.py
@@ -542,6 +542,15 @@ class Chat(models.Model):
             media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, request_chat_user.get_beep_icon(), status, '<small class="float-right text-muted">', str(created), '</small></h5><p class="mb-0', is_read ,'" style="white-space: nowrap;">', preview_text, '</p><span class="typed"></span></div>'])
             return ''.join(['<div class="media">', figure, media_body, self.get_unread_count_message(user_id), '</div>'])
 
+        elif self.is_manager():
+            chat_user = self.get_chat_user(user_id)
+            member = chat_user.user
+            figure = '<figure><svg fill="currentColor" class="svg_default svg_default_50" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></figure>'
+            chat_name = self.name
+            status = ''
+            media_body = ''.join(['<div class="media-body"><h5 class="time-title mb-0">', chat_name, request_chat_user.get_beep_icon(), status, '<small class="float-right text-muted">', str(created), '</small></h5><p class="mb-0" style="white-space: nowrap;">', preview_text, '</p><span class="typed"></span></div>'])
+            return ''.join(['<div class="media">', figure, media_body, self.get_unread_count_message(user_id), '</div>'])
+
     def get_name(self, request_user_id):
         if self.name:
             return self.name
