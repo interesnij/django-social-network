034ee51fa37121026d5dad762817a0f3b4c7b3bd
Serg <ochkarik1983@mail.ru@>
2022-02-06 11:46:32 +0300

    J

--- music/url/user_progs.py
+++ music/url/user_progs.py
@@ -3,7 +3,6 @@ from music.view.user_progs import *
 
 
 urlpatterns = [
-    url(r'^create_tracks/$', UserTracksCreate.as_view()),
     url(r'^edit_track/(?P<pk>\d+)/$', UserTrackEdit.as_view()),
     url(r'^delete_track/(?P<pk>\d+)/$', UserTrackRemove.as_view()),
     url(r'^restore_track/(?P<pk>\d+)/$', UserTrackAbortRemove.as_view()),
