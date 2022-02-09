188670909020be1ef7ca028b486854035e4e32cc
Serg <ochkarik1983@mail.ru@>
2022-02-09 14:42:38 +0300

    J

--- video/models.py
+++ video/models.py
@@ -1020,7 +1020,7 @@ class Video(models.Model):
 
     def get_uri(self):
         if self.file:
-            return "/static/images/demo_video/travel.mp4"
+            return "static/images/demo_video/travel.mp4"
         elif self.uri:
             return self.uri
         else:
