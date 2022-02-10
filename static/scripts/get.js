5585cbaea8f76a89ad8bb6ad59827c94624463fa
Serg <ochkarik1983@mail.ru@>
2022-02-10 18:15:16 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -482,7 +482,7 @@ on('#ajax', 'click', '#add_list_selected_lists_btn', function() {
   users_list = users_block.querySelectorAll(".custom_color");
   final_list = "Выбраны списки: ";
   for (var i = 0; i < users_list.length; i++){
-    users_list[i].querySelector(".pointer").remove();
+    users_list[i].querySelector("span").remove();
     final_list += users_list[i].innerHTML;
     final_list += '<input type="hidden" name="lists" value="' + users_list[i].getAttribute("data-pk") + '" />'
   };
@@ -500,7 +500,7 @@ on('#ajax', 'click', '#add_list_selected_chats_items_btn', function() {
   users_list = users_block.querySelectorAll(".custom_color");
   final_list = "Выбраны получатели: ";
   for (var i = 0; i < users_list.length; i++){
-    users_list[i].querySelector(".pointer").remove();
+    users_list[i].querySelector("span").remove();
     final_list += users_list[i].innerHTML;
     final_list += '<input type="hidden" name="chat_items" value="' + users_list[i].getAttribute("data-pk") + '" />'
   };
@@ -518,7 +518,7 @@ on('#ajax', 'click', '#add_list_selected_communities_btn', function() {
   users_list = users_block.querySelectorAll(".custom_color");
   final_list = "Выбраны сообщества: ";
   for (var i = 0; i < users_list.length; i++){
-    users_list[i].querySelector(".pointer").remove();
+    users_list[i].querySelector("span").remove();
     final_list += users_list[i].innerHTML;
     final_list += '<input type="hidden" name="u_c" value="' + users_list[i].getAttribute("data-pk") + '" />'
   };
