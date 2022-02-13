2de8cd4dd1883e74bb9a6ec2293a3185b984a55f
Serg <ochkarik1983@mail.ru@>
2022-02-13 18:38:44 +0300

    J

--- survey/forms.py
+++ survey/forms.py
@@ -5,7 +5,14 @@ from survey.models import Survey, SurveyList
 class SurveyForm(forms.ModelForm):
 	class Meta:
 		model = Survey
-		fields = ['title']
+		fields = [
+			"title",
+			"image",
+			"is_anonymous",
+			"is_multiple",
+			"is_no_edited",
+			"is_no_edited",
+		]
 
 class SurveyListForm(forms.ModelForm):
 	class Meta:
