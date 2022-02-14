cd83fe63246e62292de454ce925484fc6400692c
Serg <ochkarik1983@mail.ru@>
2022-02-14 10:20:34 +0300

    J

--- survey/models.py
+++ survey/models.py
@@ -621,7 +621,7 @@ class Survey(models.Model):
         return survey
 
     def is_user_voted(self, user_id):
-        return SurveyVote.objects.filter(answer__survey_id=self.pk, user_id=user_id).exists()
+        return not self.is_time_end() and SurveyVote.objects.filter(answer__survey_id=self.pk, user_id=user_id).exists()
 
     def is_time_end(self):
         if self.time_end:
