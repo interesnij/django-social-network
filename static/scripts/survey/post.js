a60157db1584b36a19a63ab1df84ec56da218698
Serg <ochkarik1983@mail.ru@>
2022-02-14 12:23:19 +0300

    J

--- static/scripts/survey/post.js
+++ static/scripts/survey/post.js
@@ -59,13 +59,11 @@ on('#ajax', 'click', '.survey_vote', function() {
   };
   if (_this.querySelector(".vote_svg").innerHTML) {
     _this.querySelector(".vote_svg").innerHTML = '';
-    console.log("This vote remove")
-    for (var i = 0; i < answers.length; i++) {
-      if (answers[i].querySelector("input")) {
-        is_have_vote = true;
-        console.log("Survey have vote!")
-      }
-     };
+    console.log("This vote remove");
+    if (parent.querySelector("input")) {
+      is_have_vote = true;
+      console.log("Survey have vote!")
+    }
   } else {
     _this.querySelector(".vote_svg").innerHTML = '<input type="hidden" name="votes" value="' + _this.getAttribute("data-pk") + '"><svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>';
     is_have_vote = true
