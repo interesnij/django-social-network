cc7e22d5a6fd5003976e3ad624eb0276f9738757
Serg <ochkarik1983@mail.ru@>
2022-02-14 12:35:37 +0300

    J

--- static/scripts/survey/post.js
+++ static/scripts/survey/post.js
@@ -73,8 +73,10 @@ on('#ajax', 'click', '.survey_vote', function() {
     if (_this.querySelector(".vote_svg").innerHTML) {
       _this.querySelector(".vote_svg").innerHTML = '';
       is_have_vote = false
-    };
-    is_have_vote = true
+    } else {
+      is_have_vote = true;
+      _this.querySelector(".vote_svg").innerHTML = '<input type="hidden" name="votes" value="' + _this.getAttribute("data-pk") + '"><svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>';
+    }
   };
 
   footer = parent.nextElementSibling;
