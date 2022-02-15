7244b148f6553fe42a650706d8f2790601eba28d
Serg <ochkarik1983@mail.ru@>
2022-02-15 16:44:09 +0300

    H

--- survey/models.py
+++ survey/models.py
@@ -769,7 +769,7 @@ class Answer(models.Model):
     class Meta:
         verbose_name = 'Вариант ответа'
         verbose_name_plural = 'Варианты ответа'
-        ordering = ["-order"]
+        ordering = ["order"]
 
     def __str__(self):
         return self.text
