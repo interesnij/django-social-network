586c0b5da40e8111c82a510e1a5507c6b82b0bbe
Serg <ochkarik1983@mail.ru@>
2022-02-16 13:15:57 +0300

    J

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,40 +1,40 @@
-loadScripts('/static/scripts/functions/other.js?ver2');
-loadScripts('/static/scripts/functions/preview.js');
-loadScripts('/static/scripts/functions/comment_attach.js');
-loadScripts('/static/scripts/functions/post_attach.js');
-loadScripts('/static/scripts/functions/message_attach.js');
-loadScripts('/static/scripts/functions/reload.js?ver2');
+loadScripts('/static/scripts/functions/other.js?ver3');
+loadScripts('/static/scripts/functions/preview.js?ver3');
+loadScripts('/static/scripts/functions/comment_attach.js?ver3');
+loadScripts('/static/scripts/functions/post_attach.js?ver3');
+loadScripts('/static/scripts/functions/message_attach.js?ver3');
+loadScripts('/static/scripts/functions/reload.js?ver3');
 
-loadScripts('/static/scripts/community/get.js?ver2');
-loadScripts('/static/scripts/community/post.js');
+loadScripts('/static/scripts/community/get.js?ver3');
+loadScripts('/static/scripts/community/post.js?ver3');
 loadScripts('/static/scripts/community/manage.js');
 
-loadScripts('/static/scripts/docs/get.js');
-loadScripts('/static/scripts/docs/post.js');
+loadScripts('/static/scripts/docs/get.js?ver3');
+loadScripts('/static/scripts/docs/post.js?ver3');
 
-loadScripts('/static/scripts/gallery/user_get.js');
-loadScripts('/static/scripts/gallery/user_post.js');
-loadScripts('/static/scripts/gallery/community_post.js');
+loadScripts('/static/scripts/gallery/user_get.js?ver3');
+loadScripts('/static/scripts/gallery/user_post.js?ver3');
+loadScripts('/static/scripts/gallery/community_post.js?ver3');
 
-loadScripts('/static/scripts/goods/user_get.js');
-loadScripts('/static/scripts/goods/user_post.js');
-loadScripts('/static/scripts/goods/community_post.js');
+loadScripts('/static/scripts/goods/user_get.js?ver3');
+loadScripts('/static/scripts/goods/user_post.js?ver3');
+loadScripts('/static/scripts/goods/community_post.js?ver3');
 
-loadScripts('/static/scripts/music/get.js');
-loadScripts('/static/scripts/music/post.js');
+loadScripts('/static/scripts/music/get.js?ver3');
+loadScripts('/static/scripts/music/post.js?ver3');
 
-loadScripts('/static/scripts/posts/user_get.js?ver2');
-loadScripts('/static/scripts/posts/community_get.js');
-loadScripts('/static/scripts/posts/user_post.js?ver2');
-loadScripts('/static/scripts/posts/community_post.js');
+loadScripts('/static/scripts/posts/user_get.js?ver3');
+loadScripts('/static/scripts/posts/community_get.js?ver3');
+loadScripts('/static/scripts/posts/user_post.js?ver3');
+loadScripts('/static/scripts/posts/community_post.js?ver3');
 
-loadScripts('/static/scripts/user/settings.js');
-loadScripts('/static/scripts/user/connect.js?ver2');
-loadScripts('/static/scripts/user/chat.js?ver2');
+loadScripts('/static/scripts/user/settings.js?ver3');
+loadScripts('/static/scripts/user/connect.js?ver3');
+loadScripts('/static/scripts/user/chat.js?ver3');
 
-loadScripts('/static/scripts/video/user_get.js');
-loadScripts('/static/scripts/video/user_post.js');
-loadScripts('/static/scripts/video/community_post.js');
+loadScripts('/static/scripts/video/user_get.js?ver3');
+loadScripts('/static/scripts/video/user_post.js?ver3');
+loadScripts('/static/scripts/video/community_post.js?ver3');
 
-loadScripts('/static/scripts/survey/get.js');
-loadScripts('/static/scripts/survey/post.js');
+loadScripts('/static/scripts/survey/get.js?ver3');
+loadScripts('/static/scripts/survey/post.js?ver3');
