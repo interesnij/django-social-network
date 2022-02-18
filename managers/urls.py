3a0760e167b8e6716a7171baccfe39473efc26b3
Serg <ochkarik1983@mail.ru@>
2022-02-18 18:28:55 +0300

    H

--- managers/urls.py
+++ managers/urls.py
@@ -6,13 +6,12 @@ urlpatterns = [
     url(r'^$', ManagersView.as_view(), name='managers'),
     url(r'^high_officer/$', SuperManagersView.as_view(), name='super_managers'),
 
-    url(r'^moderation_list/', include('managers.url.moderation_list')),
-    url(r'^penalty_list/', include('managers.url.penalty_list')),
-
     url(r'^create_sanction/$', SanctionItemCreate.as_view()),
     url(r'^delete_sanction/$', SanctionItemDelete.as_view()),
     url(r'^rejected_claims/$', RejectedItemClaims.as_view()),
     url(r'^unverify_sanction/$', UnverifyItemCreate.as_view()),
-
     url(r'^send_messages/$', SendManagerMessages.as_view()),
+
+    url(r'^moderation_list/', include('managers.url.moderation_list')),
+    url(r'^penalty_list/', include('managers.url.penalty_list')),
 ]
