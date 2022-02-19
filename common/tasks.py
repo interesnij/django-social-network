07389e97bca6f0b57035926f291899f00c3ce095
Serg <ochkarik1983@mail.ru@>
2022-02-19 19:06:05 +0300

    J

--- common/tasks.py
+++ common/tasks.py
@@ -1,3 +1,15 @@
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+from datetime import datetime, timedelta
+from celery import shared_task, current_task, task
+from celery import Celery
+from posts.models import PostsList
+
+app = Celery()
+
+@app.task
+def test():
+    list = PostsList.objects.get(pk=1)
+    list.name = "бубубу"
+    list.save(update_fields=["name"])
+
+
+test.apply_async(eta=timezone.now() + timezone.timedelta(seconds=3))
