f9431e746ccc62354220a8584f985a39b73a96d2
Serg <ochkarik1983@mail.ru@>
2022-02-19 14:35:07 +0300

    J

--- posts/models.py
+++ posts/models.py
@@ -66,7 +66,8 @@ class PostsList(models.Model):
         return True
 
     def get_longest_penalties(self):
-        return self.manager_penalties.filter(type="POL", object_id=self.pk)[0].expiration
+        from managers.models import ModerationPenalty
+        return ModerationPenalty.objects.filter(type="POL", object_id=self.pk)[0].expiration
     def get_moderated_description(self):
         from managers.models import Moderated
         obj = Moderated.objects.filter(type="POL", object_id=self.pk)[0]
