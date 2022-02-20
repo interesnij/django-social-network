fdfdc9f696742ca652522f8839860fa2f6b6ea09
Serg <ochkarik1983@mail.ru@>
2022-02-20 13:03:57 +0300

    J

--- main/views.py
+++ main/views.py
@@ -219,8 +219,8 @@ class NewsListView(ListView):
 	def get(self,request,*args,**kwargs):
 		self.template_name = get_detect_main_template("main/lists/news_list.html", request.user, request.META['HTTP_USER_AGENT'])
 
-		from common.tasks import MyTask
-		MyTask.delay()
+		from common.tasks import custom
+		custom.delay()
 		return super(NewsListView,self).get(request,*args,**kwargs)
 
 	def get_queryset(self):
