15b25ef1b95af731b423801c1c5d7f038e794980
Serg <ochkarik1983@mail.ru@>
2022-02-20 11:40:21 +0300

    J

--- tr/celery.py
+++ tr/celery.py
@@ -25,4 +25,4 @@ def test():
     #list.save(update_fields=["name"])
 
 
-test.apply_async(eta=timezone.now() + timezone.timedelta(seconds=20))
+test.apply_async(eta=datetime.datetime(2022, 2, 20, 11, 42))
