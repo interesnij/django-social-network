caa4624b7044b5d2022e990d3d8f9575ce5c06b2
Serg <ochkarik1983@mail.ru@>
2022-02-20 12:20:13 +0300

    J

--- common/tasks.py
+++ common/tasks.py
@@ -12,4 +12,4 @@ class MyTask(celery_app.Task):
     list.save(update_fields=["name"])
 
 
-test.apply_async(eta=timezone.now() + timezone.timedelta(seconds=20))
+MyTask.apply_async(eta=timezone.now() + timezone.timedelta(seconds=20))
