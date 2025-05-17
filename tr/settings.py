d7baaf8f87ee570d8d7e5193a4a9101eec75786d
interesnij <ochkarik1983@mail.ru>
2025-05-17 13:02:33 +0300

    J

--- tr/settings.py
+++ tr/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'tr.urls'
