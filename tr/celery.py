556631e2a4aa0fbb95a94b7a4c05b2002b0a68d3
interesnij <ochkarik1983@mail.ru>
2025-05-17 12:58:11 +0300

    J

--- tr/celery.py
+++ tr/celery.py
@@ -2,24 +2,24 @@ from __future__ import absolute_import
 
 import os
 import sys
-from celery import Celery
+#from celery import Celery
 
 from django.utils import timezone
 from datetime import datetime, timedelta
 from django.conf import settings
 
 # Установите модуль настроек Django по умолчанию для программы "celery".
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tr.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tr.settings')
 
-project_name = 'tr'
-app = Celery(project_name, broker='redis://localhost')
+#project_name = 'tr'
+#app = Celery(project_name, broker='redis://localhost')
 
 # Использование строки здесь означает, что рабочему не нужно сериализовать
 # объект конфигурации для дочерних процессов.
 # - namespace='СЕЛЬДЕРЕЙ' означает все ключи конфигурации, связанные с сельдереем
 # должен иметь префикс `CELERY_`.
 
-app.config_from_object('django.conf:settings')
+#app.config_from_object('django.conf:settings')
 
 # Загружайте модули задач из всех зарегистрированных приложений Django.
-app.autodiscover_tasks(lambda : settings.INSTALLED_APPS)
+#app.autodiscover_tasks(lambda : settings.INSTALLED_APPS)
