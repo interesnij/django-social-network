54885b9eebc32c9ca355bc921229b4ad79a5a0a8
interesnij <ochkarik1983@mail.ru>
2025-05-22 18:02:34 +0300

    J

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -296,7 +296,7 @@ on('#ajax', 'click', '.genre_item', function() {
 on('#ajax', 'click', '.music_list_post', function() {
   document.body.querySelector("#saved_playlist").setAttribute("track-pk", this.nextElementSibling.getAttribute("data-pk"));
   var track_id = this.parentElement.parentElement.getAttribute('music-counter');
-  item = this.parentElement.parentElement.parentElement.parentElement;
+  item = this.parentElement.parentElement.parentElement.parentElement.parentElement;
   var item_pk = item.getAttribute('data-pk');
   if (!document.body.classList.contains("item_" + item_pk)){
     document.querySelector("body").classList = "";
