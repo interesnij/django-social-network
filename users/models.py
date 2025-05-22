5d9b731806dd563722473cfd8a7ccec9c24f3b95
interesnij <ochkarik1983@mail.ru>
2025-05-22 14:53:46 +0300

    J

--- users/models.py
+++ users/models.py
@@ -466,7 +466,7 @@ class User(AbstractUser):
         else:
             device = '&nbsp;<svg style="width: 17px;" class="svg_default" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z"/></svg>'
         naive = self.last_activity.replace(tzinfo=None)
-        if datetime.now().timestamp() < naive + timedelta(minutes=3):
+        if datetime.now() < naive + timedelta(minutes=3):
             return '<i>Онлайн</i>' + device
         else:
             if self.is_women():
