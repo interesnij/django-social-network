855b2bc0249dfa5a27b8c720aab5cef22269e5de
interesnij <ochkarik1983@mail.ru>
2025-11-26 19:02:14 +0300

    J

--- users/models.py
+++ users/models.py
@@ -114,6 +114,7 @@ class User(AbstractUser):
     language = models.CharField(max_length=7, choices=settings.LANGUAGES, default="ru")
     perm = models.PositiveSmallIntegerField(choices=PERM, default=1, verbose_name="Статус пользователя")
     level = models.PositiveSmallIntegerField(default=100, verbose_name="Порядочность")
+    username = None
     USERNAME_FIELD = 'phone'
 
     class Meta:
